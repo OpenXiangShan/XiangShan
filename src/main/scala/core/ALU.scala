9b8a3c386648bc0b4d9bfb336066a2a3d22f57f3
Zihao Yu <yuzihao@ict.ac.cn>
2019-02-06 22:52:37 +0800

    core,ALU: add MuxTree option

--- src/main/scala/core/ALU.scala
+++ src/main/scala/core/ALU.scala
@@ -14,6 +14,8 @@ object ALU {
   def FN_AND  = "b0111".U
   def FN_SUB  = FN_ADD | "b1000".U
   def FN_SAR  = FN_SLR | "b1000".U
+
+  val useMuxTree = true
 }
 
 import ALU._
@@ -26,16 +28,20 @@ class ALU extends Module {
     val out = Output(UInt(32.W))
   })
 
-  io.out := MuxLookup(io.func, 0.U, List(
+  val shamt = io.b(4, 0)
+  val funcList = List(
     (FN_ADD , io.a  +  io.b),
-    (FN_SLL , io.a  << io.b(4, 0)),
-    (FN_SLT , Cat(0.U(31.W), (io.a.asSInt < io.b.asSInt))),
-    (FN_SLTU, Cat(0.U(31.W), (io.a < io.b))),
+    (FN_SLL , (io.a  << shamt)(31, 0)),
+    (FN_SLT , (io.a.asSInt < io.b.asSInt).asUInt),
+    (FN_SLTU, (io.a < io.b).asUInt),
     (FN_XOR , io.a  ^  io.b),
-    (FN_SLR , io.a  >> io.b(4, 0)),
+    (FN_SLR , io.a  >> shamt),
     (FN_OR  , io.a  |  io.b),
     (FN_AND , io.a  &  io.b),
     (FN_SUB , io.a  -  io.b),
-    (FN_SAR , (io.a.asSInt >> io.b(4, 0)).asUInt)
-  ))
+    (FN_SAR , (io.a.asSInt >> shamt).asUInt)
+  )
+
+  io.out := (if (useMuxTree) Mux1H(funcList.map { case (func, res) => (func === io.func, res) })
+             else MuxLookup(io.func, 0.U, funcList))
 }
