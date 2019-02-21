b2ea3b5205861b319ee30cad6cf72e5252be2cbb
Zihao Yu <yuzihao@ict.ac.cn>
2019-02-21 20:11:45 +0800

    fpga,zedboard,rtl: set ID bits long enough to fix truncation bug

--- fpga/board/zedboard/rtl/addr_mapper.v
+++ fpga/board/zedboard/rtl/addr_mapper.v
@@ -1,6 +1,6 @@
 module addr_mapper (
-   `axi_slave_if(s_axi, 32, 1),
-   `axi_master_if(m_axi, 32, 1)
+   `axi_slave_if(s_axi, 32, 8),
+   `axi_master_if(m_axi, 32, 8)
 );
 
     assign  m_axi_awaddr = {4'b0001, s_axi_awaddr[27:0]};
