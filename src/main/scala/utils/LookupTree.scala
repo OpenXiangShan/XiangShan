1a3902e940e252711b6df5e4ed64ea18d1a80c89
Zihao Yu <yuzihao@ict.ac.cn>
2019-02-24 22:44:43 +0800

    noop,utils,LookupTree: do not use tree-Mux
    * this yields better timing result in vivado

--- src/main/scala/utils/LookupTree.scala
+++ src/main/scala/utils/LookupTree.scala
@@ -4,7 +4,7 @@ import chisel3._
 import chisel3.util._
 
 object LookupTree {
-  private val useMuxTree = true
+  private val useMuxTree = false
 
   def apply[T <: Data](key: UInt, mapping: Iterable[(UInt, T)]): T =
     Mux1H(mapping.map(p => (p._1 === key, p._2)))
