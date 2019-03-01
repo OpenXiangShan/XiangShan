834384158cb7cf101c896afe16c3f2051dc61432
Zihao Yu <yuzihao@ict.ac.cn>
2019-03-01 13:38:37 +0800

    bus,axi4,AXI4RAM: refactor to support axi4-lite

--- src/main/scala/bus/axi4/AXI4RAM.scala
+++ src/main/scala/bus/axi4/AXI4RAM.scala
@@ -6,9 +6,10 @@ import chisel3._
 import chisel3.util._
 import chisel3.util.experimental.loadMemoryFromFile
 
-class AXI4RAM(memByte: Int, beatBytes: Int = 4, dataFile: String = "") extends Module {
+sealed abstract class RAM[T <: AXI4Lite](_type: T,
+  memByte: Int, beatBytes: Int = 4, dataFile: String = "") extends Module {
   val io = IO(new Bundle{
-    val in = Flipped(new AXI4)
+    val in = Flipped(_type)
   })
 
   val in = io.in
@@ -19,16 +20,8 @@ class AXI4RAM(memByte: Int, beatBytes: Int = 4, dataFile: String = "") extends M
   val w_addr = in.aw.bits.addr >> log2Ceil(beatBytes)
 
   val w_full = RegInit(false.B)
-  val w_id   = Reg(UInt())
-  val w_user = Reg(UInt())
-
   when (in. b.fire()) { w_full := false.B }
   when (in.aw.fire()) { w_full := true.B }
-
-  when (in.aw.fire()) {
-    w_id := in.aw.bits.id
-    w_user := in.aw.bits.user
-  }
 
   val wdata = VecInit.tabulate(beatBytes) { i => in.w.bits.data(8*(i+1)-1, 8*i) }
   when (in.aw.fire()) {
@@ -38,22 +31,11 @@ class AXI4RAM(memByte: Int, beatBytes: Int = 4, dataFile: String = "") extends M
   in. b.valid := w_full
   in.aw.ready := in. w.valid && (in.b.ready || !w_full)
   in. w.ready := in.aw.valid && (in.b.ready || !w_full)
-
-  in.b.bits.id   := w_id
   in.b.bits.resp := AXI4Parameters.RESP_OKAY
-  in.b.bits.user := w_user
 
   val r_full = RegInit(false.B)
-  val r_id   = Reg(UInt())
-  val r_user = Reg(UInt())
-
   when (in. r.fire()) { r_full := false.B }
   when (in.ar.fire()) { r_full := true.B }
-
-  when (in.ar.fire()) {
-    r_id := in.ar.bits.id
-    r_user := in.ar.bits.user
-  }
 
   def holdUnless[T <: Data](x: T, enable: Bool): T = Mux(enable, x, RegEnable(x, enable))
 
@@ -62,10 +44,19 @@ class AXI4RAM(memByte: Int, beatBytes: Int = 4, dataFile: String = "") extends M
 
   in. r.valid := r_full
   in.ar.ready := in.r.ready || !r_full
-
-  in.r.bits.id   := r_id
   in.r.bits.resp := AXI4Parameters.RESP_OKAY
   in.r.bits.data := Cat(rdata.reverse)
-  in.r.bits.user := r_user
+}
+
+class AXI4LiteRAM(memByte: Int, beatBytes: Int = 4, dataFile: String = "")
+  extends RAM(new AXI4Lite, memByte, beatBytes, dataFile)
+
+class AXI4RAM(memByte: Int, beatBytes: Int = 4, dataFile: String = "")
+  extends RAM(new AXI4, memByte, beatBytes, dataFile) {
+
+  in.b.bits.id := RegEnable(in.aw.bits.id, in.aw.fire())
+  in.b.bits.user := RegEnable(in.aw.bits.user, in.aw.fire())
+  in.r.bits.id := RegEnable(in.ar.bits.id, in.ar.fire())
+  in.r.bits.user := RegEnable(in.ar.bits.user, in.ar.fire())
   in.r.bits.last := true.B
 }
