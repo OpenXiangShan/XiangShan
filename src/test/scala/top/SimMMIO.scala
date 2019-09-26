9c43f7c7a82f1e814463c15d7002db50222253e2
Zihao Yu <yuzihao@ict.ac.cn>
2019-09-26 19:37:26 +0800

    test,SimMMIO: use crossbar to connect timer

--- src/test/scala/top/SimMMIO.scala
+++ src/test/scala/top/SimMMIO.scala
@@ -4,6 +4,7 @@ import chisel3._
 import chisel3.util._
 
 import bus.simplebus._
+import device._
 
 class DeviceHelper extends BlackBox {
   val io = IO(new Bundle {
@@ -23,18 +24,30 @@ class SimMMIO extends Module {
     val rw = Flipped(new SimpleBusUC)
   })
 
+  val devAddrSpace = List(
+    (0x40600000L, 0x10L), // uart
+    (0x40700000L, 0x10L)  // timer
+  )
+
+  val xbar = Module(new SimpleBusCrossbar(1, devAddrSpace))
+  xbar.io.in(0) <> io.rw
+
+  val timer = Module(new AXI4Timer)
+  timer.io.in <> xbar.io.out(1).toAXI4Lite()
+
   val helper = Module(new DeviceHelper)
+  val helperIO = xbar.io.out(0)
   helper.io.clk := clock
   helper.io.reset := reset.asBool
-  helper.io.reqValid := io.rw.req.valid
-  helper.io.reqWen := io.rw.isWrite()
-  helper.io.reqAddr := io.rw.req.bits.addr
-  helper.io.reqWdata := io.rw.req.bits.wdata
-  helper.io.reqWmask := io.rw.req.bits.wmask
-  io.rw.resp.bits.rdata := helper.io.respRdata
-  io.rw.resp.bits.cmd := 0.U
-  io.rw.resp.bits.user := 0.U
+  helper.io.reqValid := helperIO.req.valid
+  helper.io.reqWen := helperIO.isWrite()
+  helper.io.reqAddr := helperIO.req.bits.addr
+  helper.io.reqWdata := helperIO.req.bits.wdata
+  helper.io.reqWmask := helperIO.req.bits.wmask
+  helperIO.resp.bits.rdata := helper.io.respRdata
+  helperIO.resp.bits.cmd := 0.U
+  helperIO.resp.bits.user := 0.U
 
-  io.rw.req.ready := true.B
-  io.rw.resp.valid := RegNext(io.rw.req.valid)
+  helperIO.req.ready := true.B
+  helperIO.resp.valid := RegNext(helperIO.req.valid)
 }
