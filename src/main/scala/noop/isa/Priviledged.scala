8c4be239b358bdfbe5e7cd45e7f483432a2a1545
William Wang <zeweiwang@outlook.com>
2019-10-29 02:53:39 +0800

    add(decode): implement flush as nop

--- src/main/scala/noop/isa/Priviledged.scala
+++ src/main/scala/noop/isa/Priviledged.scala
@@ -7,11 +7,15 @@ object Priviledged extends HasInstrType {
   def ECALL   = BitPat("b000000000000_00000_000_00000_1110011")
   def MRET    = BitPat("b001100000010_00000_000_00000_1110011")
   def SRET    = BitPat("b000100000010_00000_000_00000_1110011")
+  def FENCE   = BitPat("b????????????_?????_000_?????_0001111")
   def SFANCE_VMA = BitPat("b000100100000_00000_000_00000_1110011")
 
   val table = Array(
     ECALL          -> List(InstrI, FuType.csr, CSROpType.jmp),
     MRET           -> List(InstrI, FuType.csr, CSROpType.jmp),
-    SRET           -> List(InstrI, FuType.csr, CSROpType.jmp)
+    SRET           -> List(InstrI, FuType.csr, CSROpType.jmp),
+    FENCE          -> List(InstrS, FuType.alu, ALUOpType.add) // InstrS -> !wen
+    // FENCE          -> List(InstrB, FuType.mou, MOUOpType.fencei)
+
   )
 }
