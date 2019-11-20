938620c38f383e3aef1d6a6e350097e0deb17b2e
zhangzifei <1773908404@qq.com>
2019-11-20 19:10:48 +0800

    rm xbarNto1Special, which is not useful now

--- src/main/scala/bus/simplebus/Crossbar.scala
+++ src/main/scala/bus/simplebus/Crossbar.scala
@@ -61,56 +61,6 @@ class SimpleBusCrossbar1toN(addressSpace: List[(Long, Long)]) extends Module {
   }
 }
 
-class SimpleBusCrossbarNto1Special(n: Int, userBits:Int = 0, name: String = "default") extends Module {
-  val io = IO(new Bundle {
-    val in = Flipped(Vec(n, new SimpleBusUC(userBits)))
-    val out = new SimpleBusUC(userBits)
-  })
-
-  val s_idle :: s_readResp :: s_writeResp :: Nil = Enum(3)
-  val state = RegInit(s_idle)
-
-  val lockWriteFun = ((x: SimpleBusReqBundle) => x.isWrite() && x.isBurst())
-  val inputArb = Module(new LockingArbiter(chiselTypeOf(io.in(0).req.bits), n, 8, Some(lockWriteFun)))
-  (inputArb.io.in zip io.in.map(_.req)).map{ case (arb, in) => arb <> in }
-  val thisReq = inputArb.io.out
-  assert(!(thisReq.valid && !thisReq.bits.isRead() && !thisReq.bits.isWrite()))
-  val inflightSrc = Reg(UInt(log2Up(n).W))
-
-  io.out.req.bits := thisReq.bits
-  // bind correct valid and ready signals
-  io.out.req.valid := thisReq.valid && (state === s_idle)
-  thisReq.ready := io.out.req.ready && (state === s_idle)
-
-  io.in.map(_.resp.bits := io.out.resp.bits)
-  io.in.map(_.resp.valid := false.B)
-  (io.in(inflightSrc).resp, io.out.resp) match { case (l, r) => {
-    l.valid := r.valid
-    r.ready := l.ready
-  }}
-
-  switch (state) {
-    is (s_idle) {
-      when (thisReq.fire()) {
-        inflightSrc := inputArb.io.chosen
-        when (thisReq.bits.isRead()) { state := s_readResp }
-        .elsewhen (thisReq.bits.isWriteLast() || thisReq.bits.isWriteSingle()) { state := s_writeResp } ///
-      }
-    }
-    is (s_readResp) { when ((io.out.resp.fire() && io.out.resp.bits.isReadLast())) { state := s_idle } }
-    is (s_writeResp) { when (io.out.resp.fire()) { state := s_idle } } //could change to true.B for write not need resp.fire()
-  }
-
-  Debug() {
-    when(true.B) {
-      printf("%d:" + name + " state:%d inflightSrc:%d chosen:%d ThisReqReady:%d ThisReqValid:%d ", GTimer(), state, inflightSrc, inputArb.io.chosen, thisReq.ready, thisReq.valid)
-      printf(p"ThisReqBits:${thisReq.bits}\n")
-      printf("%d:" + name + " OutReqValid:%d OutReqReady:%d OutRespValid:%d OutRespReady:%d ", GTimer(), io.out.req.valid, io.out.req.ready, io.out.resp.valid, io.out.resp.ready)
-      printf(p"OutReqBits:${io.out.req.bits}, OutRespBits:${io.out.resp.bits}\n")
-    }
-  }
-}
-
 class SimpleBusCrossbarNto1(n: Int, userBits:Int = 0) extends Module {
   val io = IO(new Bundle {
     val in = Flipped(Vec(n, new SimpleBusUC(userBits)))
