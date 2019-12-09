b4cc98d2e1083036b70daa90cb1a11c8302784f6
Zihao Yu <yuzihao@ict.ac.cn>
2019-12-09 11:58:01 +0800

    device: add flash to jump to dram

---
+++ src/main/scala/device/AXI4Flash.scala
@@ -0,0 +1,26 @@
+package device
+
+import chisel3._
+import chisel3.util._
+
+import bus.axi4._
+import utils._
+
+class AXI4Flash extends AXI4SlaveModule(new AXI4Lite) {
+  val jmpToDramInstr1 = "h0010029b".U  // addiw t0,zero,1
+  val jmpToDramInstr2 = "h01f29293".U  // slli  t0,t0,0x1f
+  val jmpToDramInstr3 = "h00028067".U  // jr t0
+
+  val mapping = Map(
+    RegMap(0x0, jmpToDramInstr1, RegMap.Unwritable),
+    RegMap(0x4, jmpToDramInstr2, RegMap.Unwritable),
+    RegMap(0x8, jmpToDramInstr3, RegMap.Unwritable)
+  )
+  def getOffset(addr: UInt) = addr(12,0)
+
+  val rdata = Wire(UInt())
+  RegMap.generate(mapping, getOffset(raddr), rdata,
+    getOffset(waddr), in.w.fire(), in.w.bits.data, MaskExpand(in.w.bits.strb))
+
+  in.r.bits.data := RegEnable(RegNext(rdata), ren)
+}
--- src/test/scala/top/SimMMIO.scala
+++ src/test/scala/top/SimMMIO.scala
@@ -15,8 +15,9 @@ class SimMMIO extends Module {
   val devAddrSpace = List(
     (0x40600000L, 0x10L), // uart
     (0x40700000L, 0x10000L), // timer
-    (0x40000000L, 0x400000L), // vmem
-    (0x40800000L, 0x8L)  // vga ctrl
+    (0x41000000L, 0x400000L), // vmem
+    (0x40800000L, 0x8L),  // vga ctrl
+    (0x40000000L, 0x1000L)  // flash
   )
 
   val xbar = Module(new SimpleBusCrossbar1toN(devAddrSpace))
@@ -25,10 +26,12 @@ class SimMMIO extends Module {
   val uart = Module(new AXI4UART)
   val timer = Module(new AXI4Timer(sim = true))
   val vga = Module(new AXI4VGA(sim = true))
+  val flash = Module(new AXI4Flash)
   uart.io.in <> xbar.io.out(0).toAXI4Lite()
   timer.io.in <> xbar.io.out(1).toAXI4Lite()
   vga.io.in.fb <> xbar.io.out(2).toAXI4Lite()
   vga.io.in.ctrl <> xbar.io.out(3).toAXI4Lite()
+  flash.io.in <> xbar.io.out(4).toAXI4Lite()
   vga.io.vga := DontCare
 
   io.mtip := timer.io.extra.get.mtip
