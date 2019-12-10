4bf9a9786a26f6ac8c9e7a902e22fe3ac746c55a
Zihao Yu <yuzihao@ict.ac.cn>
2019-12-11 00:57:13 +0800

    device: add AXI4DummySD

---
+++ src/test/csrc/sdcard.cpp
@@ -0,0 +1,24 @@
+#include "common.h"
+
+extern "C" {
+
+static FILE *fp = NULL;
+
+void sd_setaddr(uint32_t addr) {
+  fseek(fp, addr, SEEK_SET);
+  //printf("set addr to 0x%08x\n", addr);
+  //assert(0);
+}
+
+void sd_read(uint32_t *data) {
+  fread(data, 4, 1, fp);
+  //printf("read data = 0x%08x\n", *data);
+  //assert(0);
+}
+
+void init_sd(void) {
+  fp = fopen("/home/yzh/projectn/debian.img", "r");
+  assert(fp);
+}
+
+}
