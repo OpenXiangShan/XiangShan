30b5c5b642efd337460bb956bf7b64ffa078d1b6
Zihao Yu <yuzihao@ict.ac.cn>
2019-12-24 10:18:10 +0800

    top,TopMain: add AXI4Flash

--- src/main/scala/top/TopMain.scala
+++ src/main/scala/top/TopMain.scala
@@ -2,7 +2,7 @@ package top
 
 import noop.NOOPConfig
 import system.NOOPSoC
-import device.{AXI4Timer, AXI4VGA}
+import device.{AXI4Timer, AXI4VGA, AXI4Flash}
 import gpu._
 
 import chisel3._
@@ -12,15 +12,18 @@ class Top extends Module {
   val noop = Module(new NOOPSoC()(NOOPConfig()))
   val timer = Module(new AXI4Timer)
   val vga = Module(new AXI4VGA)
+  val flash = Module(new AXI4Flash)
 //  val gpu = Module(new AXI4GPU)
 
   noop.io := DontCare
   timer.io := DontCare
   vga.io := DontCare
+  flash.io := DontCare
 //  gpu.io := DontCare
   dontTouch(noop.io)
   dontTouch(timer.io)
   dontTouch(vga.io)
+  dontTouch(flash.io)
 //  dontTouch(gpu.io)
 }
 
