6010ee9fd2bc011f704cca33007b6bf85510fb6e
LinJiawei <linjiav@outlook.com>
2020-05-09 11:45:30 +0800

    [IntToFloat]: Count leading zeros of positive and negative number in parallel because IntToFloat is in the critical path

--- src/main/scala/fpu/IntToFloat.scala
+++ src/main/scala/fpu/IntToFloat.scala
@@ -13,10 +13,16 @@ class IntToFloat extends FPUSubModule with HasPipelineReg {
   val op = io.in.bits.op
   val isDouble = io.in.bits.isDouble
   val a = io.in.bits.a
-  val aNeg = -a
+  val aNeg = (~a).asUInt()
+  val aComp = aNeg + 1.U
   val aSign = Mux(op(0), false.B, Mux(op(1), a(63), a(31)))
-  val aVal = Mux(aSign, Mux(op(1), aNeg, aNeg(31, 0)), Mux(op(1), a, a(31, 0)))
-  val leadingZeros = PriorityEncoder(aVal.asBools().reverse)
+
+  val leadingZerosComp = PriorityEncoder(Mux(op(1), aComp, aComp(31, 0)).asBools().reverse)
+  val leadingZerosNeg = PriorityEncoder(Mux(op(1), aNeg, aNeg(31, 0)).asBools().reverse)
+  val leadingZerosPos = PriorityEncoder(Mux(op(1), a, a(31,0)).asBools().reverse)
+
+  val aVal = Mux(aSign, Mux(op(1), aComp, aComp(31, 0)), Mux(op(1), a, a(31, 0)))
+  val leadingZeros = Mux(aSign, leadingZerosNeg, leadingZerosPos)
 
   // exp = xlen - 1 - leadingZeros + bias
   val expUnrounded = S1Reg(
@@ -25,21 +31,23 @@ class IntToFloat extends FPUSubModule with HasPipelineReg {
       (64 - 1 + Float32.expBiasInt).U - leadingZeros
     )
   )
+  val leadingZeroHasError = S1Reg(aSign && (leadingZerosComp=/=leadingZerosNeg))
   val rmReg = S1Reg(io.in.bits.rm)
   val opReg = S1Reg(op)
   val isDoubleReg = S1Reg(isDouble)
   val aIsZeroReg = S1Reg(a===0.U)
   val aSignReg = S1Reg(aSign)
-  val aShifted = S1Reg(aVal << leadingZeros)
+  val aShifted = S1Reg((aVal << leadingZeros)(63, 0))
 
   /** Stage 2: Rounding
     */
-  val mantD = aShifted(62, 62-51)
-  val mantS = aShifted(62, 62-22)
+  val aShiftedFix = Mux(leadingZeroHasError, aShifted(63, 1), aShifted(62, 0))
+  val mantD = aShiftedFix(62, 62-51)
+  val mantS = aShiftedFix(62, 62-22)
 
-  val g = Mux(isDoubleReg, aShifted(62-52), aShifted(62-23))
-  val r = Mux(isDoubleReg, aShifted(62-53), aShifted(62-24))
-  val s = Mux(isDoubleReg, ORTree(aShifted(62-54, 0)), ORTree(aShifted(62-25, 0)))
+  val g = Mux(isDoubleReg, aShiftedFix(62-52), aShiftedFix(62-23))
+  val r = Mux(isDoubleReg, aShiftedFix(62-53), aShiftedFix(62-24))
+  val s = Mux(isDoubleReg, ORTree(aShiftedFix(62-54, 0)), ORTree(aShiftedFix(62-25, 0)))
 
   val roudingUnit = Module(new RoundingUnit(Float64.mantWidth))
   roudingUnit.io.in.rm := rmReg
@@ -53,7 +61,7 @@ class IntToFloat extends FPUSubModule with HasPipelineReg {
   val expRounded = Mux(isDoubleReg,
     expUnrounded + roudingUnit.io.out.mantCout,
     expUnrounded + mantRounded(Float32.mantWidth)
-  )
+  ) + leadingZeroHasError
 
   val resS = Cat(
     aSignReg,
