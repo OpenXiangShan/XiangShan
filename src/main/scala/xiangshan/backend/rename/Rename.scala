57c4f8d6c40b210d99a6e8c1f324345547eb16db
LinJiawei <linjiav@outlook.com>
2020-06-20 18:15:25 +0800

    Rename: send phy-reg status(rdy/busy) to dispatch-2

--- src/main/scala/xiangshan/backend/rename/Rename.scala
+++ src/main/scala/xiangshan/backend/rename/Rename.scala
@@ -8,7 +8,13 @@ class Rename extends XSModule with NeedImpl {
   val io = IO(new Bundle() {
     val redirect = Flipped(ValidIO(new Redirect))
     val roqCommits = Vec(CommitWidth, Flipped(ValidIO(new RoqCommit)))
+    val wbIntResults = Vec(NRWritePorts, Flipped(ValidIO(new ExuOutput)))
+    val wbFpResults = Vec(NRWritePorts, Flipped(ValidIO(new ExuOutput)))
+    val intPregRdy = Vec(NRReadPorts, Output(Bool()))
+    val fpPregRdy = Vec(NRReadPorts, Output(Bool()))
+    // from decode buffer
     val in = Vec(RenameWidth, Flipped(DecoupledIO(new CfCtrl)))
+    // to dispatch1
     val out = Vec(RenameWidth, DecoupledIO(new MicroOp))
   })
 }
