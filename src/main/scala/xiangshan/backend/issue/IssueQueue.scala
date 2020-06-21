7026d899400163e0d75f946b792d3247e75c2783
jinyue <jinyue161@mails.ucas.ac.cn>
2020-06-21 09:57:15 +0800

    IsssueQueue.scala: add tag queue and data queue as issue queue content

--- src/main/scala/xiangshan/backend/issue/IssueQueue.scala
+++ src/main/scala/xiangshan/backend/issue/IssueQueue.scala
@@ -4,6 +4,11 @@ import chisel3._
 import chisel3.util._
 import xiangshan._
 
+trait IQConst{
+  val iqSize = 4
+
+}
+
 class IssueQueue(val fuTypeInt: BigInt, wakeupCnt: Int, val bypassCnt: Int) extends XSModule with NeedImpl {
 
   val useBypass = bypassCnt > 0
@@ -30,4 +35,16 @@ class IssueQueue(val fuTypeInt: BigInt, wakeupCnt: Int, val bypassCnt: Int) exte
     val bypassUops = if(useBypass) Vec(bypassCnt, Flipped(DecoupledIO(new MicroOp))) else null
     val bypassData = if(useBypass) Vec(bypassCnt, Flipped(DecoupledIO(new ExuOutput))) else null
   })
+  //---------------------------------------------------------
+  // Issue Queue
+  //---------------------------------------------------------
+  val valid   = RegInit(VecInit(Seq.fill(iqSize)(false.B)))
+  val src1Rdy = RegInit(VecInit(Seq.fill(iqSize)(false.B)))
+  val src2Rdy = RegInit(VecInit(Seq.fill(iqSize)(false.B)))
+  val brMask  = RegInit(VecInit(Seq.fill(iqSize)(0.U(robInstCapacity.W))))
+  val prfSrc1 = Reg(Vec(iqSize, UInt(prfAddrWidth.W)))
+  val prfSrc2 = Reg(Vec(iqSize, UInt(prfAddrWidth.W)))
+  val src1    = Reg(Vec(iqSize, UInt(XLEN.W)))
+  val src2    = Reg(Vec(iqSize, UInt(XLEN.W)))
+  
 }
