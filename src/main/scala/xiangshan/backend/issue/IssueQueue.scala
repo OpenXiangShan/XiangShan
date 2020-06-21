71d5424cf9d4f06fbf31f80a324326760915db54
jinyue <jinyue161@mails.ucas.ac.cn>
2020-06-21 11:01:16 +0800

    IssueQueue.scala: add tag queue enqueue logic

--- src/main/scala/xiangshan/backend/issue/IssueQueue.scala
+++ src/main/scala/xiangshan/backend/issue/IssueQueue.scala
@@ -5,7 +5,7 @@ import chisel3.util._
 import xiangshan._
 
 trait IQConst{
-  val iqSize = 4
+  val iqSize = 8
 
 }
 
@@ -38,13 +38,48 @@ class IssueQueue(val fuTypeInt: BigInt, wakeupCnt: Int, val bypassCnt: Int) exte
   //---------------------------------------------------------
   // Issue Queue
   //---------------------------------------------------------
+
+  //Tag Queue
+  val ctrlFlow = Mem(iqSize,new CtrlFlow)
+  val ctrlSig = Mem(iqSize,new CtrlSignals)
   val valid   = RegInit(VecInit(Seq.fill(iqSize)(false.B)))
   val src1Rdy = RegInit(VecInit(Seq.fill(iqSize)(false.B)))
   val src2Rdy = RegInit(VecInit(Seq.fill(iqSize)(false.B)))
-  val brMask  = RegInit(VecInit(Seq.fill(iqSize)(0.U(robInstCapacity.W))))
-  val prfSrc1 = Reg(Vec(iqSize, UInt(prfAddrWidth.W)))
-  val prfSrc2 = Reg(Vec(iqSize, UInt(prfAddrWidth.W)))
-  val src1    = Reg(Vec(iqSize, UInt(XLEN.W)))
-  val src2    = Reg(Vec(iqSize, UInt(XLEN.W)))
-  
+  val brMask  = RegInit(VecInit(Seq.fill(iqSize)(0.U(BrqSize.W))))
+  val prfSrc1 = Reg(Vec(iqSize, UInt(PhyRegIdxWidth.W)))
+  val prfSrc2 = Reg(Vec(iqSize, UInt(PhyRegIdxWidth.W)))
+  val roqIdx  = Reg(Vec(iqSize, UInt(RoqIdxWidth.W)))
+
+  //tag enqueue
+  val iqEmty = !valid.asUInt.orR
+  val iqFull =  valid.asUInt.andR
+  val iqAllowIn = !iqFull 
+  io.enqCtrl.ready := iqAllowIn
+
+  //enqueue pointer
+  val emptySlot = ~valid.asUInt
+  val enqueueSelect = PriorityEncoder(emptySlot)
+
+  when(io.enqCtrl.fire()){
+    ctrlFlow(enqueueSelect) := io.enqCtrl.bits.cf
+    ctrlSig(enqueueSelect) := io.enqCtrl.bits.ctrl
+    valid(enqueueSelect) := true.B
+    prfSrc1(enqueueSelect) := io.enqCtrl.bits.psrc1
+    prfSrc2(enqueueSelect) := io.enqCtrl.bits.psrc2
+    src1Rdy(enqueueSelect) := io.enqCtrl.bits.src1State === SrcState.rdy
+    src2Rdy(enqueueSelect) := io.enqCtrl.bits.src2State === SrcState.rdy
+    brMask(enqueueSelect) := io.enqCtrl.bits.brMask
+  }
+
+  //Data Queue
+  val src1Data    = Reg(Vec(iqSize, UInt(XLEN.W)))
+  val src2Data    = Reg(Vec(iqSize, UInt(XLEN.W)))
+
+
+
+  //---------------------------------------------------------
+  // Select Circuit
+  //---------------------------------------------------------
+
+
 }
