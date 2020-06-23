a30e3946cb36efba06285e9f7e3787344ec3cbf7
Yinan Xu <xuyinan1997@gmail.com>
2020-06-23 17:10:11 +0800

    backend,dispatch: remove unused test code

--- src/main/scala/xiangshan/backend/dispatch/Dispatch.scala
+++ src/main/scala/xiangshan/backend/dispatch/Dispatch.scala
@@ -1,7 +1,5 @@
 package xiangshan.backend.dispatch
 
-//import org.scalatest._
-//import chiseltest._
 import chisel3._
 import chisel3.util._
 import xiangshan._
@@ -60,83 +58,3 @@ class Dispatch extends XSModule {
   dispatch2.io.enqIQCtrl <> io.enqIQCtrl
   dispatch2.io.enqIQData <> io.enqIQData
 }
-
-class DispatchWrapper extends XSModule {
-  val io = IO(new Bundle() {
-    val redirect = Flipped(ValidIO(new Redirect))
-    // from rename
-    val fromRename = Vec(RenameWidth, Flipped(DecoupledIO(new MicroOp)))
-    // enq Roq
-    val toRoq =  Vec(RenameWidth, DecoupledIO(new MicroOp))
-    // get RoqIdx
-    val roqIdxs = Input(Vec(RenameWidth, UInt(RoqIdxWidth.W)))
-    // read regfile
-    val readIntRf = Vec(NRReadPorts, Flipped(new RfReadPort))
-    val readFpRf = Vec(NRReadPorts, Flipped(new RfReadPort))
-    // read reg status (busy/ready)
-    val intPregRdy = Vec(NRReadPorts, Input(Bool()))
-    val fpPregRdy = Vec(NRReadPorts, Input(Bool()))
-    // ro reservation stations
-    val enqIQCtrl = Vec(exuConfig.ExuCnt, DecoupledIO(new MicroOp))
-    val enqIQData = Vec(exuConfig.ExuCnt, ValidIO(new ExuInput))
-  })
-  io <> DontCare
-
-  val dispatch = Module(new Dispatch())
-  dispatch.io <> DontCare
-
-  dispatch.io.redirect <> io.redirect
-  for (i <- 0 until RenameWidth) {
-    dispatch.io.fromRename(i).valid := io.fromRename(i).valid
-    dispatch.io.fromRename(i).bits.cf.pc := io.fromRename(i).bits.cf.pc
-    dispatch.io.fromRename(i).bits.psrc1 := io.fromRename(i).bits.psrc1
-    dispatch.io.fromRename(i).bits.psrc2 := io.fromRename(i).bits.psrc2
-    dispatch.io.fromRename(i).bits.psrc3 := io.fromRename(i).bits.psrc3
-    dispatch.io.fromRename(i).bits.ctrl.fuType := io.fromRename(i).bits.ctrl.fuType
-    io.fromRename(i).ready := dispatch.io.fromRename(i).ready
-
-    dispatch.io.toRoq(i).ready := io.toRoq(i).ready
-    io.toRoq(i).valid := dispatch.io.toRoq(i).valid
-    io.toRoq(i).bits.cf.pc := dispatch.io.toRoq(i).bits.cf.pc
-
-    dispatch.io.roqIdxs(i) := io.roqIdxs(i)
-  }
-
-  for (i <- 0 until NRReadPorts) {
-    dispatch.io.readIntRf(i).data := io.readIntRf(i).data
-    io.readIntRf(i).addr := dispatch.io.readIntRf(i).addr
-    dispatch.io.readFpRf(i).data := io.readFpRf(i).data
-    io.readFpRf(i).addr := dispatch.io.readFpRf(i).addr
-    dispatch.io.intPregRdy(i) := io.intPregRdy(i)
-    dispatch.io.fpPregRdy(i) := io.fpPregRdy(i)
-  }
-
-  for (i <- 0 until exuConfig.ExuCnt) {
-    io.enqIQCtrl(i).valid := dispatch.io.enqIQCtrl(i).valid
-    dispatch.io.enqIQCtrl(i).ready := io.enqIQCtrl(i).ready
-    io.enqIQCtrl(i).bits.cf.pc := dispatch.io.enqIQCtrl(i).bits.cf.pc
-    io.enqIQCtrl(i).bits.psrc1 := dispatch.io.enqIQCtrl(i).bits.psrc1
-    io.enqIQCtrl(i).bits.psrc2 := dispatch.io.enqIQCtrl(i).bits.psrc2
-    io.enqIQCtrl(i).bits.psrc3 := dispatch.io.enqIQCtrl(i).bits.psrc3
-    io.enqIQCtrl(i).bits.roqIdx := dispatch.io.enqIQCtrl(i).bits.roqIdx
-
-    io.enqIQData(i).valid := dispatch.io.enqIQData(i).valid
-    io.enqIQData(i).bits.uop.cf.pc := dispatch.io.enqIQData(i).bits.uop.cf.pc
-    io.enqIQData(i).bits.uop.psrc1 := dispatch.io.enqIQData(i).bits.uop.psrc1
-    io.enqIQData(i).bits.uop.psrc2 := dispatch.io.enqIQData(i).bits.uop.psrc2
-    io.enqIQData(i).bits.uop.psrc3 := dispatch.io.enqIQData(i).bits.uop.psrc3
-    io.enqIQData(i).bits.uop.roqIdx := dispatch.io.enqIQData(i).bits.uop.roqIdx
-    io.enqIQData(i).bits.src1 := dispatch.io.enqIQData(i).bits.src1
-    io.enqIQData(i).bits.src2 := dispatch.io.enqIQData(i).bits.src2
-    io.enqIQData(i).bits.src3 := dispatch.io.enqIQData(i).bits.src3
-  }
-}
-
-object DispatchTest extends App {
-  Driver.execute(args, () => new DispatchWrapper())
-}
-
-//class DispatchTest extends FlapSpec with ChiselScalatestTester with Matchers {
-//
-//}
-
--- src/main/scala/xiangshan/backend/dispatch/Dispatch1.scala
+++ src/main/scala/xiangshan/backend/dispatch/Dispatch1.scala
@@ -3,7 +3,7 @@ package xiangshan.backend.dispatch
 import chisel3._
 import chisel3.util._
 import xiangshan._
-import utils.{GTimer, PipelineConnect}
+import utils.{GTimer}
 
 case class DP1Config
 (
@@ -106,77 +106,3 @@ class Dispatch1 extends XSModule{
     }
   }
 }
-
-//class Dispatch1Debug extends XSModule {
-//  val io = IO(new Bundle() {
-//    val redirect = Flipped(ValidIO(new Redirect))
-//    // from rename
-//    val fromRename = Vec(RenameWidth, Flipped(DecoupledIO(new MicroOp)))
-//    // enq Roq
-//    val toRoq =  Vec(RenameWidth, DecoupledIO(new MicroOp))
-//    // get RoqIdx
-//    val roqIdxs = Input(Vec(RenameWidth, UInt(RoqIdxWidth.W)))
-//    val fromIntDq = Vec(IntDqDeqWidth, DecoupledIO(UInt(46.W)))
-//    val fromFpDq = Vec(FpDqDeqWidth, DecoupledIO(UInt(46.W)))
-//    val fromLsDq = Vec(LsDqDeqWidth, DecoupledIO(UInt(46.W)))
-//    // read regfile
-//    //    val readIntRf = Vec(NRReadPorts, Flipped(new RfReadPort))
-//    //    val readFpRf = Vec(NRReadPorts, Flipped(new RfReadPort))
-//    // ro reservation stations
-//    //    val enqIQCtrl = Vec(exuConfig.ExuCnt, DecoupledIO(new MicroOp))
-//    //    val enqIQData = Vec(exuConfig.ExuCnt, ValidIO(new   ExuInput))
-//  })
-//  // pipeline between rename and dispatch
-//  val dispatch1 = Module(new Dispatch1())
-//  for (i <- 0 until RenameWidth) {
-////    dispatch1.io.fromRename(i) <> Queue(io.fromRename(i))
-//    PipelineConnect(io.fromRename(i), dispatch1.io.fromRename(i), dispatch1.io.recv(i), false.B)
-//  }
-//
-////  dispatch1.io.fromRename <> DontCare
-////  io.fromRename.foreach( x => x.ready <> DontCare)
-//
-//  val intDq = Module(new DispatchQueue(new MicroOp, dp1Config.IntDqSize, RenameWidth, IntDqDeqWidth))
-//  val fpDq = Module(new DispatchQueue(new MicroOp, dp1Config.FpDqSize, RenameWidth, FpDqDeqWidth))
-//  val lsDq = Module(new DispatchQueue(new MicroOp, dp1Config.LsDqSize, RenameWidth, LsDqDeqWidth))
-////  val dispatch2 = new Dispatch2()
-//
-//  dispatch1.io.redirect <> io.redirect
-//  dispatch1.io.toRoq <> io.toRoq
-//  dispatch1.io.roqIdxs <> io.roqIdxs
-//  dispatch1.io.toIntDq <> intDq.io.enq
-//  dispatch1.io.toFpDq <> fpDq.io.enq
-//  dispatch1.io.toLsDq <> lsDq.io.enq
-//
-//  for (i <- 0 until IntDqDeqWidth) {
-//    intDq.io.deq(i).ready := io.fromIntDq(i).ready
-//    io.fromIntDq(i).valid := intDq.io.deq(i).valid
-//    io.fromIntDq(i).bits := Cat(intDq.io.deq(i).bits.roqIdx, intDq.io.deq(i).bits.cf.pc)
-//    when (io.fromIntDq(i).fire()) {
-//      printf("[Dispatch1:%d]: instruction 0x%x leaves Int dispatch queue with nroq %d\n",
-//        GTimer(), io.fromIntDq(i).bits(38, 0), io.fromIntDq(i).bits(45, 39))
-//    }
-//  }
-//  for (i <- 0 until FpDqDeqWidth) {
-//    fpDq.io.deq(i).ready := io.fromFpDq(i).ready
-//    io.fromFpDq(i).valid := fpDq.io.deq(i).valid
-//    io.fromFpDq(i).bits := Cat(fpDq.io.deq(i).bits.roqIdx, fpDq.io.deq(i).bits.cf.pc)
-//    when (io.fromFpDq(i).fire()) {
-//      printf("[Dispatch1:%d]: instruction 0x%x leaves FP dispatch queue with nroq %d\n",
-//        GTimer(), io.fromFpDq(i).bits(38, 0), io.fromIntDq(i).bits(45, 39))
-//    }
-//  }
-//  for (i <- 0 until LsDqDeqWidth) {
-//    lsDq.io.deq(i).ready := io.fromLsDq(i).ready
-//    io.fromLsDq(i).valid := lsDq.io.deq(i).valid
-//    io.fromLsDq(i).bits := Cat(lsDq.io.deq(i).bits.roqIdx, lsDq.io.deq(i).bits.cf.pc)
-//    when (io.fromLsDq(i).fire()) {
-//      printf("[Dispatch1:%d]: instruction 0x%x leaves LS dispatch queue with nroq %d\n",
-//        GTimer(), io.fromLsDq(i).bits(38, 0), io.fromIntDq(i).bits(45, 39))
-//    }
-//  }
-//}
-//
-//object Dispatch1Top extends App {
-//  Driver.execute(args, () => new Dispatch1Debug ())
-//}
