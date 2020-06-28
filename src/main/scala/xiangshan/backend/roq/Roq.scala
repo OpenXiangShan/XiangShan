cd79f2b35af121ff55c108ad0ffc15e01db5f8a5
William Wang <zeweiwang@outlook.com>
2020-06-28 16:58:15 +0800

    difftest: fix retire difftest logic

--- src/main/scala/xiangshan/backend/roq/Roq.scala
+++ src/main/scala/xiangshan/backend/roq/Roq.scala
@@ -76,6 +76,9 @@ class Roq(implicit val p: XSConfig) extends XSModule {
   when(PopCount(firedWriteback) > 0.U){
     XSInfo("writebacked %d insts\n", PopCount(firedWriteback))
   }
+  for(i <- 0 until exuConfig.ExuCnt){
+    XSInfo("writebacked pc %x wen %d data %d\n", microOp(io.exeWbResults(i).bits.uop.roqIdx).cf.pc, microOp(io.exeWbResults(i).bits.uop.roqIdx).ctrl.rfWen, io.exeWbResults(i).bits.data)
+  }
 
   // Commit uop to Rename
   for(i <- 0 until CommitWidth){
@@ -83,7 +86,7 @@ class Roq(implicit val p: XSConfig) extends XSModule {
       val canCommit = if(i!=0) io.commits(i-1).valid else true.B
       io.commits(i).valid := valid(ringBufferTail+i.U) && writebacked(ringBufferTail+i.U) && canCommit
       io.commits(i).bits.uop := microOp(ringBufferTail+i.U)
-      when(microOp(i).ctrl.rfWen){ archRF(microOp(i).ctrl.ldest) := exuData(i) }
+      when(microOp(ringBufferTail+i.U).ctrl.rfWen){ archRF(microOp(ringBufferTail+i.U).ctrl.ldest) := exuData(ringBufferTail+i.U) }
       when(io.commits(i).valid){valid(ringBufferTail+i.U) := false.B}
     }.otherwise{//state === s_walk
       io.commits(i).valid := valid(ringBufferWalk+i.U) && writebacked(ringBufferWalk+i.U)
