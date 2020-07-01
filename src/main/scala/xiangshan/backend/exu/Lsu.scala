f682f5fdbd0d9bdd9162863ba18729f588799231
William Wang <zeweiwang@outlook.com>
2020-07-01 19:26:43 +0800

    Lsu: fix scommit condition

--- src/main/scala/xiangshan/backend/exu/Lsu.scala
+++ src/main/scala/xiangshan/backend/exu/Lsu.scala
@@ -46,9 +46,11 @@ object LSUOpType {
 class StoreQueueEntry extends XSBundle{
   val src1  = UInt(XLEN.W)
   val src2  = UInt(XLEN.W)
+  val addr  = UInt(XLEN.W)
   val src3  = UInt(XLEN.W)
   val wdata = UInt(XLEN.W)
   val func  = UInt(6.W)
+  val pc  = UInt(VAddrBits.W) //for debug
   val brMask = UInt(BrqSize.W) //FIXIT
 }
 
@@ -69,7 +71,7 @@ class Lsu extends Exu(
   val stqTail = stqPtr(0)
   val stqCommited = RegInit(0.U(3.W))
   val stqFull = stqHead === 7.U //stq_valid.reduce(_.valid && _.valid)
-  val emptySlot = PriorityMux(~stqValid.asUInt, VecInit(List.tabulate(CommitWidth)(_.U)))
+  val emptySlot = PriorityMux(~stqValid.asUInt, VecInit(List.tabulate(8)(_.U)))
 
   // when retiringStore, block all input insts
   val isStoreIn = io.in.valid && LSUOpType.isStore(io.in.bits.uop.ctrl.fuOpType)
@@ -144,40 +146,82 @@ class Lsu extends Exu(
       LSUOpType.lwu  -> ZeroExt(rdataSel(31, 0), XLEN)
   ))
 
+  // pop store queue if insts have been commited and dmem req fired successfully
+  val stqDequeue = retiringStore && state === s_partialLoad || !stqValid(stqTail) && stqHead > 0.U
+  when(stqDequeue){
+    stqValid(stqTail) := false.B
+    // update stq ptr
+    for(i <- 1 until 8){
+      stqPtr(i-1) := stqPtr(i) 
+    }
+  }
 
   // if store, add it to store queue
-  val stqEnqueue = validIn && isStoreIn && !stqFull
+  val stqEnqueue = validIn && isStoreIn && !stqFull && !retiringStore
   when(stqEnqueue){
     stqPtr(stqHead) := emptySlot
     stqData(emptySlot).src1 := src1In
     stqData(emptySlot).src2 := src2In
+    stqData(emptySlot).addr := src1In + src2In
     stqData(emptySlot).src3 := src3In
+    stqData(emptySlot).pc := io.in.bits.uop.cf.pc
     stqData(emptySlot).func := funcIn
     stqValid(emptySlot) := true.B
   }
 
   // if store insts have been commited, send dmem req
-  retiringStore := stqCommited > 0.U
-
-  // pop store queue if insts have been commited and dmem req fired successfully
-  val stqDequeue = retiringStore && state === s_partialLoad
-  when(stqDequeue){
-    stqValid(stqTail) := false.B
-  }
+  retiringStore := stqCommited > 0.U && stqValid(stqTail)
 
   // update stqTail, stqCommited
   stqCommited := stqCommited + io.scommit - stqDequeue
   stqHead := stqHead + stqEnqueue - stqDequeue
 
+  // Store addr forward match
+  // If match, get data from store queue
+  val loadResult = Mux(partialLoad, rdataPartialLoad, rdata)
+  val dataBackVec = Wire(Vec(XLEN/8, (UInt((XLEN/8).W))))
+  for(j <- (0 to (XLEN/8 - 1))){
+    dataBackVec(j) := loadResult(8*(j+1)-1, 8*j)
+  }
+  
+  for(i <- 0 until 8){
+    when(stqValid(stqPtr(i)) && i.U < stqHead){
+      when(addr(PAddrBits-1, log2Up(XLEN/8)) === stqData(stqPtr(i)).addr(PAddrBits-1, log2Up(XLEN/8))){
+        for(j <- (0 to (XLEN/8 - 1))){
+          when(genWmask(stqData(stqPtr(i)).addr, stqData(stqPtr(i)).func(1, 0))(j)){
+            dataBackVec(j) := stqData(stqPtr(i)).src3(8*(j+1)-1, 8*j)
+          }
+        }
+      }
+      XSDebug("sbuffer id %d ptr %d pc %x addr %x data %x func %x wmask %b\n", 
+        i.U, stqPtr(i), stqData(stqPtr(i)).pc, stqData(stqPtr(i)).src1 + stqData(stqPtr(i)).src2, stqData(stqPtr(i)).src3, stqData(stqPtr(i)).func, genWmask(stqData(stqPtr(i)).addr, stqData(stqPtr(i)).func(1, 0))
+      )
+    }
+  }
+
+  val expRedirect = io.redirect.valid && io.redirect.bits.isException
+  val brRedirect = io.redirect.valid && !io.redirect.bits.isException
+  for(i <- 0 until 8){
+    when(expRedirect || brRedirect && (UIntToOH(io.redirect.bits.brTag) & stqData(i).brMask(i)).orR && stqValid(i)){
+      stqValid(i) := false.B
+    }
+    XSDebug("sptrtable: id %d ptr %d valid  %d\n", i.U, stqPtr(i), stqValid(stqPtr(i)))
+  }
+  when(expRedirect){
+    //invalidate uncommited store
+    //FIXME
+  }
+
   io.in.ready := io.out.fire()
 
   io.out.valid := (!isStoreIn && !retiringStore && Mux(partialLoad, state === s_partialLoad, dmem.resp.fire() && (state === s_wait_resp)) || stqEnqueue) && io.in.valid
   io.out.bits.uop <> io.in.bits.uop
-  io.out.bits.data := Mux(partialLoad, rdataPartialLoad, rdata)
+  io.out.bits.data := dataBackVec.asUInt
   // io.out.bits.debug.isMMIO := AddressSpace.isMMIO(addr) && io.out.valid
   io.out.bits.debug.isMMIO := AddressSpace.isMMIO(addr) //for debug
   when(io.out.fire()){
-    XSDebug("LSU fire: addr %x mmio %x isStoreIn %x retiringStore %x partialLoad %x dmem %x stqEnqueue %x state %x \n",
+    XSDebug("LSU fire: pc %x addr %x mmio %x isStoreIn %x retiringStore %x partialLoad %x dmem %x stqEnqueue %x state %x dmemres %x fwdres %x\n",
+      io.in.bits.uop.cf.pc,
       addr,
       io.out.bits.debug.isMMIO,
       isStoreIn,
@@ -185,13 +229,16 @@ class Lsu extends Exu(
       partialLoad,
       dmem.resp.fire(),
       stqEnqueue,
-      state
+      state,
+      loadResult,
+      io.out.bits.data
     )
   }
 
   // debug
   XSDebug("state: %d (valid, ready): in (%d,%d) out (%d,%d)\n", state, io.in.valid, io.in.ready, io.out.valid, io.out.ready)
   XSDebug("stqinfo: stqValid.asUInt %b stqHead %d stqTail %d stqCommited %d emptySlot %d\n", stqValid.asUInt, stqHead, stqTail, stqCommited, emptySlot)
+  XSDebug(retiringStore, "retiringStore now...\n")
   XSInfo(io.dmem.req.fire() && io.dmem.req.bits.cmd =/= SimpleBusCmd.write, "[DMEM LOAD  REQ] addr 0x%x wdata 0x%x size %d\n", dmem.req.bits.addr, dmem.req.bits.wdata, dmem.req.bits.size)
   XSInfo(io.dmem.req.fire() && io.dmem.req.bits.cmd === SimpleBusCmd.write, "[DMEM STORE REQ] addr 0x%x wdata 0x%x size %d\n", dmem.req.bits.addr, dmem.req.bits.wdata, dmem.req.bits.size)
   XSInfo(io.dmem.resp.fire(), "[DMEM RESP] data %x\n", rdata)
