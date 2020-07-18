67feaed137504f53191eac343f1c7295ed57b130
YikeZhou <u201714805@hust.edu.cn>
2020-07-18 09:18:39 +0800

    SBufferTest.scala: Add SBufferDut and implement part of buffer emu

--- src/test/scala/xiangshan/backend/exu/SBufferTest.scala
+++ src/test/scala/xiangshan/backend/exu/SBufferTest.scala
@@ -1,12 +1,19 @@
 package xiangshan.backend.exu
 
+import chisel3.{Bundle, Flipped, Module, Output, Vec}
+import chisel3.internal.DontCareBinding
+import chisel3.util.Decoupled
+import chisel3.util.experimental.BoringUtils
 import chiseltest.ChiselScalatestTester
 import org.scalatest.{FlatSpec, Matchers, ParallelTestExecution}
-import xiangshan.HasXSParameter
-import xiangshan.mem.pipeline.HasSBufferConst
-import xiangshan.testutils.HasPartialDecoupledDriver
+import xiangshan.{HasXSParameter, XSConfig}
+import xiangshan.mem.HasMEMConst
+import xiangshan.mem.pipeline.{HasSBufferConst, LsPipelineBundle, SBufferCacheLine, Sbuffer}
+import xiangshan.testutils.{AddSinks, HasPartialDecoupledDriver}
 
 import scala.collection.mutable.ArrayBuffer
+
+class SBufferToDCache
 
 class CacheLine extends HasSBufferConst {
   var valid = false
@@ -47,8 +54,91 @@ class CacheLine extends HasSBufferConst {
   }
 }
 
-class StoreBuffer extends HasXSParameter with HasSBufferConst {
-  // TODO
+class StoreBuffer(implicit dCacheDelay: Int = 1)
+  extends HasXSParameter
+    with HasSBufferConst
+    with HasMEMConst {
+  val cache: Array[CacheLine] = Array.fill[CacheLine](SbufferSize)(new CacheLine)
+  val cacheNext: Array[CacheLine] = Array.fill[CacheLine](SbufferSize)(new CacheLine)
+
+  def cacheCopy(src: Array[CacheLine], dst: Array[CacheLine]): Unit = {
+    assert(src.length == SbufferSize && dst.length == SbufferSize)
+
+    for (i <- 0 until SbufferSize) {
+//      dst(i) = src(i)
+      dst(i).valid = src(i).valid
+      dst(i).tag = src(i).tag
+//      dst(i).data = src(i).data
+      for (j <- dst(i).data.indices) {
+        dst(i).data(j) = src(i).data(j)
+      }
+//      dst(i).mask = src(i).mask
+      for (j <- dst(i).mask.indices) {
+        dst(i).mask(j) = src(i).mask(j)
+      }
+      dst(i).lruCnt = src(i).lruCnt
+    }
+  }
+
+  def getTag(paddr: Int): Int = paddr >> offsetWidth
+
+  def hasEmpty: Boolean = {
+    !cache.reduce((x, y) => x.valid & y.valid)
+  }
+
+  // get index of empty line which has the biggest index
+  def nextEmpty(): Int = {
+    var rev = -1
+    for (i <- 0 until SbufferSize) {
+      if (!cache(i).valid)
+        rev = i
+    }
+    rev
+  }
+
+  // pretend to receive ready from d-cache
+  var dCacheDelayCounter: Int = 0
+  def setDelayCounter(): Unit = dCacheDelayCounter = dCacheDelay
+  def decDelayCounter(): Unit = dCacheDelayCounter -= 1
+  def dCacheReady: Boolean = dCacheDelayCounter == 0
+
+
+  def pokeIn(pa: Array[Int], mask: Array[Boolean], data: Array[Int]) = {
+    assert(pa.length == StorePipelineWidth && mask.length == StorePipelineWidth && data.length == StorePipelineWidth)
+
+    for (queryIdx <- 0 until StorePipelineWidth)
+      for (bufIdx <- 0 until SbufferSize) {
+        // First, check if there exists a line for this pa
+        if (getTag(pa(queryIdx)) == cache(bufIdx).tag) {
+          isUpdate(bufIdx)
+        }
+      }
+  }
+
+  def nextCycle(): Unit = {
+    decDelayCounter()
+    // cache = cacheNext
+    cacheCopy(cacheNext, cache)
+  }
+  /*
+  Todo: implement in / d-cache / forward as functions
+  Todo: compare cache line data to verify for now, so implement a getAllData function
+   */
+}
+
+class SBufferDut(dispBegin: Int, dispEnd: Int) extends Module with HasMEMConst with HasSBufferConst {
+  val io = IO(new Bundle() {
+    val in = Vec(StorePipelineWidth, Flipped(Decoupled(new LsPipelineBundle)))
+    val cacheLines = Vec(SbufferSize, Output(new SBufferCacheLine))
+  })
+  val sBuffer = Module(new Sbuffer)
+  // in:
+  io.in <> sBuffer.io.in
+
+  // cacheLines contain data and mask and valid and tag in cache lines
+  BoringUtils.bore(sBuffer.cache, Seq(io.cacheLines))
+
+  AddSinks(dispBegin, dispEnd)
 }
 
 class SBufferTest extends FlatSpec
@@ -58,6 +148,7 @@ class SBufferTest extends FlatSpec
   with HasPartialDecoupledDriver
 {
   it should "behave like its emulator" in {
-    // TODO
+    // TODO: 1. add a test case generator (only generate in for now)
+    // TODO: 2. use a s-buffer instance to compare with emu
   }
 }
