262c775805bcc95af43312f189718dba7e83dcb3
Zihao Yu <yuzihao@ict.ac.cn>
2020-07-25 22:05:36 +0800

    emu,snapshot: buffer the snapshot in memory

--- src/test/csrc/snapshot.cpp
+++ src/test/csrc/snapshot.cpp
@@ -1,4 +1,50 @@
 #include "emu.h"
+#include <verilated_save.h>
+
+class VerilatedSaveMem : public VerilatedSave {
+  const static long buf_size = 1024 * 1024 * 1024;
+  uint8_t *buf;
+  long size;
+
+public:
+  VerilatedSaveMem(const char *filename) {
+    buf = new uint8_t[buf_size];
+    size = 0;
+    m_filename = filename;
+    header();
+  }
+
+  ~VerilatedSaveMem() {
+    delete buf;
+  }
+
+  void mywrite(const void* __restrict datap, size_t size) VL_MT_UNSAFE_ONE {
+    memcpy(buf + this->size, datap, size);
+    this->size += size;
+  }
+
+  void close() { }
+  void flush() {
+    long flush_size = m_cp - m_bufp;
+    assert(buf_size - size > flush_size);
+    memcpy(buf + size, m_bufp, flush_size);
+    size += flush_size;
+    m_cp = m_bufp;
+  }
+
+  void clear() { size = 0; }
+
+  void save() {
+    if (size == 0) return;
+    trailer();
+    flush();
+    FILE *fp = fopen(m_filename.c_str(), "w");
+    assert(fp != NULL);
+    fwrite(buf, size, 1, fp);
+    fclose(fp);
+    size = 0;
+  }
+};
 
 void* get_ram_start();
 long get_ram_size();
@@ -18,13 +64,15 @@ char* Emulator::snapshot_filename(const char *name) {
 }
 
 void Emulator::snapshot_save(const char *filename) {
-  VerilatedSave stream;
-  stream.open(filename);
+  VerilatedSaveMem stream(filename);
   stream << *dut_ptr;
+  stream.flush();
 
   long size = get_ram_size();
-  stream.write(&size, sizeof(size));
-  stream.write(get_ram_start(), size);
+  stream.mywrite(&size, sizeof(size));
+  stream.mywrite(get_ram_start(), size);
+
+  stream.save();
 }
 
 void Emulator::snapshot_load(const char *filename) {
