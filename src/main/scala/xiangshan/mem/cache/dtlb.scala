a303166317538fbdec215755eb564c6cb49e09a3
ZhangZifei <1773908404@qq.com>
2020-07-27 11:33:59 +0800

    TLB: add simple tlb const

--- src/main/scala/xiangshan/mem/cache/dtlb.scala
+++ src/main/scala/xiangshan/mem/cache/dtlb.scala
@@ -10,6 +10,66 @@ import xiangshan.mem._
 import xiangshan.mem.pipeline._
 import bus.simplebus._
 
+trait DtlbConst extends HasXSParameter with HasMEMConst {
+  val L1EntryNum = 32
+  val Level = 3
+  val L2EntryNum = 1024 // every layer has L2EntryNum entries
+
+  val offLen  = 12
+  val ppn0Len = 9
+  val ppn1Len = 9
+  val ppn2Len = PAddrBits - offLen - ppn0Len - ppn1Len
+  val ppnLen  = ppn0Len + ppn1Len + ppn2Len
+  val vpn0Len = 9
+  val vpn1Len = 9
+  val vpn2Len = VAddrBits - offLen - vpn0Len - vpn1Len
+  val vpnLen  = vpn0Len + vpn1Len + vpn2Len
+
+  val entryLen = XLEN
+  val flagLen = 8
+  val pteResLen = XLEN - ppnLen - 2 - flagLen
+
+  def vaBundle = new Bundle {
+    val vpn2 = UInt(vpn2Len.W)
+    val vpn1 = UInt(vpn1Len.W)
+    val vpn0 = UInt(vpn0Len.W)
+    val off  = UInt( offLen.W)
+  }
+
+  def vaBundle2 = new Bundle {
+    val vpn  = UInt(vpnLen.W)
+    val off  = UInt(offLen.W)
+  }
+
+  def paBundle = new Bundle {
+    val ppn2 = UInt(ppn2Len.W)
+    val ppn1 = UInt(ppn1Len.W)
+    val ppn0 = UInt(ppn0Len.W)
+    val off  = UInt( offLen.W)
+  }
+
+  def paBundle2 = new Bundle {
+    val ppn  = UInt(ppnLen.W)
+    val off  = UInt(offLen.W)
+  }
+
+   def pteBundle = new Bundle {
+    val reserved  = UInt(pteResLen.W)
+    val ppn  = UInt(ppnLen.W)
+    val rsw  = UInt(2.W)
+    val flag = new Bundle {
+      val d    = UInt(1.W)
+      val a    = UInt(1.W)
+      val g    = UInt(1.W)
+      val u    = UInt(1.W)
+      val x    = UInt(1.W)
+      val w    = UInt(1.W)
+      val r    = UInt(1.W)
+      val v    = UInt(1.W)
+    }
+  }
+}
+
 class DtlbReq extends XSBundle with HasMEMConst {
   val vaddr = UInt(VAddrBits.W)
 }
