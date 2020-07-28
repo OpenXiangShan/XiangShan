ef49dd3f2034e58892ac77309cd561633f28df50
William Wang <zeweiwang@outlook.com>
2020-07-28 15:48:28 +0800

    Mem: add miss channel

--- src/main/scala/xiangshan/mem/cache/dcache.scala
+++ src/main/scala/xiangshan/mem/cache/dcache.scala
@@ -64,9 +64,15 @@ class DCacheStoreIO extends XSBundle
   val resp = DecoupledIO(new DCacheResp)
 }
 
+class MissReqIO extends XSBundle
+{
+  val paddr = UInt(PAddrBits.W)
+}
+
 class DcacheToLsuIO extends XSBundle with HasMEMConst {
   val load = Vec(LoadPipelineWidth, new DCacheLoadIO)
   val store = new DCacheStoreIO
+  val refill = new DCacheStoreIO
   val redirect = Flipped(ValidIO(new Redirect))
 }
 
