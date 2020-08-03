255f2bca32db8615878ec212b4f2a9114a079869
jinyue110 <jinyue161@mails.ucas.ac.cn>
2020-08-03 21:08:40 +0800

    RAS: add RAS logic

---
+++ src/main/scala/xiangshan/frontend/RAS.scala
@@ -0,0 +1,77 @@
+package xiangshan.frontend
+
+import chisel3._
+import chisel3.util._
+import xiangshan._
+import xiangshan.backend.ALUOpType
+import utils._
+
+class RAS extends BasePredictor
+    with RASParameter
+{
+    class RASResp extends Resp
+    {
+        val target =UInt(VAddrBits.W)
+    }
+
+    class RASBranchInfo extends Meta
+    {
+        val rasSp = UInt(log2Up(RasSize).W)
+        val rasTopCtr = UInt(8.W)
+    }
+
+    class RASIO extends DefaultBasePredictorIO 
+    {
+        val retIdx = Flipped(ValidIO(UInt(log2Ceil(PredictWidth).W)))
+        val callIdx = Flipped(ValidIO(UInt(log2Ceil(PredictWidth).W)))
+        val isRVC = Input(Bool())
+        val redirect =  Flipped(ValidIO(new Redirect)))
+        val out = ValidIO(new RASResp)
+        val branchInfo = ValidIO(new RASBranchInfo)
+    }
+
+    def rasEntry() = new Bundle {
+    val retAddr = UInt(VAddrBits.W)
+    val ctr = UInt(8.W) // layer of nested call functions
+    }
+    val ras = RegInit(VecInit(RasSize, 0.U.asTypeOf(rasEntry())))
+    val sp = RegInit(0.U(log2Up(RasSize).W))
+    val ras_top_entry = ras(sp)
+    val ras_top_addr = ras_top_entry.retAddr
+
+    val is_empty = sp === 0.U
+    val is_full = sp === (RasSize - 1).U
+    // save ras checkpoint info
+    io.out.bits.rasSp := sp
+    io.out.bits.rasTopCtr := rasTop.ctr
+
+    // update RAS
+    // speculative update RAS
+
+    when (!is_full && io.callIdx.valid) {
+        //push
+        when (ras_top_entry.ctr === 1.U) {
+            sp := Mux(sp.value === 0.U, 0.U, sp - 1.U)
+        }.otherwise {
+            ras_top_entry.ctr := ras_top_entry.ctr - 1.U
+        }
+    }.elsewhen ((!is_empty && io.retIdx.valid) {
+        //pop
+        io.out.bits.target := ras_top_addr
+        val rasWrite = WireInit(0.U.asTypeOf(rasEntry()))
+        val allocNewEntry = rasWrite.retAddr =/= rasTopAddr
+        rasWrite.ctr := Mux(allocNewEntry, 1.U, rasTop.ctr + 1.U)
+        rasWrite.retAddr := io.pc.bits + (io.callIdx.bits << 2.U) + 4.U
+        ras(sp) := in.target
+        when(allocNewEntry){sp := sp + 1.U }
+    }
+    // TODO: back-up stack for ras
+    // use checkpoint to recover RAS
+    val recoverSp = io.redirect.rasSp
+    val recoverCtr = io.redirect.rasTopCtr
+    when (io.redirect.valid && io.redirect.isMisPred) {
+        sp := recoverSp
+        ras(recoverSp) := Cat(recoverCtr, ras(recoverSp).retAddr).asTypeOf(rasEntry())
+    }
+
+}
