8fe0c3d9b3d0978ea7027fb3c72cbe269040cfc0
ljw <linjiav@outlook.com>
2020-08-15 11:41:45 +0800

    Fix brq performance bug

--- src/main/scala/xiangshan/backend/brq/Brq.scala
+++ src/main/scala/xiangshan/backend/brq/Brq.scala
@@ -101,9 +101,7 @@ class Brq extends XSModule {
   // dequeue
   val headIdx = headPtr.value
 
-  val skipMask = Cat(stateQueue.zipWithIndex.map({
-    case (s, i) => (s.isWb && !brQueue(i.U).exuOut.brUpdate.isMisPred) || s.isCommit
-  }).reverse)
+  val skipMask = Cat(stateQueue.map(_.isCommit).reverse)
 
   /*
       example: headIdx       = 2
@@ -130,16 +128,15 @@ class Brq extends XSModule {
   val useLo = skipHi && findLo
 
 
-  val commitIdx = Mux(stateQueue(commitIdxHi).isWb && brQueue(commitIdxHi).exuOut.brUpdate.isMisPred,
+  val commitIdx = Mux(stateQueue(commitIdxHi).isWb,
     commitIdxHi,
-    Mux(useLo && stateQueue(commitIdxLo).isWb && brQueue(commitIdxLo).exuOut.brUpdate.isMisPred,
+    Mux(useLo && stateQueue(commitIdxLo).isWb,
       commitIdxLo,
       headIdx
     )
   )
 
-  val commitIsHead = commitIdx===headIdx
-  val deqValid = !stateQueue(headIdx).isIdle && commitIsHead && brCommitCnt=/=0.U
+  val deqValid = stateQueue(headIdx).isCommit && brCommitCnt=/=0.U
   val commitValid = stateQueue(commitIdx).isWb
   val commitEntry = brQueue(commitIdx)
   val commitIsMisPred = commitEntry.exuOut.redirect.isMisPred
@@ -148,7 +145,7 @@ class Brq extends XSModule {
 
   XSDebug(p"brCommitCnt:$brCommitCnt\n")
   assert(brCommitCnt+io.bcommit >= deqValid)
-  io.inOrderBrInfo.valid := deqValid
+  io.inOrderBrInfo.valid := commitValid
   io.inOrderBrInfo.bits := commitEntry.exuOut.brUpdate
   XSDebug(io.inOrderBrInfo.valid, "inOrderValid: pc=%x\n", io.inOrderBrInfo.bits.pc)
 
@@ -169,13 +166,14 @@ class Brq extends XSModule {
   XSDebug(false, true.B, "\n")
 
   val headPtrNext = WireInit(headPtr + deqValid)
-  stateQueue(commitIdx):= Mux(deqValid,
-    s_idle,
-    Mux(commitValid,
-      s_commited,
-      stateQueue(commitIdx)
-    )
-  )
+
+  when(commitValid){
+    stateQueue(commitIdx) := s_commited
+  }
+  when(deqValid){
+    stateQueue(headIdx) := s_idle
+  }
+  assert(!(commitIdx===headIdx && commitValid && deqValid), "Error: deq and commit a same entry!")
 
   headPtr := headPtrNext
   io.redirect.valid := commitValid && commitIsMisPred && !io.roqRedirect.valid
