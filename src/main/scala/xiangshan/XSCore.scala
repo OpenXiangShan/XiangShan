df99f5c1a05cc52f3600e0d302fb0bfcb8dffce4
GouLingrui <goulingrui15@gmail.com>
2020-08-16 00:01:50 +0800

    BPU: enable ras

--- src/main/scala/xiangshan/XSCore.scala
+++ src/main/scala/xiangshan/XSCore.scala
@@ -27,7 +27,7 @@ case class XSCoreParameters
   FectchWidth: Int = 8,
   EnableBPU: Boolean = true,
   EnableBPD: Boolean = true,
-  EnableRAS: Boolean = false,
+  EnableRAS: Boolean = true,
   EnableLB: Boolean = false,
   HistoryLength: Int = 64,
   BtbSize: Int = 2048,
