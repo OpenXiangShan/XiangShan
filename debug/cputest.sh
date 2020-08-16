4317f084dd5f1a9d4b4e41e0eb774fc7b39c4e97
zhanglinjuan <zhanglinjuan16@mails.ucas.ac.cn>
2020-08-16 10:58:21 +0800

    ubtb: update ubtb when branch prediction is correct

--- debug/cputest.sh
+++ debug/cputest.sh
@@ -5,7 +5,7 @@ TEST_HOME=$AM_HOME/tests/cputest
 for t in ${$(ls $TEST_HOME/tests)%.c}
 do
     echo -n "\x1b[0m $t: "
-    make -C $TEST_HOME ARCH=riscv64-noop E=0 ALL=$t run 2>/dev/null | grep "HIT GOOD TRAP" 
+    make -C $TEST_HOME ARCH=riscv64-noop ALL=$t run 2>&1 | tee > $TEST_HOME/build/$t.log | grep "HIT GOOD TRAP" 
     if [[ $? == 1 ]];
     then
         echo "\x1b[31mfail"
