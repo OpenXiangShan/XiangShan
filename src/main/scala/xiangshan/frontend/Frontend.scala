77409b73295fa376234d91e714790fdd6a69856c
jinyue110 <jinyue161@mails.ucas.ac.cn>
2020-08-18 16:50:06 +0800

    Frontend: connect icache into frontend

--- src/main/scala/xiangshan/frontend/Frontend.scala
+++ src/main/scala/xiangshan/frontend/Frontend.scala
@@ -4,6 +4,7 @@ import chisel3._
 import chisel3.util._
 import utils.PipelineConnect
 import xiangshan._
+import xiangshan.cache._
 
 
 class Frontend extends XSModule {
@@ -12,18 +13,25 @@ class Frontend extends XSModule {
   })
 
   val ifu = Module(new IFU)
+  val icache = Module(new icache)
   val fakeicache = Module(new FakeCache)
   val ibuffer =  if(EnableLB) Module(new LoopBuffer) else Module(new Ibuffer)
 
   val needFlush = io.backend.redirect.valid
 
+  //backend
   ifu.io.redirect <> io.backend.redirect
   ifu.io.inOrderBrInfo <> io.backend.inOrderBrInfo
   ifu.io.outOfOrderBrInfo <> io.backend.outOfOrderBrInfo
-  fakeicache.io.in <> ifu.io.icacheReq
-  ifu.io.icacheResp <> fakeicache.io.out
-  fakeicache.io.flush := ifu.io.icacheFlush
 
+  //cache
+  icache.io.req <> ifu.io.icacheReq
+  fakeicache.io.in <> icache.io.mem_acqure
+  icache.io.mem_grant <> fakeicache.io.out
+  ifu.io.icacheResp <> icache.io.resp
+  icache.io.flush := ifu.io.icacheFlush
+
+  //ibuffer
   ibuffer.io.in <> ifu.io.fetchPacket
   ibuffer.io.flush := needFlush
 
