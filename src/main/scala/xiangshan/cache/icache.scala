3d4c9cda034bb40f5435492d3283b70606496718
jinyue110 <jinyue161@mails.ucas.ac.cn>
2020-09-04 17:29:55 +0800

    icahce: optimize icache code style

--- src/main/scala/xiangshan/cache/icache.scala
+++ src/main/scala/xiangshan/cache/icache.scala
@@ -14,9 +14,6 @@ import freechips.rocketchip.diplomacy.{AddressSet,IdRange,LazyModule, LazyModule
 import freechips.rocketchip.tilelink.{TLClientNode, TLClientParameters, TLMasterParameters, TLMasterPortParameters, TLArbiter}
 import bus.tilelink.{TLParameters, TLPermissions, ClientMetadata}
 
-// DCache specific parameters
-// L1 DCache is 64set, 8way-associative, with 64byte block, a total of 32KB
-// It's a virtually indexed, physically tagged cache.
 case class ICacheParameters(
     nSets: Int = 64,
     nWays: Int = 4,
@@ -33,7 +30,6 @@ case class ICacheParameters(
 
   def tagCode: Code = Code.fromString(tagECC)
   def dataCode: Code = Code.fromString(dataECC)
-
   def replacement = new RandomReplacement(nWays)
 }
 
@@ -41,50 +37,30 @@ trait HasICacheParameters extends HasL1CacheParameters {
   val cacheParams = icacheParameters
 
   // the width of inner CPU data interface
-  // override  def tagBits = VAddrBits - untagBits
   def wordBits = DataBits
   def wordBytes = DataBytes
   def wordOffBits = log2Up(wordBytes)
   def beatBytes = cacheParams.blockBytes / cacheDataBeats
   def beatWords = beatBytes / wordBytes
   def beatOffBits = log2Up(beatBytes)
-  def idxMSB = untagBits-1
-  def idxLSB = blockOffBits
-  def offsetmsb = idxLSB-1
-  def offsetlsb = wordOffBits
-  def rowWords = rowBits/wordBits
-  def doNarrowRead = DataBits * nWays % rowBits == 0
-  def eccBytes = cacheParams.dataECCBytes
-  val eccBits = cacheParams.dataECCBytes * 8
-  val encBits = cacheParams.dataCode.width(eccBits)
-  val encWordBits = encBits * (wordBits / eccBits)
-  def encDataBits = cacheParams.dataCode.width(wordBits) // NBDCache only
-  def encRowBits = encDataBits*rowWords
   def cacheID = 0
   def RVCInsLen = cacheDataBits/4 
 
   def get_beat(addr: UInt) = addr(blockOffBits - 1, beatOffBits)
   def get_tag(addr: UInt) = addr >> untagBits
   def get_idx(addr: UInt) = addr(untagBits-1, blockOffBits)
-  def get_block_addr(addr: UInt) = (addr >> blockOffBits) << blockOffBits
   
   val groupAlign = log2Up(FetchWidth * 4 * 2)
-  // def groupPC(pc: UInt): UInt = Cat(pc(VAddrBits-1, groupAlign), 0.U(groupAlign.W))
   def groupPC(pc: UInt): UInt = Cat(pc(PAddrBits-1, groupAlign), 0.U(groupAlign.W))
 
   require(isPow2(nSets), s"nSets($nSets) must be pow2")
-  // To make things easier, now we assume:
-  // core_data_width(wordBits) == L1_basic_storage_unit_width(rowBits) ==
-  // outer_tilelink_interface_width(cacheDataBits)
   require(rowBits == wordBits, s"rowBits($rowBits) != wordBits($wordBits)")
   require(rowBits == cacheDataBits, s"rowBits($rowBits) != cacheDataBits($cacheDataBits)")
 }
 
-// sealed abstract class ICacheModule extends XSModule
-//   with HasICacheParameters
-
 abstract class ICacheBundle extends XSBundle
   with HasICacheParameters
+
 abstract class ICacheModule(outer: ICache) extends LazyModuleImp(outer)
   with HasICacheParameters 
   with HasXSLog
@@ -94,7 +70,6 @@ abstract class ICacheModule(outer: ICache) extends LazyModuleImp(outer)
 sealed class ICacheMetaBundle extends ICacheBundle
 {
   val tag = UInt(tagBits.W)
-  //val coh = new ClientMetadata
 }
 
 sealed class ICacheDataBundle extends ICacheBundle
@@ -104,14 +79,12 @@ sealed class ICacheDataBundle extends ICacheBundle
 
 class ICacheReq extends ICacheBundle
 {
-  //TODO
   val addr = UInt(VAddrBits.W)
   val mask = UInt(PredictWidth.W)
 }
 
 class ICacheResp extends ICacheBundle
 {
-  //TODO
   val pc = UInt(VAddrBits.W)
   val data = UInt((FetchWidth * 32).W)
   val mask = UInt(PredictWidth.W)
@@ -127,6 +100,10 @@ class ICacheIO(edge: TLEdgeOut) extends ICacheBundle
   val flush = Input(UInt(2.W))
 }
 
+/* ------------------------------------------------------------
+ * The 3-stage pipeline register
+ * ------------------------------------------------------------
+ */
 trait ICacheBase extends HasICacheParameters
 {
   //----------------------------
@@ -156,7 +133,10 @@ trait ICacheBase extends HasICacheParameters
 
 }
 
-
+/* ------------------------------------------------------------
+ * This module is the Top tilelink module of Icache
+ * ------------------------------------------------------------
+ */
 class ICache()(implicit p: Parameters) extends LazyModule
   with HasICacheParameters
 {
@@ -172,20 +152,43 @@ class ICache()(implicit p: Parameters) extends LazyModule
 
 /* ------------------------------------------------------------
  * This module is a SRAM with 4-way associated mapping
+ * The hardware implementation of ICache
  * ------------------------------------------------------------
  */
 class ICacheImp(outer: ICache) extends ICacheModule(outer)
 {
+  // cut a cacheline into a fetch packet
+  def cutHelper(sourceVec: Vec[UInt], startPtr: UInt, mask: UInt): UInt = {
+    val sourceVec_16bit = Wire(Vec(cacheDataBeats * 4,UInt(RVCInsLen.W)))
+    (0 until cacheDataBeats).foreach{ i =>
+      (0 until 4).foreach{ j =>
+        sourceVec_16bit(i*4 + j) := sourceVec(i)(j*16+15, j*16)
+      }
+    }
+    val cutPacket = WireInit(VecInit(Seq.fill(cacheDataBeats * 2){0.U(RVCInsLen.W)}))
+    (0 until cacheDataBeats * 2).foreach{ i =>
+      cutPacket(i) := Mux(mask(i).asBool,sourceVec_16bit(startPtr + i.U),0.U)
+    }
+    cutPacket.asUInt
+  }
+
+  // generate the one hot code according to a UInt between 0-8
+  def PriorityMask(sourceVec: UInt) : UInt = {
+    val OH = Mux(sourceVec >= 8.U, "b1000".U,
+             Mux(sourceVec >= 4.U, "b0100".U,
+             Mux(sourceVec >= 2.U, "b0010".U, "b0001".U)))
+    OH
+  }
+
   val (bus, edge) = outer.clientNode.out.head
   val io = IO(new ICacheIO(edge))
-
   val (_, _, refill_done, refill_cnt) = edge.count(bus.d)
 
-  //------------------------------------
-  //Memory
+  //----------------------------
+  //    Memory Part
+  //----------------------------
   val metaArray = Module(new SRAMTemplate(new ICacheMetaBundle, set=nSets, way=nWays, shouldReset = true))
   val dataArray = List.fill(cacheDataBeats){ Module(new SRAMTemplate(new ICacheDataBundle, set=nSets, way = nWays))}
-
   val validArray = RegInit(0.U((nSets * nWays).W))
 
   //----------------------------
@@ -197,6 +200,7 @@ class ICacheImp(outer: ICache) extends ICacheModule(outer)
   s2_ready := WireInit(false.B)
   s1_fire := s1_valid && (s2_ready || io.flush(0))
   
+  // SRAM(Meta and Data) read request
   val s1_idx = get_idx(s1_req_pc)
   metaArray.io.r.req.valid := s1_valid
   metaArray.io.r.req.bits.apply(setIdx=s1_idx)
@@ -220,17 +224,18 @@ class ICacheImp(outer: ICache) extends ICacheModule(outer)
   .elsewhen(s1_fire) { s2_valid := s1_valid}
   .elsewhen(s2_fire) { s2_valid := false.B}
 
+  // SRAM(Meta and Data) read reseponse
   val metas = metaArray.io.r.resp.asTypeOf(Vec(nWays,new ICacheMetaBundle))
-  val validMeta = Cat((0 until nWays).map{w => validArray(Cat(s2_idx, w.U))}.reverse).asUInt
   val datas =dataArray.map(b => RegEnable(next=b.io.r.resp.asTypeOf(Vec(nWays,new ICacheDataBundle)), enable=s2_fire))
 
+  val validMeta = Cat((0 until nWays).map{w => validArray(Cat(s2_idx, w.U))}.reverse).asUInt
+
+  // hit check and generate victim cacheline mask
   val hitVec = VecInit((0 until nWays).map{w => metas(w).tag === s2_tag && validMeta(w) === 1.U})
   val victimWayMask = (1.U << LFSR64()(log2Up(nWays)-1,0))
   val invalidVec = ~validMeta
   val hasInvalidWay = invalidVec.orR
-  val refillInvalidWaymask = Mux(invalidVec >= 8.U, "b1000".U,
-    Mux(invalidVec >= 4.U, "b0100".U,
-    Mux(invalidVec >= 2.U, "b0010".U, "b0001".U)))
+  val refillInvalidWaymask = PriorityMask(invalidVec)
   
   val waymask = Mux(s2_hit, hitVec.asUInt, Mux(hasInvalidWay, refillInvalidWaymask, victimWayMask))
  
@@ -256,23 +261,10 @@ class ICacheImp(outer: ICache) extends ICacheModule(outer)
   when(io.flush(1)) { s3_valid := false.B }
   .elsewhen(s2_fire) { s3_valid := s2_valid }
   .elsewhen(io.resp.fire()) { s3_valid := false.B } 
+  val refillDataReg = Reg(Vec(cacheDataBeats,new ICacheDataBundle))
 
-  def cutHelper(sourceVec: Vec[UInt], startPtr: UInt, mask: UInt): UInt = {
-    val sourceVec_16bit = Wire(Vec(cacheDataBeats * 4,UInt(RVCInsLen.W)))
-    (0 until cacheDataBeats).foreach{ i =>
-      (0 until 4).foreach{ j =>
-        sourceVec_16bit(i*4 + j) := sourceVec(i)(j*16+15, j*16)
-      }
-    }
-    val cutPacket = WireInit(VecInit(Seq.fill(cacheDataBeats * 2){0.U(RVCInsLen.W)}))
-    (0 until cacheDataBeats * 2).foreach{ i =>
-      cutPacket(i) := Mux(mask(i).asBool,sourceVec_16bit(startPtr + i.U),0.U)
-    }
-    cutPacket.asUInt
-  }
-
-  //icache hit 
-  //val allInBlock = s3_req_mask.andR
+  // icache hit 
+  // simply cut the hit cacheline
   val dataHitWay = s3_data.map(b => Mux1H(s3_wayMask,b).asUInt)
   val outPacket =  Wire(UInt((FetchWidth * 32).W))
   outPacket := cutHelper(VecInit(dataHitWay),s3_req_pc(5,1).asUInt,s3_req_mask.asUInt)
@@ -297,8 +289,7 @@ class ICacheImp(outer: ICache) extends ICacheModule(outer)
 
   val waitForRefillDone = needFlush || cacheflushed
 
-  val refillDataReg = Reg(Vec(cacheDataBeats,new ICacheDataBundle))   //TODO: this is ugly
-
+  // state change to wait for a cacheline refill
   switch(state){
     is(s_idle){
       when(s3_miss && io.flush === 0.U){
@@ -342,15 +333,6 @@ class ICacheImp(outer: ICache) extends ICacheModule(outer)
   metaArray.io.w.req.valid := refillFinalOneBeat
   metaArray.io.w.req.bits.apply(data=metaWrite, setIdx=get_idx(s3_req_pc), waymask=s3_wayMask)
 
-  when(refillFinalOneBeat && !cacheflushed){
-    validArray := validArray.bitSet(validPtr, true.B)
-  }
-
-  when(icacheFlush){ validArray := 0.U }
-
-  //val refillDataOut = refillDataReg.asUInt >> (s3_req_pc(5,1) << 4)
-  val refillDataVec = refillDataReg.asTypeOf(Vec(cacheDataBeats,UInt(cacheDataBits.W)))
-  val refillDataOut = cutHelper(refillDataVec, s3_req_pc(5,1),s3_req_mask )
   for(b <- 0 until cacheDataBeats){
     val writeOneBeat = (state === s_memReadResp) && bus.d.fire() && (b.U === readBeatCnt.value)
     dataArray(b).io.w.req.valid := writeOneBeat
@@ -359,6 +341,16 @@ class ICacheImp(outer: ICache) extends ICacheModule(outer)
                                         waymask=s3_wayMask)
 
   }
+
+  when(refillFinalOneBeat && !cacheflushed){
+    validArray := validArray.bitSet(validPtr, true.B)
+  }
+
+  //icache flush: only flush valid Array register
+  when(icacheFlush){ validArray := 0.U }
+
+  val refillDataVec = refillDataReg.asTypeOf(Vec(cacheDataBeats,UInt(cacheDataBits.W)))
+  val refillDataOut = cutHelper(refillDataVec, s3_req_pc(5,1),s3_req_mask )
 
   s3_ready := ((io.resp.fire() || !s3_valid) && !waitForRefillDone) || (waitForRefillDone && state === s_wait_resp)
 
@@ -377,16 +369,22 @@ class ICacheImp(outer: ICache) extends ICacheModule(outer)
   }
   XSDebug("[Stage 3] outPacket :%x\n",outPacket)
   XSDebug("[Stage 3] refillDataOut :%x\n",refillDataOut)
-  //-----------out put------------
+
+  //----------------------------
+  //    Out Put
+  //----------------------------
+  //icache request
   val dataArrayReadyVec = dataArray.map(b => b.io.r.req.ready)
   io.req.ready := metaArray.io.r.req.ready && ParallelOR(dataArrayReadyVec) && s2_ready
   
+  //icache response: to pre-decoder
   io.resp.valid := s3_valid && (s3_hit || state === s_wait_resp)
   io.resp.bits.data := Mux((s3_valid && s3_hit),outPacket,refillDataOut)
   io.resp.bits.mask := s3_req_mask
   io.resp.bits.pc := s3_req_pc
   io.resp.bits.ipf := s3_tlb_resp.excp.pf.instr
 
+  //to itlb
   io.tlb.resp.ready := s3_ready
   io.tlb.req.valid := s2_valid
   io.tlb.req.bits.vaddr := s2_req_pc
@@ -395,20 +393,17 @@ class ICacheImp(outer: ICache) extends ICacheModule(outer)
   io.tlb.req.bits.debug.pc := s2_req_pc
   io.tlb.req.bits.debug.lsroqIdx := DontCare
   
+  //tilelink
   bus.b.ready := true.B
   bus.c.valid := false.B
   bus.e.valid := false.B
   bus.a.valid := (state === s_memReadReq)
   bus.a.bits  := edge.Get(
     fromSource      = cacheID.U,
-    // toAddress       = groupPC(s3_req_pc),
     toAddress       = groupPC(s3_tlb_resp.paddr),
     lgSize          = (log2Up(cacheParams.blockBytes)).U)._2 
-
   bus.d.ready := true.B
 
   XSDebug("[flush] flush_0:%d  flush_1:%d\n",io.flush(0),io.flush(1))
 }
-
-//TODO: consider L2 or L3 cache connection
 
