9cea7460d1a9f3cbfd2815e8e4debd2f7f0f8a87
Steve Gou <goulingrui15@gmail.com>
2020-09-28 17:36:42 +0800

    BPU: disable log

--- src/main/scala/xiangshan/frontend/BPU.scala
+++ src/main/scala/xiangshan/frontend/BPU.scala
@@ -9,9 +9,9 @@ import xiangshan.backend.JumpOpType
 
 trait HasBPUParameter extends HasXSParameter {
   val BPUDebug = false
-  val EnableCFICommitLog = true
-  val EnbaleCFIPredLog = true
-  val EnableBPUTimeRecord = true
+  val EnableCFICommitLog = false
+  val EnbaleCFIPredLog = false
+  val EnableBPUTimeRecord = false
 }
 
 class TableAddr(val idxBits: Int, val banks: Int) extends XSBundle {
