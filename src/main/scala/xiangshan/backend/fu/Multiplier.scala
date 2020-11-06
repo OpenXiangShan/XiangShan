5083e6693981c9247ca512bcf34d889f4c02b5db
LinJiawei <linjiav@outlook.com>
2020-11-06 20:01:33 +0800

    Fix bit-width bug in multiplier

--- src/main/scala/xiangshan/backend/fu/Multiplier.scala
+++ src/main/scala/xiangshan/backend/fu/Multiplier.scala
@@ -31,48 +31,12 @@ abstract class Multiplier
 (
   val len: Int,
   latency: Int = 3
-) extends FunctionUnit(cfg = mulCfg, extIn = new MulDivCtrl, latency = latency)
+) extends FunctionUnit(cfg = mulCfg, len, extIn = new MulDivCtrl, latency = latency)
   with HasPipelineReg[MulDivCtrl, Null]
 {
 
   val (src1, src2) = (io.in.bits.src(0), io.in.bits.src(1))
 }
-
-//trait HasPipelineReg { this: ArrayMultiplier =>
-//
-//  val validVec = io.in.valid +: Array.fill(latency)(RegInit(false.B))
-//  val rdyVec = Array.fill(latency)(Wire(Bool())) :+ io.out.ready
-//  val ctrlVec = io.in.bits.ctrl +: Array.fill(latency)(Reg(new MulDivCtrl))
-//  val flushVec = ctrlVec.zip(validVec).map(x => x._2 && x._1.uop.needFlush(io.redirect))
-//
-//  for(i <- 0 until latency){
-//    rdyVec(i) := !validVec(i+1) || rdyVec(i+1)
-//  }
-//
-//  for(i <- 1 to latency){
-//    when(flushVec(i-1) || rdyVec(i) && !validVec(i-1)){
-//      validVec(i) := false.B
-//    }.elsewhen(rdyVec(i-1) && validVec(i-1) && !flushVec(i-1)){
-//      validVec(i) := validVec(i-1)
-//      ctrlVec(i) := ctrlVec(i-1)
-//    }
-//  }
-//
-//  io.in.ready := rdyVec(0)
-//  io.out.valid := validVec.last && !flushVec.last
-//  io.out.bits.uop := ctrlVec.last.uop
-//
-//  def PipelineReg[T<:Data](i: Int)(next: T) = RegEnable(
-//    next,
-//    enable = validVec(i-1) && rdyVec(i-1) && !flushVec(i-1)
-//  )
-//
-//  def S1Reg[T<:Data](next: T):T = PipelineReg[T](1)(next)
-//  def S2Reg[T<:Data](next: T):T = PipelineReg[T](2)(next)
-//  def S3Reg[T<:Data](next: T):T = PipelineReg[T](3)(next)
-//  def S4Reg[T<:Data](next: T):T = PipelineReg[T](4)(next)
-//  def S5Reg[T<:Data](next: T):T = PipelineReg[T](5)(next)
-//}
 
 class ArrayMultiplier
 (
