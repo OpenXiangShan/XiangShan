bbf8d26a5dc516077ef8c887bc829a371dd372b4
BigWhiteDog <legend.z@foxmail.com>
2020-11-18 14:27:59 +0800

    SIMRAMTOP generate simtop for dramsim

--- src/test/scala/top/XSSim.scala
+++ src/test/scala/top/XSSim.scala
@@ -196,3 +196,135 @@ object TestMain extends App {
     )
   )
 }
+
+class XSSimRAM()(implicit p: config.Parameters) extends LazyModule with HasXSParameter {
+
+  // address space[0G - 1024G)
+  val fullRange = AddressSet(0x0L, 0xffffffffffL)
+  // MMIO address space[0G - 2G)
+  val mmioRange = AddressSet(base = 0x0000000000L, mask = 0x007fffffffL)
+  // DRAM address range[2G - 1024G)
+  val dramRange = fullRange.subtract(mmioRange)
+
+  val soc = LazyModule(new XSSoc())
+  // AXIRam
+  // -----------------------------------
+  val axiSimRam = LazyModule(new AXI4FakeMemNode(
+    dramRange,
+    beatBytes = L3BusWidth / 8
+  ))
+
+  val xbar = AXI4Xbar()
+  soc.mem.map{mem => xbar := mem}
+  axiSimRam.node :=
+    xbar
+
+  // AXI DMA
+  // -----------------------------------
+  val burst = LazyModule(new AXI4BurstMaster(
+    startAddr = 0x80000000L,
+    nOp = 0,
+    beatBytes = L3BusWidth / 8))
+  soc.dma := burst.node
+
+  // AXI MMIO
+  // -----------------------------------
+  val axiMMIO = LazyModule(new SimMMIO())
+  axiMMIO.axiBus :=
+    AXI4UserYanker() :=
+    TLToAXI4() :=
+    soc.extDev
+
+  lazy val module = new LazyModuleImp(this) {
+    val io = IO(new Bundle {
+      val difftest = new DiffTestIO
+      val logCtrl = new LogCtrlIO
+      val trap = new TrapIO
+      val uart = new UARTIO
+      val memAXI = chiselTypeOf(axiSimRam.module.io)
+    })
+    io.memAXI <> axiSimRam.module.io
+
+    io.uart <> axiMMIO.module.io.uart
+    soc.module.io.meip := false.B
+
+    val difftest = WireInit(0.U.asTypeOf(new DiffTestIO))
+    if (!env.FPGAPlatform) {
+      ExcitingUtils.addSink(difftest.commit, "difftestCommit", Debug)
+      ExcitingUtils.addSink(difftest.thisPC, "difftestThisPC", Debug)
+      ExcitingUtils.addSink(difftest.thisINST, "difftestThisINST", Debug)
+      ExcitingUtils.addSink(difftest.skip, "difftestSkip", Debug)
+      ExcitingUtils.addSink(difftest.isRVC, "difftestIsRVC", Debug)
+      ExcitingUtils.addSink(difftest.wen, "difftestWen", Debug)
+      ExcitingUtils.addSink(difftest.wdata, "difftestWdata", Debug)
+      ExcitingUtils.addSink(difftest.wdst, "difftestWdst", Debug)
+      ExcitingUtils.addSink(difftest.wpc, "difftestWpc", Debug)
+      ExcitingUtils.addSink(difftest.intrNO, "difftestIntrNO", Debug)
+      ExcitingUtils.addSink(difftest.cause, "difftestCause", Debug)
+      ExcitingUtils.addSink(difftest.r, "difftestRegs", Debug)
+      ExcitingUtils.addSink(difftest.priviledgeMode, "difftestMode", Debug)
+      ExcitingUtils.addSink(difftest.mstatus, "difftestMstatus", Debug)
+      ExcitingUtils.addSink(difftest.sstatus, "difftestSstatus", Debug)
+      ExcitingUtils.addSink(difftest.mepc, "difftestMepc", Debug)
+      ExcitingUtils.addSink(difftest.sepc, "difftestSepc", Debug)
+      ExcitingUtils.addSink(difftest.mtval, "difftestMtval", Debug)
+      ExcitingUtils.addSink(difftest.stval, "difftestStval", Debug)
+      ExcitingUtils.addSink(difftest.mtvec, "difftestMtvec", Debug)
+      ExcitingUtils.addSink(difftest.stvec, "difftestStvec", Debug)
+      ExcitingUtils.addSink(difftest.mcause, "difftestMcause", Debug)
+      ExcitingUtils.addSink(difftest.scause, "difftestScause", Debug)
+      ExcitingUtils.addSink(difftest.satp, "difftestSatp", Debug)
+      ExcitingUtils.addSink(difftest.mip, "difftestMip", Debug)
+      ExcitingUtils.addSink(difftest.mie, "difftestMie", Debug)
+      ExcitingUtils.addSink(difftest.mscratch, "difftestMscratch", Debug)
+      ExcitingUtils.addSink(difftest.sscratch, "difftestSscratch", Debug)
+      ExcitingUtils.addSink(difftest.mideleg, "difftestMideleg", Debug)
+      ExcitingUtils.addSink(difftest.medeleg, "difftestMedeleg", Debug)
+      ExcitingUtils.addSink(difftest.scFailed, "difftestScFailed", Debug)
+    }
+
+    // BoringUtils.addSink(difftest.lrscAddr, "difftestLrscAddr")
+    io.difftest := difftest
+
+    val trap = WireInit(0.U.asTypeOf(new TrapIO))
+    if (!env.FPGAPlatform) {
+      ExcitingUtils.addSink(trap.valid, "trapValid")
+      ExcitingUtils.addSink(trap.code, "trapCode")
+      ExcitingUtils.addSink(trap.pc, "trapPC")
+      ExcitingUtils.addSink(trap.cycleCnt, "trapCycleCnt")
+      ExcitingUtils.addSink(trap.instrCnt, "trapInstrCnt")
+    }
+
+    io.trap := trap
+
+    if (env.EnableDebug) {
+      val timer = GTimer()
+      val logEnable = (timer >= io.logCtrl.log_begin) && (timer < io.logCtrl.log_end)
+      ExcitingUtils.addSource(logEnable, "DISPLAY_LOG_ENABLE")
+      ExcitingUtils.addSource(timer, "logTimestamp")
+    }
+
+    // Check and dispaly all source and sink connections
+    ExcitingUtils.checkAndDisplay()
+  }
+}
+object SimRAMMain extends App {
+  // set parameters
+  Parameters.set(
+    if(args.contains("--fpga-platform")) {
+      if (args.contains("--dual-core")) Parameters.dualCoreParameters
+      else Parameters()
+    }
+    else if(args.contains("--disable-log")) Parameters.simParameters // sim only, disable log
+    else Parameters.debugParameters // open log
+  )
+
+  implicit val p = config.Parameters.empty
+  // generate verilog
+  XiangShanStage.execute(
+    args.filterNot(_ == "--disable-log").filterNot(_ == "--fpga-platform").filterNot(_ == "--dual-core"),
+    Seq(
+      ChiselGeneratorAnnotation(() => LazyModule(new XSSimRAM).module)
+    )
+  )
+}
