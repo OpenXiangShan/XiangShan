d0319d89c5648650cae3013ba8829faf0905bbbf
ZhangZifei <1773908404@qq.com>
2020-11-30 15:46:43 +0800

    ram.cpp: addpageSv39 use TOPSIZE instead of RAMSIZE

--- src/test/csrc/ram.cpp
+++ src/test/csrc/ram.cpp
@@ -22,12 +22,12 @@ void addpageSv39() {
 //addr range: 0x0000000080000000 - 0x0000000088000000 for 128MB from 2GB - 2GB128MB
 //the first layer: one entry for 1GB. (512GB in total by 512 entries). need the 2th entries
 //the second layer: one entry for 2MB. (1GB in total by 512 entries). need the 0th-63rd entries
-//the third layer: one entry for 4KB (2MB in total by 512 entries). need 64 with each one all  
-
+//the third layer: one entry for 4KB (2MB in total by 512 entries). need 64 with each one all
+#define TOPSIZE (128 * 1024 * 1024)
 #define PAGESIZE (4 * 1024)  // 4KB = 2^12B
 #define ENTRYNUM (PAGESIZE / 8) //512 2^9
 #define PTEVOLUME (PAGESIZE * ENTRYNUM) // 2MB
-#define PTENUM (RAMSIZE / PTEVOLUME) // 128MB / 2MB = 64
+#define PTENUM (TOPSIZE / PTEVOLUME) // 128MB / 2MB = 64
 #define PDDENUM 1
 #define PDENUM 1
 #define PDDEADDR (0x88000000 - (PAGESIZE * (PTENUM + 2))) //0x88000000 - 0x1000*66
@@ -41,7 +41,7 @@ void addpageSv39() {
   uint64_t pdde[ENTRYNUM];
   uint64_t pde[ENTRYNUM];
   uint64_t pte[PTENUM][ENTRYNUM];
-  
+
   // special addr for mmio 0x40000000 - 0x4fffffff
   uint64_t pdemmio[ENTRYNUM];
   uint64_t ptemmio[PTEMMIONUM][ENTRYNUM];
@@ -69,13 +69,13 @@ void addpageSv39() {
   for(int i = 0; i < PTEMMIONUM; i++) {
     pdemmio[i] = (((PDDEADDR-PAGESIZE*(PTEMMIONUM+PDEMMIONUM-i)) & 0xfffff000) >> 2) | 0x1;
   }
-  
+
   for(int outidx = 0; outidx < PTEMMIONUM; outidx++) {
     for(int inidx = 0; inidx < ENTRYNUM; inidx++) {
       ptemmio[outidx][inidx] = (((0x40000000 + outidx*PTEVOLUME + inidx*PAGESIZE) & 0xfffff000) >> 2) | 0xf;
     }
   }
-  
+
   //0x800000000 - 0x87ffffff
   pdde[2] = ((PDEADDR & 0xfffff000) >> 2) | 0x1;
   //pdde[2] = ((0x80000000&0xc0000000) >> 2) | 0xf;
@@ -91,13 +91,13 @@ void addpageSv39() {
     }
   }
 
-  memcpy((char *)ram+(RAMSIZE-PAGESIZE*(PTENUM+PDDENUM+PDENUM+PDEMMIONUM+PTEMMIONUM+PDEDEVNUM+PTEDEVNUM)),ptedev,PAGESIZE*PTEDEVNUM);
-  memcpy((char *)ram+(RAMSIZE-PAGESIZE*(PTENUM+PDDENUM+PDENUM+PDEMMIONUM+PTEMMIONUM+PDEDEVNUM)),pdedev,PAGESIZE*PDEDEVNUM);
-  memcpy((char *)ram+(RAMSIZE-PAGESIZE*(PTENUM+PDDENUM+PDENUM+PDEMMIONUM+PTEMMIONUM)),ptemmio, PAGESIZE*PTEMMIONUM);
-  memcpy((char *)ram+(RAMSIZE-PAGESIZE*(PTENUM+PDDENUM+PDENUM+PDEMMIONUM)), pdemmio, PAGESIZE*PDEMMIONUM);
-  memcpy((char *)ram+(RAMSIZE-PAGESIZE*(PTENUM+PDDENUM+PDENUM)), pdde, PAGESIZE*PDDENUM);
-  memcpy((char *)ram+(RAMSIZE-PAGESIZE*(PTENUM+PDENUM)), pde, PAGESIZE*PDENUM);
-  memcpy((char *)ram+(RAMSIZE-PAGESIZE*PTENUM), pte, PAGESIZE*PTENUM);
+  memcpy((char *)ram+(TOPSIZE-PAGESIZE*(PTENUM+PDDENUM+PDENUM+PDEMMIONUM+PTEMMIONUM+PDEDEVNUM+PTEDEVNUM)),ptedev,PAGESIZE*PTEDEVNUM);
+  memcpy((char *)ram+(TOPSIZE-PAGESIZE*(PTENUM+PDDENUM+PDENUM+PDEMMIONUM+PTEMMIONUM+PDEDEVNUM)),pdedev,PAGESIZE*PDEDEVNUM);
+  memcpy((char *)ram+(TOPSIZE-PAGESIZE*(PTENUM+PDDENUM+PDENUM+PDEMMIONUM+PTEMMIONUM)),ptemmio, PAGESIZE*PTEMMIONUM);
+  memcpy((char *)ram+(TOPSIZE-PAGESIZE*(PTENUM+PDDENUM+PDENUM+PDEMMIONUM)), pdemmio, PAGESIZE*PDEMMIONUM);
+  memcpy((char *)ram+(TOPSIZE-PAGESIZE*(PTENUM+PDDENUM+PDENUM)), pdde, PAGESIZE*PDDENUM);
+  memcpy((char *)ram+(TOPSIZE-PAGESIZE*(PTENUM+PDENUM)), pde, PAGESIZE*PDENUM);
+  memcpy((char *)ram+(TOPSIZE-PAGESIZE*PTENUM), pte, PAGESIZE*PTENUM);
 }
 
 // Return whether the file is a gz file
@@ -177,7 +177,7 @@ void init_ram(const char *img) {
     ret = fread(ram, img_size, 1, fp);
 
     assert(ret == 1);
-    fclose(fp); 
+    fclose(fp);
   }
 
   //new add
