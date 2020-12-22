c8b1e4db9cf506f40d3cbddfbd259cfecf0168b7
Allen <njuallen@foxmail.com>
2020-12-22 20:59:47 +0800

    Coverage: scripts, add support for toggle coverage.

--- scripts/coverage/statistics.py
+++ scripts/coverage/statistics.py
@@ -5,19 +5,31 @@ import re
 import copy
 import pprint
 
-COVERRED = "COVERRED"
-NOT_COVERRED = "NOT_COVERRED"
+LINE_COVERRED = "LINE_COVERRED"
+NOT_LINE_COVERRED = "NOT_LINE_COVERRED"
+TOGGLE_COVERRED = "TOGGLE_COVERRED"
+NOT_TOGGLE_COVERRED = "NOT_TOGGLE_COVERRED"
 DONTCARE = "DONTCARE"
+
 BEGIN = "BEGIN"
 END = "END"
 CHILDREN = "CHILDREN"
 MODULE = "MODULE"
 INSTANCE = "INSTANCE"
-TYPE="TYPE"
-ROOT="ROOT"
-NODE="NODE"
-SELFCOVERAGE="SELFCOVERAGE"
-TREECOVERAGE="TREECOVERAGE"
+TYPE = "TYPE"
+ROOT = "ROOT"
+NODE = "NODE"
+SELFCOVERAGE = "SELFCOVERAGE"
+TREECOVERAGE = "TREECOVERAGE"
+LINECOVERAGE = 0 
+TOGGLECOVERAGE = 1
+
+def check_one_hot(l):
+    cnt = 0
+    for e in l:
+        if e:
+            cnt += 1
+    return cnt <= 1
 
 def get_lines(input_file):
     lines = []
@@ -31,41 +43,85 @@ def get_line_annotation(lines):
     # pattern_1: 040192     if(array_0_MPORT_en & array_0_MPORT_mask) begin
     # pattern_2: 2218110        end else if (_T_30) begin // @[Conditional.scala 40:58]
     # pattern_2: 000417     end else begin
-    coverred_pattern_1 = re.compile('^\s*(\d+)\s+if')
-    coverred_pattern_2 = re.compile('^\s*(\d+)\s+end else')
-    not_coverred_pattern_1 = re.compile('^\s*(%0+)\s+if')
-    not_coverred_pattern_2 = re.compile('^\s*(%0+)\s+end else')
+    line_coverred_pattern_1 = re.compile('^\s*(\d+)\s+if')
+    line_coverred_pattern_2 = re.compile('^\s*(\d+)\s+end else')
+    not_line_coverred_pattern_1 = re.compile('^\s*(%0+)\s+if')
+    not_line_coverred_pattern_2 = re.compile('^\s*(%0+)\s+end else')
+
+    toggle_coverred_pattern_1 = re.compile('^\s*(\d+)\s+reg')
+    toggle_coverred_pattern_2 = re.compile('^\s*(\d+)\s+wire')
+    toggle_coverred_pattern_3 = re.compile('^\s*(\d+)\s+input')
+    toggle_coverred_pattern_4 = re.compile('^\s*(\d+)\s+output')
+
+    not_toggle_coverred_pattern_1 = re.compile('^\s*(%0+)\s+reg')
+    not_toggle_coverred_pattern_2 = re.compile('^\s*(%0+)\s+wire')
+    not_toggle_coverred_pattern_3 = re.compile('^\s*(%0+)\s+input')
+    not_toggle_coverred_pattern_4 = re.compile('^\s*(%0+)\s+output')
+
+    line_cnt = 0
 
     for line in lines:
-        coverred_match = coverred_pattern_1.search(line) or coverred_pattern_2.search(line)
-        not_coverred_match = not_coverred_pattern_1.search(line) or not_coverred_pattern_2.search(line)
+        line_coverred_match = line_coverred_pattern_1.search(line) or line_coverred_pattern_2.search(line)
+        not_line_coverred_match = not_line_coverred_pattern_1.search(line) or not_line_coverred_pattern_2.search(line)
 
-        assert not (coverred_match and not_coverred_match)
+        assert not (line_coverred_match and not_line_coverred_match)
+
+        toggle_coverred_match = toggle_coverred_pattern_1.search(line) or toggle_coverred_pattern_2.search(line) or \
+                toggle_coverred_pattern_3.search(line) or toggle_coverred_pattern_4.search(line)
+        not_toggle_coverred_match = not_toggle_coverred_pattern_1.search(line) or not_toggle_coverred_pattern_2.search(line) or \
+                not_toggle_coverred_pattern_3.search(line) or not_toggle_coverred_pattern_4.search(line)
+
+        assert not (toggle_coverred_match and not_toggle_coverred_match)
         
-        if coverred_match:
-            line_annotations.append(COVERRED)
-        elif not_coverred_match:
-            line_annotations.append(NOT_COVERRED)
+        all_match = (line_coverred_match, not_line_coverred_match,
+                toggle_coverred_match, not_toggle_coverred_match)
+        if not check_one_hot(all_match):
+            print("not_one_hot")
+            print(line_cnt)
+            print(all_match)
+            assert False, "This line matches multiple patterns"
+        if line_coverred_match:
+            line_annotations.append(LINE_COVERRED)
+        elif not_line_coverred_match:
+            line_annotations.append(NOT_LINE_COVERRED)
+        elif toggle_coverred_match:
+            line_annotations.append(TOGGLE_COVERRED)
+        elif not_toggle_coverred_match:
+            line_annotations.append(NOT_TOGGLE_COVERRED)
         else:
             line_annotations.append(DONTCARE)
+        line_cnt += 1
     return line_annotations
 
 # get the line coverage statistics in line range [start, end)
 def get_coverage_statistics(line_annotations, start, end):
-    coverred = 0
-    not_coverred = 0
+    line_coverred = 0
+    not_line_coverred = 0
+    toggle_coverred = 0
+    not_toggle_coverred = 0
     for i in range(start, end):
-        if line_annotations[i] == COVERRED:
-            coverred += 1
+        if line_annotations[i] == LINE_COVERRED:
+            line_coverred += 1
 
-        if line_annotations[i] == NOT_COVERRED:
-            not_coverred += 1
+        if line_annotations[i] == NOT_LINE_COVERRED:
+            not_line_coverred += 1
+
+        if line_annotations[i] == TOGGLE_COVERRED:
+            toggle_coverred += 1
+
+        if line_annotations[i] == NOT_TOGGLE_COVERRED:
+            not_toggle_coverred += 1
 
     # deal with divide by zero
-    coverage = 1.0
-    if coverred + not_coverred != 0:
-        coverage = float(coverred) / (coverred + not_coverred)
-    return (coverred, not_coverred, coverage)
+    line_coverage = 1.0
+    if line_coverred + not_line_coverred != 0:
+        line_coverage = float(line_coverred) / (line_coverred + not_line_coverred)
+
+    toggle_coverage = 1.0
+    if toggle_coverred + not_toggle_coverred != 0:
+        toggle_coverage = float(toggle_coverred) / (toggle_coverred + not_toggle_coverred)
+    return ((line_coverred, not_line_coverred, line_coverage),
+            (toggle_coverred, not_toggle_coverred, toggle_coverage))
 
 # get modules and all it's submodules
 def get_modules(lines):
@@ -140,18 +196,26 @@ def get_tree_coverage(modules, coverage):
             if CHILDREN not in modules[module]:
                 modules[module][TREECOVERAGE] = self_coverage
             else:
-                coverred = self_coverage[0]
-                not_coverred = self_coverage[1]
+                line_coverred = self_coverage[LINECOVERAGE][0]
+                not_line_coverred = self_coverage[LINECOVERAGE][1]
+                toggle_coverred = self_coverage[TOGGLECOVERAGE][0]
+                not_toggle_coverred = self_coverage[TOGGLECOVERAGE][1]
                 # the dfs part
                 for child in modules[module][CHILDREN]:
                     child_coverage = dfs(child[MODULE])
-                    coverred += child_coverage[0]
-                    not_coverred += child_coverage[1]
+                    line_coverred += child_coverage[LINECOVERAGE][0]
+                    not_line_coverred += child_coverage[LINECOVERAGE][1]
+                    toggle_coverred += child_coverage[TOGGLECOVERAGE][0]
+                    not_toggle_coverred += child_coverage[TOGGLECOVERAGE][1]
                 # deal with divide by zero
-                coverage = 1.0
-                if coverred + not_coverred != 0:
-                    coverage = float(coverred) / (coverred + not_coverred)
-                modules[module][TREECOVERAGE] = (coverred, not_coverred, coverage)
+                line_coverage = 1.0
+                if line_coverred + not_line_coverred != 0:
+                    line_coverage = float(line_coverred) / (line_coverred + not_line_coverred)
+                toggle_coverage = 1.0
+                if toggle_coverred + not_toggle_coverred != 0:
+                    toggle_coverage = float(toggle_coverred) / (toggle_coverred + not_toggle_coverred)
+                modules[module][TREECOVERAGE] = ((line_coverred, not_line_coverred, line_coverage),
+                        (toggle_coverred, not_toggle_coverred, toggle_coverage))
         return modules[module][TREECOVERAGE]
 
     for module in modules:
@@ -163,8 +227,8 @@ def get_tree_coverage(modules, coverage):
 
 # arg1: tree coverage results
 # arg2: coverage type
-def sort_coverage(coverage, coverage_type):
-    l = [(module, coverage[module][coverage_type])for module in coverage]
+def sort_coverage(coverage, self_or_tree, coverage_type):
+    l = [(module, coverage[module][self_or_tree][coverage_type])for module in coverage]
     l.sort(key=lambda x:x[1][2])
     return l
 
@@ -174,10 +238,15 @@ def print_tree_coverage(tree_coverage):
         tree = tree_coverage[module][TREECOVERAGE]
         self = tree_coverage[module][SELFCOVERAGE]
         print("  " * level + "- " + module)
-        print("  " * level + "  tree", end="")
-        print("(%d, %d, %.2f)" % (tree[0], tree[1], tree[2] * 100.0))
-        print("  " * level + "  self", end="")
-        print("(%d, %d, %.2f)" % (self[0], self[1], self[2] * 100.0))
+        print("  " * level + "  tree_line", end="")
+        print("(%d, %d, %.2f)" % (tree[LINECOVERAGE][0], tree[LINECOVERAGE][1], tree[LINECOVERAGE][2] * 100.0))
+        print("  " * level + "  self_line", end="")
+        print("(%d, %d, %.2f)" % (self[LINECOVERAGE][0], self[LINECOVERAGE][1], self[LINECOVERAGE][2] * 100.0))
+
+        print("  " * level + "  tree_toggle", end="")
+        print("(%d, %d, %.2f)" % (tree[TOGGLECOVERAGE][0], tree[TOGGLECOVERAGE][1], tree[TOGGLECOVERAGE][2] * 100.0))
+        print("  " * level + "  self_toggle", end="")
+        print("(%d, %d, %.2f)" % (self[TOGGLECOVERAGE][0], self[TOGGLECOVERAGE][1], self[TOGGLECOVERAGE][2] * 100.0))
 
         # print children nodes
         if CHILDREN in modules[module]:
@@ -215,11 +284,15 @@ if __name__ == "__main__":
     # print("tree_coverage:")
     # pp.pprint(tree_coverage)
 
-    print("SelfCoverage:")
-    pp.pprint(sort_coverage(tree_coverage, SELFCOVERAGE))
+    print("LineSelfCoverage:")
+    pp.pprint(sort_coverage(tree_coverage, SELFCOVERAGE, LINECOVERAGE))
+    print("LineTreeCoverage:")
+    pp.pprint(sort_coverage(tree_coverage, TREECOVERAGE, LINECOVERAGE))
 
-    print("TreeCoverage:")
-    pp.pprint(sort_coverage(tree_coverage, TREECOVERAGE))
+    print("ToggleSelfCoverage:")
+    pp.pprint(sort_coverage(tree_coverage, SELFCOVERAGE, TOGGLECOVERAGE))
+    print("ToggleTreeCoverage:")
+    pp.pprint(sort_coverage(tree_coverage, TREECOVERAGE, TOGGLECOVERAGE))
 
     print("AllCoverage:")
     print_tree_coverage(tree_coverage)
