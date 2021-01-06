f25ec55036f8b98b9ea2516c16e969713a144368
Lingrui98 <goulingrui15@gmail.com>
2021-01-06 23:26:12 +0800

    ifu: use ParallelOR instead of ParallelPriorityMux for if3(4)_predTakenRedirect

--- src/main/scala/xiangshan/frontend/IFU.scala
+++ src/main/scala/xiangshan/frontend/IFU.scala
@@ -239,7 +239,7 @@ class IFU extends XSModule with HasIFUConst
   val if3_predTakenRedirectVec = VecInit((0 until PredictWidth).map(i => !if3_pendingPrevHalfInstr && if3_bp.realTakens(i) && if3_nextValidPCNotEquals(if3_bp.targets(i))))
   val if3_prevHalfMetRedirect    = if3_pendingPrevHalfInstr && if3_prevHalfInstrMet && if3_prevHalfInstr.bits.taken && if3_nextValidPCNotEquals(if3_prevHalfInstr.bits.target)
   val if3_prevHalfNotMetRedirect = if3_pendingPrevHalfInstr && !if3_prevHalfInstrMet && if3_nextValidPCNotEquals(if3_prevHalfInstr.bits.npc)
-  val if3_predTakenRedirect    = ParallelPriorityMux(if3_bp.realTakens, if3_predTakenRedirectVec)
+  val if3_predTakenRedirect    = ParallelOR(if3_predTakenRedirectVec)
   val if3_predNotTakenRedirect = !if3_pendingPrevHalfInstr && !if3_bp.taken && if3_nextValidPCNotEquals(if3_snpc)
   // when pendingPrevHalfInstr, if3_GHInfo is set to the info of last prev half instr
   // val if3_ghInfoNotIdenticalRedirect = !if3_pendingPrevHalfInstr && if3_GHInfo =/= if3_lastGHInfo && enableGhistRepair.B
@@ -382,7 +382,7 @@ class IFU extends XSModule with HasIFUConst
   val if4_predTakenRedirectVec = VecInit((0 until PredictWidth).map(i => if4_bp.realTakens(i) && if4_nextValidPCNotEquals(if4_bp.targets(i))))
 
   val if4_prevHalfNextNotMet = hasPrevHalfInstrReq && if4_nextValidPCNotEquals(prevHalfInstrReq.bits.pc+2.U)
-  val if4_predTakenRedirect = ParallelPriorityMux(if4_bp.realTakens, if4_predTakenRedirectVec)
+  val if4_predTakenRedirect = ParallelORR(if4_predTakenRedirectVec)
   val if4_predNotTakenRedirect = !if4_bp.taken && if4_nextValidPCNotEquals(if4_snpc)
   // val if4_ghInfoNotIdenticalRedirect = if4_GHInfo =/= if4_lastGHInfo && enableGhistRepair.B
 
