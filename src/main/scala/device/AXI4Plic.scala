c699b48a76ce90c4151c9fe9ef7bb9f9dfabc15a
wangkaifan <wangkaifan@ict.ac.cn>
2021-01-08 17:31:58 +0800

    plic: employ axi interface instead of TL

--- src/main/scala/device/AXI4Plic.scala
+++ src/main/scala/device/AXI4Plic.scala
@@ -2,13 +2,10 @@ package device
 
 import chisel3._
 import chisel3.util._
-import freechips.rocketchip.tilelink._
 import chipsalliance.rocketchip.config._
 import freechips.rocketchip.diplomacy._
-import freechips.rocketchip.regmapper.RegField
-import utils.{HasTLDump, XSDebug}
-import xiangshan.HasXSLog
-
+import utils.MaskExpand
+import utils.{HasTLDump, XSDebug, RegMap}
 
 /*  base + 0x000000: Reserved (interrupt source 0 does not exist)
     base + 0x000004: Interrupt source 1 priority
@@ -75,55 +72,92 @@ object PLICConsts
   require(hartBase >= enableBase(maxHarts))
 }
 
-class TLPLIC(address: Seq[AddressSet], sim: Boolean)(implicit p: Parameters) extends LazyModule {
+class PlicIO extends Bundle with xiangshan.HasXSParameter {
+  val intrVec = Input(UInt(NrExtIntr.W))
+  val meip = Output(Vec(top.Parameters.get.socParameters.NumCores, Bool()))
+}
 
-  val device = new SimpleDevice("interrupt-controller", Seq("riscv,plic0"))
-  val node = TLRegisterNode(address, device, beatBytes = 8/*, concurrency = 1*/)  // TODO: decide concurrency argument
-  val NumCores = top.Parameters.get.socParameters.NumCores
-
-  lazy val module = new LazyModuleImp(this) with HasXSLog with xiangshan.HasXSParameter with HasTLDump{
-    val io = IO(new Bundle() {
-      val intrVec = Input(UInt(NrExtIntr.W))
-      val meip = Output(Vec(NumCores, Bool()))
-    })
-    
+class AXI4Plic
+(
+  sim: Boolean = false,
+  address: Seq[AddressSet]
+)(implicit p: Parameters)
+  extends AXI4SlaveModule(address, executable = false, _extra = new PlicIO) with xiangshan.HasXSParameter
+{
+  override lazy val module = new AXI4SlaveModuleImp[PlicIO](this) {
+    val NumCores = top.Parameters.get.socParameters.NumCores
     require(NrExtIntr <= PLICConsts.maxDevices)
-    require(NumCores  <= PLICConsts.maxHarts)
-    println(s"Interrupt map with ${NumCores} harts ${NrExtIntr} interrupts")
-
+    require(NumCores <= PLICConsts.maxHarts)
     val addressSpaceSize = 0x4000000
     val addressBits = log2Up(addressSpaceSize)
-    def getOffset(addr: UInt) = addr(addressBits-1,0)
 
-    // MMIO regs
-    val nrIntrWord = (NrExtIntr + 31) / 32  // roundup
+    def getOffset(addr: UInt) = addr(addressBits - 1, 0)
+
     val priority = List.fill(NrExtIntr)(Reg(UInt(32.W)))
+    val priorityMap = priority.zipWithIndex.map { case (r, intr) => RegMap((intr + 1) * 4, r) }.toMap
+
+    val nrIntrWord = (NrExtIntr + 31) / 32 // roundup
+    // pending bits are updated in the unit of bit by PLIC,
+    // so define it as vectors of bits, instead of UInt(32.W)
     val pending = List.fill(nrIntrWord)(RegInit(0.U.asTypeOf(Vec(32, Bool()))))
-    val enable = List.fill(NumCores)( List.fill(nrIntrWord)(RegInit(0.U(32.W))) )
+    val pendingMap = pending.zipWithIndex.map { case (r, intrWord) =>
+      RegMap(0x1000 + intrWord * 4, Cat(r.reverse), RegMap.Unwritable)
+    }.toMap
+
+    val enable = List.fill(NumCores)(List.fill(nrIntrWord)(RegInit(0.U(32.W))))
+    val enableMap = enable.zipWithIndex.map { case (l, hart) =>
+      l.zipWithIndex.map { case (r, intrWord) => RegMap(0x2000 + hart * 0x80 + intrWord * 4, r) }
+    }.reduce(_ ++ _).toMap
+
     val threshold = List.fill(NumCores)(Reg(UInt(32.W)))
-    val claimCompletion = List.fill(NumCores)(Reg(UInt(32.W)))
-    /* pending bits are updated in the unit of bit by PLIC,
-       so define it as vectors of bits, instead of UInt(32.W) */
-    
-    // Intr generation
+    val thresholdMap = threshold.zipWithIndex.map {
+      case (r, hart) => RegMap(0x200000 + hart * 0x1000, r)
+    }.toMap
+
     val inHandle = RegInit(0.U.asTypeOf(Vec(NrExtIntr + 1, Bool())))
+
     def completionFn(wdata: UInt) = {
-      inHandle(wdata(31,0)) := false.B
+      inHandle(wdata(31, 0)) := false.B
       0.U
     }
 
-    io.intrVec.asBools.zipWithIndex.map { case (intr, i) => {
+    val claimCompletion = List.fill(NumCores)(Reg(UInt(32.W)))
+    val claimCompletionMap = claimCompletion.zipWithIndex.map {
+      case (r, hart) => {
+        val addr = 0x200004 + hart * 0x1000
+        when(in.r.fire() && (getOffset(raddr) === addr.U)) {
+          inHandle(r) := true.B
+        }
+        RegMap(addr, r, completionFn)
+      }
+    }.toMap
+
+    io.extra.get.intrVec.asBools.zipWithIndex.map { case (intr, i) => {
       val id = i + 1
-      when (intr) { pending(id / 32)(id % 32) := true.B }
-      when (inHandle(id)) { pending(id / 32)(id % 32) := false.B }  // 如果被读取了，则清除相应的 pending 位
-    } }
+      when(intr) {
+        pending(id / 32)(id % 32) := true.B
+      }
+      when(inHandle(id)) {
+        pending(id / 32)(id % 32) := false.B
+      }
+    }
+    }
 
     val pendingVec = Cat(pending.map(x => Cat(x.reverse)))
     claimCompletion.zipWithIndex.map { case (r, hart) => {
       val takenVec = pendingVec & Cat(enable(hart))
       r := Mux(takenVec === 0.U, 0.U, PriorityEncoder(takenVec))
-    } }
+    }
+    }
 
+    val mapping = priorityMap ++ pendingMap ++ enableMap ++ thresholdMap ++ claimCompletionMap
+
+    val rdata = Wire(UInt(32.W))
+    RegMap.generate(mapping, getOffset(raddr), rdata,
+      getOffset(waddr), in.w.fire(), in.w.bits.data, MaskExpand(in.w.bits.strb >> waddr(2, 0)))
+    // narrow read
+    in.r.bits.data := Fill(2, rdata)
+
+    io.extra.get.meip.zipWithIndex.map { case (ip, hart) => ip := claimCompletion(hart) =/= 0.U }
   }
 }
-
