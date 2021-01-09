3ef996e98b45cfccede05303f348c9a64bc97a76
LinJiawei <linjiav@outlook.com>
2021-01-09 22:36:51 +0800

    Alu: opt timing

--- src/main/scala/xiangshan/backend/fu/Alu.scala
+++ src/main/scala/xiangshan/backend/fu/Alu.scala
@@ -2,7 +2,7 @@ package xiangshan.backend.fu
 
 import chisel3._
 import chisel3.util._
-import utils.{LookupTree, LookupTreeDefault, SignExt, XSDebug, ZeroExt}
+import utils.{LookupTree, LookupTreeDefault, ParallelMux, SignExt, XSDebug, ZeroExt}
 import xiangshan._
 import xiangshan.backend.ALUOpType
 
@@ -20,9 +20,10 @@ class Alu extends FunctionUnit with HasRedirectOut {
   val valid = io.in.valid
 
   val isAdderSub = (func =/= ALUOpType.add) && (func =/= ALUOpType.addw)
-  val adderRes = (src1 +& (src2 ^ Fill(XLEN, isAdderSub))) + isAdderSub
+  val addRes = src1 +& src2
+  val subRes = (src1 +& (~src2).asUInt()) + 1.U
   val xorRes = src1 ^ src2
-  val sltu = !adderRes(XLEN)
+  val sltu = !subRes(XLEN)
   val slt = xorRes(XLEN-1) ^ sltu
 
   val shsrc1 = LookupTreeDefault(func, src1, List(
@@ -30,16 +31,23 @@ class Alu extends FunctionUnit with HasRedirectOut {
     ALUOpType.sraw -> SignExt(src1(31,0), 64)
   ))
   val shamt = Mux(ALUOpType.isWordOp(func), src2(4, 0), src2(5, 0))
-  val res = LookupTreeDefault(func(3, 0), adderRes, List(
-    ALUOpType.sll  -> ((shsrc1  << shamt)(XLEN-1, 0)),
+
+  val miscRes = ParallelMux(List(
+    ALUOpType.sll  -> (shsrc1 << shamt)(XLEN-1, 0),
     ALUOpType.slt  -> ZeroExt(slt, XLEN),
     ALUOpType.sltu -> ZeroExt(sltu, XLEN),
     ALUOpType.xor  -> xorRes,
-    ALUOpType.srl  -> (shsrc1  >> shamt),
-    ALUOpType.or   -> (src1  |  src2),
-    ALUOpType.and  -> (src1  &  src2),
-    ALUOpType.sra  -> ((shsrc1.asSInt >> shamt).asUInt)
-  ))
+    ALUOpType.srl  -> (shsrc1 >> shamt),
+    ALUOpType.or   -> (src1 | src2),
+    ALUOpType.and  -> (src1 & src2),
+    ALUOpType.sra  -> (shsrc1.asSInt >> shamt).asUInt
+  ).map(x => (x._1 === func(3, 0), x._2)))
+
+  val res = Mux(ALUOpType.isAddSub(func),
+    Mux(isAdderSub, subRes, addRes),
+    miscRes
+  )
+
   val aluRes = Mux(ALUOpType.isWordOp(func), SignExt(res(31,0), 64), res)
 
   val branchOpTable = List(
@@ -48,10 +56,10 @@ class Alu extends FunctionUnit with HasRedirectOut {
     ALUOpType.getBranchType(ALUOpType.bltu) -> sltu
   )
 
-  val isBranch = uop.cf.brUpdate.pd.isBr// ALUOpType.isBranch(func)
-  val isRVC = uop.cf.brUpdate.pd.isRVC//(io.in.bits.cf.instr(1,0) =/= "b11".U)
+  val isBranch = uop.cf.brUpdate.pd.isBr
+  val isRVC = uop.cf.brUpdate.pd.isRVC
   val taken = LookupTree(ALUOpType.getBranchType(func), branchOpTable) ^ ALUOpType.isBranchInvert(func)
-  val target = Mux(isBranch, pc + offset, adderRes)(VAddrBits-1,0)
+  val target = (pc + offset)(VAddrBits-1,0)
   val snpc = Mux(isRVC, pc + 2.U, pc + 4.U)
 
   redirectOutValid := io.out.valid && isBranch
