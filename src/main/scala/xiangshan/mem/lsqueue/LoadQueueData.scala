bf6b6e2124d1bd2201ad3da9f323bdcf900c1571
William Wang <zeweiwang@outlook.com>
2021-01-13 03:15:36 +0800

    LoadQueueData: use sep dataModule

--- src/main/scala/xiangshan/mem/lsqueue/LoadQueueData.scala
+++ src/main/scala/xiangshan/mem/lsqueue/LoadQueueData.scala
@@ -19,6 +19,193 @@ class LQDataEntry extends XSBundle {
   val fwdMask = Vec(8, Bool())
 }
 
+// Data module define
+// These data modules are like SyncDataModuleTemplate, but support cam-like ops
+class paddrModule(numEntries: Int, numRead: Int, numWrite: Int, numMatch: Int) extends XSModule {
+  val io = IO(new Bundle {
+    val raddr = Vec(numRead,  Input(UInt(log2Up(numEntries).W)))
+    val rdata = Vec(numRead,  Output(UInt((PAddrBits-3).W)))
+    val wen   = Vec(numWrite, Input(Bool()))
+    val waddr = Vec(numWrite, Input(UInt(log2Up(numEntries).W)))
+    val wdata = Vec(numWrite, Input(UInt((PAddrBits-3).W)))
+    val mdata = Vec(numMatch, Input(UInt((PAddrBits-3).W)))
+    val mmask = Vec(numMatch, Output(Vec(numEntries, Bool())))
+  })
+
+  val data = Mem(numEntries, UInt((PAddrBits-3).W))
+
+  // read ports
+  for (i <- 0 until numRead) {
+    io.rdata(i) := data(io.raddr(i))
+  }
+
+  // below is the write ports (with priorities)
+  for (i <- 0 until numWrite) {
+    when (io.wen(i)) {
+      data(io.waddr(i)) := io.wdata(i)
+    }
+  }
+  
+  // content addressed match
+  for (i <- 0 until numMatch) {
+    io.mmask(i) := 0.U.asBools
+    for (j <- 0 until numEntries) {
+      when (io.mdata(i) === data(j)) {
+        io.mmask(i)(j) := true.B
+      }
+    }
+  }
+
+  // DataModuleTemplate should not be used when there're any write conflicts
+  for (i <- 0 until numWrite) {
+    for (j <- i+1 until numWrite) {
+      assert(!(io.wen(i) && io.wen(j) && io.waddr(i) === io.waddr(j)))
+    }
+  }
+}
+
+class maskModule(numEntries: Int, numRead: Int, numWrite: Int, numMatch: Int) extends XSModule {
+  val io = IO(new Bundle {
+    val raddr = Vec(numRead,  Input(UInt(log2Up(numEntries).W)))
+    val rdata = Vec(numRead,  Output(UInt(8.W)))
+    val wen   = Vec(numWrite, Input(Bool()))
+    val waddr = Vec(numWrite, Input(UInt(log2Up(numEntries).W)))
+    val wdata = Vec(numWrite, Input(UInt(8.W)))
+    val mdata = Vec(numMatch, Input(UInt(8.W)))
+    val mmask = Vec(numMatch, Output(Vec(numEntries, Bool())))
+  })
+
+  val data = Mem(numEntries, UInt(8.W))
+
+  // read ports
+  for (i <- 0 until numRead) {
+    io.rdata(i) := data(io.raddr(i))
+  }
+
+  // below is the write ports (with priorities)
+  for (i <- 0 until numWrite) {
+    when (io.wen(i)) {
+      data(io.waddr(i)) := io.wdata(i)
+    }
+  }
+  
+  // content addressed match
+  for (i <- 0 until numMatch) {
+    io.mmask(i) := 0.U.asBools
+    for (j <- 0 until numEntries) {
+      when ((io.mdata(i) & data(j)).orR) {
+        io.mmask(i)(j) := true.B
+      }
+    }
+  }
+
+  // DataModuleTemplate should not be used when there're any write conflicts
+  for (i <- 0 until numWrite) {
+    for (j <- i+1 until numWrite) {
+      assert(!(io.wen(i) && io.wen(j) && io.waddr(i) === io.waddr(j)))
+    }
+  }
+}
+
+class exceptionModule(numEntries: Int, numRead: Int, numWrite: Int) extends XSModule {
+  val io = IO(new Bundle {
+    // read
+    val raddr = Vec(numRead,  Input(UInt(log2Up(numEntries).W)))
+    val rdata = Vec(numRead,  Output(UInt(16.W)))
+    // address indexed write
+    val wen   = Vec(numWrite, Input(Bool()))
+    val waddr = Vec(numWrite, Input(UInt(log2Up(numEntries).W)))
+    val wdata = Vec(numWrite, Input(UInt(16.W)))
+    // masked write
+    val mwmask = Input(Vec(numEntries, Bool()))
+    val mwdata = Input(UInt(16.W))
+  })
+
+  val data = Mem(numEntries, UInt(16.W))
+
+  // read ports
+  for (i <- 0 until numRead) {
+    io.rdata(i) := data(io.raddr(i))
+  }
+
+  // below is the write ports (with priorities)
+  for (i <- 0 until numWrite) {
+    when (io.wen(i)) {
+      data(io.waddr(i)) := io.wdata(i)
+    }
+  }
+  
+  // masked write
+  for (j <- 0 until numEntries) {
+    when (io.mwmask(j)) {
+      data(j) := io.mwdata
+    }
+  }
+
+  // DataModuleTemplate should not be used when there're any write conflicts
+  for (i <- 0 until numWrite) {
+    for (j <- i+1 until numWrite) {
+      assert(!(io.wen(i) && io.wen(j) && io.waddr(i) === io.waddr(j)))
+    }
+  }
+}
+
+class coredataModule(numEntries: Int, numRead: Int, numWrite: Int) extends XSModule {
+  val io = IO(new Bundle {
+    // data io
+    // read
+    val raddr = Vec(numRead,  Input(UInt(log2Up(numEntries).W)))
+    val rdata = Vec(numRead,  Output(UInt(XLEN.W)))
+    // address indexed write
+    val wen   = Vec(numWrite, Input(Bool()))
+    val waddr = Vec(numWrite, Input(UInt(log2Up(numEntries).W)))
+    val wdata = Vec(numWrite, Input(UInt(XLEN.W)))
+    // masked write
+    val mwmask = Input(Vec(numEntries, Bool()))
+    val mwdata = Input(UInt(XLEN.W))
+    
+    // fwdMask io
+    val fwdMaskWdata = Vec(numWrite, Input(UInt(8.W)))
+  })
+
+  val data = Mem(numEntries, UInt(XLEN.W))
+  val fwdMask = Mem(numEntries, UInt(8.W))
+
+  // read ports
+  for (i <- 0 until numRead) {
+    io.rdata(i) := data(io.raddr(i))
+  }
+
+  // below is the write ports (with priorities)
+  for (i <- 0 until numWrite) {
+    when (io.wen(i)) {
+      data(io.waddr(i)) := io.wdata(i)
+    }
+  }
+
+  // masked write
+  // refill missed load
+  def mergeRefillData(refill: UInt, fwd: UInt, fwdMask: UInt): UInt = {
+    val res = Wire(Vec(8, UInt(8.W)))
+    (0 until 8).foreach(i => {
+      res(i) := Mux(fwdMask(i), fwd(8 * (i + 1) - 1, 8 * i), refill(8 * (i + 1) - 1, 8 * i))
+    })
+    res.asUInt
+  }
+
+  for (j <- 0 until numEntries) {
+    when (io.mwmask(j)) {
+      data(j) := mergeRefillData(io.mwdata, data(j), fwdMask(j))
+    }
+  }
+
+  // DataModuleTemplate should not be used when there're any write conflicts
+  for (i <- 0 until numWrite) {
+    for (j <- i+1 until numWrite) {
+      assert(!(io.wen(i) && io.wen(j) && io.waddr(i) === io.waddr(j)))
+    }
+  }
+}
 
 class LoadQueueData(size: Int, wbNumRead: Int, wbNumWrite: Int) extends XSModule with HasDCacheParameters with HasCircularQueuePtrHelper {
   val io = IO(new Bundle() {
