6d5490864c87df200a89be7effad6d97f3773ece
jinyue110 <jinyue161@mails.ucas.ac.cn>
2021-01-19 23:47:28 +0800

    IFU: set reset vector to 0x10000000

--- src/main/scala/xiangshan/frontend/IFU.scala
+++ src/main/scala/xiangshan/frontend/IFU.scala
@@ -17,7 +17,7 @@ trait HasInstrMMIOConst extends HasXSParameter{
 }
 
 trait HasIFUConst extends HasXSParameter {
-  val resetVector = 0x80000000L//TODO: set reset vec
+  val resetVector = 0x10000000L//TODO: set reset vec
   def align(pc: UInt, bytes: Int): UInt = Cat(pc(VAddrBits-1, log2Ceil(bytes)), 0.U(log2Ceil(bytes).W))
   val instBytes = if (HasCExtension) 2 else 4
   val instOffsetBits = log2Ceil(instBytes)
