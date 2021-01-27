7ec1c9ed11a16820c64906589b9228e767a02df2
jinyue110 <jinyue161@mails.ucas.ac.cn>
2021-01-27 14:20:18 +0800

    icache: delete stupid ICacheBase trait

--- src/main/scala/xiangshan/cache/icache.scala
+++ src/main/scala/xiangshan/cache/icache.scala
@@ -75,7 +75,6 @@ abstract class ICacheBundle extends XSBundle
 
 abstract class ICacheModule extends XSModule
   with HasICacheParameters
-  with ICacheBase
   with HasFrontEndExceptionNo
 
 abstract class ICacheArray extends XSModule
@@ -120,39 +119,6 @@ class ICacheIO extends ICacheBundle
   val prev_pc = Input(UInt(VAddrBits.W))
   val prev_ipf = Input(Bool())
   val pd_out = Output(new PreDecodeResp)
-}
-
-/* ------------------------------------------------------------
- * The 3-stage pipeline register
- * ------------------------------------------------------------
- */
-trait ICacheBase extends HasICacheParameters
-{
-  //----------------------------
-  //    Stage 1
-  //----------------------------
-  // val s1_valid = WireInit(false.B)
-  val s1_req_pc = Wire(UInt(VAddrBits.W))
-  val s1_req_mask = Wire(UInt(PredictWidth.W))
-  val s1_fire = WireInit(false.B)
-
-  //----------------------------
-  //    Stage 2
-  //----------------------------
-  val s2_valid = RegInit(false.B)
-  val s2_req_pc = RegEnable(next = s1_req_pc,init = 0.U, enable = s1_fire)
-  val s2_req_mask = RegEnable(next = s1_req_mask,init = 0.U, enable = s1_fire)
-  val s2_ready = WireInit(false.B)
-  val s2_fire = WireInit(false.B)
-
-  //----------------------------
-  //    Stage 3
-  //----------------------------
-  val s3_valid = RegInit(false.B)
-  val s3_req_pc = RegEnable(next = s2_req_pc,init = 0.U, enable = s2_fire)
-  val s3_req_mask = RegEnable(next = s2_req_mask,init = 0.U, enable = s2_fire)
-  val s3_ready = WireInit(false.B)
-
 }
 
 class ICacheMetaWriteBundle extends ICacheBundle
@@ -308,67 +274,23 @@ class ICacheDataArray extends ICachArray
  */
 class ICache extends ICacheModule
 {
-  // cut a cacheline into a fetch packet
-  def cutHelper(sourceVec: Vec[UInt], pc: UInt, mask: UInt): UInt = {
-    val sourceVec_inst = Wire(Vec(blockRows*rowBytes/instBytes,UInt(insLen.W)))
-    (0 until blockRows).foreach{ i =>
-      (0 until rowBytes/instBytes).foreach{ j =>
-        sourceVec_inst(i*rowBytes/instBytes + j) := sourceVec(i)(j*insLen+insLen-1, j*insLen)
-      }
-    }
-    val cutPacket = WireInit(VecInit(Seq.fill(PredictWidth){0.U(insLen.W)}))
-    val start = Cat(pc(ptrHighBit,ptrLowBit),0.U(packetInstNumBit.W))
-    (0 until PredictWidth ).foreach{ i =>
-      cutPacket(i) := Mux(mask(i).asBool,sourceVec_inst(start + i.U),0.U)
-    }
-    cutPacket.asUInt
-  }
-
-  def cutHelperMMIO(sourceVec: Vec[UInt], pc: UInt, mask: UInt) = {
-    val sourceVec_inst = Wire(Vec(mmioBeats * mmioBusBytes/instBytes,UInt(insLen.W)))
-    (0 until mmioBeats).foreach{ i =>
-      (0 until mmioBusBytes/instBytes).foreach{ j =>
-        sourceVec_inst(i*mmioBusBytes/instBytes + j) := sourceVec(i)(j*insLen+insLen-1, j*insLen)
-      }
-    }
-    val cutPacket = WireInit(VecInit(Seq.fill(PredictWidth){0.U(insLen.W)}))
-    val insLenLog = log2Ceil(insLen)
-    val start = (pc >> insLenLog.U)(log2Ceil(mmioBeats * mmioBusBytes/instBytes) -1, 0)
-    val outMask = mask >> start
-    (0 until PredictWidth ).foreach{ i =>
-      cutPacket(i) := Mux(outMask(i).asBool,sourceVec_inst(start + i.U),0.U)
-    }
-    (cutPacket.asUInt, outMask.asUInt)
-  }
-
-  // generate the one hot code according to a UInt between 0-8
-  def PriorityMask(sourceVec: UInt) : UInt = {
-    val oneHot = Mux(sourceVec >= 8.U, "b1000".U,
-             Mux(sourceVec >= 4.U, "b0100".U,
-             Mux(sourceVec >= 2.U, "b0010".U, "b0001".U)))
-    oneHot
-  }
-
   val io = IO(new ICacheIO)
 
-  val s2_flush = io.flush(0)
-  val s3_flush = io.flush(1)
+  val (s2_flush,s3_flush) = (io.flush(0), io.flush(1))
   //----------------------------
   //    Memory Part
   //----------------------------
   val metaArray = Module(new ICacheMetaArray)
   val dataArray = Module(new ICacheDataArray)
-  // 256-bit valid
-  val validArray = RegInit(0.U((nSets * nWays).W))
+  val validArray = RegInit(0.U((nSets * nWays).W))  
 
   //----------------------------
   //    Stage 1
-  //----------------------------
-  s1_fire := io.req.valid
-  s1_req_pc := io.req.bits.addr
-  s1_req_mask := io.req.bits.mask
-  s2_ready := WireInit(false.B)
-  // s1_fire := s1_valid && (s2_ready || s2_flush)
+  //----------------------------  
+  val req_in = io.req.bits
+  val req_valid =  io.req.valid
+
+  val (s1_fire, s1_req_pc, s1_req_mask) = {(req_valid, req_in.addr, req_in.mask)}
 
   // SRAM(Meta and Data) read request
   val s1_idx = get_idx(s1_req_pc)
@@ -378,40 +300,48 @@ class ICache extends ICacheModule
   dataArray.io.read.valid := s1_fire
   dataArray.io.read.bits  :=s1_idx
 
-  XSDebug("[Stage 1] r : f  (%d  %d)  request pc: 0x%x  mask: %b\n",s2_ready,s1_fire,s1_req_pc,s1_req_mask)
-  XSDebug("[Stage 1] index: %d\n",s1_idx)
+  // XSDebug("[Stage 1] r : f  (%d  %d)  request pc: 0x%x  mask: %b\n",s2_ready,s1_fire,s1_req_pc,s1_req_mask)
+  // XSDebug("[Stage 1] index: %d\n",s1_idx)
 
 
   //----------------------------
-  //    Stage 2 
+  //    Stage 2
   //----------------------------
-  val s2_idx = get_idx(s2_req_pc)
-  val s2_tlb_resp = WireInit(io.tlb.resp.bits)
-  val s2_tag = get_tag(s2_tlb_resp.paddr)
   val s2_hit = WireInit(false.B)
-  val s2_allValid = s2_valid && io.tlb.resp.valid
   val s2_mmio = WireInit(false.B)
+  val s3_ready = WireInit(false.B)
+  val s2_tlb_resp = WireInit(io.tlb.resp.bits)
+  val s2_valid = RegInit(false.B)
+  val s2_req_pc = RegEnable(next = s1_req_pc,init = 0.U, enable = s1_fire)
+  val s2_req_mask = RegEnable(next = s1_req_mask,init = 0.U, enable = s1_fire)
 
-  s2_fire := s2_allValid && s3_ready
-  s2_ready := s3_ready || !s2_valid
+  val (s2_idx, s2_tag) = { (get_idx(s2_req_pc), get_tag(s2_tlb_resp.paddr)) }
+  val (s2_ready, s2_allValid)  = {((s3_ready || !s2_valid),  (s2_valid && io.tlb.resp.valid)) }
+  val s2_fire = s2_allValid && s3_ready
+
   when(s1_fire)       { s2_valid := true.B }
   .elsewhen(s2_flush) { s2_valid := false.B }
   .elsewhen(s2_fire)  { s2_valid := false.B }
 
   // SRAM(Meta and Data) read reseponse
   // TODO :Parity wrong excetion
-  val metas = metaArray.io.readResp 
-
-  val datas =RegEnable(next=dataArray.io.readResp, enable=s2_fire)
-
+  val (metas, datas)  = {(metaArray.io.readResp , RegEnable(next=dataArray.io.readResp, enable=s2_fire))}
   val validMeta = Cat((0 until nWays).map{w => validArray(Cat(s2_idx, w.U(log2Ceil(nWays).W)))}.reverse).asUInt
 
   // hit check and generate victim cacheline mask
+  def PriorityMask(sourceVec: UInt) : UInt = {
+    val oneHot = Mux(sourceVec >= 8.U, "b1000".U,
+             Mux(sourceVec >= 4.U, "b0100".U,
+             Mux(sourceVec >= 2.U, "b0010".U, "b0001".U)))
+    oneHot
+  }
   val hitVec = VecInit((0 until nWays).map{w => metas(w)=== s2_tag && validMeta(w) === 1.U})
-  val victimWayMask = (1.U << LFSR64()(log2Up(nWays)-1,0))
   val invalidVec = ~validMeta
   val hasInvalidWay = invalidVec.orR
   val refillInvalidWaymask = PriorityMask(invalidVec)
+
+  val replacer = cacheParams.replacement
+  val victimWayMask = UIntToOH(replacer.way)
 
 
   //deal with icache exception
@@ -428,41 +358,65 @@ class ICache extends ICacheModule
 
   assert(!(s2_hit && s2_mmio),"MMIO address should not hit in icache")
 
-  XSDebug("[Stage 2] v : r : f  (%d  %d  %d)  pc: 0x%x  mask: %b mmio:%d \n",s2_valid,s3_ready,s2_fire,s2_req_pc,s2_req_mask,s2_mmio)
-  XSDebug("[Stage 2] exception: af:%d  pf:%d  \n",icacheExceptionVec(accessFault),icacheExceptionVec(pageFault))
-  XSDebug(p"[Stage 2] tlb req:  v ${io.tlb.req.valid} r ${io.tlb.req.ready} ${io.tlb.req.bits}\n")
-  XSDebug(p"[Stage 2] tlb resp: v ${io.tlb.resp.valid} r ${io.tlb.resp.ready} ${s2_tlb_resp}\n")
-  XSDebug("[Stage 2] tag: %x  hit:%d mmio:%d\n",s2_tag,s2_hit,s2_mmio)
-  XSDebug("[Stage 2] validMeta: %b  victimWayMaks:%b   invalidVec:%b    hitVec:%b    waymask:%b \n",validMeta,victimWayMask,invalidVec.asUInt,hitVec.asUInt,waymask.asUInt)
+  // XSDebug("[Stage 2] v : r : f  (%d  %d  %d)  pc: 0x%x  mask: %b mmio:%d \n",s2_valid,s3_ready,s2_fire,s2_req_pc,s2_req_mask,s2_mmio)
+  // XSDebug("[Stage 2] exception: af:%d  pf:%d  \n",icacheExceptionVec(accessFault),icacheExceptionVec(pageFault))
+  // XSDebug(p"[Stage 2] tlb req:  v ${io.tlb.req.valid} r ${io.tlb.req.ready} ${io.tlb.req.bits}\n")
+  // XSDebug(p"[Stage 2] tlb resp: v ${io.tlb.resp.valid} r ${io.tlb.resp.ready} ${s2_tlb_resp}\n")
+  // XSDebug("[Stage 2] tag: %x  hit:%d mmio:%d\n",s2_tag,s2_hit,s2_mmio)
+  // XSDebug("[Stage 2] validMeta: %b  victimWayMaks:%b   invalidVec:%b    hitVec:%b    waymask:%b \n",validMeta,victimWayMask,invalidVec.asUInt,hitVec.asUInt,waymask.asUInt)
 
 
   //----------------------------
   //    Stage 3
   //----------------------------
+  val s3_valid = RegInit(false.B)
+  val s3_miss  = WireInit(false.B)
+  val s3_req_pc = RegEnable(next = s2_req_pc,init = 0.U, enable = s2_fire)
+  val s3_req_mask = RegEnable(next = s2_req_mask,init = 0.U, enable = s2_fire)
   val s3_tlb_resp = RegEnable(next = s2_tlb_resp, init = 0.U.asTypeOf(new TlbResp), enable = s2_fire)
-  val s3_data = datas
   val s3_tag = RegEnable(s2_tag, s2_fire)
   val s3_hit = RegEnable(next=s2_hit,init=false.B,enable=s2_fire)
   val s3_mmio = RegEnable(next=s2_mmio,init=false.B,enable=s2_fire)
   val s3_wayMask = RegEnable(next=waymask,init=0.U,enable=s2_fire)
-  val s3_idx = get_idx(s3_req_pc)
   val s3_exception_vec = RegEnable(next= icacheExceptionVec,init=0.U.asTypeOf(Vec(8,Bool())), enable=s2_fire)
-  val s3_has_exception = s3_exception_vec.asUInt.orR
-  val s3_miss = s3_valid && !s3_hit && !s3_mmio && !s3_has_exception
+  val s3_has_exception = RegEnable(next= hasIcacheException,init=false.B,enable=s2_fire)
+  val s3_idx = get_idx(s3_req_pc)
+  val s3_data = datas
+    
+
   when(s3_flush)                  { s3_valid := false.B }
   .elsewhen(s2_fire && !s2_flush) { s3_valid := true.B }
   .elsewhen(io.resp.fire())       { s3_valid := false.B }
 
-  // icache hit
-  // data Parity encoding
-  // simply cut the hit cacheline
+
+  /* icache hit
+   * simply cut the cacheline into a fetchpacket according to the req_pc
+   * use hitVec to do data way choosing
+   */
+  def cutHelper(sourceVec: Vec[UInt], pc: UInt, mask: UInt): UInt = {
+    val sourceVec_inst = Wire(Vec(blockRows*rowBytes/instBytes,UInt(insLen.W)))
+    (0 until blockRows).foreach{ i =>
+      (0 until rowBytes/instBytes).foreach{ j =>
+        sourceVec_inst(i*rowBytes/instBytes + j) := sourceVec(i)(j*insLen+insLen-1, j*insLen)
+      }
+    }
+    val cutPacket = WireInit(VecInit(Seq.fill(PredictWidth){0.U(insLen.W)}))
+    val start = Cat(pc(ptrHighBit,ptrLowBit),0.U(packetInstNumBit.W))
+    (0 until PredictWidth ).foreach{ i =>
+      cutPacket(i) := Mux(mask(i).asBool,sourceVec_inst(start + i.U),0.U)
+    }
+    cutPacket.asUInt
+  }
   val dataHitWay = Mux1H(s3_wayMask,s3_data)
   val outPacket =  Wire(UInt((FetchWidth * 32).W))
   outPacket := cutHelper(dataHitWay,s3_req_pc.asUInt,s3_req_mask.asUInt)
 
 
 
-  //ICache MissQueue
+  /* icache miss
+   * send a miss req to ICache Miss Queue, excluding exception/flush/blocking
+   * block the pipeline until refill finishes
+   */
   val icacheMissQueue = Module(new IcacheMissQueue)
   val blocking = RegInit(false.B)
   val isICacheResp = icacheMissQueue.io.resp.valid && icacheMissQueue.io.resp.bits.clientID === cacheID.U(2.W)
@@ -474,17 +428,18 @@ class ICache extends ICacheModule
   when(icacheMissQueue.io.req.fire() || io.mmio_acquire.fire()){blocking := true.B}
   .elsewhen(blocking && ((icacheMissQueue.io.resp.fire() && isICacheResp) ||  io.mmio_grant.fire() || s3_flush) ){blocking := false.B}
 
+  // deal with same cacheline miss in s3 and s2
+  val is_same_cacheline = s3_miss && s2_valid  && (groupAligned(s2_req_pc) ===groupAligned(s3_req_pc))
+  val useRefillReg = RegNext(is_same_cacheline && icacheMissQueue.io.resp.fire())
+  val refillDataVecReg = RegEnable(next=refillDataVec, enable= (is_same_cacheline && icacheMissQueue.io.resp.fire()))
+
+  s3_miss := s3_valid && !s3_hit && !s3_mmio && !s3_has_exception && !useRefillReg
+
+
+
   XSDebug(blocking && s3_flush,"check for icache non-blocking")
-  //cache flush register
-  val icacheFlush = io.fencei
-  val cacheflushed = RegInit(false.B)
-  XSDebug("[Fence.i] icacheFlush:%d, cacheflushed:%d\n",icacheFlush,cacheflushed)
-  when(icacheFlush && blocking && !isICacheResp){ cacheflushed := true.B}
-  .elsewhen(isICacheResp && cacheflushed) {cacheflushed := false.B }
 
-  //TODO: Prefetcher
-
-  //refill write
+  //refill meta write
   val metaWriteReq = icacheMissQueue.io.meta_write.bits
   icacheMissQueue.io.meta_write.ready := true.B
   metaArray.io.write.valid := icacheMissQueue.io.meta_write.valid
@@ -498,33 +453,63 @@ class ICache extends ICacheModule
     validArray := validArray.bitSet(validPtr, true.B)
   }
 
-  //data
+  //refill data write
   icacheMissQueue.io.refill.ready := true.B
   val refillReq = icacheMissQueue.io.refill.bits
   dataArray.io.write.valid := icacheMissQueue.io.refill.valid
   dataArray.io.write.bits.apply(data=refillReq.refill_data,
                                 idx=refillReq.refill_idx,
                                 waymask=refillReq.refill_waymask)
+                            
+  s3_ready := ((io.resp.ready && s3_hit || !s3_valid) && !blocking) || (blocking && ((icacheMissQueue.io.resp.fire()) || io.mmio_grant.fire()))
 
-  //icache flush: only flush valid Array register
+
+  /* icache flush
+   * backend send fence.i signal to flush all the cacheline in icache for consistency
+   * set a flag to inform the refill meta that should not write in validArray
+   */
+  val icacheFlush = io.fencei
+  val cacheflushed = RegInit(false.B)
+  when(icacheFlush && blocking && !isICacheResp){ cacheflushed := true.B}
+  .elsewhen(isICacheResp && cacheflushed) {cacheflushed := false.B }
+
   when(icacheFlush){ validArray := 0.U }
+  XSDebug("[Fence.i] icacheFlush:%d, cacheflushed:%d\n",icacheFlush,cacheflushed)
 
+
+  /* refill output
+   * cut the refill data cacheline into a fetch packet for responsing to predecoder
+   */
   val refillDataVec = icacheMissQueue.io.resp.bits.data.asTypeOf(Vec(blockRows,UInt(wordBits.W)))
   val refillDataOut = cutHelper(refillDataVec, s3_req_pc,s3_req_mask )
 
-  val is_same_cacheline = s3_miss && s2_valid  && (groupAligned(s2_req_pc) ===groupAligned(s3_req_pc))
-  val useRefillReg = RegNext(is_same_cacheline && icacheMissQueue.io.resp.fire())
-  val refillDataVecReg = RegEnable(next=refillDataVec, enable= (is_same_cacheline && icacheMissQueue.io.resp.fire()))
 
-  //FIXME!!
+  /* mmio response output
+   * cut the mmio response data cacheline into a fetch packet for responsing to predecoder
+   * TODO: no need to wait for a whole fetch packet(once per beat)?
+   */
+  def cutHelperMMIO(sourceVec: Vec[UInt], pc: UInt, mask: UInt) = {
+    val sourceVec_inst = Wire(Vec(mmioBeats * mmioBusBytes/instBytes,UInt(insLen.W)))
+    (0 until mmioBeats).foreach{ i =>
+      (0 until mmioBusBytes/instBytes).foreach{ j =>
+        sourceVec_inst(i*mmioBusBytes/instBytes + j) := sourceVec(i)(j*insLen+insLen-1, j*insLen)
+      }
+    }
+    val cutPacket = WireInit(VecInit(Seq.fill(PredictWidth){0.U(insLen.W)}))
+    val insLenLog = log2Ceil(insLen)
+    val start = (pc >> insLenLog.U)(log2Ceil(mmioBeats * mmioBusBytes/instBytes) -1, 0)
+    val outMask = mask >> start
+    (0 until PredictWidth ).foreach{ i =>
+      cutPacket(i) := Mux(outMask(i).asBool,sourceVec_inst(start + i.U),0.U)
+    }
+    (cutPacket.asUInt, outMask.asUInt)
+  }
   val mmioDataVec = io.mmio_grant.bits.data.asTypeOf(Vec(mmioBeats,UInt(mmioBusWidth.W)))
-  val mmio_packet = cutHelperMMIO(mmioDataVec, s3_req_pc, mmioMask)._1
-  val mmio_mask   = cutHelperMMIO(mmioDataVec, s3_req_pc, mmioMask)._2
+  val (mmio_packet,mmio_mask)  = cutHelperMMIO(mmioDataVec, s3_req_pc, mmioMask)
 
   XSDebug("mmio data  %x\n", mmio_packet)
 
 
-  s3_ready := ((io.resp.ready && s3_hit || !s3_valid) && !blocking) || (blocking && ((icacheMissQueue.io.resp.fire()) || io.mmio_grant.fire()))
 
 
   val pds = Seq.fill(nWays)(Module(new PreDecode))
@@ -544,7 +529,6 @@ class ICache extends ICacheModule
   }
   
   
-  // if a fetch packet triggers page fault, at least send a valid instruction
   io.pd_out := Mux1H(s3_wayMask, pds.map(_.io.out))
   val s3_noHit = s3_wayMask === 0.U
 
@@ -607,6 +591,7 @@ class ICache extends ICacheModule
   io.l1plusflush := icacheFlush
 
   XSDebug("[flush] flush_0:%d  flush_1:%d\n",s2_flush,s3_flush)
+
 
   //Performance Counter
   if (!env.FPGAPlatform ) {
