22fd6b01ef643fcb7d211eee22d81d7d12fd6665
Yinan Xu <xuyinan1997@gmail.com>
2021-02-10 08:45:08 +0800

    emu,difftest: fix get_nemu_this_pc and set_nemu_this_pc api for snapshots

--- src/test/csrc/difftest.h
+++ src/test/csrc/difftest.h
@@ -98,7 +98,7 @@ int difftest_step(DiffState *s, int coreid);
 int difftest_store_step(uint64_t *saddr, uint64_t *sdata, uint8_t *smask, int coreid);
 void difftest_display(uint8_t mode, int coreid);
 
-uint64_t get_nemu_this_pc();
-void set_nemu_this_pc(uint64_t pc);
+uint64_t get_nemu_this_pc(int coreid);
+void set_nemu_this_pc(uint64_t pc, int coreid);
 
 #endif
