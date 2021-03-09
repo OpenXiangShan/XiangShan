cce49d4697135f6f73336e828b840c1030d8271f
jinyue110 <jinyue161@mails.ucas.ac.cn>
2021-03-09 13:49:09 +0800

    ICache/L1+: add performance counters for each way.

--- src/main/scala/xiangshan/cache/ICache.scala
+++ src/main/scala/xiangshan/cache/ICache.scala
@@ -483,6 +483,12 @@ class ICache extends ICacheModule
   touch_ways(1).valid := icacheMissQueue.io.meta_write.valid
   touch_ways(1).bits  := wayNum
 
+  (0 until nWays).map{ w => 
+    XSPerf("hit way_" + Integer.toString(w, 10),  s2_hit && OHToUInt(hitVec)  === w.U)
+    XSPerf("refill way_" + Integer.toString(w, 10), icacheMissQueue.io.meta_write.valid && wayNum === w.U)
+    XSPerf("access way_" + Integer.toString(w, 10), (icacheMissQueue.io.meta_write.valid && wayNum === w.U) || (s2_hit && OHToUInt(hitVec)  === w.U))
+  }
+
   val validPtr = Cat(metaWriteReq.meta_write_idx,wayNum)
   when(icacheMissQueue.io.meta_write.valid && !cacheflushed){
     validArray := validArray.bitSet(validPtr, true.B)
