181f157d88cea0dd47791431a70b90c031a791bd
ZhangZifei <1773908404@qq.com>
2021-03-25 21:34:11 +0800

    RS: fix bug that fp src's flushed enqueue conflicts with next enqueue

--- src/main/scala/xiangshan/backend/issue/ReservationStation.scala
+++ src/main/scala/xiangshan/backend/issue/ReservationStation.scala
@@ -137,7 +137,7 @@ class ReservationStation
   io.numExist := select.io.numExist
   select.io.redirectVec := ctrl.io.redirectVec
   select.io.readyVec := ctrl.io.readyVec
-  select.io.enq.valid := io.fromDispatch.valid
+  select.io.enq.valid := io.fromDispatch.valid && !(io.redirect.valid || io.flush) 
   io.fromDispatch.ready := select.io.enq.ready
   select.io.deq.ready := io.deq.ready
   if (feedback) {
@@ -145,7 +145,7 @@ class ReservationStation
     select.io.flushState := io.memfeedback.bits.flushState
   }
 
-  ctrl.io.in.valid := select.io.enq.fire()// && !(io.redirect.valid || io.flush) // NOTE: same as select
+  ctrl.io.in.valid := select.io.enq.ready && io.fromDispatch.valid // NOTE: ctrl doesnt care redirect for timing optimization
   ctrl.io.flush := io.flush
   ctrl.io.in.bits.addr := select.io.enq.bits
   ctrl.io.in.bits.uop := io.fromDispatch.bits
@@ -164,9 +164,9 @@ class ReservationStation
     ctrl.io.stIssuePtr := RegNext(io.stIssuePtr)
   }
 
-  data.io.in.valid := ctrl.io.in.valid
+  data.io.in.valid := select.io.enq.fire()
   data.io.in.addr := select.io.enq.bits
-  data.io.in.uop := io.fromDispatch.bits // NOTE: use for imm-pc src value mux
+  data.io.in.uop := io.fromDispatch.bits // NOTE: used for imm-pc src value mux
   data.io.in.enqSrcReady := ctrl.io.enqSrcReady
   data.io.srcRegValue := io.srcRegValue
   if(exuCfg == Exu.jumpExeUnitCfg) {
@@ -370,7 +370,7 @@ class ReservationStationSelect
   // enq
   isFull := tailPtr.flag
   // agreement with dispatch: don't fire when io.redirect.valid
-  val enqueue = io.enq.fire() && !(io.redirect.valid || io.flush)
+  val enqueue = io.enq.fire()
   val tailInc = tailPtr + 1.U
   val tailDec = tailPtr - 1.U
   val nextTailPtr = Mux(io.flush, 0.U.asTypeOf(new CircularQueuePtr(iqSize)), Mux(dequeue === enqueue, tailPtr, Mux(dequeue, tailDec, tailInc)))
