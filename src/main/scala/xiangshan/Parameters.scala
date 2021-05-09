d580f7edd299a88ce35ddb93616b4eb527a09f26
ZhangZifei <zhangzifei20z@ict.ac.cn>
2021-05-09 08:29:04 +0800

    PTW: set PTWMSHRSIZE 16

--- src/main/scala/xiangshan/Parameters.scala
+++ src/main/scala/xiangshan/Parameters.scala
@@ -93,7 +93,7 @@ case class XSCoreParameters
   PtwSPEntrySize: Int = 16,
   PtwL1EntrySize: Int = 16,
   PtwL2EntrySize: Int = 2048, //(256 * 8)
-  PtwMissQueueSize: Int = 4,
+  PtwMissQueueSize: Int = 16,
   NumPerfCounters: Int = 16,
 ){
   val loadExuConfigs = Seq.fill(exuParameters.LduCnt)(LdExeUnitCfg)
