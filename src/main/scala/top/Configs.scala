cdbd1605d4dd0a9f5def13bca55726ec9c3cc98e
William Wang <zeweiwang@outlook.com>
2021-05-10 17:04:46 +0800

    Configs: limit frontend width in MinimalConfig

--- src/main/scala/top/Configs.scala
+++ src/main/scala/top/Configs.scala
@@ -20,47 +20,17 @@ class DefaultConfig(n: Int) extends Config((site, here, up) => {
   )
 })
 
-class MinimalSimConfig(n: Int = 1) extends Config(
+// Synthesizable minimal XiangShan
+// * It is still an out-of-order, super-scalaer arch
+// * L1 cache included
+// * L2/L3 cache included
+class MinimalConfig(n: Int = 1) extends Config(
   new DefaultConfig(n).alter((site, here, up) => {
     case SoCParamsKey => up(SoCParamsKey).copy(
       cores = up(SoCParamsKey).cores.map(_.copy(
-        IssQueSize = 8,
-        NRPhyRegs = 80,
-        LoadQueueSize = 16,
-        StoreQueueSize = 16,
-        RoqSize = 32,
-        BrqSize = 8,
-        FtqSize = 16,
-        IBufSize = 16,
-        dpParams = DispatchParameters(
-          IntDqSize = 8,
-          FpDqSize = 8,
-          LsDqSize = 8,
-          IntDqDeqWidth = 4,
-          FpDqDeqWidth = 4,
-          LsDqDeqWidth = 4
-        ),
-        EnableBPD = false, // disable TAGE
-        EnableLoop = false,
-        // TlbEntrySize = 4,
-        // TlbSPEntrySize = 2,
-        // PtwL1EntrySize = 2,
-        // PtwL2EntrySize = 16,
-        // PtwL3EntrySize = 32,
-        // PtwSPEntrySize = 2,
-        useFakeDCache = true,
-        useFakePTW = true,
-        useFakeL1plusCache = true,
-      )),
-      useFakeL3Cache = true
-    )
-  })
-)
-
-class MinimalFPGAConfig(n: Int = 1) extends Config(
-  new DefaultConfig(n).alter((site, here, up) => {
-    case SoCParamsKey => up(SoCParamsKey).copy(
-      cores = up(SoCParamsKey).cores.map(_.copy(
+        DecodeWidth = 2,
+        RenameWidth = 2,
+        FetchWidth = 4,
         IssQueSize = 8,
         NRPhyRegs = 80,
         LoadQueueSize = 16,
@@ -86,6 +56,20 @@ class MinimalFPGAConfig(n: Int = 1) extends Config(
         // PtwL3EntrySize = 128,
         // PtwSPEntrySize = 2,
       )),
+    )
+  })
+)
+
+// Non-synthesizable MinimalConfig, for fast simulation only
+class MinimalSimConfig(n: Int = 1) extends Config(
+  new MinimalConfig(n).alter((site, here, up) => {
+    case SoCParamsKey => up(SoCParamsKey).copy(
+      cores = up(SoCParamsKey).cores.map(_.copy(
+        useFakeDCache = true,
+        useFakePTW = true,
+        useFakeL1plusCache = true,
+      )),
+      useFakeL3Cache = true
     )
   })
 )
