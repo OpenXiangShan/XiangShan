c6d439803a044ea209139672b25e35fe8d7f4aa0
Lemover <zhangzifei20z@ict.ac.cn>
2021-06-04 09:06:35 +0800

    Add MulanPSL-2.0 License (#824)
    In this commit, we add License for XiangShan project.

--- scripts/autorun/config.py
+++ scripts/autorun/config.py
@@ -1,11 +1,26 @@
 # Copyright 2020 zyy
+#***************************************************************************************
+# Copyright (c) 2020-2021 Institute of Computing Technology, Chinese Academy of Sciences
+#
+# XiangShan is licensed under Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#          http://license.coscl.org.cn/MulanPSL2
+#
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+#
+# See the Mulan PSL v2 for more details.
+#***************************************************************************************
+
 from common.simulator_task_goback import SimulatorTaskGoBack
 
 class EmuTasksConfig(SimulatorTaskGoBack):
     def __init__(self, exe: str, top_data_dir: str, task_name: str, workload: str, sub_phase: int, emu: str, max_instr: int):
         super().__init__(exe, top_data_dir, task_name, workload, sub_phase)
         self.window_size = 192
-        
+
         self.add_direct_options(
             [ emu ],
         )
