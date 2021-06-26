f9a5982b85f3f84c449553c25512ded0bcf201a8
Jiawei Lin <linjiawei20s@ict.ac.cn>
2021-06-26 13:57:31 +0800

    Refactor print control transform (#845)

--- src/main/scala/top/XiangShanStage.scala
+++ src/main/scala/top/XiangShanStage.scala
@@ -17,74 +17,10 @@ package top
 
 import chisel3.stage.ChiselCli
 import firrtl.AnnotationSeq
-import firrtl.annotations.NoTargetAnnotation
-import firrtl.options.PhaseManager.PhaseDependency
 import firrtl.options.{Dependency, HasShellOptions, Shell, ShellOption}
 import firrtl.stage.{FirrtlCli, RunFirrtlTransformAnnotation}
-import freechips.rocketchip.stage.phases.GenerateArtefacts
 import freechips.rocketchip.transforms.naming.{OverrideDesiredNameAnnotation, RenameDesiredNames}
-import xstransforms.ShowPrintTransform
-import xstransforms.PrintModuleName
-
-case class DisablePrintfAnnotation(m: String) extends NoTargetAnnotation
-
-object DisablePrintfAnnotation extends HasShellOptions{
-
-  val options = Seq(
-    new ShellOption[String](
-      longOption = "disable-module-print",
-      toAnnotationSeq = s => Seq(DisablePrintfAnnotation(s)),
-      helpText =
-        "The verilog 'printf' in the <module> and it's submodules will be removed\n",
-      shortOption = Some("dm"),
-      helpValueName = Some("<module>")
-    )
-  )
-
-}
-
-case class EnablePrintfAnnotation(m: String) extends NoTargetAnnotation
-
-object EnablePrintfAnnotation extends HasShellOptions {
-  val options = Seq(
-    new ShellOption[String](
-      longOption = "enable-module-print",
-      toAnnotationSeq = s => Seq(EnablePrintfAnnotation(s)),
-      helpText =
-        "The verilog 'printf' except the <module> and it's submodules will be removed\n",
-      shortOption = Some("em"),
-      helpValueName = Some("<module>")
-    )
-  )
-
-}
-
-case class DisableAllPrintAnnotation() extends NoTargetAnnotation
-
-object DisableAllPrintAnnotation extends HasShellOptions {
-  val options = Seq(
-    new ShellOption[Unit](
-      longOption = "disable-all",
-      toAnnotationSeq = _ => Seq(DisableAllPrintAnnotation()),
-      helpText =
-        "All the verilog 'printf' will be removed\n",
-      shortOption = Some("dall")
-    )
-  )
-}
-
-case class RemoveAssertAnnotation() extends NoTargetAnnotation
-
-object RemoveAssertAnnotation extends HasShellOptions{
-  val options = Seq(
-    new ShellOption[Unit](
-      longOption = "remove-assert",
-      toAnnotationSeq = _ => Seq(RemoveAssertAnnotation()),
-      helpText = "All the 'assert' will be removed\n",
-      shortOption = None
-    )
-  )
-}
+import xstransforms._
 
 trait XiangShanCli { this: Shell =>
   parser.note("XiangShan Options")
@@ -110,7 +46,7 @@ object XiangShanStage {
     (new XiangShanStage).execute(
       args,
       annotations ++ Seq(
-        RunFirrtlTransformAnnotation(new ShowPrintTransform),
+        RunFirrtlTransformAnnotation(new PrintControl),
         RunFirrtlTransformAnnotation(new PrintModuleName),
         RunFirrtlTransformAnnotation(new RenameDesiredNames)
       )
