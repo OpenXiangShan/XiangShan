d1d85a2e013e9da69426470ddda2d5359cfbc36d
JinYue <jinyue20s@ict.ac.cn>
2021-07-03 14:50:19 +0800

    [WIP]IFU: add fetch and fetch_resp logic.
    * TODO: need a solution for fetch-on-miss

--- src/main/scala/xiangshan/decoupled-frontend/IFU.scala
+++ src/main/scala/xiangshan/decoupled-frontend/IFU.scala
@@ -8,11 +8,10 @@ import utils._
 
 class FtqInterface(implicit p: Parameters) extends XSBundle {
   val startAddr = UInt(64.W)  
-  val target = UInt(64.W)
-  val hit = Bool()
+  val endAddr   = UInt(5.W)
+  val ftqIdx    = UInt(log2Ceil(FtqSize).W)
 }
 
-class ICacheMeta
 
 class IFUIO(implicit p: Parameters) extends XSBundle {
   val FtqInterface 
@@ -25,7 +24,26 @@ class IFUIO(implicit p: Parameters) extends XSBundle {
 class IFU(implicit p: Parameters) extends XSModule
 {
   val io = IO(new IFUIO)
-  val toFtq    = io.FtqInterface.toFtq
-  val fromtFtq = io.FtqInterface.fromFtq
+  val (toFtq, fromFtq)    = (io.FtqInterface.toFtq, io.FtqInterface.fromFtq)
+  val (toMeta, toData, meta_resp, data_resp) =  (io.ICacheInterface.toMeta, io.ICacheInterface.toData, io.ICacheInterface.fromMeta, io.ICacheInterface.fromData)
+  
+  //fetch: send addr to Meta/TLB and Data simultaneously
+  val fetch_req = Seq(toMeta, toData)
+  fetch_req.map(channel => 
+    channel.valid        := fromFtq.valid 
+    channel.bits.start   := fromFtq.bits.startAddr 
+    channel.bits.offset  := fromFtq.bits.offset
+  )
+
+  //fetch response
+  val fetch_valid = meta_resp.valid && meta_resp.bits.hit 
+  val fetch_fail  = meta_resp.valid && !meta_resp.bits.hit 
+  
+  io.toIbuffer.valid := fetch_valid
+  (0 until FetchWidth).map(i  => io.toIbuffer.bits.instr(i) := data_resp.bits.instr(i))
+
+  //TODO: sulotion to fetch-on-miss problem
+  io.toFtq.valid := (fectch_valid) || (fetch_fail && meta_resp.bits.refill.valid)
+  io.toFtq.bits.replayIdx := RegNext(fromFtq.bits.idx)
 
 }
