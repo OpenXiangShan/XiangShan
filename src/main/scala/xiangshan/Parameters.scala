a972d85e042505384e7b173b17dd8c1f9820507c
ZhangZifei <zhangzifei20z@ict.ac.cn>
2021-07-11 00:09:55 +0800

    ptw: add parameter to choose repeater and filter(default)
    simple ci test show that: the filter is critical for perf
    like mcf(5m):
      old ptw:2.38
      new ptw with repeater: 2.41
      new ptw with filter: 2.58

--- src/main/scala/xiangshan/Parameters.scala
+++ src/main/scala/xiangshan/Parameters.scala
@@ -132,6 +132,7 @@ case class XSCoreParameters
   ),
   L2Size: Int = 512 * 1024, // 512KB
   L2NWays: Int = 8,
+  usePTWRepeater: Boolean = false,
   useFakePTW: Boolean = false,
   useFakeDCache: Boolean = false,
   useFakeL1plusCache: Boolean = false,
@@ -259,6 +260,7 @@ trait HasXSParameter {
   // cache hierarchy configurations
   val l1BusDataWidth = 256
 
+  val usePTWRepeater = coreParams.usePTWRepeater
   val useFakeDCache = coreParams.useFakeDCache
   val useFakePTW = coreParams.useFakePTW
   val useFakeL1plusCache = coreParams.useFakeL1plusCache
