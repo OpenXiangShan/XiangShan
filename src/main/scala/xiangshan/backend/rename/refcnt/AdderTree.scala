bd417a9f2d1e19735fd9f33af9d287288bc65982
YikeZhou <yikezhou@outlook.com>
2021-07-10 21:07:01 +0800

    AdderTree: Add testbench for AdderTree (passed)

--- src/main/scala/xiangshan/backend/rename/refcnt/AdderTree.scala
+++ src/main/scala/xiangshan/backend/rename/refcnt/AdderTree.scala
@@ -8,28 +8,29 @@ import utils.{OneHot, ParallelOR}
 
 class AdderTree(implicit p: Parameters) extends XSModule {
   val io = IO(new Bundle() {
+    // inc/dec Vec must not contain duplicate indices
     val incVec = Vec(RenameWidth, Input(UInt(PhyRegIdxWidth.W)))
     val decVec = Vec(CommitWidth, Input(UInt(PhyRegIdxWidth.W)))
 
     val cntVec = Vec(NRPhyRegs, Input(UInt(2.W)))
     val nextCntVec = Vec(NRPhyRegs, Output(UInt(2.W)))
   })
-  
-  val incBitVec = Vec(NRPhyRegs, Wire(Bool()))
-  val decBitVec = Vec(NRPhyRegs, Wire(Bool()))
 
-  incBitVec zip ParallelOR(io.incVec.map(OneHot.UIntToOH1(_))).asBools() foreach {
+  val incBitVec = Wire(Vec(NRPhyRegs, Bool()))
+  val decBitVec = Wire(Vec(NRPhyRegs, Bool()))
+
+  incBitVec zip ParallelOR(io.incVec.map(UIntToOH(_))).asBools() foreach {
     case (bit, i) => bit := i
   }
 
-  decBitVec zip ParallelOR(io.decVec.map(OneHot.UIntToOH1(_))).asBools() foreach {
+  decBitVec zip ParallelOR(io.decVec.map(UIntToOH(_))).asBools() foreach {
     case (bit, d) => bit := d
   }
 
-  incBitVec zip decBitVec zip io.cntVec zip io.nextCntVec foreach {
-    case (((inc, dec), cnt), nextCnt) =>
-      val adder = Module(new Adder)
-      adder.io.inc := inc 
+  val adderVec = Array.fill(NRPhyRegs)(Module(new Adder))
+  incBitVec zip decBitVec zip io.cntVec zip io.nextCntVec zip adderVec foreach {
+    case ((((inc, dec), cnt), nextCnt), adder) =>
+      adder.io.inc := inc
       adder.io.dec := dec
       adder.io.cnt := cnt
       nextCnt := adder.io.nextCnt
