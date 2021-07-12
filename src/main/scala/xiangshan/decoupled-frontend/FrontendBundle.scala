0df7c1d6cf477789a62bd5dd6f560c80916ebcfb
JinYue <jinyue20s@ict.ac.cn>
2021-07-12 21:36:45 +0800

    FrontendBundle: update IfuToFtq Bundle

--- src/main/scala/xiangshan/decoupled-frontend/FrontendBundle.scala
+++ src/main/scala/xiangshan/decoupled-frontend/FrontendBundle.scala
@@ -7,38 +7,19 @@ import xiangshan._
 import utils._
 
 class FetchRequestBundle(implicit p: Parameters) extends XSBundle {
-  val startAddr = UInt(VAddrBits.W)
+  val startAddr    = UInt(VAddrBits.W)
   val fallThruAddr = UInt(VAddrBits.W)
-  val ftqIdx    = UInt(log2Ceil(FTQSIZE).W)
+  val ftqIdx       = UInt(log2Ceil(FTQSIZE).W)
+  val ftqOffset    = Valid(UInt(log2Ceil(FETCHWIDTH).W))
+  val target       = UInt(VAddrBits.W)
 }
 
 class PredecodeWritebackBundle(implicit p:Parameters) extends XSBundle {
-  val pd = Vec(16, new Predecode) // TODO: redefine Predecode
-  val ftqIdx = UInt(log2Ceil(FTQSIZE).W)
-}
-
-
-class IfuToFtq(implicit p: Parameters) extends XSBundle {
-}
-
-class IMetaToFetch(implicit p: Parameters) extends XSBundle {
-
-}
-
-class IDataToFetch(implicit p: Parameters) extends XSBundle {
-
-}
-
-class FetchToICache(implicit p: Parameters) extends XSBundle {
-
-}
-
-class FetchToMissQueue(implicit p: Parameters) extends XSBundle {
-
-}
-
-class MissQueueToFetch(implicit p: Parameters) extends XSBundle{
-
+  val pd        = Vec(16, new Predecode) // TODO: redefine Predecode
+  val ftqIdx    = UInt(log2Ceil(FTQSIZE).W)
+  val ftqOffset = UInt(log2Ceil(FETCHWIDTH).W)
+  val misPred   = Bool()
+  val target    = UInt(VAddrBits.W)
 }
 
 class BpuToFtq(implicit p: Parameters) extends XSBundle {
@@ -54,17 +35,14 @@ class FetchToBpu(implicit p: Parameters) extends XSBundle {
   val ifu_redirect = Flipped(Valid(UInt(VAddrBits.W)))
 }
 
+class Exception(implicit p: Parameters) extends XSBundle {
+
+}
 
 class FetchToIBuffer(implicit p: Parameters) extends XSBundle {
-  val instrs = Vec(PredictWidth, UInt(32.W))
-  val mask = UInt(PredictWidth.W)
-  val pdmask = UInt(PredictWidth.W)
-  //val pc = Vec(PredictWidth, UInt(VAddrBits.W))
-  //val foldpc = Vec(PredictWidth, UInt(MemPredPCWidth.W))
-  val pd = Vec(PredictWidth, new PreDecodeInfo)
-  val ipf = Bool()
-  val acf = Bool()
-  val crossPageIPFFix = Bool()
-  val pred_taken = UInt(PredictWidth.W)
-  val ftqPtr = new FtqPtr
+  val instrs    = Vec(PredictWidth, UInt(32.W))
+  val mask      = UInt(PredictWidth.W)
+  val pd        = Vec(PredictWidth, new PreDecodeInfo)
+  val exception = new Exception
+  val ftqPtr    = new FtqPtr
 }
