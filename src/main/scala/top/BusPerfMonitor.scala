15a30e289b434ceb4ab115ca87ffc3ba8fa70ff7
LinJiawei <linjiawei20s@ict.ac.cn>
2021-08-06 22:26:13 +0800

    Add more counters in bus pmu

--- src/main/scala/top/BusPerfMonitor.scala
+++ src/main/scala/top/BusPerfMonitor.scala
@@ -1,9 +1,8 @@
 package top
 
-import bus.tilelink.TLChannel
 import chipsalliance.rocketchip.config.Parameters
 import freechips.rocketchip.diplomacy.{LazyModule, LazyModuleImp}
-import freechips.rocketchip.tilelink.{TLAdapterNode, TLBundle, TLBundleBase, TLEdge}
+import freechips.rocketchip.tilelink._
 import chisel3._
 import chisel3.util._
 import utils.{XSPerfAccumulate, XSPerfPrint}
@@ -30,38 +29,61 @@ class BaseBusPerfMonitorImp(outer: BusPerfMonitor)
 class BusPerfMonitorImp(outer: BusPerfMonitor)
   extends BaseBusPerfMonitorImp(outer)
 {
-  def CHANNEL_CONFLICT[T <: Data](in: Seq[(DecoupledIO[T], String)], channelName: String): Unit = {
-    val (chns, names) = in.unzip
-    val valids = Cat(chns.map(_.valid))
-    val conflict = PopCount(valids) > 1.U
-    when(conflict){
-      XSPerfPrint(p"CHN_CONFLICT: [$channelName] [${names.mkString(" ")}] ${Binary(valids)}\n")
+
+  def PERF_CHN[T <: TLChannel](clientName: String, chn: DecoupledIO[T]) = {
+
+    XSPerfAccumulate(s"$clientName ${chn.bits.channelName} fire", chn.fire())
+    XSPerfAccumulate(s"$clientName ${chn.bits.channelName} stall", chn.valid && !chn.ready)
+
+    val ops = chn.bits match {
+      case _: TLBundleA => TLMessages.a.map(_._1)
+      case _: TLBundleB => TLMessages.b.map(_._1)
+      case _: TLBundleC => TLMessages.c.map(_._1)
+      case _: TLBundleD => TLMessages.d.map(_._1)
+      case _: TLBundleE => Nil
     }
-    XSPerfAccumulate(s"TOTAL_CHN_CONFLICT: [$channelName]", conflict)
+    for((op, i) <- ops.zipWithIndex){
+      chn.bits match {
+        case a: TLBundleA =>
+          XSPerfAccumulate(s"$clientName ${chn.bits.channelName} $op fire",
+            i.U === a.opcode && chn.fire()
+          )
+          XSPerfAccumulate(s"$clientName ${chn.bits.channelName} $op stall",
+            i.U === a.opcode && chn.valid && !chn.ready
+          )
+        case b: TLBundleB =>
+          XSPerfAccumulate(s"$clientName ${chn.bits.channelName} $op fire",
+            i.U === b.opcode && chn.fire()
+          )
+          XSPerfAccumulate(s"$clientName ${chn.bits.channelName} $op stall",
+            i.U === b.opcode && chn.valid && !chn.ready
+          )
+        case c: TLBundleC =>
+          XSPerfAccumulate(s"$clientName ${chn.bits.channelName} $op fire",
+            i.U === c.opcode && chn.fire()
+          )
+          XSPerfAccumulate(s"$clientName ${chn.bits.channelName} $op stall",
+            i.U === c.opcode && chn.valid && !chn.ready
+          )
+        case d: TLBundleD =>
+          XSPerfAccumulate(s"$clientName ${chn.bits.channelName} $op fire",
+            i.U === d.opcode && chn.fire()
+          )
+          XSPerfAccumulate(s"$clientName ${chn.bits.channelName} $op stall",
+            i.U === d.opcode && chn.valid && !chn.ready
+          )
+      }
+    }
   }
 
-  CHANNEL_CONFLICT(outer.node.in.map(in => (in._1.a, in._2.master.masters.head.name)), "A")
-  CHANNEL_CONFLICT(outer.node.in.map(in => (in._1.c, in._2.master.masters.head.name)), "C")
-  CHANNEL_CONFLICT(outer.node.in.map(in => (in._1.e, in._2.master.masters.head.name)), "E")
-
-  for((in, edgeIn) <- outer.node.in){
-    val client = edgeIn.client.clients.head.name
-    when(in.a.fire()){
-      XSPerfPrint(p"CHN_STAT_A: [$client] op: ${in.a.bits.opcode} param: ${in.a.bits.param}\n")
-    }
-    when(in.d.fire()){
-      XSPerfPrint(p"CHN_STAT_D: [$client] op: ${in.d.bits.opcode} param: ${in.d.bits.param}\n")
-    }
+  for((in, edgeIn) <- outer.node.in) {
+    val clientName = edgeIn.master.masters.head.name
+    PERF_CHN(clientName, in.a)
+    PERF_CHN(clientName, in.d)
     if(in.params.hasBCE){
-      when(in.b.fire()){
-        XSPerfPrint(p"CHN_STAT_B: [$client] op: ${in.b.bits.opcode} param: ${in.b.bits.param}\n")
-      }
-      when(in.c.fire()){
-        XSPerfPrint(p"CHN_STAT_C: [$client] op: ${in.c.bits.opcode} param: ${in.c.bits.param}\n")
-      }
-      when(in.e.fire()){
-        XSPerfPrint(p"CHN_STAT_E: [$client]\n")
-      }
+      PERF_CHN(clientName, in.b)
+      PERF_CHN(clientName, in.c)
+      PERF_CHN(clientName, in.e)
     }
   }
 }
