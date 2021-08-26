872cd090c30135a69bf3e59a82000f4ec70cf238
Lingrui98 <goulingrui19s@ict.ac.cn>
2021-08-26 19:30:17 +0800

    ubtb: code clean ups

--- src/main/scala/xiangshan/decoupled-frontend/uBTB.scala
+++ src/main/scala/xiangshan/decoupled-frontend/uBTB.scala
@@ -86,8 +86,8 @@ class MicroBTB(implicit p: Parameters) extends BasePredictor
     val io = IO(new Bundle {
       val read_pc = Flipped(Valid(UInt(VAddrBits.W)))
       // val read_taken_mask = Input(Vec(numBr, Bool()))
-      val read_resp = Output(new ReadResp)
-      // val read_hit = Output(Bool())
+      val read_entry = Output(new FTBEntry)
+      val read_hit = Output(Bool())
 
       val update_write_meta = Input(new MicroBTBMeta)
       val update_write_data = Flipped(Valid(new MicroBTBData))
@@ -96,15 +96,15 @@ class MicroBTB(implicit p: Parameters) extends BasePredictor
     })
 
     val tagCam = Module(new CAMTemplate(UInt(tagSize.W), nWays, 2))
-    val metaMem = Module(new AsyncDataModuleTemplate(Bool(), nWays, nWays, 1)) // valids
+    val validMem = Module(new AsyncDataModuleTemplate(Bool(), nWays, nWays, 1)) // valids
     val dataMem = Module(new AsyncDataModuleTemplate(new MicroBTBData, nWays, 1, 1))
 
 
     for (w <- 0 until nWays) {
-      metaMem.io.raddr(w) := w.U
+      validMem.io.raddr(w) := w.U
     }
 
-    val meta = metaMem.io.rdata
+    val valids = validMem.io.rdata
     // val rbims = bims.io.rdata.take(nWays)
     // val rdatas = data.io.rdata
     
@@ -113,7 +113,7 @@ class MicroBTB(implicit p: Parameters) extends BasePredictor
     val read_tag = getTag(read_pc)
 
     // val hits = VecInit(rmetas.map(m => m.valid && m.tag === read_tag))
-    val hits = VecInit((0 until nWays).map(i => meta(i) && tagCam.io.r.resp(0)(i)))
+    val hits = VecInit((0 until nWays).map(i => valids(i) && tagCam.io.r.resp(0)(i)))
     val hit = hits.reduce(_||_)
     val hitWay = OHToUInt(hits)
 
@@ -123,29 +123,27 @@ class MicroBTB(implicit p: Parameters) extends BasePredictor
 
     val hit_data = Mux(hit, dataMem.io.rdata(0), 0.U.asTypeOf(new MicroBTBData))
 
-    io.read_resp := DontCare
-    io.read_resp.valid := io.read_pc.valid
+    io.read_entry := DontCare
+    io.read_entry.valid := io.read_pc.valid
 
-    io.read_resp.tag          := read_tag
-    io.read_resp.brOffset     := hit_data.brOffset
-    io.read_resp.brTargets    := hit_data.brTargets
-    io.read_resp.brValids     := hit_data.brValids
+    io.read_entry.tag          := read_tag
+    io.read_entry.brOffset     := hit_data.brOffset
+    io.read_entry.brTargets    := hit_data.brTargets
+    io.read_entry.brValids     := hit_data.brValids
 
-    io.read_resp.jmpOffset    := hit_data.jmpOffset
-    io.read_resp.jmpTarget    := hit_data.jmpTarget
-    io.read_resp.jmpValid     := hit_data.jmpValid
+    io.read_entry.jmpOffset    := hit_data.jmpOffset
+    io.read_entry.jmpTarget    := hit_data.jmpTarget
+    io.read_entry.jmpValid     := hit_data.jmpValid
 
-    io.read_resp.pftAddr      := hit_data.pftAddr
-    io.read_resp.carry        := hit_data.carry
+    io.read_entry.pftAddr      := hit_data.pftAddr
+    io.read_entry.carry        := hit_data.carry
 
-    io.read_resp.isCall       := hit_data.isCall
-    io.read_resp.isRet        := hit_data.isRet
-    io.read_resp.isJalr       := hit_data.isJalr
+    io.read_entry.isCall       := hit_data.isCall
+    io.read_entry.isRet        := hit_data.isRet
+    io.read_entry.isJalr       := hit_data.isJalr
 
-    io.read_resp.oversize     := hit_data.oversize
-    io.read_resp.last_is_rvc  := hit_data.last_is_rvc
-
-    io.read_resp.hit          := hit
+    io.read_entry.oversize     := hit_data.oversize
+    io.read_entry.last_is_rvc  := hit_data.last_is_rvc
 
     val do_reset = RegInit(true.B)
     val reset_way = RegInit(0.U(log2Ceil(nWays).W))
@@ -157,7 +155,7 @@ class MicroBTB(implicit p: Parameters) extends BasePredictor
     when (reset_way === (nWays-1).U) { do_reset := false.B }
 
     val update_tag = io.update_write_meta.tag
-    val update_hits = VecInit((0 until nWays).map(i => meta(i) && tagCam.io.r.resp(1)(i)))
+    val update_hits = VecInit((0 until nWays).map(i => valids(i) && tagCam.io.r.resp(1)(i)))
     val update_hit = update_hits.reduce(_||_)
     val update_hitWay = OHToUInt(update_hits)
     
@@ -180,7 +178,7 @@ class MicroBTB(implicit p: Parameters) extends BasePredictor
     
     // }
 
-    val update_emptys = meta.map(m => !m)
+    val update_emptys = valids.map(!_)
     val update_has_empty_way = update_emptys.reduce(_||_)
     val update_empty_way = ParallelPriorityEncoder(update_emptys)
     val update_way = Mux(update_hit, update_hitWay, Mux(update_has_empty_way, update_empty_way, update_alloc_way))
@@ -193,9 +191,9 @@ class MicroBTB(implicit p: Parameters) extends BasePredictor
       0.U(tagSize.W),
       RegNext(io.update_write_meta.tag))
 
-    metaMem.io.wen(0)   := do_reset || RegNext(io.update_write_data.valid)
-    metaMem.io.waddr(0) := Mux(do_reset, reset_way, RegNext(update_way))
-    metaMem.io.wdata(0) := Mux(do_reset,
+    validMem.io.wen(0)   := do_reset || RegNext(io.update_write_data.valid)
+    validMem.io.waddr(0) := Mux(do_reset, reset_way, RegNext(update_way))
+    validMem.io.wdata(0) := Mux(do_reset,
       false.B,
       RegNext(io.update_write_meta.valid))
 
@@ -208,47 +206,32 @@ class MicroBTB(implicit p: Parameters) extends BasePredictor
 
   } // uBTBBank
 
-  val ubtbBanks = Module(new UBTBBank(numWays))
-  val banks = ubtbBanks.io
-  val read_resps = banks.read_resp
+  val ubtbBank = Module(new UBTBBank(numWays))
+  val bank = ubtbBank.io
+  val read_entry = bank.read_entry
   val outMeta = Wire(new MicroBTBOutMeta)
 
   XSDebug(p"uBTB entry, read_pc=${Hexadecimal(s1_pc)}\n")
-  //XSDebug(p"v=${read_resps.valid}, brValids=${Binary(read_resps.brValids.asUInt)}, jmpValid=${read_resps.jmpValid}, pred0=${Binary(read_resps.pred(0).asUInt)}, pred1=${Binary(read_resps.pred(1).asUInt)}, hit=${read_resps.hit}\n")
-  // XSDebug(p"v=${read_resps.valid}, brValids=${Binary(read_resps.brValids.asUInt)}, jmpValid=${read_resps.jmpValid}, pred0=${Binary(read_resps.pred(0).asUInt)}, pred1=${Binary(read_resps.pred(1).asUInt)}, hit=${read_resps.hit}\n")
-  // XSDebug(p"v=${read_resps.valid}, brValids=${Binary(read_resps.brValids.asUInt)}, jmpValid=${read_resps.jmpValid}, pred0=${Binary(read_resps.pred(0).asUInt)}, hit=${read_resps.hit}\n")
+  //XSDebug(p"v=${read_entrys.valid}, brValids=${Binary(read_entrys.brValids.asUInt)}, jmpValid=${read_entrys.jmpValid}, pred0=${Binary(read_entrys.pred(0).asUInt)}, pred1=${Binary(read_entrys.pred(1).asUInt)}, hit=${read_entrys.hit}\n")
+  // XSDebug(p"v=${read_entrys.valid}, brValids=${Binary(read_entrys.brValids.asUInt)}, jmpValid=${read_entrys.jmpValid}, pred0=${Binary(read_entrys.pred(0).asUInt)}, pred1=${Binary(read_entrys.pred(1).asUInt)}, hit=${read_entrys.hit}\n")
+  // XSDebug(p"v=${read_entrys.valid}, brValids=${Binary(read_entrys.brValids.asUInt)}, jmpValid=${read_entrys.jmpValid}, pred0=${Binary(read_entrys.pred(0).asUInt)}, hit=${read_entrys.hit}\n")
 
-  banks.read_pc.valid := io.s1_fire
-  banks.read_pc.bits := s1_pc
+  bank.read_pc.valid := io.s1_fire
+  bank.read_pc.bits := s1_pc
 
   io.out.resp := io.in.bits.resp_in(0)
   io.out.resp.s1.pc := s1_pc
   io.out.resp.s1.preds.taken_mask := io.in.bits.resp_in(0).s1.preds.taken_mask
-  io.out.resp.s1.preds.hit := read_resps.hit
-  io.out.resp.s1.ftb_entry := DontCare
-  io.out.resp.s1.ftb_entry.valid := read_resps.valid
+  io.out.resp.s1.preds.hit := bank.read_hit
+  io.out.resp.s1.ftb_entry := read_entry
 
-  io.out.resp.s1.ftb_entry.brOffset     := read_resps.brOffset
-  io.out.resp.s1.ftb_entry.brTargets    := read_resps.brTargets
-  io.out.resp.s1.ftb_entry.brValids     := read_resps.brValids
-
-  io.out.resp.s1.ftb_entry.jmpOffset    := read_resps.jmpOffset
-  io.out.resp.s1.ftb_entry.jmpTarget    := read_resps.jmpTarget
-  io.out.resp.s1.ftb_entry.jmpValid     := read_resps.jmpValid
-
-  when(read_resps.hit) {
-    io.out.resp.s1.ftb_entry.pftAddr  := read_resps.pftAddr
-    io.out.resp.s1.ftb_entry.carry    := read_resps.carry
-    io.out.resp.s1.ftb_entry.oversize := read_resps.oversize
-  }.otherwise {
+  when(!bank.read_hit) {
     io.out.resp.s1.ftb_entry.pftAddr := s1_pc(instOffsetBits + log2Ceil(PredictWidth), instOffsetBits) ^ (1 << log2Ceil(PredictWidth)).U
     io.out.resp.s1.ftb_entry.carry := s1_pc(instOffsetBits + log2Ceil(PredictWidth)).asBool
     io.out.resp.s1.ftb_entry.oversize := false.B
   }
 
-  io.out.resp.s1.ftb_entry.last_is_rvc  := read_resps.last_is_rvc
-
-  outMeta.hit := read_resps.hit
+  outMeta.hit := bank.read_hit
   io.out.s3_meta := RegEnable(RegEnable(outMeta.asUInt, io.s1_fire), io.s2_fire)
 
   // Update logic
@@ -288,24 +271,24 @@ class MicroBTB(implicit p: Parameters) extends BasePredictor
   update_write_datas.brTargets    := update.ftb_entry.brTargets
   update_write_datas.pftAddr      := update.ftb_entry.pftAddr
 
-  banks.update_write_meta := update_write_metas
-  banks.update_write_data.valid := data_write_valid
-  banks.update_write_data.bits := update_write_datas
-  banks.update_taken_mask := u_taken_mask
-  banks.update_mask := LowerMaskFromLowest(u_taken_mask.asUInt)
+  bank.update_write_meta := update_write_metas
+  bank.update_write_data.valid := data_write_valid
+  bank.update_write_data.bits := update_write_datas
+  bank.update_taken_mask := u_taken_mask
+  bank.update_mask := LowerMaskFromLowest(u_taken_mask.asUInt)
 
   if (debug && !env.FPGAPlatform && env.EnablePerfDebug) {
-    XSDebug("req_v=%b, req_pc=%x, hit=%b\n", io.s1_fire, s1_pc, read_resps.hit)
+    XSDebug("req_v=%b, req_pc=%x, hit=%b\n", io.s1_fire, s1_pc, bank.read_hit)
     XSDebug("target=%x, real_taken_mask=%b, taken_mask=%b, brValids=%b, jmpValid=%b\n",
-      io.out.resp.s1.target, io.out.resp.s1.real_taken_mask.asUInt, io.out.resp.s1.preds.taken_mask.asUInt, read_resps.brValids.asUInt, read_resps.jmpValid.asUInt)
+      io.out.resp.s1.target, io.out.resp.s1.real_taken_mask.asUInt, io.out.resp.s1.preds.taken_mask.asUInt, read_entry.brValids.asUInt, read_entry.jmpValid.asUInt)
 
     XSDebug(u_valid, "[update]Update from ftq\n")
     XSDebug(u_valid, "[update]update_pc=%x, tag=%x\n", u_pc, getTag(u_pc))
     XSDebug(u_valid, "[update]taken_mask=%b, brValids=%b, jmpValid=%b\n",
       u_taken_mask.asUInt, update.ftb_entry.brValids.asUInt, update.ftb_entry.jmpValid)
 
-    XSPerfAccumulate("ubtb_read_hits", RegNext(io.s1_fire) && read_resps.hit)
-    XSPerfAccumulate("ubtb_read_misses", RegNext(io.s1_fire) && !read_resps.hit)
+    XSPerfAccumulate("ubtb_read_hits", RegNext(io.s1_fire) && bank.read_hit)
+    XSPerfAccumulate("ubtb_read_misses", RegNext(io.s1_fire) && !bank.read_hit)
 
     XSPerfAccumulate("ubtb_commit_hits", u_valid && u_meta.hit)
     XSPerfAccumulate("ubtb_commit_misses", u_valid && !u_meta.hit)
