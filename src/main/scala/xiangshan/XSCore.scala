718f8a602efec088ddf656b15382dc44ab3f996f
Yinan Xu <xuyinan@ict.ac.cn>
2021-09-02 09:18:01 +0800

    rs,mem: support fast load-to-load wakeup and issue (#984)
    This PR adds support for fast load-to-load wakeup and issue. In load-to-load fast wakeup and issue, load-to-load latency is reduced to 2 cycles.
    
    Now a load instruction can wakeup another load instruction at LOAD stage 1. When the producer load instruction arrives at stage 2, the consumer load instruction is issued to load stage 0 and using data from the producer to generate load address.
    
    In reservation station, load can be dequeued from staged 1 when stage 2 does not have a valid instruction. If the fast load is not accepted, from the next cycle on, the load will dequeue as normal.
    
    Timing in reservation station (for imm read) and load unit (for writeback data selection) to be optimized later.
    
    * backend,rs: issue load one cycle earlier when possible
    
    This commit adds support for issuing load instructions one cycle
    earlier if the load instruction is wakeup by another load. An extra
    2-bit UInt is added to IO.
    
    * mem: add load to load addr fastpath framework
    
    * mem: enable load to load forward
    
    * mem: add load-load forward counter
    
    Co-authored-by: William Wang <zeweiwang@outlook.com>

--- src/main/scala/xiangshan/XSCore.scala
+++ src/main/scala/xiangshan/XSCore.scala
@@ -291,6 +291,7 @@ class XSCoreImp(outer: XSCoreBase) extends LazyModuleImp(outer)
   memScheduler.io.extra.jumpPc <> ctrlBlock.io.jumpPc
   memScheduler.io.extra.jalr_target <> ctrlBlock.io.jalr_target
   memScheduler.io.extra.stIssuePtr <> memBlock.io.stIssuePtr
+  memScheduler.io.extra.loadFastMatch.get <> memBlock.io.loadFastMatch
   memScheduler.io.extra.debug_int_rat <> ctrlBlock.io.debug_int_rat
   memScheduler.io.extra.debug_fp_rat <> ctrlBlock.io.debug_fp_rat
 
