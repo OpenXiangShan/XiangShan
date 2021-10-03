563d3f6f9858d62a525bf7fe6c8fe73bcaa11ca6
Zhangfw <471348957@qq.com>
2021-10-03 16:05:43 +0800

    alu: fix src1 generated by opcode

--- src/main/scala/xiangshan/backend/fu/Alu.scala
+++ src/main/scala/xiangshan/backend/fu/Alu.scala
@@ -194,7 +194,7 @@ class AluDataModule(implicit p: Parameters) extends XSModule {
   val leftShiftModule = Module(new LeftShiftModule)
   val sll = leftShiftModule.io.sll
   val revSll = leftShiftModule.io.revSll
-  leftShiftModule.io.sllSrc := Cat(Fill(32, func(0)), Fill(32, 1.U)) & src1
+  leftShiftModule.io.sllSrc := Cat(Fill(32, func(3)), Fill(32, 1.U)) & src1
   leftShiftModule.io.shamt := shamt
   leftShiftModule.io.revShamt := revShamt
 
