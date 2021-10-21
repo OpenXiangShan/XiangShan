cad346d1b86b5c3c99d4c8ac6528d8d7b3f717bd
lixin <1037997956@qq.com>
2021-10-21 22:33:16 +0800

    ci: add rv64mi-p-svinval to ci

--- scripts/xiangshan.py
+++ scripts/xiangshan.py
@@ -219,7 +219,8 @@ class XiangShan(object):
             "coremark_rv64gc_o3/coremark-riscv64-xs.bin",
             "coremark_rv64gcb_o3/coremark-riscv64-xs.bin",
             "ext_intr/amtest-riscv64-xs.bin",
-            "cache-alias/aliastest-riscv64-xs.bin"
+            "cache-alias/aliastest-riscv64-xs.bin",
+            "Svinval/rv64mi-p-svinval.bin"
         ]
         misc_tests = map(lambda x: os.path.join(base_dir, x), workloads)
         return misc_tests
