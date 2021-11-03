6262467f25434a3c09e5c452005fcfa0f1898dd5
JinYue <jinyue20s@ict.ac.cn>
2021-11-03 14:51:26 +0800

    ReleaseUnit: delete release write logic

--- src/main/scala/xiangshan/frontend/icache/ReleaseUnit.scala
+++ src/main/scala/xiangshan/frontend/icache/ReleaseUnit.scala
@@ -7,9 +7,8 @@ import freechips.rocketchip.tilelink.{ClientMetadata, ClientStates, TLArbiter, T
 import xiangshan._
 import utils._
 
-class RealeaseReq(implicit p: Parameters) extends ICacheBundle{
+class ReleaseReq(implicit p: Parameters) extends ICacheBundle{
   val addr = UInt(PAddrBits.W)
-  val vaddr = UInt(VAddrBits.W)
   val param  = UInt(TLPermissions.cWidth.W)
   val voluntary = Bool()
   val hasData = Bool()
@@ -18,25 +17,23 @@ class RealeaseReq(implicit p: Parameters) extends ICacheBundle{
 }
 
 class ICacheReleaseBundle(implicit p: Parameters) extends  ICacheBundle{
-  val req = Vec(2, Flipped(DecoupledIO(new RealeaseReq)))
+  val req = Vec(2, Flipped(DecoupledIO(new ReleaseReq)))
 }
 
 class RealeaseEntry(edge: TLEdgeOut)(implicit p: Parameters) extends ICacheModule
 {
   val io = IO(new Bundle {
     val id = Input(UInt())
-    val req = Flipped(DecoupledIO(new RealeaseReq))
+    val req = Flipped(DecoupledIO(new ReleaseReq))
 
     val mem_release = DecoupledIO(new TLBundleC(edge.bundle))
     val mem_grant = Flipped(DecoupledIO(new TLBundleD(edge.bundle)))
-
-    val release_meta_write = DecoupledIO(new ICacheMetaWriteBundle)
   })
 
   val s_invalid :: s_release_req :: s_release_resp :: s_meta_write :: Nil = Enum(4)
   val state = RegInit(s_invalid)
 
-  val req  = Reg(new RealeaseReq)
+  val req  = Reg(new ReleaseReq)
 
   // internal regs
   // remaining beats
@@ -49,8 +46,6 @@ class RealeaseEntry(edge: TLEdgeOut)(implicit p: Parameters) extends ICacheModul
 
   io.req.ready := state === s_invalid
   io.mem_grant.ready := false.B
-  io.release_meta_write.bits.generate(tag = get_phy_tag(req.addr), coh = ClientMetadata.onReset, idx = get_idx(req.vaddr), waymask = req.waymask, bankIdx = get_idx(req.vaddr)(0))
-
 
   when (io.req.fire()) {
     req        := io.req.bits
@@ -95,35 +90,20 @@ class RealeaseEntry(edge: TLEdgeOut)(implicit p: Parameters) extends ICacheModul
   when (state === s_release_resp) {
     io.mem_grant.ready := true.B
     when (io.mem_grant.fire()) {
-      state := Mux(req.voluntary,s_meta_write,s_invalid)
-    }
-  }
-
-  assert((io.mem_release.fire() && io.mem_release.bits.data =/= 0.U && !req.voluntary) || !io.mem_release.fire() || (io.mem_release.fire() && req.voluntary))
-
-
-
-  when(state === s_meta_write) {
-    when(io.release_meta_write.fire()){
       state := s_invalid
     }
   }
 
-
-  io.release_meta_write.valid := (state === s_meta_write) && req.voluntary
+  //assert((io.mem_release.fire() && io.mem_release.bits.data =/= 0.U && !req.voluntary) || !io.mem_release.fire() || (io.mem_release.fire() && req.voluntary))
 
 }
 
 class ReleaseUnit(edge: TLEdgeOut)(implicit p: Parameters) extends ICacheModule
 {
   val io = IO(new Bundle {
-    val req = Vec(2, Flipped(DecoupledIO(new RealeaseReq)))
+    val req = Vec(2, Flipped(DecoupledIO(new ReleaseReq)))
     val mem_release = DecoupledIO(new TLBundleC(edge.bundle))
     val mem_grant = Flipped(DecoupledIO(new TLBundleD(edge.bundle)))
-
-    val release_meta_write = DecoupledIO(new ICacheMetaWriteBundle)
-
-    val probe_slot = Vec(cacheParams.nProbeEntries, Flipped(Valid(new ProbeSlot)))
   })
 
   val req = io.req
@@ -131,8 +111,6 @@ class ReleaseUnit(edge: TLEdgeOut)(implicit p: Parameters) extends ICacheModule
   io.mem_release.valid := false.B
   io.mem_release.bits  := DontCare
   io.mem_grant.ready   := false.B
-
-  val meta_write_arb = Module(new Arbiter(new ICacheMetaWriteBundle,  cacheParams.nReleaseEntries))
 
   val entries = (0 until cacheParams.nReleaseEntries) map { i =>
     val entry = Module(new RealeaseEntry(edge))
@@ -144,7 +122,6 @@ class ReleaseUnit(edge: TLEdgeOut)(implicit p: Parameters) extends ICacheModule
     entry.io.req.bits  := io.req(i).bits
     io.req(i).ready    := entry.io.req.ready
 
-    meta_write_arb.io.in(i) <> entry.io.release_meta_write
 
     entry.io.mem_grant.valid := (i.U === io.mem_grant.bits.source) && io.mem_grant.valid
     entry.io.mem_grant.bits  := io.mem_grant.bits
@@ -154,8 +131,6 @@ class ReleaseUnit(edge: TLEdgeOut)(implicit p: Parameters) extends ICacheModule
 
     entry
   }
-
-  io.release_meta_write <> meta_write_arb.io.out
 
   TLArbiter.robin(edge, io.mem_release, entries.map(_.io.mem_release):_*)
 
