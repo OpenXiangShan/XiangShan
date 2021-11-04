8798f355d502059bb429b9207d002db1c27d126a
JinYue <jinyue20s@ict.ac.cn>
2021-11-04 20:53:46 +0800

    ReleaseUnit: add probe merge status register

--- src/main/scala/xiangshan/frontend/icache/ReleaseUnit.scala
+++ src/main/scala/xiangshan/frontend/icache/ReleaseUnit.scala
@@ -9,6 +9,7 @@ import utils._
 
 class ReleaseReq(implicit p: Parameters) extends ICacheBundle{
   val addr = UInt(PAddrBits.W)
+  val vidx  = UInt(idxBits.W)
   val param  = UInt(TLPermissions.cWidth.W)
   val voluntary = Bool()
   val hasData = Bool()
@@ -28,12 +29,18 @@ class RealeaseEntry(edge: TLEdgeOut)(implicit p: Parameters) extends ICacheModul
 
     val mem_release = DecoupledIO(new TLBundleC(edge.bundle))
     val mem_grant = Flipped(DecoupledIO(new TLBundleD(edge.bundle)))
+    val probeMerge  = Input(new ICacheVictimInfor)
+    val probeMergeFix = Output(Bool())
+
+    val release_meta_write = DecoupledIO(new ICacheMetaWriteBundle)
   })
 
   val s_invalid :: s_release_req :: s_release_resp :: s_meta_write :: Nil = Enum(4)
   val state = RegInit(s_invalid)
 
   val req  = Reg(new ReleaseReq)
+  val req_ptag = get_phy_tag(req.addr)
+  val req_idx  = req.vidx
 
   // internal regs
   // remaining beats
@@ -49,7 +56,7 @@ class RealeaseEntry(edge: TLEdgeOut)(implicit p: Parameters) extends ICacheModul
 
   when (io.req.fire()) {
     req        := io.req.bits
-    remain_set := Mux(io.req.bits.hasData, ~0.U(refillCycles.W), 1.U(refillCycles.W))
+    remain_set := ~0.U(refillCycles.W)
     state      := s_release_req
   }
 
@@ -74,14 +81,19 @@ class RealeaseEntry(edge: TLEdgeOut)(implicit p: Parameters) extends ICacheModul
     shrinkPermissions = req.param
   )._2
 
-  io.mem_release.valid := busy
+  val needMergeProbe = (io.probeMerge.valid && state === s_release_req && io.probeMerge.ptag === req_ptag && io.probeMerge.vidx === req_idx)
+
+  io.mem_release.valid := Mux(!req.voluntary && req.hasData, busy,  state === s_release_req && !needMergeProbe)
   io.mem_release.bits  := Mux(!req.voluntary, probeResponseData,voluntaryRelease)
+
 
   when (io.mem_release.fire()) { remain_clr := PriorityEncoderOH(remain) }
 
   val (_, _, release_done, _) = edge.count(io.mem_release)
 
-  when (state === s_release_req && release_done) {
+  when(state === s_release_req && needMergeProbe && req.voluntary){
+    state := s_invalid
+  }.elsewhen (state === s_release_req && release_done) {
     state := Mux(req.voluntary, s_release_resp, s_invalid)
   }
 
@@ -90,11 +102,30 @@ class RealeaseEntry(edge: TLEdgeOut)(implicit p: Parameters) extends ICacheModul
   when (state === s_release_resp) {
     io.mem_grant.ready := true.B
     when (io.mem_grant.fire()) {
-      state := s_invalid
+      state := s_invalid//Mux(req.voluntary,s_meta_write,s_invalid)
     }
   }
 
-  //assert((io.mem_release.fire() && io.mem_release.bits.data =/= 0.U && !req.voluntary) || !io.mem_release.fire() || (io.mem_release.fire() && req.voluntary))
+  //change release into a ProbeAck
+  //WARNING: no change to param, default TtoN
+  // when(needMergeProbe){
+  //   io.mem_release.bits  := probeResponseData
+  // }
+
+  io.probeMergeFix := needMergeProbe && io.mem_release.fire()
+
+  io.release_meta_write <> DontCare
+
+//  when(state === s_meta_write) {
+//    when(io.release_meta_write.fire()){
+//      state := s_invalid
+//    }
+//  }
+//
+//
+//  io.release_meta_write.valid := false.B//(state === s_meta_write) && req.voluntary
+//  io.release_meta_write.bits.generate(tag = get_phy_tag(req.addr), coh = ClientMetadata.onReset, idx = req.vidx, waymask = req.waymask, bankIdx = req.vidx(0))
+
 
 }
 
@@ -104,6 +135,9 @@ class ReleaseUnit(edge: TLEdgeOut)(implicit p: Parameters) extends ICacheModule
     val req = Vec(2, Flipped(DecoupledIO(new ReleaseReq)))
     val mem_release = DecoupledIO(new TLBundleC(edge.bundle))
     val mem_grant = Flipped(DecoupledIO(new TLBundleD(edge.bundle)))
+    val probeMerge  = Flipped(ValidIO(new ICacheVictimInfor))
+
+    val release_meta_write = DecoupledIO(new ICacheMetaWriteBundle)
   })
 
   val req = io.req
@@ -111,6 +145,20 @@ class ReleaseUnit(edge: TLEdgeOut)(implicit p: Parameters) extends ICacheModule
   io.mem_release.valid := false.B
   io.mem_release.bits  := DontCare
   io.mem_grant.ready   := false.B
+
+  val probeMerge = RegInit(0.U.asTypeOf(new ICacheVictimInfor))
+  val probeMergeFix = VecInit(Seq.fill(2)(WireInit(false.B)))
+  val meta_write_arb = Module(new Arbiter(new ICacheMetaWriteBundle,  cacheParams.nReleaseEntries))
+
+  // when(io.probeMerge.valid){
+  //   probeMerge.ptag   := io.probeMerge.bits.ptag
+  //   probeMerge.vidx   := io.probeMerge.bits.vidx
+  //   probeMerge.valid  := true.B
+  // }
+
+  // when(probeMergeFix.reduce(_||_)){
+  //   probeMerge.valid := false.B
+  // }
 
   val entries = (0 until cacheParams.nReleaseEntries) map { i =>
     val entry = Module(new RealeaseEntry(edge))
@@ -122,6 +170,10 @@ class ReleaseUnit(edge: TLEdgeOut)(implicit p: Parameters) extends ICacheModule
     entry.io.req.bits  := io.req(i).bits
     io.req(i).ready    := entry.io.req.ready
 
+    entry.io.probeMerge := probeMerge
+    probeMergeFix(i)    := entry.io.probeMergeFix
+
+    meta_write_arb.io.in(i) <> entry.io.release_meta_write
 
     entry.io.mem_grant.valid := (i.U === io.mem_grant.bits.source) && io.mem_grant.valid
     entry.io.mem_grant.bits  := io.mem_grant.bits
@@ -129,8 +181,10 @@ class ReleaseUnit(edge: TLEdgeOut)(implicit p: Parameters) extends ICacheModule
       io.mem_grant.ready := entry.io.mem_grant.ready
     }
 
+
     entry
   }
+  io.release_meta_write <> meta_write_arb.io.out
 
   TLArbiter.robin(edge, io.mem_release, entries.map(_.io.mem_release):_*)
 
