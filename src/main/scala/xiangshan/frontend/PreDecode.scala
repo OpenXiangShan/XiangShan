444835e0359ab7deef2f0de5e47d102f57019d36
JinYue <jinyue20s@ict.ac.cn>
2021-11-11 16:59:22 +0800

    PreDecode: fix beyondFetch and taken to wrong target bug
    * assuming that when fallThrough - start = 34B, oversize must be 1

--- src/main/scala/xiangshan/frontend/PreDecode.scala
+++ src/main/scala/xiangshan/frontend/PreDecode.scala
@@ -209,7 +209,7 @@ class PreDecode(implicit p: Parameters) extends XSModule with HasPdConst{
   val jumpIsRVC               =  rvcOH(jumpOffset)
   val jumpNextPC              =  jumpPC + Mux(jumpIsRVC, 2.U, 4.U)
   val (hasFalseHit, hasJump)  =  (ParallelOR(falseHit), ParallelOR(jumpOH))
-  val endRange                =  ((Fill(PredictWidth, 1.U(1.W)) >> (~getBasicBlockIdx(realEndPC, pcStart))) | (Fill(PredictWidth, oversize || beyondFetch)))
+  val endRange                =  ((Fill(PredictWidth, 1.U(1.W)) >> (~getBasicBlockIdx(realEndPC, pcStart))) | (Fill(PredictWidth, oversize)))
   val takeRange               =  Fill(PredictWidth, !ParallelOR(takens))   | Fill(PredictWidth, 1.U(1.W)) >> (~PriorityEncoder(takens))
   val fixCross                =  ((pcStart + (FetchWidth * 4).U) > nextLinePC || (pcStart + (FetchWidth * 4).U) === nextLinePC) && !isDoubleLine
   val boundPC                 =  Mux(fixCross, nextLinePC - 2.U  ,pcStart + (FetchWidth * 4).U)
