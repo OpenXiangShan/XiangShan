cbe9a847e233bbea87e2323dec94801d76742d57
Yinan Xu <xuyinan@ict.ac.cn>
2021-11-12 22:24:01 +0800

    difftest: add basic difftest features for releases (#1219)
    * difftest: add basic difftest features for releases
    
    This commit adds basic difftest features for every release, no matter
    it's for simulation or physical design. The macro SYNTHESIS is used to
    skip these logics when synthesizing the design. This commit aims at
    allowing designs for physical design to be verified.
    
    * bump ready-to-run
    
    * difftest: add int and fp writeback data

--- src/main/scala/xiangshan/Parameters.scala
+++ src/main/scala/xiangshan/Parameters.scala
@@ -230,6 +230,7 @@ case class DebugOptions
 (
   FPGAPlatform: Boolean = false,
   EnableDifftest: Boolean = false,
+  AlwaysBasicDiff: Boolean = true,
   EnableDebug: Boolean = false,
   EnablePerfDebug: Boolean = true,
   UseDRAMSim: Boolean = false
