2fe6ef0f877c05b632e6b9f31ac7768675d2a211
William Wang <zeweiwang@outlook.com>
2021-11-16 16:00:20 +0800

    sbuffer: optimize forward mask gen timing

--- src/main/scala/xiangshan/mem/sbuffer/Sbuffer.scala
+++ src/main/scala/xiangshan/mem/sbuffer/Sbuffer.scala
@@ -511,19 +511,26 @@ class Sbuffer(implicit p: Parameters) extends DCacheModule with HasSbufferConst 
     val valid_tag_match_reg = valid_tag_matches.map(RegNext(_))
     val inflight_tag_match_reg = inflight_tag_matches.map(RegNext(_))
     val line_offset_reg = RegNext(line_offset_mask)
+    val forward_mask_candidate_reg = RegEnable(
+      VecInit(mask.map(entry => entry(getWordOffset(forward.paddr)))),
+      forward.valid
+    )
     val forward_data_candidate_reg = RegEnable(
       VecInit(data.map(entry => entry(getWordOffset(forward.paddr)))),
       forward.valid
     )
 
-    val selectedValidMask = Mux1H(line_offset_reg, Mux1H(valid_tag_match_reg, mask).asTypeOf(Vec(CacheLineWords, Vec(DataBytes, Bool()))))
+    val selectedValidMask = Mux1H(valid_tag_match_reg, forward_mask_candidate_reg)
     val selectedValidData = Mux1H(valid_tag_match_reg, forward_data_candidate_reg)
+    selectedValidMask.suggestName("selectedValidMask_"+i)
     selectedValidData.suggestName("selectedValidData_"+i)
 
-    val selectedInflightMask = Mux1H(line_offset_reg, Mux1H(inflight_tag_match_reg, mask).asTypeOf(Vec(CacheLineWords, Vec(DataBytes, Bool()))))
+    val selectedInflightMask = Mux1H(inflight_tag_match_reg, forward_mask_candidate_reg)
     val selectedInflightData = Mux1H(inflight_tag_match_reg, forward_data_candidate_reg)
+    selectedInflightMask.suggestName("selectedInflightMask_"+i)
     selectedInflightData.suggestName("selectedInflightData_"+i)
 
+    // currently not being used
     val selectedInflightMaskFast = Mux1H(line_offset_mask, Mux1H(inflight_tag_matches, mask).asTypeOf(Vec(CacheLineWords, Vec(DataBytes, Bool()))))
     val selectedValidMaskFast = Mux1H(line_offset_mask, Mux1H(valid_tag_matches, mask).asTypeOf(Vec(CacheLineWords, Vec(DataBytes, Bool()))))
 
