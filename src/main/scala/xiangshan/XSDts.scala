c98e0916a0b21e726d8677f3918ac433e2858377
Jiawei Lin <linjiawei20s@ict.ac.cn>
2021-12-07 07:09:16 +0800

    DTS: add interrupt-controller into cpu (#1298)

--- src/main/scala/xiangshan/XSDts.scala
+++ src/main/scala/xiangshan/XSDts.scala
@@ -84,7 +84,40 @@ trait HasXSDts {
       Description(name, mapping ++ cpuProperties ++ nextLevelCacheProperty ++ tileProperties)
     }
   }
+
+  val intcDevice = new DeviceSnippet {
+    override def parent = Some(device)
+    def describe(): Description = {
+      Description("interrupt-controller", Map(
+        "compatible"           -> "riscv,cpu-intc".asProperty,
+        "interrupt-controller" -> Nil,
+        "#interrupt-cells"     -> 1.asProperty))
+    }
+  }
+
   ResourceBinding {
     Resource(device, "reg").bind(ResourceAddress(coreParams.HartId))
+    val int_resources = (
+      clint_int_sink.edges.in.flatMap(_.source.sources) ++
+      plic_int_sink.edges.in.flatMap(_.source.sources) ++
+      debug_int_sink.edges.in.flatMap(_.source.sources)
+      ).flatMap {
+      s =>
+        println(s.resources.map(_.key), s.range)
+        (s.range.start until s.range.`end`).map(_ => s.resources)
+    }
+    val int_ids = Seq(
+      3,    // msip  [clint]
+      7,    // mtip  [clint]
+      11,   // meip  [plic]
+      9,    // seip  [plic]
+      65535 // debug [debug]
+    )
+    assert(int_resources.size == int_ids.size)
+    for((resources, id) <- int_resources.zip(int_ids)){
+      for(r <- resources){
+        r.bind(intcDevice, ResourceInt(id))
+      }
+    }
   }
 }
