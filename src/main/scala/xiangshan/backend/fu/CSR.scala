dacedae578c02dbbe82a1be2cc9ce6956c5f8f40
William Wang <zeweiwang@outlook.com>
2021-12-16 16:01:01 +0800

    mem,csr: enable cache error by default

--- src/main/scala/xiangshan/backend/fu/CSR.scala
+++ src/main/scala/xiangshan/backend/fu/CSR.scala
@@ -497,13 +497,20 @@ class CSR(implicit p: Parameters) extends FunctionUnit with HasCSRConst with PMP
   val smblockctl_init_val =
     ("hf".U & StoreBufferThreshold.U) |
     (EnableLdVioCheckAfterReset.B.asUInt << 4) |
-    GenMask(5) // enable soft prefetch by default
+    (EnableSoftPrefetchAfterReset.B.asUInt << 5) |
+    (EnableCacheErrorAfterReset.B.asUInt << 6)
   val smblockctl = RegInit(UInt(XLEN.W), smblockctl_init_val)
   csrio.customCtrl.sbuffer_threshold := smblockctl(3, 0)
   // bits 4: enable load load violation check
   csrio.customCtrl.ldld_vio_check_enable := smblockctl(4)
   csrio.customCtrl.soft_prefetch_enable := smblockctl(5)
   csrio.customCtrl.cache_error_enable := smblockctl(6)
+
+  println("CSR smblockctl init value:")
+  println("  Store buffer replace threshold: " + StoreBufferThreshold)
+  println("  Enable ld-ld vio check after reset: " + EnableLdVioCheckAfterReset)
+  println("  Enable soft prefetch after reset: " + EnableSoftPrefetchAfterReset)
+  println("  Enable cache error after reset: " + EnableCacheErrorAfterReset)
 
   val srnctl = RegInit(UInt(XLEN.W), "h3".U)
   csrio.customCtrl.move_elim_enable := srnctl(0)
