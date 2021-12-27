1eb5d8ca096bb563f3cdbee9c15604828b565cca
rvcoresjw <shangjiawei@rvcore.com>
2021-12-27 09:54:46 +0800

    add tl pma buffer for timing

--- src/main/scala/system/SoC.scala
+++ src/main/scala/system/SoC.scala
@@ -288,7 +288,10 @@ class SoCMisc()(implicit p: Parameters) extends BaseSoC
   }
 
   val pma = LazyModule(new TLPMA)
-  pma.node := peripheralXbar
+  pma.node := 
+    AXI4Buffer() :=
+    AXI4Buffer() :=
+    peripheralXbar
 
   lazy val module = new LazyModuleImp(this){
 
