1ac72f94d950d75fac8934f53941880d56ea1f45
Yinan Xu <xuyinan@ict.ac.cn>
2022-04-28 21:20:54 +0800

    core,rob: support the WFI instruction
    The RISC-V WFI instruction is previously decoded as NOP. This commit
    adds support for the real wait-for-interrupt (WFI).
    
    We add a state_wfi FSM in the ROB. After WFI leaves the ROB, the next
    instruction will wait in the ROB until an interrupt.

--- src/main/scala/xiangshan/XSCore.scala
+++ src/main/scala/xiangshan/XSCore.scala
@@ -243,6 +243,7 @@ class XSCoreImp(outer: XSCoreBase) extends LazyModuleImp(outer)
   with HasSoCParameter {
   val io = IO(new Bundle {
     val hartId = Input(UInt(64.W))
+    val cpu_halt = Output(Bool())
     val l2_pf_enable = Output(Bool())
     val perfEvents = Input(Vec(numPCntHc * coreParams.L2NBanks, new PerfEvent))
     val beu_errors = Output(new XSL1BusErrors())
@@ -262,6 +263,8 @@ class XSCoreImp(outer: XSCoreBase) extends LazyModuleImp(outer)
   exuBlocks.foreach(_.io.hartId := io.hartId)
   memBlock.io.hartId := io.hartId
   outer.wbArbiter.module.io.hartId := io.hartId
+
+  io.cpu_halt := ctrlBlock.io.cpu_halt
 
   outer.wbArbiter.module.io.redirect <> ctrlBlock.io.redirect
   val allWriteback = exuBlocks.flatMap(_.io.fuWriteback) ++ memBlock.io.writeback
--- src/main/scala/xiangshan/package.scala
+++ src/main/scala/xiangshan/package.scala
@@ -190,6 +190,7 @@ package object xiangshan {
     def wrt  = "b001".U
     def set  = "b010".U
     def clr  = "b011".U
+    def wfi  = "b100".U
     def wrti = "b101".U
     def seti = "b110".U
     def clri = "b111".U
