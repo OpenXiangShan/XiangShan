d978640aa5b6af147330b11aaa99b94c1fd36541
Yinan Xu <xuyinan@ict.ac.cn>
2022-06-21 10:46:09 +0800

    config: use 512KB/2MB L2/L3 in DefaultConfig

--- src/main/scala/top/Configs.scala
+++ src/main/scala/top/Configs.scala
@@ -299,8 +299,8 @@ class MediumConfig(n: Int = 1) extends Config(
 )
 
 class DefaultConfig(n: Int = 1) extends Config(
-  new WithNKBL3(3 * 1024, inclusive = false, banks = 4, ways = 6)
-    ++ new WithNKBL2(2 * 512, inclusive = false, banks = 4, alwaysReleaseData = true)
+  new WithNKBL3(2 * 1024, inclusive = false, banks = 4, ways = 8)
+    ++ new WithNKBL2(512, inclusive = false, banks = 2, alwaysReleaseData = true)
     ++ new WithNKBL1D(128)
     ++ new BaseConfig(n)
 )
