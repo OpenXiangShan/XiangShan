ee5099c9a410ea5889c0f9198a2e624fb67d8e71
William Wang <zeweiwang@outlook.com>
2022-07-01 16:22:32 +0800

    lq: do not use refill mask to select wb entry
    It will add l1 dcache miss latency by 1 cycle

--- src/main/scala/xiangshan/mem/lsqueue/LoadQueue.scala
+++ src/main/scala/xiangshan/mem/lsqueue/LoadQueue.scala
@@ -330,7 +330,8 @@ class LoadQueue(implicit p: Parameters) extends XSModule
   val loadWbSelV = Wire(Vec(LoadPipelineWidth, Bool())) // index selected in last cycle is valid
 
   val loadWbSelVec = VecInit((0 until LoadQueueSize).map(i => {
-    allocated(i) && !writebacked(i) && (datavalid(i) || refilling(i))
+    // allocated(i) && !writebacked(i) && (datavalid(i) || refilling(i))
+    allocated(i) && !writebacked(i) && datavalid(i) // query refilling will cause bad timing
   })).asUInt() // use uint instead vec to reduce verilog lines
   val remDeqMask = Seq.tabulate(LoadPipelineWidth)(getRemBits(deqMask)(_))
   // generate lastCycleSelect mask
