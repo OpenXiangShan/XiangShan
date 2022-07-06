5059eed44500e985011923b3e0de8ca7c5bc373c
Jenius <jinyue20s@ict.ac.cn>
2022-07-06 18:09:29 +0800

    <bug-fix> fix page fault cause fetch finish bug

--- src/main/scala/xiangshan/frontend/icache/ICacheMainPipe.scala
+++ src/main/scala/xiangshan/frontend/icache/ICacheMainPipe.scala
@@ -538,10 +538,11 @@ class ICacheMainPipe(implicit p: Parameters) extends ICacheModule
   reservedRefillData(1) := DataHoldBypass(data = missSlot(1).m_data, valid = miss_1_s2_0 || miss_1_s2_1)
 
   /*** miss state machine ***/
+  def only_pmp_af(portNum: Int) =  s2_except_pmp_af(portNum) && !s2_port_hit(portNum) && !s2_except(portNum)
 
   switch(wait_state){
     is(wait_idle){
-      when((s2_except_pmp_af(0) && !s2_port_hit(0)) || (s2_except_pmp_af(1) && !s2_port_hit(1)) || s2_mmio){
+      when(only_pmp_af(0) || only_pmp_af(1) || s2_mmio){
         //should not send req to MissUnit when there is an access exception in PMP
         //But to avoid using pmp exception in control signal (like s2_fire), should delay 1 cycle.
         //NOTE: pmp exception cache line also could hit in ICache, but the result is meaningless. Just give the exception signals.
