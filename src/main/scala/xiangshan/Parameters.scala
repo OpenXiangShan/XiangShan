4dfcec8ee6466d680534a70a93dff7bb8ff13d71
William Wang <zeweiwang@outlook.com>
2022-07-27 19:04:21 +0800

    ldu: disable load to load forward
    Imm read from rs causes timing problem. Load to load will
    be disabled until we find a way to fix imm read problem

--- src/main/scala/xiangshan/Parameters.scala
+++ src/main/scala/xiangshan/Parameters.scala
@@ -161,7 +161,7 @@ case class XSCoreParameters
   StorePipelineWidth: Int = 2,
   StoreBufferSize: Int = 16,
   StoreBufferThreshold: Int = 7,
-  EnableLoadToLoadForward: Boolean = true,
+  EnableLoadToLoadForward: Boolean = false,
   EnableFastForward: Boolean = false,
   EnableLdVioCheckAfterReset: Boolean = true,
   EnableSoftPrefetchAfterReset: Boolean = true,
