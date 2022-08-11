810034e463575a49a4acb89f54640a2824fb1571
ZhangZifei <1773908404@qq.com>
2022-08-11 21:22:43 +0800

    l2tlb: rm wrong assert 2

--- src/main/scala/xiangshan/cache/mmu/L2TLB.scala
+++ src/main/scala/xiangshan/cache/mmu/L2TLB.scala
@@ -278,7 +278,6 @@ class PTWImp(outer: PTW)(implicit p: Parameters) extends PtwModule(outer) with H
 
   llptw_out.ready := outReady(llptw_out.bits.req_info.source, outArbMqPort)
   for (i <- 0 until PtwWidth) {
-    XSError(outArb(i).out.valid && !outArb(i).out.ready, "L2TLB resp but tlb not ready")
     outArb(i).in(outArbCachePort).valid := cache.io.resp.valid && cache.io.resp.bits.hit && cache.io.resp.bits.req_info.source===i.U
     outArb(i).in(outArbCachePort).bits.entry := cache.io.resp.bits.toTlb
     outArb(i).in(outArbCachePort).bits.pf := !cache.io.resp.bits.toTlb.v
