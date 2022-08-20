e5eb6595356dead7509daf1778d3e4a0a2bd40a6
Yinan Xu <xuyinan@ict.ac.cn>
2022-08-20 14:46:03 +0800

    scripts: copy simv when vcs aborts

--- scripts/xiangshan.py
+++ scripts/xiangshan.py
@@ -380,8 +380,7 @@ class XiangShan(object):
             if ret:
                 if self.args.default_wave_home != self.args.wave_home:
                     print("copy wave file to " + self.args.wave_home)
-                    self.__exec_cmd(f"cp $NOOP_HOME/build/*.vcd $WAVE_HOME")
-                    self.__exec_cmd(f"cp $NOOP_HOME/build/emu $WAVE_HOME")
+                    self.__exec_cmd(f"cp $NOOP_HOME/difftest/simv $WAVE_HOME")
                     self.__exec_cmd(f"cp $NOOP_HOME/build/SimTop.v $WAVE_HOME")
                 return ret
         return 0
