f9329730ecb34f71f9362ae12ae563f86f534bf7
ZhangZifei <1773908404@qq.com>
2022-08-20 23:33:58 +0800

    dtlb: move pmp check's ParallelMux to next cycle

--- src/main/scala/xiangshan/backend/MemBlock.scala
+++ src/main/scala/xiangshan/backend/MemBlock.scala
@@ -223,7 +223,7 @@ class MemBlockImp(outer: MemBlock) extends LazyModuleImp(outer)
   val pmp = Module(new PMP())
   pmp.io.distribute_csr <> csrCtrl.distribute_csr
 
-  val pmp_check = VecInit(Seq.fill(exuParameters.LduCnt + exuParameters.StuCnt)(Module(new PMPChecker(3)).io))
+  val pmp_check = VecInit(Seq.fill(exuParameters.LduCnt + exuParameters.StuCnt)(Module(new PMPChecker(3, leaveHitMux = true)).io))
   val tlbcsr_pmp = tlbcsr_dup.drop(2).map(RegNext(_))
   for (((p,d),i) <- (pmp_check zip dtlb_pmps) zipWithIndex) {
     p.apply(tlbcsr_pmp(i).priv.dmode, pmp.io.pmp, pmp.io.pma, d)
