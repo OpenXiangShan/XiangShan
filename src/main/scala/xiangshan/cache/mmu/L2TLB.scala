69d6090f035fbd9976bc418c2b569b71bf845370
wangkaifan <wangkaifan@ict.ac.cn>
2022-11-16 19:03:17 +0800

    ptw: setup prefercache ctrl-bits to decide user bits of Get in PTW

--- src/main/scala/xiangshan/cache/mmu/L2TLB.scala
+++ src/main/scala/xiangshan/cache/mmu/L2TLB.scala
@@ -26,6 +26,7 @@ import xiangshan.cache.{HasDCacheParameters, MemoryOpConstants}
 import utils._
 import freechips.rocketchip.diplomacy.{IdRange, LazyModule, LazyModuleImp}
 import freechips.rocketchip.tilelink._
+import huancun.PreferCacheKey
 import xiangshan.backend.fu.{PMP, PMPChecker, PMPReqBundle, PMPRespBundle}
 import xiangshan.backend.fu.util.HasCSRConst
 
@@ -240,6 +241,7 @@ class PTWImp(outer: PTW)(implicit p: Parameters) extends PtwModule(outer) with H
   )._2
   mem.a.bits := memRead
   mem.a.valid := mem_arb.io.out.valid && !flush
+  mem.a.bits.user.lift(PreferCacheKey).foreach(_ := RegNext(io.csr.prefercache, true.B))
   mem.d.ready := true.B
   // mem -> data buffer
   val refill_data = Reg(Vec(blockBits / l1BusDataWidth, UInt(l1BusDataWidth.W)))
