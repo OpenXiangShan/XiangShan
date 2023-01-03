d3119b873cc64dbb60e9c953aa4abcad323437d8
good-circle <fenghaoyuan19@mails.ucas.ac.cn>
2023-01-03 16:22:29 +0800

    PTW: Fix bug when resp valid but not fire

--- src/main/scala/xiangshan/cache/mmu/PageTableWalker.scala
+++ src/main/scala/xiangshan/cache/mmu/PageTableWalker.scala
@@ -95,7 +95,8 @@ class PTW()(implicit p: Parameters) extends XSModule with HasPtwConst with HasPe
   val mem_addr_update = RegInit(false.B)
 
   val idle = RegInit(true.B)
-  val sent_to_pmp = idle === false.B && (s_pmp_check === false.B || mem_addr_update)
+  val finish = WireInit(false.B)
+  val sent_to_pmp = idle === false.B && (s_pmp_check === false.B || mem_addr_update) && !finish
 
   val pageFault = memPte.isPf(level)
   val accessFault = RegEnable(io.pmp.resp.ld || io.pmp.resp.mmio, sent_to_pmp)
@@ -162,7 +163,7 @@ class PTW()(implicit p: Parameters) extends XSModule with HasPtwConst with HasPe
     w_mem_resp := false.B
   }
 
-  when(mem.resp.fire() &&  w_mem_resp === false.B){
+  when(mem.resp.fire() && w_mem_resp === false.B){
     w_mem_resp := true.B
     af_level := af_level + 1.U
     s_llptw_req := false.B
@@ -170,20 +171,26 @@ class PTW()(implicit p: Parameters) extends XSModule with HasPtwConst with HasPe
   }
 
   when(mem_addr_update){
-    when(level === 0.U && !(find_pte||accessFault)){
+    when(level === 0.U && !(find_pte || accessFault)){
       level := levelNext
       s_mem_req := false.B
       s_llptw_req := true.B
       mem_addr_update := false.B
-    }.elsewhen(io.llptw.fire()){
-      idle := true.B
-      s_llptw_req := true.B
-      mem_addr_update := false.B
-    }.elsewhen(io.resp.fire()){
-      idle := true.B
-      s_llptw_req := true.B
-      mem_addr_update := false.B
-      accessFault := false.B
+    }.elsewhen(io.llptw.valid){
+      when(io.llptw.fire()) {
+        idle := true.B
+        s_llptw_req := true.B
+        mem_addr_update := false.B
+      }
+      finish := true.B
+    }.elsewhen(io.resp.valid){
+      when(io.resp.fire()) {
+        idle := true.B
+        s_llptw_req := true.B
+        mem_addr_update := false.B
+        accessFault := false.B
+      }
+      finish := true.B
     }
   }
 
