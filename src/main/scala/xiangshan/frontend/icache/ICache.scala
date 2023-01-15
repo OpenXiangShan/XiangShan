0a9829d7761468f1b8492c87b5a45a7346fe68b2
huanglingfeng <1335841400@qq.com>
2023-01-15 12:57:49 +0800

    fixed maxIPFMoveConf formula

--- src/main/scala/xiangshan/frontend/icache/ICache.scala
+++ src/main/scala/xiangshan/frontend/icache/ICache.scala
@@ -88,8 +88,7 @@ trait HasICacheParameters extends HasL1CacheParameters with HasInstrMMIOConst wi
 
   def nPrefetchEntries = cacheParams.nPrefetchEntries
   def nIPFBufferSize   = cacheParams.nPrefBufferEntries
-  // def maxIPFMoveConf   = blockBytes/(instBytes*PredictWidth)
-  def maxIPFMoveConf   = 1
+  def maxIPFMoveConf   = blockBytes/(instBytes*PredictWidth)
 
   def generatePipeControl(lastFire: Bool, thisFire: Bool, thisFlush: Bool, lastFlush: Bool): Bool = {
     val valid  = RegInit(false.B)
