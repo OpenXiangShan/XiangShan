4722e8826518d7f2ad09b3933eb6b2ee19177048
William Wang <zeweiwang@outlook.com>
2023-02-02 17:14:23 +0800

    chore: fix minimal config with new prefetch path

--- src/main/scala/top/Configs.scala
+++ src/main/scala/top/Configs.scala
@@ -161,7 +161,8 @@ class MinimalConfig(n: Int = 1) extends Config(
           l3nWays = 8,
           spSize = 2,
         ),
-        L2CacheParamsOpt = None // remove L2 Cache
+        L2CacheParamsOpt = None, // remove L2 Cache
+        prefetcher = None // if L2 pf_recv_node does not exist, disable SMS prefetcher
       )
     )
     case SoCParamsKey =>
