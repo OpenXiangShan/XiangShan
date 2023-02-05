0c1bb10c8aca4f590df3f454ec14053bf9cf8059
Haoyuan Feng <fenghaoyuan19@mails.ucas.ac.cn>
2023-02-05 09:26:25 +0800

    MMU: Add Fake L1 TLB (#1888)

--- src/main/scala/xiangshan/cache/mmu/TLBStorage.scala
+++ src/main/scala/xiangshan/cache/mmu/TLBStorage.scala
@@ -18,11 +18,12 @@ package xiangshan.cache.mmu
 
 import chipsalliance.rocketchip.config.Parameters
 import chisel3._
-import chisel3.experimental.chiselName
+import chisel3.experimental.{ExtModule, chiselName}
 import chisel3.util._
 import utils._
 import utility._
 import freechips.rocketchip.formal.PropertyClass
+import xiangshan.backend.fu.util.HasCSRConst
 
 import scala.math.min
 
@@ -337,6 +338,77 @@ class TLBSA(
   println(s"${parentName} tlb_sa: nSets:${nSets} nWays:${nWays}")
 }
 
+@chiselName
+class TLBFakeSP(
+             ports: Int,
+             nSets: Int,
+             nWays: Int,
+             useDmode: Boolean = false
+           )(implicit p: Parameters) extends TlbModule with HasCSRConst{
+
+  val io = IO(new TlbStorageIO(nSets, nWays, ports))
+  io.r.req.map(_.ready := true.B)
+  val mode = if (useDmode) io.csr.priv.dmode else io.csr.priv.imode
+  val vmEnable = if (EnbaleTlbDebug) (io.csr.satp.mode === 8.U)
+    else (io.csr.satp.mode === 8.U && (mode < ModeM))
+
+  for (i <- 0 until ports) {
+    val req = io.r.req(i)
+    val resp = io.r.resp(i)
+
+    val helper = Module(new PTEHelper())
+    helper.clock := clock
+    helper.satp := io.csr.satp.ppn
+    helper.enable := req.fire && vmEnable
+    helper.vpn := req.bits.vpn
+
+    val pte = helper.pte.asTypeOf(new PteBundle)
+    val ppn = pte.ppn
+    val vpn_reg = RegNext(req.bits.vpn)
+    val pf = helper.pf
+    val level = helper.level
+
+    resp.valid := RegNext(req.valid)
+    resp.bits.hit := true.B
+    resp.bits.perm(0).pf := pf
+    resp.bits.perm(0).af := false.B
+    resp.bits.perm(0).d := pte.perm.d
+    resp.bits.perm(0).a := pte.perm.a
+    resp.bits.perm(0).g := pte.perm.g
+    resp.bits.perm(0).u := pte.perm.u
+    resp.bits.perm(0).x := pte.perm.x
+    resp.bits.perm(0).w := pte.perm.w
+    resp.bits.perm(0).r := pte.perm.r
+    resp.bits.perm(0).pm := DontCare
+
+    resp.bits.ppn(0) := MuxLookup(level, 0.U, Seq(
+      0.U -> Cat(ppn(ppn.getWidth-1, vpnnLen*2), vpn_reg(vpnnLen*2-1, 0)),
+      1.U -> Cat(ppn(ppn.getWidth-1, vpnnLen), vpn_reg(vpnnLen-1, 0)),
+      2.U -> ppn)
+    )
+  }
+
+  io.access := DontCare
+  io.victim.out := DontCare
+
+}
+
+@chiselName
+class TLBFakeNP(
+             ports: Int,
+             nDups: Int,
+             nSets: Int,
+             nWays: Int
+           )(implicit p: Parameters) extends TlbModule {
+
+  val io = IO(new TlbStorageIO(nSets, nWays, ports, nDups))
+
+  io.r.req.map(_.ready :=  true.B)
+  io.r.resp := DontCare
+  io.access := DontCare
+  io.victim.out := DontCare
+}
+
 object TlbStorage {
   def apply
   (
@@ -348,16 +420,30 @@ object TlbStorage {
     nWays: Int,
     saveLevel: Boolean = false,
     normalPage: Boolean,
-    superPage: Boolean
+    superPage: Boolean,
+    useDmode: Boolean,
+    SoftTLB: Boolean
   )(implicit p: Parameters) = {
-    if (associative == "fa") {
-       val storage = Module(new TLBFA(parentName, ports, nSets, nWays, saveLevel, normalPage, superPage))
-       storage.suggestName(s"${parentName}_fa")
-       storage.io
+    if (SoftTLB) {
+      if (superPage == true) {
+        val storage = Module(new TLBFakeSP(ports, nSets, nWays, useDmode))
+        storage.suggestName(s"${parentName}_fakesp")
+        storage.io
+      } else {
+        val storage = Module(new TLBFakeNP(ports, nDups, nSets, nWays))
+        storage.suggestName(s"${parentName}_fakenp")
+        storage.io
+      }
     } else {
-       val storage = Module(new TLBSA(parentName, ports, nDups, nSets, nWays, normalPage, superPage))
-       storage.suggestName(s"${parentName}_sa")
-       storage.io
+      if (associative == "fa") {
+        val storage = Module(new TLBFA(parentName, ports, nSets, nWays, saveLevel, normalPage, superPage))
+        storage.suggestName(s"${parentName}_fa")
+        storage.io
+      } else {
+        val storage = Module(new TLBSA(parentName, ports, nDups, nSets, nWays, normalPage, superPage))
+        storage.suggestName(s"${parentName}_sa")
+        storage.io
+      }
     }
   }
 }
@@ -375,7 +461,9 @@ class TlbStorageWrapper(ports: Int, q: TLBParameters, nDups: Int = 1)(implicit p
     nWays = q.normalNWays,
     saveLevel = q.saveLevel,
     normalPage = true,
-    superPage = false
+    superPage = false,
+    useDmode = q.useDmode,
+    SoftTLB = coreParams.softTLB
   )
   val superPage = TlbStorage(
     parentName = q.name + "_sp_storage",
@@ -385,6 +473,8 @@ class TlbStorageWrapper(ports: Int, q: TLBParameters, nDups: Int = 1)(implicit p
     nWays = q.superNWays,
     normalPage = q.normalAsVictim,
     superPage = true,
+    useDmode = q.useDmode,
+    SoftTLB = coreParams.softTLB
   )
 
   for (i <- 0 until ports) {
