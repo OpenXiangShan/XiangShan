8754ae997f1880b01c6128dbb5cec6495747ec18
guohongyu <20373696@buaa.edu.cn>
2023-03-10 10:31:05 +0800

    ICache: nPrefetchEntrys 2 -> 12

--- src/main/scala/xiangshan/Parameters.scala
+++ src/main/scala/xiangshan/Parameters.scala
@@ -237,7 +237,7 @@ case class XSCoreParameters
     replacer = Some("setplru"),
     nMissEntries = 2,
     nProbeEntries = 2,
-    nPrefetchEntries = 2,
+    nPrefetchEntries = 12,
     nPrefBufferEntries = 64,
     hasPrefetch = true,
   ),
