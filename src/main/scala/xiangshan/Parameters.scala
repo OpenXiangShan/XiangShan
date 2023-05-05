7480fc8fb3a7b7eb52241a2bff89cb00aea11cdc
Maxpicca-Li <maxpicca@qq.com>
2023-05-06 04:47:28 +0800

    exp1: iwpu-utag

--- src/main/scala/xiangshan/Parameters.scala
+++ src/main/scala/xiangshan/Parameters.scala
@@ -176,7 +176,7 @@ case class XSCoreParameters
   ReSelectLen: Int = 6, // load replay queue replay select counter len
   iwpuParameters: WPUParameters = WPUParameters(
     enWPU = true,
-    algoName = "mru",
+    algoName = "utag",
     isICache = true,
   ),
   dwpuParameters: WPUParameters = WPUParameters(
