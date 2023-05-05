21086cdff0dafdfbc12ca6f2ce476490184012ef
Maxpicca-Li <maxpicca@qq.com>
2023-05-06 04:46:55 +0800

    exp1: iwpu-mmru

--- src/main/scala/xiangshan/Parameters.scala
+++ src/main/scala/xiangshan/Parameters.scala
@@ -175,14 +175,14 @@ case class XSCoreParameters
   MMUAsidLen: Int = 16, // max is 16, 0 is not supported now
   ReSelectLen: Int = 6, // load replay queue replay select counter len
   iwpuParameters: WPUParameters = WPUParameters(
-    enWPU = false,
+    enWPU = true,
     algoName = "mmru",
     isICache = true,
   ),
   dwpuParameters: WPUParameters = WPUParameters(
-    enWPU = true,
-    algoName = "mru",
-    enCfPred = true,
+    enWPU = false,
+    algoName = "mmru",
+    enCfPred = false,
     isICache = false,
   ),
   itlbParameters: TLBParameters = TLBParameters(
