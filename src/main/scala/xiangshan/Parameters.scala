047e34f9536258d2008cf19a60dd3e6a9fa1d88a
Maxpicca-Li <maxpicca@qq.com>
2023-05-09 10:50:24 +0800

    Fix constant (#2071)
    * constant: fix dead loop
    
    * util: fix constant dynamic switch
    
    * util: fix constant

--- src/main/scala/xiangshan/Parameters.scala
+++ src/main/scala/xiangshan/Parameters.scala
@@ -287,6 +287,7 @@ case class DebugOptions
   EnableDebug: Boolean = false,
   EnablePerfDebug: Boolean = true,
   UseDRAMSim: Boolean = false,
+  EnableConstantin: Boolean = false,
   EnableTopDown: Boolean = false
 )
 
