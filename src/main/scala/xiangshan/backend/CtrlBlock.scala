ee55449b5ed68b069d1db74655d56d01c5043354
Lyn <lyn@Lyns-MacBook-Pro.local>
2023-05-12 13:18:30 +0800

    remove mdp modify

--- src/main/scala/xiangshan/backend/CtrlBlock.scala
+++ src/main/scala/xiangshan/backend/CtrlBlock.scala
@@ -417,14 +417,11 @@ class CtrlBlockImp(outer: CtrlBlock)(implicit p: Parameters) extends LazyModuleI
   waittable.io.csrCtrl := RegNext(io.csrCtrl)
 
   // LFST lookup and update
-  dispatch.io.lfst := DontCare
-  if (LFSTEnable) {
-    val lfst = Module(new LFST)
-    lfst.io.redirect <> RegNext(io.redirect)
-    lfst.io.storeIssue <> RegNext(io.stIn)
-    lfst.io.csrCtrl <> RegNext(io.csrCtrl)
-    lfst.io.dispatch <> dispatch.io.lfst
-  }
+  val lfst = Module(new LFST)
+  lfst.io.redirect <> RegNext(io.redirect)
+  lfst.io.storeIssue <> RegNext(io.stIn)
+  lfst.io.csrCtrl <> RegNext(io.csrCtrl)
+  lfst.io.dispatch <> dispatch.io.lfst
 
   rat.io.redirect := stage2Redirect.valid
   rat.io.robCommits := rob.io.commits
