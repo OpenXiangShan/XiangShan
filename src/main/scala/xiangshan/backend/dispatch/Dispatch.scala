e6863fd494acf3618f919ca4b8e61a85d4be7af0
Xuan Hu <huxuan@bosc.ac.cn>
2023-05-17 01:39:21 +0800

    dispatch: add vector preg allocation

--- src/main/scala/xiangshan/backend/dispatch/Dispatch.scala
+++ src/main/scala/xiangshan/backend/dispatch/Dispatch.scala
@@ -253,7 +253,7 @@ class Dispatch(implicit p: Parameters) extends XSModule with HasPerfEvents {
     )
 
     io.allocPregs(i).isInt := io.fromRename(i).valid && io.fromRename(i).bits.rfWen && (io.fromRename(i).bits.ldest =/= 0.U) && !io.fromRename(i).bits.eliminatedMove
-    io.allocPregs(i).isFp  := io.fromRename(i).valid && io.fromRename(i).bits.fpWen
+    io.allocPregs(i).isFp  := io.fromRename(i).valid && (io.fromRename(i).bits.fpWen || io.fromRename(i).bits.vecWen)
     io.allocPregs(i).preg  := io.fromRename(i).bits.pdest
   }
   val renameFireCnt = PopCount(io.recv)
