012589d9c35747a611483563c1909542bd4025fd
wangkaifan <wangkaifan@ict.ac.cn>
2023-05-23 12:59:17 +0800

    icache: set icache as non-coherent node

--- src/main/scala/xiangshan/frontend/icache/ICache.scala
+++ src/main/scala/xiangshan/frontend/icache/ICache.scala
@@ -509,8 +509,6 @@ class ICache()(implicit p: Parameters) extends LazyModule with HasICacheParamete
     Seq(TLMasterParameters.v1(
       name = "icache",
       sourceId = IdRange(0, cacheParams.nMissEntries + cacheParams.nPrefetchEntries),
-      supportsProbe = TransferSizes(blockBytes),
-      supportsHint = TransferSizes(blockBytes)
     )),
     requestFields = cacheParams.reqFields,
     echoFields = cacheParams.echoFields
