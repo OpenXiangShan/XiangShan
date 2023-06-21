bac6c8853b07841df7962311d6f8ca9c72837df3
Xuan Hu <huxuan@bosc.ac.cn>
2023-06-21 17:33:33 +0800

    iq: fix valid of WakeUpQueue

--- src/main/scala/xiangshan/backend/issue/IssueQueue.scala
+++ src/main/scala/xiangshan/backend/issue/IssueQueue.scala
@@ -386,7 +386,7 @@ class IssueQueueImp(override val wrapper: IssueQueue)(implicit p: Parameters, va
     wakeUpQueueOption.foreach {
       wakeUpQueue =>
         wakeUpQueue.io.flush := io.flush
-        wakeUpQueue.io.enq.valid := io.deq(i).valid
+        wakeUpQueue.io.enq.valid := io.deq(i).fire
         wakeUpQueue.io.enq.bits.uop := io.deq(i).bits.common
         wakeUpQueue.io.enq.bits.lat := getDeqLat(i, io.deq(i).bits.common.fuType)
     }
