ffc9de54938a9574f465b83a71d5252cfd37cf30
wakafa <wangkaifan@ict.ac.cn>
2023-08-30 20:07:43 +0800

    Support temporal prefetcher (disabled by default) (#2264)
    * bump huancun
    
    * bump coupledL2
    
    * dcache: pass vaddr to coupledL2 through TL bus
    
    * prefetch: support sending pf src towards l2
    
    * bump huancun
    
    * bump coupledL2
    
    * bump utility

--- src/main/scala/xiangshan/mem/prefetch/BasePrefecher.scala
+++ src/main/scala/xiangshan/mem/prefetch/BasePrefecher.scala
@@ -1,16 +1,36 @@
+/***************************************************************************************
+  * Copyright (c) 2020-2021 Institute of Computing Technology, Chinese Academy of Sciences
+  * Copyright (c) 2020-2021 Peng Cheng Laboratory
+  *
+  * XiangShan is licensed under Mulan PSL v2.
+  * You can use this software according to the terms and conditions of the Mulan PSL v2.
+  * You may obtain a copy of Mulan PSL v2 at:
+  *          http://license.coscl.org.cn/MulanPSL2
+  *
+  * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+  * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+  * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+  *
+  * See the Mulan PSL v2 for more details.
+  ***************************************************************************************/
+
 package xiangshan.mem.prefetch
 
 import chisel3._
 import chisel3.util._
 import chipsalliance.rocketchip.config.Parameters
+import utility.MemReqSource
 import xiangshan._
 import xiangshan.cache.mmu.TlbRequestIO
-import xiangshan.mem.{LdPrefetchTrainBundle, L1PrefetchReq}
+import xiangshan.mem.{L1PrefetchReq, LdPrefetchTrainBundle}
 
 class PrefetcherIO()(implicit p: Parameters) extends XSBundle {
   val ld_in = Flipped(Vec(exuParameters.LduCnt, ValidIO(new LdPrefetchTrainBundle())))
   val tlb_req = new TlbRequestIO(nRespDups = 2)
-  val pf_addr = ValidIO(UInt(PAddrBits.W))
+  val l2_req = ValidIO(new Bundle() {
+    val addr = UInt(PAddrBits.W)
+    val source = UInt(MemReqSource.reqSourceBits.W)
+  })
   val l1_req = DecoupledIO(new L1PrefetchReq())
   val enable = Input(Bool())
 }
