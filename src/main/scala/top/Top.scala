34f38695595bef0ac495f3c1c1f0690fbcffa16b
Tang Haojin <tanghaojin@bosc.ac.cn>
2023-09-27 09:24:32 +0800

    Log: remove unexpected `LogPerfHelper` when `RELEASE=1` (#2343)

--- src/main/scala/top/Top.scala
+++ src/main/scala/top/Top.scala
@@ -69,7 +69,10 @@ class XSTop()(implicit p: Parameters) extends BaseXSSoc() with HasSoCParameter
 
   val l3cacheOpt = soc.L3CacheParamsOpt.map(l3param =>
     LazyModule(new HuanCun()(new Config((_, _, _) => {
-      case HCCacheParamsKey => l3param.copy(hartIds = tiles.map(_.HartId))
+      case HCCacheParamsKey => l3param.copy(
+        hartIds = tiles.map(_.HartId),
+        FPGAPlatform = debugOpts.FPGAPlatform
+      )
     })))
   )
 
