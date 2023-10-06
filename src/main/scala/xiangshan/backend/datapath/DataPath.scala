b22e46807327010ef528e04a022b33507c0b3dc0
Haojin Tang <tanghaojin@outlook.com>
2023-10-07 01:53:29 +0800

    DataPath: DontCare `dataInvalidSqIdx` for ogResps

--- src/main/scala/xiangshan/backend/datapath/DataPath.scala
+++ src/main/scala/xiangshan/backend/datapath/DataPath.scala
@@ -337,6 +337,7 @@ class DataPathImp(override val wrapper: DataPath)(implicit p: Parameters, params
           og0FailedVec2(iqIdx)(iuIdx) := fromIQ(iqIdx)(iuIdx).valid && (!fromIQFire(iqIdx)(iuIdx))
           og0resp.valid := og0FailedVec2(iqIdx)(iuIdx)
           og0resp.bits.respType := RSFeedbackType.rfArbitFail
+          og0resp.bits.dataInvalidSqIdx := DontCare
           og0resp.bits.robIdx := fromIQ(iqIdx)(iuIdx).bits.common.robIdx
           og0resp.bits.rfWen := fromIQ(iqIdx)(iuIdx).bits.common.rfWen.getOrElse(false.B)
           og0resp.bits.fuType := fromIQ(iqIdx)(iuIdx).bits.common.fuType
@@ -347,6 +348,7 @@ class DataPathImp(override val wrapper: DataPath)(implicit p: Parameters, params
           og1resp.bits.respType := Mux(!og1FailedVec2(iqIdx)(iuIdx),
             if (toIU.issueQueueParams.isMemAddrIQ) RSFeedbackType.fuUncertain else RSFeedbackType.fuIdle,
             RSFeedbackType.fuBusy)
+          og1resp.bits.dataInvalidSqIdx := DontCare
           og1resp.bits.robIdx := s1_toExuData(iqIdx)(iuIdx).robIdx
           og1resp.bits.rfWen := s1_toExuData(iqIdx)(iuIdx).rfWen.getOrElse(false.B)
           og1resp.bits.fuType := s1_toExuData(iqIdx)(iuIdx).fuType
