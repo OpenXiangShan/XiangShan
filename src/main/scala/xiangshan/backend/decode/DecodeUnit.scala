3ac810de07a47f038c9bb755bd6f59e377efa22d
Ziyue Zhang <zhangziyue21@mails.ucas.edu.cn>
2023-10-26 15:10:45 +0800

    rv64v: add veew in VPUCtrlSignals

--- src/main/scala/xiangshan/backend/decode/DecodeUnit.scala
+++ src/main/scala/xiangshan/backend/decode/DecodeUnit.scala
@@ -808,6 +808,7 @@ class DecodeUnit(implicit p: Parameters) extends XSModule with DecodeUnitConstan
     decodedInst.vpu.vlmul := io.enq.vtype.vlmul
     decodedInst.vpu.vm := inst.VM
     decodedInst.vpu.nf := inst.NF
+    decodedInst.vpu.veew := inst.WIDTH
     decodedInst.vpu.needScalaSrc := Category.needScalaSrc(inst.VCATEGORY)
     decodedInst.vpu.permImmTruncate := Category.permImmTruncate(inst.VCATEGORY)
     decodedInst.vpu.isReverse := needReverseInsts.map(_ === inst.ALL).reduce(_ || _)
