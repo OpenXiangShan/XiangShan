cf041f41398bea1a3726f65fd69976db7022d4ce
xiaofeibao-xjtu <1441675923@qq.com>
2023-10-26 17:01:01 +0800

    CtrlBlock optimize timing: read rat at rename stage, piped walkVtype to decode

--- src/main/scala/xiangshan/backend/CtrlBlock.scala
+++ src/main/scala/xiangshan/backend/CtrlBlock.scala
@@ -236,8 +236,8 @@ class CtrlBlockImp(
   val walkVTypeReverse = rob.io.commits.info.map(info => info.vtype).reverse
   val walkVType = PriorityMux(isVsetSeq, walkVTypeReverse)
 
-  decode.io.walkVType.bits := walkVType.asTypeOf(new VType)
-  decode.io.walkVType.valid := rob.io.commits.isWalk && isVsetSeq.reduce(_ || _)
+  decode.io.walkVType.bits := RegNext(walkVType.asTypeOf(new VType))
+  decode.io.walkVType.valid := RegNext(rob.io.commits.isWalk && isVsetSeq.reduce(_ || _))
 
   decode.io.isRedirect := s1_s3_redirect.valid
 
