59f5c64b9cc2370eb5909aad2dc72840bd434d45
lihuijin <501296508@qq.com>
2023-11-14 15:45:16 +0800

    modify load replay fwd data from tlbundD

--- src/main/scala/xiangshan/cache/dcache/DCacheWrapper.scala
+++ src/main/scala/xiangshan/cache/dcache/DCacheWrapper.scala
@@ -1031,7 +1031,7 @@ class DCacheImp(outer: DCache) extends LazyModuleImp(outer) with HasDCacheParame
 val isKeyword = bus.d.bits.echo.lift(IsKeywordKey).getOrElse(false.B)
   (0 until LoadPipelineWidth).map(i => {
     val (_, _, done, _) = edge.count(bus.d)
-    when(bus.d.bits.opcode === TLMessages.GrantData) {
+    when(bus.d.bits.opcode === TLMessages.GrantData && done) {
       io.lsu.forward_D(i).apply(bus.d.valid, bus.d.bits.data, bus.d.bits.source, !bus.d.bits.echo.lift(IsKeywordKey).getOrElse(false.B))
    //   io.lsu.forward_D(i).apply(bus.d.valid, bus.d.bits.data, bus.d.bits.source,done)
     }.otherwise {
