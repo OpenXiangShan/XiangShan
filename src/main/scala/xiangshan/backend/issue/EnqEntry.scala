6004bbe6a28ea4eba0eb377e4d2d188b14694d46
zhanglyGit <zhangly@stu.pku.edu.cn>
2023-11-30 14:06:27 +0800

    Backend: fix lat>0 cancel error

--- src/main/scala/xiangshan/backend/issue/EnqEntry.scala
+++ src/main/scala/xiangshan/backend/issue/EnqEntry.scala
@@ -170,7 +170,7 @@ class EnqEntry(implicit p: Parameters, params: IssueBlockParams) extends XSModul
     val wakeupVec: IndexedSeq[IndexedSeq[Bool]] = io.wakeUpFromIQ.map((bundle: ValidIO[IssueQueueIQWakeUpBundle]) =>
       bundle.bits.wakeUp(entryReg.status.psrc zip entryReg.status.srcType, bundle.valid)
     ).toIndexedSeq.transpose
-    val cancelSel = params.wakeUpSourceExuIdx.map(io.og0Cancel(_))
+    val cancelSel = io.wakeUpFromIQ.map(x => io.og0Cancel(x.bits.exuIdx) && x.bits.is0Lat)
     srcWakeUpByIQVec := wakeupVec.map(x => VecInit(x.zip(cancelSel).map { case (wakeup, cancel) => wakeup && !cancel }))
     srcWakeUpButCancel := wakeupVec.map(x => VecInit(x.zip(cancelSel).map { case (wakeup, cancel) => wakeup && cancel }))
     srcWakeUpByIQWithoutCancel := wakeupVec.map(x => VecInit(x))
@@ -187,7 +187,7 @@ class EnqEntry(implicit p: Parameters, params: IssueBlockParams) extends XSModul
     val wakeupVec: IndexedSeq[IndexedSeq[Bool]] = io.enqDelayWakeUpFromIQ.map( x =>
       x.bits.wakeUp(entryReg.status.psrc.zip(entryReg.status.srcType), x.valid)
     ).toIndexedSeq.transpose
-    val cancelSel = params.wakeUpSourceExuIdx.map(io.enqDelayOg0Cancel(_))
+    val cancelSel = io.enqDelayWakeUpFromIQ.map(x => io.enqDelayOg0Cancel(x.bits.exuIdx) && x.bits.is0Lat)
     enqDelaySrcWakeUpByIQVec := wakeupVec.map(x => VecInit(x.zip(cancelSel).map { case (wakeup, cancel) => wakeup && !cancel }))
   } else {
     enqDelaySrcWakeUpByIQVec := 0.U.asTypeOf(enqDelaySrcWakeUpByIQVec)
@@ -355,7 +355,7 @@ class EnqEntry(implicit p: Parameters, params: IssueBlockParams) extends XSModul
     io.srcTimer.get.zip(currentStatus.srcTimer.get).zip(srcWakeUpByIQWithoutCancel).zip(srcWakeUp).foreach {
       case (((srcTimerOut, srcTimer), wakeUpByIQOH: Vec[Bool]), wakeUpAll) =>
         when(wakeUpByIQOH.asUInt.orR) {
-          srcTimerOut := 1.U
+          srcTimerOut := Mux1H(wakeUpByIQOH, io.wakeUpFromIQ.map(_.bits.is0Lat).toSeq).asUInt
         }.otherwise {
           srcTimerOut := srcTimer
         }
