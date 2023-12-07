9646f4b2c6edfc5ef3940f6e0102f320b20678d3
YukunXue <1004205984@qq.com>
2023-12-07 15:15:41 +0800

    fix bug

--- src/main/scala/xiangshan/cache/dcache/mainpipe/MissQueue.scala
+++ src/main/scala/xiangshan/cache/dcache/mainpipe/MissQueue.scala
@@ -1021,10 +1021,11 @@ class MissQueue(edge: TLEdgeOut)(implicit p: Parameters) extends DCacheModule wi
   arbiter_with_pipereg(entries.map(_.io.replace_pipe_req), io.replace_pipe_req, Some("replace_pipe_req"))
 
   // amo's main pipe req out
-  val main_pipe_req_vec = entries.map(_.io.main_pipe_req)
-  io.main_pipe_req.valid := VecInit(main_pipe_req_vec.map(_.valid)).asUInt.orR
-  io.main_pipe_req.bits := Mux1H(main_pipe_req_vec.map(_.valid), main_pipe_req_vec.map(_.bits))
-  assert(PopCount(VecInit(main_pipe_req_vec.map(_.valid))) <= 1.U, "multi main pipe req")
+  //val main_pipe_req_vec = entries.map(_.io.main_pipe_req)
+  //io.main_pipe_req.valid := VecInit(main_pipe_req_vec.map(_.valid)).asUInt.orR
+  //io.main_pipe_req.bits := Mux1H(main_pipe_req_vec.map(_.valid), main_pipe_req_vec.map(_.bits))
+  //assert(PopCount(VecInit(main_pipe_req_vec.map(_.valid))) <= 1.U, "multi main pipe req")
+  fastArbiter(entries.map(_.io.main_pipe_req), io.main_pipe_req, Some("main_pipe_req"))
 
   io.probe_block := Cat(probe_block_vec).orR
 
