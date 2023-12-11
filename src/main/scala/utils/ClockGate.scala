9dad3bc0f1079fed902ee071aede648694c6eda3
sinceforYy <1017657683@qq.com>
2023-12-11 15:34:37 +0800

    ClockGate: modify the latch to adapt to verilator 5.218

--- src/main/scala/utils/ClockGate.scala
+++ src/main/scala/utils/ClockGate.scala
@@ -1,8 +1,9 @@
 package utils
 
 import chisel3._
+import chisel3.util.HasBlackBoxInline
 
-class ClockGate extends Module {
+class ClockGate extends BlackBox with HasBlackBoxInline {
   val io = IO(new Bundle {
     val TE = Input(Bool())
     val E  = Input(Bool())
@@ -10,5 +11,27 @@ class ClockGate extends Module {
     val Q  = Output(Clock())
   })
 
-  io.Q := (io.CK.asBool & (io.TE | io.E)).asClock
+  val verilog =
+    """
+      |/* verilator lint_off UNOPTFLAT */
+      |module ClockGate (
+      | input TE,
+      | input E,
+      | input CK,
+      | output Q
+      |);
+      | reg en_latched /*verilator clock_enable*/;
+      |
+      | always_latch @(*) begin
+      |   if (!CK) begin
+      |     en_latched = E || TE;
+      |   end
+      | end
+      |
+      | assign Q = en_latched && CK;
+      |
+      |endmodule
+      |
+      |""".stripMargin
+  setInline("ClockGate.v", verilog)
 }
