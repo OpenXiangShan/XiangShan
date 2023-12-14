dcaa8561c1e970e3a0d122dd6317dfa6e8c53883
Haojin Tang <tanghaojin@outlook.com>
2023-12-14 16:31:32 +0800

    DispatchQueue: fix allowEnqueue condition

--- src/main/scala/xiangshan/backend/dispatch/DispatchQueue.scala
+++ src/main/scala/xiangshan/backend/dispatch/DispatchQueue.scala
@@ -218,7 +218,7 @@ class DispatchQueue(size: Int, enqnum: Int, deqnum: Int, dqIndex: Int = 0)(impli
       currentValidCounter,
       validCounter + numEnq - numDeq)
   )
-  allowEnqueue := Mux(currentValidCounter > (size - enqnum).U, false.B, numEnq <= (size - enqnum).U - currentValidCounter)
+  allowEnqueue := Mux(currentValidCounter > (size - enqnum).U +& numDeq, false.B, numEnq +& currentValidCounter <= (size - enqnum).U +& numDeq)
 
   /**
    * Part 3: set output valid and data bits
