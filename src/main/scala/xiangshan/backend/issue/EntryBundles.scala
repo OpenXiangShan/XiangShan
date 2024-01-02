0a492c117574e1f5dd1ea3dddc1c9d1d01aaa773
sinsanction <1602723930@qq.com>
2024-01-02 14:58:13 +0800

    Entries: add some perf counter

--- src/main/scala/xiangshan/backend/issue/EntryBundles.scala
+++ src/main/scala/xiangshan/backend/issue/EntryBundles.scala
@@ -134,6 +134,9 @@ object EntryBundles extends HasCircularQueuePtrHelper {
     val transEntry            = ValidIO(new EntryBundle)
     // debug
     val cancel                = OptionWrapper(params.hasIQWakeUp, Output(Bool()))
+    val entryInValid          = Output(Bool())
+    val entryOutDeqValid      = Output(Bool())
+    val entryOutTransValid    = Output(Bool())
   }
 
   class CommonWireBundle(implicit p: Parameters, params: IssueBlockParams) extends XSBundle {
@@ -344,7 +347,11 @@ object EntryBundles extends HasCircularQueuePtrHelper {
     commonOut.enqReady                                := common.enqReady
     commonOut.transEntry.valid                        := validReg && !common.flushed && !common.deqSuccess
     commonOut.transEntry.bits                         := entryUpdate
+    // debug
     commonOut.cancel.foreach(_                        := hasIQWakeupGet.cancelVec.asUInt.orR)
+    commonOut.entryInValid                            := commonIn.enq.valid
+    commonOut.entryOutDeqValid                        := validReg && (common.flushed || common.deqSuccess)
+    commonOut.entryOutTransValid                      := validReg && commonIn.transSel && !(common.flushed || common.deqSuccess)
   }
 
   def EntryMemConnect(commonIn: CommonInBundle, common: CommonWireBundle, validReg: Bool, entryReg: EntryBundle, entryRegNext: EntryBundle, entryUpdate: EntryBundle, isEnq: Boolean)(implicit p: Parameters, params: IssueBlockParams) = {
