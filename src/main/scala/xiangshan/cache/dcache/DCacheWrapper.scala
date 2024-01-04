2ee98f49ea1fd0fb61ffb19702555736d6f77155
sfencevma <15155930562@163.com>
2024-01-04 22:17:13 +0800

    adjust dcache miss

--- src/main/scala/xiangshan/cache/dcache/DCacheWrapper.scala
+++ src/main/scala/xiangshan/cache/dcache/DCacheWrapper.scala
@@ -582,6 +582,7 @@ class DCacheLoadIO(implicit p: Parameters) extends DCacheWordIO
   val s1_paddr_dup_lsu = Output(UInt(PAddrBits.W)) // lsu side paddr
   val s1_paddr_dup_dcache = Output(UInt(PAddrBits.W)) // dcache side paddr
   val s1_disable_fast_wakeup = Input(Bool())
+  val s1_dcache_miss_fast = Input(Bool())
   // cycle 2: hit signal
   val s2_hit = Input(Bool()) // hit signal for lsu,
   val s2_first_hit = Input(Bool())
