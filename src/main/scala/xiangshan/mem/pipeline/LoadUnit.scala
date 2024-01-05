c5e38216964abbd330e0602e51056351a0ba404e
sfencevma <15155930562@163.com>
2024-01-05 16:43:25 +0800

    add weak exception condition

--- src/main/scala/xiangshan/mem/pipeline/LoadUnit.scala
+++ src/main/scala/xiangshan/mem/pipeline/LoadUnit.scala
@@ -776,8 +776,11 @@ class LoadUnit(implicit p: Parameters) extends XSModule
   // if such exception happen, that inst and its exception info
   // will be force writebacked to rob
   val s2_exception_vec = WireInit(s2_in.uop.cf.exceptionVec)
+  val s2_weak_exception_vec = WireInit(s2_in.uop.cf.exceptionVec)
   when (!s2_in.delayedLoadError) {
     s2_exception_vec(loadAccessFault) := s2_in.uop.cf.exceptionVec(loadAccessFault) || s2_pmp.ld ||
+                                       (io.dcache.resp.bits.tag_error && RegNext(io.csrCtrl.cache_error_enable))
+    s2_weak_exception_vec(loadAccessFault) := s2_in.uop.cf.exceptionVec(loadAccessFault) ||
                                        (io.dcache.resp.bits.tag_error && RegNext(io.csrCtrl.cache_error_enable))
   }
 
@@ -785,8 +788,10 @@ class LoadUnit(implicit p: Parameters) extends XSModule
   // be triggered)
   when (!s2_in.delayedLoadError && (s2_prf || s2_in.tlbMiss)) {
     s2_exception_vec := 0.U.asTypeOf(s2_exception_vec.cloneType)
+    s2_weak_exception_vec := 0.U.asTypeOf(s2_weak_exception_vec.cloneType)
   }
   val s2_exception = ExceptionNO.selectByFu(s2_exception_vec, lduCfg).asUInt.orR
+  val s2_weak_exception = ExceptionNO.selectByFu(s2_weak_exception_vec, lduCfg).asUInt.orR
 
   val (s2_fwd_frm_d_chan, s2_fwd_data_frm_d_chan) = io.tl_d_channel.forward(s1_valid && s1_out.forward_tlDchannel, s1_out.mshrid, s1_out.paddr)
   val (s2_fwd_data_valid, s2_fwd_frm_mshr, s2_fwd_data_frm_mshr) = io.forward_mshr.forward()
@@ -799,7 +804,7 @@ class LoadUnit(implicit p: Parameters) extends XSModule
   val s2_actually_mmio = s2_pmp.mmio && !s2_not_tlb_query
   val s2_mmio          = !s2_prf &&
                           s2_actually_mmio &&
-                         !s2_exception &&
+                         !s2_weak_exception &&
                          !s2_in.tlbMiss
 
   val s2_full_fwd      = Wire(Bool())
@@ -848,7 +853,7 @@ class LoadUnit(implicit p: Parameters) extends XSModule
   val s2_cache_tag_error = RegNext(io.csrCtrl.cache_error_enable) &&
                            io.dcache.resp.bits.tag_error
 
-  val s2_troublem        = !s2_exception &&
+  val s2_troublem        = !s2_weak_exception &&
                            !s2_mmio &&
                            !s2_prf &&
                            !s2_in.delayedLoadError
@@ -872,12 +877,6 @@ class LoadUnit(implicit p: Parameters) extends XSModule
                     !s2_fwd_fail &&
                     (s2_dcache_fast_rep || s2_nuke_fast_rep) &&
                     s2_troublem
-
-  // need allocate new entry
-  val s2_can_query = !s2_mem_amb &&
-                     !s2_tlb_miss &&
-                     !s2_fwd_fail &&
-                     s2_troublem
 
   val s2_data_fwded = s2_dcache_miss && (s2_full_fwd || s2_cache_tag_error)
 
