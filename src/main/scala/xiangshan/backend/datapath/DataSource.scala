0dbae67a60e26368067cdc62060373bf5010270d
xiaofeibao-xjtu <1441675923@qq.com>
2024-01-16 10:01:13 +0800

    DataSource: increase to 5 types

--- src/main/scala/xiangshan/backend/datapath/DataSource.scala
+++ src/main/scala/xiangshan/backend/datapath/DataSource.scala
@@ -3,26 +3,33 @@ package xiangshan.backend.datapath
 import chisel3._
 
 class DataSource extends Bundle {
-  val value = UInt(2.W)
+  val value = UInt(3.W)
 
-  def readReg: Bool = value === DataSource.reg
+  def readReg: Bool = value(2)
 
-  def readBypass: Bool = value(1)
+  def readZero: Bool = value === DataSource.zero
 
-  def readForward: Bool = value(0)
+  def readForward: Bool = value === DataSource.forward
+
+  def readBypass: Bool = value === DataSource.bypass
+
+  def readImm: Bool = value === DataSource.imm
+
 }
 
 object DataSource {
   def apply() = new DataSource
 
-  // no need to read any data
-  def none: UInt = "b00".U
+  def reg: UInt = "b100".U
 
-  def reg: UInt = "b00".U
+  // read int preg addr is 0
+  def zero: UInt = "b000".U
 
-  def forward: UInt = "b01".U
+  def forward: UInt = "b001".U
 
-  def bypass: UInt = "b10".U
+  def bypass: UInt = "b010".U
+
+  def imm: UInt = "b011".U
 
 }
 
