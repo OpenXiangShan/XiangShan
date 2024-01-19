a2d4faaadf376e014f03b73f944c215722620abd
sinsanction <1602723930@qq.com>
2024-01-19 11:30:48 +0800

    DecodeStage: block vector inst when vtype is resuming

--- src/main/scala/xiangshan/backend/CtrlBlock.scala
+++ src/main/scala/xiangshan/backend/CtrlBlock.scala
@@ -269,6 +269,7 @@ class CtrlBlockImp(
 
   io.toDataPath.vtypeAddr := rob.io.vconfigPdest
 
+  decode.io.isResumeVType := rob.io.toDecode.isResumeVType
   decode.io.walkVType := rob.io.toDecode.vtype
 
   decode.io.redirect := s1_s3_redirect.valid || s2_s4_pendingRedirectValid
