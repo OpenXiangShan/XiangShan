8d14f6934929cabca1c529f7faf441c2d8509221
lihuijin <501296508@qq.com>
2024-01-22 16:53:08 +0800

    AREA Optimiation:loadQueueRAR = 72, loadQueueRAW = 56, loadQueueReplay = 32, uncacheBuffer = 4

--- src/main/scala/xiangshan/Parameters.scala
+++ src/main/scala/xiangshan/Parameters.scala
@@ -140,11 +140,11 @@ case class XSCoreParameters
   IssQueSize: Int = 16,
   NRPhyRegs: Int = 192,
   VirtualLoadQueueSize: Int = 80,
-  LoadQueueRARSize: Int = 80,
-  LoadQueueRAWSize: Int = 64, // NOTE: make sure that LoadQueueRAWSize is power of 2.
+  LoadQueueRARSize: Int = 72,
+  LoadQueueRAWSize: Int = 56, // NOTE: make sure that LoadQueueRAWSize is power of 2.
   RollbackGroupSize: Int = 8,
-  LoadQueueReplaySize: Int = 72,
-  LoadUncacheBufferSize: Int = 20,
+  LoadQueueReplaySize: Int = 32,
+  LoadUncacheBufferSize: Int = 4,
   LoadQueueNWriteBanks: Int = 8, // NOTE: make sure that LoadQueueRARSize/LoadQueueRAWSize is divided by LoadQueueNWriteBanks
   StoreQueueSize: Int = 64,
   StoreQueueNWriteBanks: Int = 8, // NOTE: make sure that StoreQueueSize is divided by StoreQueueNWriteBanks
