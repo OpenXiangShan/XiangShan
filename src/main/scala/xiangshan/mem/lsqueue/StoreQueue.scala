86101b9de8a6396a015ddc788e547212ff58c656
sfencevma <15155930562@163.com>
2024-02-22 14:28:58 +0800

    fix st mmio uop selection

--- src/main/scala/xiangshan/mem/lsqueue/StoreQueue.scala
+++ src/main/scala/xiangshan/mem/lsqueue/StoreQueue.scala
@@ -585,10 +585,12 @@ class StoreQueue(implicit p: Parameters) extends XSModule
   //(2) when they reach ROB's head, they can be sent to uncache channel
   val s_idle :: s_req :: s_resp :: s_wb :: s_wait :: Nil = Enum(5)
   val uncacheState = RegInit(s_idle)
+  val uncacheUop = Reg(new MicroOp)
   switch(uncacheState) {
     is(s_idle) {
       when(RegNext(io.rob.pendingst && pending(deqPtr) && allocated(deqPtr) && datavalid(deqPtr) && addrvalid(deqPtr))) {
         uncacheState := s_req
+        uncacheUop := uop(deqPtr)
       }
     }
     is(s_req) {
@@ -655,7 +657,7 @@ class StoreQueue(implicit p: Parameters) extends XSModule
 
   // (4) writeback to ROB (and other units): mark as writebacked
   io.mmioStout.valid := uncacheState === s_wb
-  io.mmioStout.bits.uop := uop(deqPtr)
+  io.mmioStout.bits.uop := uncacheUop
   io.mmioStout.bits.uop.sqIdx := deqPtrExt(0)
   io.mmioStout.bits.data := shiftDataToLow(paddrModule.io.rdata(0), dataModule.io.rdata(0).data) // dataModule.io.rdata.read(deqPtr)
   io.mmioStout.bits.redirectValid := false.B
