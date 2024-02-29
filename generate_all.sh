ac3980d8e81c253c26df1dcd2e2349b96b405d53
LiHe_BOSC <lihe@bosc.ac.cn>
2024-02-29 11:01:36 +0800

    new FoldedSRAMTemplate selectOH debug

---
+++ generate_all.sh
@@ -0,0 +1,11 @@
+set -e
+set -x
+
+cd $NOOP_HOME
+
+for module in Frontend Backend MemBlock XSCore L2Top XSTile HuanCun XSTop
+do
+  python3 parser.py $module --config DefaultConfig --prefix bosc_ --sram-replace --xs-home $(pwd) > generate_$module.log
+  mv generate_$module.log bosc_${module}-Release*
+done
+
