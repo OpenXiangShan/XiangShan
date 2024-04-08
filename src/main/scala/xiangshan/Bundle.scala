09119fbc02e1785719d6c7ef389d801cd50831ad
Gao-Zeyu <81751003+Gao-Zeyu@users.noreply.github.com>
2024-04-08 16:51:14 +0800

    ftq: cut area of ftq_redirect_mem (#2856)
    dlt folded_hist/afhob/lastBrNumOH
    ftq_redirect_mem: 247*64->73*64

--- src/main/scala/xiangshan/Bundle.scala
+++ src/main/scala/xiangshan/Bundle.scala
@@ -95,7 +95,7 @@ class CfiUpdateInfo(implicit p: Parameters) extends XSBundle with HasBPUParamete
   // frontend -> backend -> frontend
   val pd = new PreDecodeInfo
   val ssp = UInt(log2Up(RasSize).W)
-  val sctr = UInt(RasCtrSize.W)
+  val sctr = UInt(log2Up(RasCtrSize).W)
   val TOSW = new RASPtr
   val TOSR = new RASPtr
   val NOS = new RASPtr
@@ -120,9 +120,6 @@ class CfiUpdateInfo(implicit p: Parameters) extends XSBundle with HasBPUParamete
 
   def fromFtqRedirectSram(entry: Ftq_Redirect_SRAMEntry) = {
     // this.hist := entry.ghist
-    this.folded_hist := entry.folded_hist
-    this.lastBrNumOH := entry.lastBrNumOH
-    this.afhob := entry.afhob
     this.histPtr := entry.histPtr
     this.ssp := entry.ssp
     this.sctr := entry.sctr
