a6742963c480ab2df796fd4e96896efc3abf5a1b
Haojin Tang <tanghaojin@outlook.com>
2024-04-09 17:25:34 +0800

    CtrlBlock: fix flushVecNext enable to avoid x state

--- src/main/scala/xiangshan/backend/CtrlBlock.scala
+++ src/main/scala/xiangshan/backend/CtrlBlock.scala
@@ -318,7 +318,7 @@ class CtrlBlockImp(
     val shouldFlushMask = (1 to RenameWidth).map(shouldFlush take _ reduce (_ || _))
     s1_s3_redirect.valid && Cat(shouldFlushMask.zip(notCFIMask).map(x => x._1 | x._2)).andR
   })
-  val flushVecNext = VecInit(flushVec.map(x => GatedValidRegNext(x, false.B)))
+  val flushVecNext = flushVec zip snpt.io.valids map (x => GatedValidRegNext(x._1 && x._2, false.B))
   snpt.io.flushVec := flushVecNext
 
   val useSnpt = VecInit.tabulate(RenameSnapshotNum)(idx =>
