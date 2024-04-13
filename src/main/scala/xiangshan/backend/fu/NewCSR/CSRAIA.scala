87cfe95f55930f05fd78412ded5f3e87f4ff8604
Xuan Hu <huxuan@bosc.ac.cn>
2024-04-13 15:35:21 +0800

    NewCSR: refactor to make IDEA happy
    * IDEA always cannot recognize CSRRWApply trait, I don't know why

--- src/main/scala/xiangshan/backend/fu/NewCSR/CSRAIA.scala
+++ src/main/scala/xiangshan/backend/fu/NewCSR/CSRAIA.scala
@@ -77,7 +77,7 @@ trait CSRAIA { self: NewCSR with HypervisorLevel =>
   )
 }
 
-class ISelectField(final val maxValue: Int, reserved: Seq[Range]) extends CSREnum with CSRWARLApply {
+class ISelectField(final val maxValue: Int, reserved: Seq[Range]) extends CSREnum with WARLApply {
   override def isLegal(enum: CSREnumType): Bool = {
     !reserved.map(range => enum.asUInt >= range.start.U && enum.asUInt <= range.end.U).reduce(_ || _)
   }
