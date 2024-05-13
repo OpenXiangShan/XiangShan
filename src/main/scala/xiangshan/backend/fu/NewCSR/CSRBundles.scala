e899bf1c1a37452a4f12ed4e1c5b163147814919
Xuan Hu <huxuan@bosc.ac.cn>
2024-05-13 11:49:33 +0800

    NewCSR: remove sdsid

--- src/main/scala/xiangshan/backend/fu/NewCSR/CSRBundles.scala
+++ src/main/scala/xiangshan/backend/fu/NewCSR/CSRBundles.scala
@@ -168,8 +168,6 @@ object CSRBundles {
     val l2_pf_store_only = Output(Bool())
     // ICache
     val icache_parity_enable = Output(Bool())
-    // Labeled XiangShan
-    val dsid = Output(UInt(8.W)) // TODO: DsidWidth as parameter
     // Load violation predictor
     val lvpred_disable = Output(Bool())
     val no_spec_load = Output(Bool())
