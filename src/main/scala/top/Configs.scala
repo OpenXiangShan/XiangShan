a093d009cb0501e85e08edd83cb04e0690491951
zhanglinjuan <zhanglinjuan16@mails.ucas.ac.cn>
2024-05-14 19:09:58 +0800

    Configs: set the number of ways to 8 in KunminghuV2Config

--- src/main/scala/top/Configs.scala
+++ src/main/scala/top/Configs.scala
@@ -382,6 +382,6 @@ class KunminghuV2Config(n: Int = 1) extends Config(
       case SoCParamsKey => up(SoCParamsKey).copy(L3CacheParamsOpt = None) // There will be no L3
     })
     ++ new WithNKBL2(2 * 512, inclusive = true, banks = 4, tp = false)
-    ++ new WithNKBL1D(64, ways = 4)
+    ++ new WithNKBL1D(64, ways = 8)
     ++ new BaseConfig(n)
 )
