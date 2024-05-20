260a087d23a27f413c006362a82c84ecef09a587
Xuan Hu <huxuan@bosc.ac.cn>
2024-05-20 15:25:55 +0800

    NewCSR: correct the produce of exception virtual address
    * As specified by RISC-V specification, there are 3 different length of virtual address. They are `Bare`, `Sv39` and `Sv39x4`.
    * The CSR mepc and mtval use `Bare` address when the privilege mode is M mode or the satp.MODE is Bare.
    * The CSR mepc and mtval use `Sv39` address when the privilege mode is S/U mode and the satp.MODE is Sv39 and when the privilege mode is VS/VU mode and the vsatp.MODE is Sv39.
    * The CSR mepc and mtval use `Sv39x4` address when then privilege mode is VS/VU and the hgatp.MODE is Sv39x4 and the vsatp.MODE is Bare.
    * The `Sv48` and `Sv48x4` support are provided partly in CSREvent for future usage.

--- src/main/scala/xiangshan/backend/fu/NewCSR/CSREvents/CSREvent.scala
+++ src/main/scala/xiangshan/backend/fu/NewCSR/CSREvents/CSREvent.scala
@@ -4,11 +4,10 @@ import chisel3._
 import chisel3.util._
 import org.chipsalliance.cde.config.Parameters
 import utility.{SignExt, ZeroExt}
-import xiangshan.{ExceptionNO, HasXSParameter}
-import xiangshan.ExceptionNO._
-import xiangshan.backend.fu.NewCSR.CSRBundles.{CauseBundle, OneFieldBundle, PrivState}
+import xiangshan.HasXSParameter
 import xiangshan.backend.fu.NewCSR.CSRBundles.{CauseBundle, PrivState}
 import xiangshan.backend.fu.NewCSR.CSRConfig._
+import xiangshan.backend.fu.NewCSR.CSRDefines.{HgatpMode, SatpMode}
 import xiangshan.backend.fu.NewCSR._
 
 trait CSREvents { self: NewCSR =>
@@ -54,6 +53,44 @@ trait CSREventBase {
   val valid = IO(Input(Bool()))
   val in: Bundle
   val out: Bundle
+
+  def genTrapVA(
+    transMode: PrivState,
+    satp: SatpBundle,
+    vsatp: SatpBundle,
+    hgatp: HgatpBundle,
+    addr: UInt,
+  ) = {
+    require(addr.getWidth >= 41)
+
+    val isBare =
+      transMode.isModeM ||
+      transMode.isModeHSorHU &&  satp.MODE === SatpMode.Bare ||
+      transMode.isVirtual    && vsatp.MODE === SatpMode.Bare && hgatp.MODE === HgatpMode.Bare
+    val isSv39 =
+      transMode.isModeHSorHU &&  satp.MODE === SatpMode.Sv39 ||
+      transMode.isVirtual    && vsatp.MODE === SatpMode.Sv39
+    val isSv48 =
+      transMode.isModeHSorHU &&  satp.MODE === SatpMode.Sv48 ||
+      transMode.isVirtual    && vsatp.MODE === SatpMode.Sv48
+    val isSv39x4 =
+      transMode.isVirtual && vsatp.MODE === SatpMode.Bare && hgatp.MODE === HgatpMode.Sv39x4
+    val isSv48x4 =
+      transMode.isVirtual && vsatp.MODE === SatpMode.Bare && hgatp.MODE === HgatpMode.Sv48x4
+
+    val bareAddr   = ZeroExt(addr(PAddrWidth - 1, 0), XLEN)
+    // When enable virtual memory, the higher bit should fill with the msb of address of Sv39/Sv48/Sv57
+    val sv39Addr   = SignExt(addr(38, 0), XLEN)
+    val sv39x4Addr = SignExt(addr(40, 0), XLEN)
+
+    val trapAddr = Mux1H(Seq(
+      isBare -> bareAddr,
+      isSv39 -> sv39Addr,
+      isSv39x4 -> sv39x4Addr,
+    ))
+
+    trapAddr
+  }
 }
 
 class TrapEntryEventInput(implicit val p: Parameters) extends Bundle with HasXSParameter {
@@ -76,6 +113,7 @@ class TrapEntryEventInput(implicit val p: Parameters) extends Bundle with HasXSP
 
   val satp = Input(new SatpBundle)
   val vsatp = Input(new SatpBundle)
+  val hgatp = Input(new HgatpBundle)
   // from mem
   val memExceptionVAddr = Input(UInt(VAddrBits.W))
   val memExceptionGPAddr = Input(UInt(GPAddrBits.W))
