2d5781acaac2e21ebedcaf71df37475ea024b186
ZhangZifei <zhangzifei16@mails.ucas.ac.cn>
2024-05-23 20:25:23 +0800

    SimTop: change reset vector to 0x80000000

--- src/test/scala/top/SimTop.scala
+++ src/test/scala/top/SimTop.scala
@@ -60,7 +60,7 @@ class SimTop(implicit p: Parameters) extends Module {
   soc.io.sram_config := 0.U
   soc.io.pll0_lock := true.B
   soc.io.cacheable_check := DontCare
-  soc.io.riscv_rst_vec.foreach(_ := 0x10000000L.U)
+  soc.io.riscv_rst_vec.foreach(_ := 0x80000000L.U)
 
   // soc.io.rtc_clock is a div100 of soc.io.clock
   val rtcClockDiv = 100
