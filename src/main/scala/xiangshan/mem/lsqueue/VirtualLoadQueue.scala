a7828dc13cdc12bfaafa2ac00aa394df04d8806b
Tang Haojin <tanghaojin@outlook.com>
2024-06-12 23:11:06 +0800

    Revert "LSQ: optimize static clock gating coverage (#3023)" (#3055)

--- src/main/scala/xiangshan/mem/lsqueue/VirtualLoadQueue.scala
+++ src/main/scala/xiangshan/mem/lsqueue/VirtualLoadQueue.scala
@@ -92,14 +92,14 @@ class VirtualLoadQueue(implicit p: Parameters) extends XSModule
   val needCancel = WireInit(VecInit((0 until VirtualLoadQueueSize).map(i => {
     uop(i).robIdx.needFlush(io.redirect) && allocated(i)
   })))
-  val lastNeedCancel = GatedValidRegNext(needCancel)
+  val lastNeedCancel = RegNext(needCancel)
   val enqCancel = canEnqueue.zip(io.enq.req).map{case (v , x) =>
     v && x.bits.robIdx.needFlush(io.redirect)
   }
   val enqCancelNum = enqCancel.zip(io.enq.req).map{case (v, req) =>
     Mux(v, req.bits.numLsElem, 0.U)
   }
-  val lastEnqCancel = GatedRegNext(enqCancelNum.reduce(_ + _))
+  val lastEnqCancel = RegNext(enqCancelNum.reduce(_ + _))
   val lastCycleCancelCount = PopCount(lastNeedCancel)
   val redirectCancelCount = RegEnable(lastCycleCancelCount + lastEnqCancel, 0.U, lastCycleRedirect.valid)
 
@@ -139,7 +139,7 @@ class VirtualLoadQueue(implicit p: Parameters) extends XSModule
   val deqCountMask = Wire(UInt(DeqPtrMoveStride.W))
   deqCountMask := deqLookup.asUInt & (~deqInSameRedirectCycle.asUInt).asUInt
   val commitCount = PopCount(PriorityEncoderOH(~deqCountMask) - 1.U)
-  val lastCommitCount = GatedRegNext(commitCount)
+  val lastCommitCount = RegNext(commitCount)
 
   // update deqPtr
   // cycle 1: generate deqPtrNext
@@ -148,7 +148,7 @@ class VirtualLoadQueue(implicit p: Parameters) extends XSModule
   deqPtrNext := deqPtr + lastCommitCount
   deqPtr := RegEnable(deqPtrNext, 0.U.asTypeOf(new LqPtr), deqPtrUpdateEna)
 
-  io.lqDeq := GatedRegNext(lastCommitCount)
+  io.lqDeq := RegNext(lastCommitCount)
   io.lqCancelCnt := redirectCancelCount
   io.ldWbPtr := deqPtr
   io.lqEmpty := RegNext(validCount === 0.U)
