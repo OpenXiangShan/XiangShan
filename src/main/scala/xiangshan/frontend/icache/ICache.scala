df00d4c402462f9a3206b98fe8b564c2130805b5
ngc7331 <ngc7331@outlook.com>
2024-06-20 10:05:48 +0800

    ICache: init io.error
    cherry-picked #3079

--- src/main/scala/xiangshan/frontend/icache/ICache.scala
+++ src/main/scala/xiangshan/frontend/icache/ICache.scala
@@ -560,8 +560,9 @@ class ICacheImp(outer: ICache) extends LazyModuleImp(outer) with HasICacheParame
 
   //Parity error port
   val errors = mainPipe.io.errors
-  io.error.bits <> RegEnable(Mux1H(errors.map(e => e.valid -> e.bits)),errors.map(e => e.valid).reduce(_|_))
-  io.error.valid := RegNext(errors.map(e => e.valid).reduce(_|_),init = false.B)
+  val errors_valid = errors.map(e => e.valid).reduce(_ | _)
+  io.error.bits <> RegEnable(Mux1H(errors.map(e => e.valid -> e.bits)), 0.U.asTypeOf(errors(0).bits), errors_valid)
+  io.error.valid := RegNext(errors_valid, false.B)
 
   val perfEvents = Seq(
     ("icache_miss_cnt  ", false.B),
