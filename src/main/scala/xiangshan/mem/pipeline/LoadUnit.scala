11d57984db4bcf88f65dc5cc6fa11aab2f397fc1
lwd <1045251744@qq.com>
2024-07-11 17:23:53 +0800

    LSU: trigger accessfault for vector mmio access request (#3168)
    This commit is to ensure robustness when vector instructions are used to
    access MMIO regions by mistake. Currently, we do not support vector
    instruction access to MMIO regions, and in order to handle the situation
    when a vector instruction incorrectly accesses an MMIO region, an
    accessfault exception is triggered when the MMIO region is accessed
    using a vector instruction.

--- src/main/scala/xiangshan/mem/pipeline/LoadUnit.scala
+++ src/main/scala/xiangshan/mem/pipeline/LoadUnit.scala
@@ -942,8 +942,11 @@ class LoadUnit(implicit p: Parameters) extends XSModule
   // will be force writebacked to rob
   val s2_exception_vec = WireInit(s2_in.uop.exceptionVec)
   when (!s2_in.delayedLoadError) {
-    s2_exception_vec(loadAccessFault) := (s2_in.uop.exceptionVec(loadAccessFault) || s2_pmp.ld ||
-                                       (io.dcache.resp.bits.tag_error && GatedValidRegNext(io.csrCtrl.cache_error_enable))) && s2_vecActive
+    s2_exception_vec(loadAccessFault) := (s2_in.uop.exceptionVec(loadAccessFault) ||
+                                         s2_pmp.ld ||
+                                         s2_isvec && s2_pmp.mmio && !s2_prf && !s2_in.tlbMiss ||
+                                         (io.dcache.resp.bits.tag_error && GatedValidRegNext(io.csrCtrl.cache_error_enable))
+                                         ) && s2_vecActive
   }
 
   // soft prefetch will not trigger any exception (but ecc error interrupt may
