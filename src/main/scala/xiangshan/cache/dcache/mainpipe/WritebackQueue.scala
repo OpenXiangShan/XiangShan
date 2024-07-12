1461d8f87130c4011b868ccaa53effed55ce5f81
CharlieLiu <67408162+bosscharlie@users.noreply.github.com>
2024-07-13 00:46:53 +0800

    DCache: Block the writeback req when the addr matching a valid req in mshr (#3179)
    Bug: 
    - When two req with different addr x and y enter MissQueue together, req
    y is real miss req, while req x is **AcquireBlock BtoT**. Req y receive
    the resp from L2 first and complete the refill operation by replacing
    the data block with addr x (decided by plru algorithm). MainPipe will
    release the data block with addr in writeback queue through req
    **Release BtoN** to L2. Addr x receive GrantData with permission toT at
    last.
    - From the view of L2, the req sequence of addr x is: Acquire BtoT ->
    GrantData toT -> Release BtoN, which is abnormal.
    
    Fix: When a valid req reaches wbq, check if there is any valid mshr with
    same block_addr. If a mshr is found, block current wbq_req.

--- src/main/scala/xiangshan/cache/dcache/mainpipe/WritebackQueue.scala
+++ src/main/scala/xiangshan/cache/dcache/mainpipe/WritebackQueue.scala
@@ -317,6 +317,8 @@ class WritebackQueue(edge: TLEdgeOut)(implicit p: Parameters) extends DCacheModu
 
     val miss_req = Flipped(Valid(UInt()))
     val block_miss_req = Output(Bool()) 
+
+    val mshr_block = Input(Bool())
   })
 
   require(cfg.nReleaseEntries > cfg.nMissEntries)
@@ -351,7 +353,7 @@ class WritebackQueue(edge: TLEdgeOut)(implicit p: Parameters) extends DCacheModu
       entry.io.id := entry_id
 
       // entry req
-      entry.io.req.valid := req.valid && !block_conflict
+      entry.io.req.valid := req.valid && !block_conflict && !io.mshr_block
       primary_ready_vec(i)   := entry.io.primary_ready
       entry.io.req.bits  := req.bits
       entry.io.req_data  := req_data
