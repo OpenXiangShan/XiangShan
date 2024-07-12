f6a48b12b30a4fe8a2f9bb54594d677177c747d3
sinceforYy <1017657683@qq.com>
2024-07-12 11:25:05 +0800

    NewCSR: Move xstatus.vs check to decodeUnit for csrr vl/vlenb

--- src/main/scala/xiangshan/backend/fu/NewCSR/CSRPermitModule.scala
+++ src/main/scala/xiangshan/backend/fu/NewCSR/CSRPermitModule.scala
@@ -93,7 +93,7 @@ class CSRPermitModule extends Module {
   private val csrIsM = addr(9, 8) === "b11".U
   private val csrIsHPM = addr >= CSRs.cycle.U && addr <= CSRs.hpmcounter31.U
   private val csrIsFp = Seq(CSRs.fflags, CSRs.frm, CSRs.fcsr).map(_.U === addr).reduce(_ || _)
-  private val csrIsVec = Seq(CSRs.vstart, CSRs.vxsat, CSRs.vxrm, CSRs.vcsr, CSRs.vl, CSRs.vtype, CSRs.vlenb).map(_.U === addr).reduce(_ || _)
+  private val csrIsVec = Seq(CSRs.vstart, CSRs.vxsat, CSRs.vxrm, CSRs.vcsr, CSRs.vtype).map(_.U === addr).reduce(_ || _)
   private val csrIsWritableVec = Seq(CSRs.vstart, CSRs.vxsat, CSRs.vxrm, CSRs.vcsr).map(_.U === addr).reduce(_ || _)
   private val counterAddr = addr(4, 0) // 32 counters
 
