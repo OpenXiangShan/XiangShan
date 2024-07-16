e24b936c36f8c3b54e2444d3bcff8c266f2a9736
ZhangZifei <zhangzifei16@mails.ucas.ac.cn>
2024-07-16 11:37:17 +0800

    trace: set TraceFetchWidth to 16

--- src/main/scala/xiangshan/Parameters.scala
+++ src/main/scala/xiangshan/Parameters.scala
@@ -864,7 +864,8 @@ trait HasXSParameter {
 
   // TraceRTLMode
   def TracePCWidth = XLEN
-  def TraceFetchWidth = PredictWidth
+  def TraceFetchWidth = 16
+  require(TraceFetchWidth >= PredictWidth)
   def TraceBufferSize = TraceFetchWidth * 4 //
   def TraceCollectorWidth = CommitWidth
   def TraceRoBMergeNum = RenameWidth
