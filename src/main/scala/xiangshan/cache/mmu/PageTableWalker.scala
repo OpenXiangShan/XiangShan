8e1b78909b7d41e97f9b7ee84e0bb516ec49d878
peixiaokun <peixiaokun23z@ict.ac.cn>
2024-08-07 19:46:19 +0800

    PTW, RVH: fix the x state of stage1 pf/af when the first s2xlate happens gpf

--- src/main/scala/xiangshan/cache/mmu/PageTableWalker.scala
+++ src/main/scala/xiangshan/cache/mmu/PageTableWalker.scala
@@ -102,8 +102,13 @@ class PTW()(implicit p: Parameters) extends XSModule with HasPtwConst with HasPe
   val vpn = Reg(UInt(vpnLen.W)) // vpn or gvpn(onlyS2xlate)
   val levelNext = level + 1.U
   val l1Hit = Reg(Bool())
-  val pte = mem.resp.bits.asTypeOf(new PteBundle().cloneType)
-
+  val pte_valid = RegInit(false.B) // avoid the x states
+  val fake_pte = 0.U.asTypeOf(new PteBundle())
+  fake_pte.perm.v := true.B
+  fake_pte.perm.r := true.B
+  fake_pte.perm.w := true.B
+  fake_pte.perm.x := true.B
+  val pte = Mux(pte_valid, mem.resp.bits.asTypeOf(new PteBundle()), fake_pte)
   // s/w register
   val s_pmp_check = RegInit(true.B)
   val s_mem_req = RegInit(true.B)
@@ -153,12 +158,6 @@ class PTW()(implicit p: Parameters) extends XSModule with HasPtwConst with HasPe
   val hpaddr = Cat(hptw_resp.genPPNS2(get_pn(gpaddr)), get_off(gpaddr))
 
   io.req.ready := idle
-  val pte_valid = RegInit(false.B) // avoid the x states
-  val fake_pte = 0.U.asTypeOf(pte)
-  fake_pte.perm.v := true.B
-  fake_pte.perm.r := true.B
-  fake_pte.perm.w := true.B
-  fake_pte.perm.x := true.B
   val ptw_resp = Wire(new PtwMergeResp)
   ptw_resp.apply(pageFault && !accessFault && !ppn_af, accessFault || ppn_af, Mux(accessFault, af_level,level), Mux(pte_valid, pte, fake_pte), vpn, satp.asid, hgatp.asid, vpn(sectortlbwidth - 1, 0), not_super = false)
 
