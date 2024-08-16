1086caf6132a0137793335f377ee6b3498290409
Haojin Tang <tanghaojin@outlook.com>
2024-08-16 16:31:59 +0800

    SoC: change NodeIDWidth to 11 for CHI

--- src/main/scala/system/SoC.scala
+++ src/main/scala/system/SoC.scala
@@ -48,7 +48,7 @@ case class SoCParameters
     sets = 2048 // 1MB per bank
   )),
   XSTopPrefix: Option[String] = None,
-  NodeIDWidth: Int = 7,
+  NodeIDWidth: Int = 11,
   NumHart: Int = 64,
   NumIRFiles: Int = 7,
   NumIRSrc: Int = 256,
