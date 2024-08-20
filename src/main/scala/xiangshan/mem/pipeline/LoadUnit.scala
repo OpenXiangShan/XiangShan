312c6568899c8f2b10b47daa16cb2a7f16660b08
lixin <1037997956@qq.com>
2024-08-20 15:52:33 +0800

    ldu: opt hlv and hlvx timing
    * fast replay load will not query tlb, so dont care about it's
      tlb.req.bits.hyperinst and tlb.req.bits.hlvx

--- src/main/scala/xiangshan/mem/pipeline/LoadUnit.scala
+++ src/main/scala/xiangshan/mem/pipeline/LoadUnit.scala
@@ -205,6 +205,8 @@ class LoadUnit(implicit p: Parameters) extends XSModule
   val s0_fire          = s0_valid && s0_can_go
   val s0_mmio_fire     = s0_mmio_select && s0_can_go
   val s0_out           = Wire(new LqWriteBundle)
+  val s0_tlb_hlv       = Wire(Bool())
+  val s0_tlb_hlvx      = Wire(Bool())
   val s0_tlb_vaddr     = Wire(UInt(VAddrBits.W))
   val s0_dcache_vaddr  = Wire(UInt(VAddrBits.W))
 
@@ -226,8 +228,6 @@ class LoadUnit(implicit p: Parameters) extends XSModule
     val prf_wr        = Bool()
     val prf_i         = Bool()
     val sched_idx     = UInt(log2Up(LoadQueueReplaySize+1).W)
-    val hlv           = Bool()
-    val hlvx          = Bool()
     // Record the issue port idx of load issue queue. This signal is used by load cancel.
     val deqPortIdx    = UInt(log2Ceil(LoadPipelineWidth).W)
     // vec only
@@ -382,8 +382,8 @@ class LoadUnit(implicit p: Parameters) extends XSModule
                                          TlbCmd.read
                                        )
   io.tlb.req.bits.vaddr              := s0_tlb_vaddr
-  io.tlb.req.bits.hyperinst          := s0_sel_src.hlv
-  io.tlb.req.bits.hlvx               := s0_sel_src.hlvx
+  io.tlb.req.bits.hyperinst          := s0_tlb_hlv
+  io.tlb.req.bits.hlvx               := s0_tlb_hlvx
   io.tlb.req.bits.size               := Mux(s0_sel_src.isvec, s0_sel_src.alignedType(2,0), LSUOpType.size(s0_sel_src.uop.fuOpType))
   io.tlb.req.bits.kill               := s0_kill
   io.tlb.req.bits.memidx.is_ld       := true.B
@@ -446,8 +446,6 @@ class LoadUnit(implicit p: Parameters) extends XSModule
     out.elemIdx       := src.elemIdx
     out.elemIdxInsideVd := src.elemIdxInsideVd
     out.alignedType   := src.alignedType
-    out.hlv           := LSUOpType.isHlv(src.uop.fuOpType)
-    out.hlvx          := LSUOpType.isHlvx(src.uop.fuOpType)
     out
   }
 
@@ -469,8 +467,6 @@ class LoadUnit(implicit p: Parameters) extends XSModule
     out.prf_wr        := false.B
     out.prf_i         := false.B
     out.sched_idx     := 0.U
-    out.hlv           := LSUOpType.isHlv(src.uop.fuOpType)
-    out.hlvx          := LSUOpType.isHlvx(src.uop.fuOpType)
     out.vecActive     := true.B
     out
   }
@@ -503,8 +499,6 @@ class LoadUnit(implicit p: Parameters) extends XSModule
     out.elemIdx       := src.elemIdx
     out.elemIdxInsideVd := src.elemIdxInsideVd
     out.alignedType   := src.alignedType
-    out.hlv           := LSUOpType.isHlv(src.uop.fuOpType)
-    out.hlvx          := LSUOpType.isHlvx(src.uop.fuOpType)
     out
   }
 
@@ -566,8 +560,6 @@ class LoadUnit(implicit p: Parameters) extends XSModule
     out.elemIdx             := src.elemIdx
     out.elemIdxInsideVd     := src.elemIdxInsideVd
     out.alignedType         := src.alignedType
-    out.hlv                 := false.B
-    out.hlvx                := false.B
     out
   }
 
@@ -589,8 +581,6 @@ class LoadUnit(implicit p: Parameters) extends XSModule
     out.prf_wr        := src.uop.fuOpType === LSUOpType.prefetch_w
     out.prf_i         := src.uop.fuOpType === LSUOpType.prefetch_i
     out.sched_idx     := 0.U
-    out.hlv           := LSUOpType.isHlv(src.uop.fuOpType)
-    out.hlvx          := LSUOpType.isHlvx(src.uop.fuOpType)
     out.vecActive     := true.B // true for scala load
     out
   }
@@ -617,8 +607,6 @@ class LoadUnit(implicit p: Parameters) extends XSModule
     out.prf_wr             := false.B
     out.prf_i              := false.B
     out.sched_idx          := 0.U
-    out.hlv                := LSUOpType.isHlv(out.uop.fuOpType)
-    out.hlvx               := LSUOpType.isHlvx(out.uop.fuOpType)
     out
   }
 
@@ -652,6 +640,25 @@ class LoadUnit(implicit p: Parameters) extends XSModule
   val int_vec_vaddr = Mux(s0_vec_iss_valid, io.vecldin.bits.vaddr, int_issue_vaddr)
   s0_tlb_vaddr := Mux((s0_super_ld_rep_valid || s0_ld_rep_valid), io.replay.bits.vaddr, int_vec_vaddr)
   s0_dcache_vaddr := Mux(s0_ld_fast_rep_select, io.fast_rep_in.bits.vaddr, Mux(s0_hw_prf_select, io.prefetch_req.bits.getVaddr(), s0_tlb_vaddr))
+
+  s0_tlb_hlv := Mux(
+    s0_super_ld_rep_valid || s0_ld_rep_valid,
+    LSUOpType.isHlv(io.replay.bits.uop.fuOpType),
+    Mux(
+      s0_int_iss_valid,
+      LSUOpType.isHlv(io.ldin.bits.uop.fuOpType),
+      false.B
+    )
+  )
+  s0_tlb_hlvx := Mux(
+    s0_super_ld_rep_valid || s0_ld_rep_valid,
+    LSUOpType.isHlvx(io.replay.bits.uop.fuOpType),
+    Mux(
+      s0_int_iss_valid,
+      LSUOpType.isHlvx(io.ldin.bits.uop.fuOpType),
+      false.B
+    )
+  )
 
   // address align check
   val s0_addr_aligned = LookupTree(Mux(s0_sel_src.isvec, s0_sel_src.alignedType(1,0), s0_sel_src.uop.fuOpType(1, 0)), List(
