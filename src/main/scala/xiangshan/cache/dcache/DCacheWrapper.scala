6070f1e93115f6dd18a0a5c6ae1af98f5e10d8cb
happy-lx <54952983+happy-lx@users.noreply.github.com>
2024-09-03 11:40:06 +0800

    fix(L1PF): fix good_prefetch Counting logic (#3474)
    Previous design:
    When a demand load hits a Cache block fetched by the prefetcher, the
    `PrefetchSource` of this block will be cleared,
    causing it to be mistakenly believed that it was not fetched by the
    prefetcher initially when it is subsequently replaced from the cache,
    resulting in not increasing the `good_prefetch` counter
    
    Fix:
    Now add a new cache block status(L1_HW_PREFETCH_CLEAR): indicating that
    this block was originally fetched by the prefetcher

--- src/main/scala/xiangshan/cache/dcache/DCacheWrapper.scala
+++ src/main/scala/xiangshan/cache/dcache/DCacheWrapper.scala
@@ -991,8 +991,8 @@ class DCacheImp(outer: DCache) extends LazyModuleImp(outer) with HasDCacheParame
     val extra_flag_prefetch = Mux1H(extra_flag_way_en, prefetchArray.io.resp.last)
     val extra_flag_access = Mux1H(extra_flag_way_en, accessArray.io.resp.last)
 
-    prefetcherMonitor.io.validity.good_prefetch := extra_flag_valid && isFromL1Prefetch(extra_flag_prefetch) && extra_flag_access
-    prefetcherMonitor.io.validity.bad_prefetch := extra_flag_valid && isFromL1Prefetch(extra_flag_prefetch) && !extra_flag_access
+    prefetcherMonitor.io.validity.good_prefetch := extra_flag_valid && isPrefetchRelated(extra_flag_prefetch) && extra_flag_access
+    prefetcherMonitor.io.validity.bad_prefetch := extra_flag_valid && isPrefetchRelated(extra_flag_prefetch) && !extra_flag_access
   }
 
   // write extra meta
