af95bc32d01e4c3190244795837729cf6e205392
Haoyuan Feng <fenghaoyuan19@mails.ucas.ac.cn>
2024-09-20 11:20:35 +0800

    fix(prefetch): MMIO address should not send prefetch requests (#3615)
    TODO: Prefetcher should check pmp & pma in order to decide whether to
    send requests

--- src/main/scala/system/SoC.scala
+++ src/main/scala/system/SoC.scala
@@ -41,6 +41,8 @@ case class SoCParameters
 (
   EnableILA: Boolean = false,
   PAddrBits: Int = 48,
+  PmemLowBound: Long = 0x80000000L,
+  PmemHighBound: Long = 0x80000000000L,
   extIntrs: Int = 64,
   L3NBanks: Int = 4,
   L3CacheParamsOpt: Option[HCCacheParameters] = Some(HCCacheParameters(
