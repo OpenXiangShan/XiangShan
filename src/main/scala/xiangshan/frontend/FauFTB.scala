933d7d87a48725deed79a7622bd79108d5084c42
YuanDL <2524247987@qq.com>
2024-09-24 18:19:46 +0800

    fix(ftb): When FTB is closed, the s2_multi_hit_enable should be lowered & FTB Reading Port Low fallthroughErr assert.

--- src/main/scala/xiangshan/frontend/FauFTB.scala
+++ src/main/scala/xiangshan/frontend/FauFTB.scala
@@ -121,6 +121,14 @@ class FauFTB(implicit p: Parameters) extends BasePredictor with FauFTBParams {
   io.fauftb_entry_out := s1_hit_fauftbentry
   io.fauftb_entry_hit_out := s1_hit && fauftb_enable
 
+  // Illegal check for FTB entry reading
+  val s1_pc_startLower             = Cat(0.U(1.W), s1_pc_dup(0)(instOffsetBits + log2Ceil(PredictWidth) - 1, instOffsetBits))
+  val uftb_entry_endLowerwithCarry = Cat(s1_hit_fauftbentry.carry, s1_hit_fauftbentry.pftAddr)
+  val fallThroughErr = s1_pc_startLower + (PredictWidth).U >= uftb_entry_endLowerwithCarry
+  when(io.s1_fire(0) && s1_hit){
+    assert(fallThroughErr, s"FauFTB read entry fallThrough address error!")
+  }
+
   // assign metas
   io.out.last_stage_meta := resp_meta.asUInt
   resp_meta.hit := RegEnable(RegEnable(s1_hit, io.s1_fire(0)), io.s2_fire(0))
