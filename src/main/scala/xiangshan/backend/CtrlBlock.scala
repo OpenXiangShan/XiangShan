20a43534ad41ed879e1336eb485e9c2fd825b145
tianxin <tianxin@iie.ac.cn>
2024-09-29 09:11:30 +0800

    [feat] Rebase Xiangshan commit id 18560912f7

--- src/main/scala/xiangshan/backend/CtrlBlock.scala
+++ src/main/scala/xiangshan/backend/CtrlBlock.scala
@@ -604,7 +604,6 @@ class CtrlBlockImp(
   io.robio.csr.perfinfo.retiredInstr <> RegNext(rob.io.csr.perfinfo.retiredInstr)
   io.robio.exception := rob.io.exception
   io.robio.exception.bits.pc := s1_robFlushPc
-  io.robio.exception.bits.gpaddr := gpaMem.io.exceptionReadData
 
   // rob to mem block
   io.robio.lsq <> rob.io.lsq
