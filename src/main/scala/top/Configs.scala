4aa305e9bf32bc7e999c69333b914c31e8f505b7
Ma-YX <71326427+Ma-YX@users.noreply.github.com>
2024-12-05 16:31:34 +0800

    feat(ECC): add ECC for coupledL2 and bump coupledL2 & openLLC (#3808)
    * add optional ECC for L2 Cache tag & data
    * add Poison & (optional) DataCheck for CHI

--- src/main/scala/top/Configs.scala
+++ src/main/scala/top/Configs.scala
@@ -299,6 +299,11 @@ class WithNKBL2
         )),
         reqField = Seq(utility.ReqSourceField()),
         echoField = Seq(huancun.DirtyField()),
+        tagECC = Some("secded"),
+        dataECC = Some("secded"),
+        enableTagECC = true,
+        enableDataECC = true,
+        dataCheck = Some("oddparity"),
         prefetch = Seq(BOPParameters()) ++
           (if (tp) Seq(TPParameters()) else Nil) ++
           (if (p.prefetcher.nonEmpty) Seq(PrefetchReceiverParams()) else Nil),
