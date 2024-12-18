b48ccab12428eaad488538489d1ccd658cacd6f3
lihuijin <501296508@qq.com>
2024-12-18 17:31:23 +0800

    ppa(LRQ): reduce LRQ entries

--- src/main/scala/xiangshan/mem/pipeline/LoadUnit.scala
+++ src/main/scala/xiangshan/mem/pipeline/LoadUnit.scala
@@ -1525,7 +1525,8 @@ class LoadUnit(implicit p: Parameters) extends XSModule
   io.fast_rep_out.bits.delayedLoadError := s3_hw_err
 
   val s3_vp_match_fail = GatedValidRegNext(s2_fwd_vp_match_invalid) && s3_troublem
-  val s3_rep_frm_fetch = s3_vp_match_fail
+  val s3_rep_frm_fetch_rep_full = io.lq_rep_full && s3_in.rep_info.need_rep && !s3_in.isLoadReplay
+  val s3_rep_frm_fetch = s3_vp_match_fail || s3_rep_frm_fetch_rep_full
   val s3_ldld_rep_inst =
       io.lsq.ldld_nuke_query.resp.valid &&
       io.lsq.ldld_nuke_query.resp.bits.rep_frm_fetch &&
