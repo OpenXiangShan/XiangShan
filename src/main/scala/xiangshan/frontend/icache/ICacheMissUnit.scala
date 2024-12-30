41ee097b7eac030ace59146c13898f14c9759184
ngc7331 <ngc7331@outlook.com>
2024-12-30 19:45:55 +0800

    fix(ICacheMissUnit): clear corrupt_r when response is sent to MainPipe

--- src/main/scala/xiangshan/frontend/icache/ICacheMissUnit.scala
+++ src/main/scala/xiangshan/frontend/icache/ICacheMissUnit.scala
@@ -345,8 +345,15 @@ class ICacheMissUnit(edge: TLEdgeOut)(implicit p: Parameters) extends ICacheModu
   // if any beat is corrupt, the whole response (to mainPipe/metaArray/dataArray) is corrupt
   private val corrupt_r = RegInit(false.B)
   when(io.mem_grant.fire && edge.hasData(io.mem_grant.bits) && io.mem_grant.bits.corrupt) {
+    // Set corrupt_r when any beat is corrupt
+    // This is actually when(xxx.fire && xxx.hasData) { corrupt_r := corrupt_r || io.mem_grant.bits.corrupt }
     corrupt_r := true.B
-  }.elsewhen(io.fetch_resp.fire) {
+  }.elsewhen(last_fire_r) {
+    // Clear corrupt_r when response it sent to mainPipe
+    // This used to be io.fetch_resp.valid (last_fire_r && mshr_resp.valid) but when mshr is flushed by io.flush/fencei,
+    // mshr_resp.valid is false.B and corrupt_r will never be cleared, that's not correct
+    // so we remove mshr_resp.valid here, and the condition leftover is last_fire_r
+    // or, actually, io.fetch_resp.valid || (last_fire_r && !mshr_resp.valid)
     corrupt_r := false.B
   }
 
