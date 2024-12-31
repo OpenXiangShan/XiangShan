64a0e441d4a5899a5dde62e2c2cc04aa178a18d2
zhaohong1988 <zhaohong@bosc.ac.cn>
2024-12-31 10:39:44 +0800

    feat:add requirement from ysw,add mtime register

--- src/main/scala/device/TIMER.scala
+++ src/main/scala/device/TIMER.scala
@@ -92,12 +92,14 @@ class TIMER(params: TIMERParams, beatBytes: Int)(implicit p: Parameters) extends
      */
 
     node.regmap(
-      0                -> RegFieldGroup ("msip", Some("MSIP Bits"), ipi.zipWithIndex.flatMap{ case (r, i) =>
-        RegField(1, r, RegFieldDesc(s"msip_$i", s"MSIP bit for Hart $i", reset=Some(0))) :: RegField(ipiWidth - 1) :: Nil }),
-      timecmpOffset(0) -> timecmp.zipWithIndex.flatMap{ case (t, i) => RegFieldGroup(s"mtimecmp_$i", Some(s"MTIMECMP for hart $i"),
-          RegField.bytes(t, Some(RegFieldDesc(s"mtimecmp_$i", "", reset=None))))}
-      // timeOffset       -> RegFieldGroup("mtime", Some("Timer Register"),
-      //  RegField.bytes(time, Some(RegFieldDesc("mtime", "", reset=Some(0), volatile=true))))
+      0 -> RegFieldGroup("msip", Some("MSIP Bits"), ipi.zipWithIndex.flatMap { case (r, i) =>
+        RegField(1, r, RegFieldDesc(s"msip_$i", s"MSIP bit for Hart $i", reset = Some(0))) :: RegField(ipiWidth - 1) :: Nil
+      }),
+      timecmpOffset(0) -> timecmp.zipWithIndex.flatMap { case (t, i) => RegFieldGroup(s"mtimecmp_$i", Some(s"MTIMECMP for hart $i"),
+        RegField.bytes(t, Some(RegFieldDesc(s"mtimecmp_$i", "", reset = None))))
+      },
+      timeOffset -> RegFieldGroup("mtime", Some("Timer Register"),
+        RegField.bytes(time, Some(RegFieldDesc("mtime", "", reset = Some(0), volatile = true))))
     )
   }
 }
