0fd007cdcda0590629591c80dd76bbe73d41dc17
linzhida <linzhida19@mails.ucas.ac.cn>
2025-01-07 14:59:01 +0800

    feat(difftest): support synchronization of custom CSR mflushpwr to REF

--- src/main/scala/xiangshan/backend/fu/NewCSR/NewCSR.scala
+++ src/main/scala/xiangshan/backend/fu/NewCSR/NewCSR.scala
@@ -1558,6 +1558,11 @@ class NewCSR(implicit val p: Parameters) extends Module
     diffAIAXtopeiEvent.mtopei := mtopei.rdata
     diffAIAXtopeiEvent.stopei := stopei.rdata
     diffAIAXtopeiEvent.vstopei := vstopei.rdata
+
+    val diffCustomMflushpwr = DifftestModule(new DiffSyncCustomMflushpwrEvent)
+    diffCustomMflushpwr.coreid := hartId
+    diffCustomMflushpwr.valid := RegNext(io.fromTop.l2FlushDone) =/= io.fromTop.l2FlushDone
+    diffCustomMflushpwr.l2FlushDone := io.fromTop.l2FlushDone
   }
 }
 
