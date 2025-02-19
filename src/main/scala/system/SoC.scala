81e10aa896f88a62c708e72ecee2ba98fbf9b831
Ma-YX <mayuexiao19@mails.ucas.ac.cn>
2025-02-19 15:57:10 +0800

    fix(Soc, CoupledL2): corret port width of CHI Issue C

--- src/main/scala/system/SoC.scala
+++ src/main/scala/system/SoC.scala
@@ -77,7 +77,7 @@ case class SoCParameters
   XSTopPrefix: Option[String] = None,
   NodeIDWidthList: Map[String, Int] = Map(
     "B" -> 7,
-    "C" -> 7,
+    "C" -> 9,
     "E.b" -> 11
   ),
   NumHart: Int = 64,
