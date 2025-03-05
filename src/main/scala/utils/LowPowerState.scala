c68fd095cdacd3db6f7faba9c91efa54a46faeb8
Zhu Yu <yulightenyu@gmail.com>
2025-03-05 12:00:26 +0800

    feat(XSNoCTop): add power on/off flow control   * WFI state fsm used to control gate clock when WFI   * CPU state fsm used to control power on/off flow   * PPU generate reset/clock signals of Core and physical signals nISOLATE/nPOWERDOWN

---
+++ src/main/scala/utils/LowPowerState.scala
@@ -0,0 +1,54 @@
+/***************************************************************************************
+ * Copyright (c) 2020-2021 Institute of Computing Technology, Chinese Academy of Sciences
+ * Copyright (c) 2020-2021 Peng Cheng Laboratory
+ *
+ * XiangShan is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
+
+package utils
+
+import chisel3._
+import chisel3.util._
+
+/* WFI state Update */
+object WfiStateNext {
+  val sNORMAL :: sGCLOCK :: sAWAKE :: Nil = Enum(3)
+
+  def apply(wfiState: UInt, isWFI: Bool, corePD: Bool, flitpend: Bool, intSrc: UInt): UInt = {
+    val nextState = MuxCase(wfiState, Array(
+      (wfiState === sNORMAL && isWFI && ~corePD && !intSrc.orR) -> sGCLOCK,
+      (wfiState === sGCLOCK && intSrc.orR) -> sAWAKE,
+      (wfiState === sGCLOCK && flitpend)   -> sNORMAL,
+      (wfiState === sAWAKE && !intSrc.orR) -> sNORMAL
+    ).toIndexedSeq)
+
+    nextState
+  }
+}
+
+/* Core low power state Update  */
+object lpStateNext {
+  val sIDLE :: sL2FLUSH :: sWAITWFI :: sPOFFREQ :: Nil = Enum(4)
+
+  def apply(lpState: UInt, corePD: Bool, l2FlushDone: Bool, isWFI: Bool): UInt = {
+    val nextState = MuxCase(lpState, Array(
+      (lpState === sIDLE && corePD) -> sL2FLUSH,
+      (lpState === sL2FLUSH && l2FlushDone && corePD) -> sWAITWFI,
+      (lpState === sL2FLUSH && !corePD) -> sIDLE,
+      (lpState === sWAITWFI && !corePD) -> sIDLE,
+      (lpState === sWAITWFI && corePD && isWFI ) -> sPOFFREQ
+    ).toIndexedSeq)
+
+    nextState
+  }
+}
+ 
