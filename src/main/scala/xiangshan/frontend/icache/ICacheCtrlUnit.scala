86a4629cf40f4b89ab2153b0a49b153ec731ef8b
ngc7331 <ngc7331@outlook.com>
2025-03-06 15:14:19 +0800

    fix(ICacheCtrlUnit): fix InjectFsmState width

--- src/main/scala/xiangshan/frontend/icache/ICacheCtrlUnit.scala
+++ src/main/scala/xiangshan/frontend/icache/ICacheCtrlUnit.scala
@@ -148,7 +148,7 @@ class ICacheCtrlUnit(params: ICacheCtrlUnitParams)(implicit p: Parameters) exten
 
     // inject FSM
     private def nInjectFsmState: Int = 5
-    private object InjectFsmState extends NamedUInt(nInjectFsmState) {
+    private object InjectFsmState extends NamedUInt(log2Up(nInjectFsmState)) {
       // scala ask identifier that begins with uppercase cannot be used in pattern matching like `X :: Nil = Enum()`
       // but we want UpperCamelCase for constants for better readability, so we dont use Enum() here
       def Idle:         UInt = 0.U(width.W)
