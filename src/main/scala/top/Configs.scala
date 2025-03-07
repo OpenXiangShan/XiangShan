b002290e9dc3e283090dbf50af156be54cd714a9
lihuijin <501296508@qq.com>
2025-03-07 17:43:26 +0800

    fix(openLLC): fix fullAddressBits 16 -> 48

--- src/main/scala/top/Configs.scala
+++ src/main/scala/top/Configs.scala
@@ -235,6 +235,7 @@ class MinimalConfig(n: Int = 1) extends Config(
           ways = 8,
           sets = 2048,
           banks = 4,
+          fullAddressBits = 48,
           clientCaches = Seq(L2Param())
         )),
         L3NBanks = 1
