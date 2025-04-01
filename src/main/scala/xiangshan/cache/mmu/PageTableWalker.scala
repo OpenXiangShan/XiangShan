220c47018a2bfc6d988c45b5d2b42f759b8842e1
Haoyuan Feng <fenghaoyuan19@mails.ucas.ac.cn>
2025-04-01 08:41:02 +0200

    fix(PTW): fix gpf_level generate logic when Sv39 (#4482)
    For historical reasons, `gpf_level` needs to be 1 greater than
    `af_level`. In the original design, only the Sv48 case was considered,
    and a similar treatment is needed for the Sv39 case.
    
    As an extra note here, `gpf_level` is a 2-bit register, so when
    `gpf_level` = 0.U, `gpf_level` - 2.U = `00` - 2 = `10` = 2.U. The PTW
    module's `gpf_level` computation uses this feature, which is extremely
    unscalable, and, except for the original author, is completely
    incomprehensible, and is in dire need of refactoring.

--- src/main/scala/xiangshan/cache/mmu/PageTableWalker.scala
+++ src/main/scala/xiangshan/cache/mmu/PageTableWalker.scala
@@ -338,7 +338,7 @@ class PTW()(implicit p: Parameters) extends XSModule with HasPtwConst with HasPe
       } .otherwise {
         level := Mux(req.l2Hit, 1.U, 2.U)
         af_level := Mux(req.l2Hit, 1.U, 2.U)
-        gpf_level := 0.U
+        gpf_level := Mux(req.l2Hit, 2.U, 0.U)
         ppn := Mux(req.l2Hit, io.req.bits.ppn, satp.ppn)
         l3Hit := false.B
         gvpn_wire := Mux(req.l2Hit, io.req.bits.ppn, satp.ppn)
@@ -346,7 +346,7 @@ class PTW()(implicit p: Parameters) extends XSModule with HasPtwConst with HasPe
     } else {
       level := Mux(req.l2Hit, 1.U, 2.U)
       af_level := Mux(req.l2Hit, 1.U, 2.U)
-      gpf_level := 0.U
+      gpf_level := Mux(req.l2Hit, 2.U, 0.U)
       ppn := Mux(req.l2Hit, io.req.bits.ppn, satp.ppn)
       l3Hit := false.B
       gvpn_wire := Mux(req.l2Hit, io.req.bits.ppn, satp.ppn)
@@ -478,7 +478,7 @@ class PTW()(implicit p: Parameters) extends XSModule with HasPtwConst with HasPe
   when(mem.resp.fire && w_mem_resp === false.B){
     w_mem_resp := true.B
     af_level := af_level - 1.U
-    gpf_level := Mux(mode === Sv39 && !pte_valid && !(l3Hit || l2Hit), gpf_level - 2.U, gpf_level - 1.U)
+    gpf_level := Mux(mode === Sv39 && !pte_valid && !l2Hit, gpf_level - 2.U, gpf_level - 1.U)
     pte_valid := true.B
     update_full_gvpn_mem_resp := true.B
     if (HasBitmapCheck) {
