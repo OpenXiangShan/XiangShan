4527578a8720d140284a6192790cabf7e801c787
xu_zh <ngc7331@outlook.com>
2025-04-08 16:16:43 +0800

    refactor(InstrUncache): follow new style guide (#4511)
    - each class in a separate file (except Bundles / Utils)
    - module IO inside module class
    - renames:
      - camel cases
      - unify bundle naming convention
      - re-organize IO
    - rewrite `InstrUncacheEntry`
      - use `switch-is` for fsm
      - avoid `DontCare`: we can assign values directly
    - replace `Mux(pc(...), data(...), Mux(pc(...), ..., ...))` tree with
    `Mux1H(UIntToOH(pc), Seq(data(...), ...))` for better readability (and
    maybe timing?)
    - remove InstrUncache's dependency on `ICacheParameters`

---
+++ src/main/scala/xiangshan/frontend/instruncache/InstrUncacheEntry.scala
@@ -0,0 +1,121 @@
+// Copyright (c) 2024 Beijing Institute of Open Source Chip (BOSC)
+// Copyright (c) 2020-2024 Institute of Computing Technology, Chinese Academy of Sciences
+// Copyright (c) 2020-2021 Peng Cheng Laboratory
+//
+// XiangShan is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//          https://license.coscl.org.cn/MulanPSL2
+//
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+// EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+// MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+//
+// See the Mulan PSL v2 for more details.
+
+package xiangshan.frontend.instruncache
+
+import chisel3._
+import chisel3.util._
+import freechips.rocketchip.tilelink.TLBundleA
+import freechips.rocketchip.tilelink.TLBundleD
+import freechips.rocketchip.tilelink.TLEdgeOut
+import org.chipsalliance.cde.config.Parameters
+import utils.NamedUInt
+
+// One miss entry deals with one mmio request
+class InstrUncacheEntry(edge: TLEdgeOut)(implicit p: Parameters) extends InstrUncacheModule {
+  class InstrUncacheEntryIO(edge: TLEdgeOut)(implicit p: Parameters) extends InstrUncacheBundle {
+    val id: UInt = Input(UInt(log2Up(nMmioEntry).W))
+    // client requests
+    val req:  DecoupledIO[InstrUncacheReq]  = Flipped(DecoupledIO(new InstrUncacheReq))
+    val resp: DecoupledIO[InstrUncacheResp] = DecoupledIO(new InstrUncacheResp)
+
+    val mmioAcquire: DecoupledIO[TLBundleA] = DecoupledIO(new TLBundleA(edge.bundle))
+    val mmioGrant:   DecoupledIO[TLBundleD] = Flipped(DecoupledIO(new TLBundleD(edge.bundle)))
+
+    val flush: Bool = Input(Bool())
+  }
+
+  val io: InstrUncacheEntryIO = IO(new InstrUncacheEntryIO(edge))
+
+  private def nState: Int = 4
+  private object State extends NamedUInt(log2Up(nState)) {
+    def Invalid:    UInt = 0.U(width.W)
+    def RefillReq:  UInt = 1.U(width.W)
+    def RefillResp: UInt = 2.U(width.W)
+    def SendResp:   UInt = 3.U(width.W)
+  }
+
+  private val state = RegInit(State.Invalid)
+
+  // hold flush state
+  private val needFlush = RegInit(false.B)
+  when(io.flush && (state =/= State.Invalid) && (state =/= State.SendResp)) {
+    needFlush := true.B
+  }.elsewhen((state === State.SendResp) && needFlush) {
+    needFlush := false.B
+  }
+
+  // receive request from InstrUncache
+  io.req.ready := state === State.Invalid
+  private val reqReg      = RegEnable(io.req.bits, 0.U.asTypeOf(io.req.bits), io.req.fire)
+  private val alignedAddr = reqReg.addr(reqReg.addr.getWidth - 1, log2Ceil(MmioBusBytes))
+
+  // send tilelink request
+  io.mmioAcquire.valid := state === State.RefillReq
+  io.mmioAcquire.bits := edge.Get(
+    fromSource = io.id,
+    toAddress = Cat(alignedAddr, 0.U(log2Ceil(MmioBusBytes).W)),
+    lgSize = log2Ceil(MmioBusBytes).U
+  )._2
+
+  // receive tilelink response
+  io.mmioGrant.ready := state === State.RefillResp
+  private val respDataReg    = RegEnable(io.mmioGrant.bits.data, 0.U(MmioBusWidth.W), io.mmioGrant.fire)
+  private val respCorruptReg = RegEnable(io.mmioGrant.bits.corrupt, false.B, io.mmioGrant.fire)
+
+  // send response to InstrUncache
+  io.resp.valid := state === State.SendResp && !needFlush
+  io.resp.bits.data := Mux1H(
+    UIntToOH(reqReg.addr(2, 1)),
+    Seq(
+      respDataReg(31, 0),
+      respDataReg(47, 16),
+      respDataReg(63, 32),
+      Cat(0.U(16.W), respDataReg(63, 48))
+    )
+  )
+  io.resp.bits.corrupt := respCorruptReg
+
+  // state transfer
+  switch(state) {
+    is(State.Invalid) {
+      when(io.req.fire) {
+        state := State.RefillReq
+      }
+    }
+
+    is(State.RefillReq) {
+      when(io.mmioAcquire.fire) {
+        state := State.RefillResp
+      }
+    }
+
+    is(State.RefillResp) {
+      when(io.mmioGrant.fire) {
+        // we request size <= mmio bus width, so we should be able to get full response in one beat,
+        // which means we can assert refillDone when io.mmioGrant.fire
+        val (_, _, refillDone, _) = edge.addr_inc(io.mmioGrant)
+        assert(refillDone)
+        state := State.SendResp
+      }
+    }
+
+    is(State.SendResp) {
+      when(io.resp.fire || needFlush) {
+        state := State.Invalid
+      }
+    }
+  }
+}
