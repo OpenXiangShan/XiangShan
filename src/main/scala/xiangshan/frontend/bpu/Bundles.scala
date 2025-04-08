0b5df8087a808a8d4c9c29164a88b47a97fe203f
ngc7331 <ngc7331@outlook.com>
2025-04-08 16:43:11 +0800

    feat(Bpu): add BranchAttribute

--- src/main/scala/xiangshan/frontend/bpu/Bundles.scala
+++ src/main/scala/xiangshan/frontend/bpu/Bundles.scala
@@ -17,3 +17,17 @@ package xiangshan.frontend.bpu
 
 import chisel3._
 import chisel3.util._
+import utils.NamedUInt
+
+object BranchAttribute extends NamedUInt(3) {
+  // no branch
+  def None: UInt = 0.U(width.W)
+  // special indirect branches: return/call, refer to risc-v spec Table3. Return-address stack prediction hints
+  def Pop:        UInt = 1.U(width.W) // return: rs1 is x1/x5 and rd is not
+  def Push:       UInt = 2.U(width.W) // call: rd is x1/x5 and rs1 is not; or rd is x1/x5 and rs1 == rd
+  def PopAndPush: UInt = 3.U(width.W) // return & call: both rd and rs1 is x1/x5, and rs1 != rd
+  // conditional branches: beq, bne, blt, bge, bltu, bgeu
+  def Conditional: UInt = 4.U(width.W)
+  // indirect branches, other than return/call
+  def Indirect: UInt = 5.U(width.W)
+}
