c92b44adf80fc4d5bdc2fd5c2eaed1f3deddeacc
ngc7331 <ngc7331@outlook.com>
2025-04-14 22:13:24 +0800

    feat(utils): add assertLegal method

--- src/main/scala/utils/EnumUInt.scala
+++ src/main/scala/utils/EnumUInt.scala
@@ -98,36 +98,44 @@ abstract class EnumUInt(
       }
     ) {
 
-  def validate(): Unit = {
+  private var names     = Seq[String]()
+  private var values    = Seq[UInt]()
+  private var litValues = Seq[BigInt]()
+
+  private def getValuesString: String =
+    (this.names zip this.litValues).sortBy { case (_, i) => i }.mkString("[", ", ", "]")
+
+  private def validate(): Unit = {
     val methodsAll = this.getClass.getDeclaredMethods
       .filter { method =>
         method.getReturnType == classOf[UInt] && // return UInt
         method.getParameterTypes.isEmpty         // no parameters
       }
     val methods = methodsAll.filter(_.getName()(0).isUpper) // UpperCamelCase
-    var values  = Seq[BigInt]()
-    methods.foreach { method =>
-      val uint   = method.invoke(this).asInstanceOf[UInt]
-      val value  = uint.litValue
-      val dupIdx = values.indexOf(value)
+    this.names = methods.map(_.getName).toSeq
+    this.values = methods.map(_.invoke(this).asInstanceOf[UInt]).toSeq
+    this.litValues = this.values.map(_.litValue)
+
+    (this.names zip this.values zip this.litValues).zipWithIndex.foreach { case (((name, value), litValue), i) =>
+      val dupIdx = this.litValues.slice(0, i).indexOf(litValue)
       assert( // check1: all values must have a correct width
-        uint.getWidth == width,
-        s"EnumUInt ${this.getClass.getName} has value with width(${uint.getWidth}), expected $width: ${method.getName}."
+        value.getWidth == this.width,
+        s"EnumUInt ${this.getClass.getName} has value with width(${value.getWidth}), expected $width: ${name}."
       )
       assert( // check2: all values must be unique, unless allowDuplicate is true
         allowDuplicate || dupIdx == -1,
-        s"EnumUInt ${this.getClass.getName} has duplicate value($value): ${method.getName} and ${methods(dupIdx).getName}."
+        s"EnumUInt ${this.getClass.getName} has duplicate value(${litValue}): ${name} and ${this.names(dupIdx)}."
       )
       assert( // check3: one-hot values must be power of 2
-        !useOneHot || isPow2(value),
-        s"EnumUInt ${this.getClass.getName} has non one-hot value($value): ${method.getName}."
+        !useOneHot || isPow2(litValue),
+        s"EnumUInt ${this.getClass.getName} has non one-hot value(${litValue}): ${name}."
       )
-      values = values :+ value
     }
 
     // check4: if allowDuplicate is true, we can have more than n methods, but we must have n unique values
     // check4: otherwise, we must have n methods and n unique values
-    if (values.toSet.size != n) {
+    val uniqueLitValues = this.litValues.toSet.size
+    if (uniqueLitValues != this.n) {
       // warn if there are methods seems to be constants but not UpperCamelCase before throw AssertionError
       val methodsNotUpperCamelCase = methodsAll.diff(methods)
       methodsNotUpperCamelCase.foreach { method =>
@@ -137,17 +145,66 @@ abstract class EnumUInt(
         )
       }
       assert(
-        false,
-        s"EnumUInt ${this.getClass.getName} has ${values.toSet.size} unique values, but expected $n." +
+        cond = false,
+        s"EnumUInt ${this.getClass.getName} has ${uniqueLitValues} unique values, but expected ${this.n}." +
           (if (methodsNotUpperCamelCase.nonEmpty) " Maybe you miss UpperCamelCase? check warnings above." else "")
       )
     }
 
     // pass!
-    println(
-      s"EnumUInt ${this.getClass.getName} validated:" +
-        s"${(methods.map(_.getName) zip values).sortBy { case (_, i) => i }.mkString("[", ",", "]")}"
-    )
+    println(s"EnumUInt ${this.getClass.getName} validated: ${this.getValuesString}")
+  }
+
+  /** Check or generate assertion for unsafe inputs
+   * @example {{{
+   *   class ExceptionType extends Bundle {
+   *     val value: UInt = ExceptionType() // this is inherit from NamedUInt.apply(), i.e. UInt(width.W)
+   *
+   *     def hasException: Bool = value =/= ExceptionType.None
+   *   }
+   *
+   *   object ExceptionType extends EnumUInt(4) {
+   *     def None: UInt = 0.U(width.W)
+   *     def Pf:   UInt = 1.U(width.W)
+   *     def Gpf:  UInt = 2.U(width.W)
+   *     def Af:   UInt = 3.U(width.W)
+   *
+   *     def apply(that: UInt): ExceptionType = {
+   *       assertLegal(that) // do check or generate assertion before do wiring
+   *       val e = Wire(new ExceptionType)
+   *       e.value := that
+   *       e
+   *     }
+   *   }
+   *
+   *   val e1 = ExceptionType(4.U) // will be checked at compile time, and fails as 4.U is not a legal value
+   *
+   *   val unsafeWire = Wire(UInt(2.W))
+   *   val e2 = ExceptionType(unsafeWire) // will be checked at runtime
+   *
+   *   val e3 = 0.U.asTypeOf(new ExceptionType) // this cannot be checked, we should avoid at best effort
+   *   // -> val e3 = ExceptionType(0.U) // this is better
+   * }}}
+   */
+  def assertLegal(that: UInt): Unit = {
+    if (that.isLit) { // if is literal, check value at compile time (if legal, we don't need to care about width)
+      assert(
+        this.litValues.contains(that.litValue),
+        s"EnumUInt ${this.getClass.getName} gets illegal input with value(${that.litValue}), " +
+          s"expected one of ${this.getValuesString}."
+      )
+    } else { // otherwise, check width at compile time, and value at runtime
+      assert(
+        this.width == that.getWidth,
+        s"EnumUInt ${this.getClass.getName} gets illegal input with width(${that.getWidth}), expected $width."
+      )
+      assert(
+        VecInit(this.values.map(_ === that)).asUInt.orR,
+        s"EnumUInt ${this.getClass.getName} gets illegal input with value(%d), " +
+          s"expected one of ${this.getValuesString}.",
+        that // that is a hardware Wire/Reg, we cannot use litValue
+      )
+    }
   }
 
   // call validate() in the constructor
