69b78670068a0a6f0fb634fc02c24fc772838119
Tang Haojin <tanghaojin@outlook.com>
2025-04-14 11:11:23 +0800

    feat: add more configuration (#4532)

--- src/main/scala/top/YamlParser.scala
+++ src/main/scala/top/YamlParser.scala
@@ -24,24 +24,38 @@ import org.chipsalliance.cde.config.Parameters
 import system.SoCParamsKey
 import xiangshan.backend.fu.{MemoryRange, PMAConfigEntry}
 import xiangshan.XSTileKey
-import freechips.rocketchip.devices.debug.DebugModuleKey
+import freechips.rocketchip.devices.debug.{DebugAttachParams, ExportDebug}
+import freechips.rocketchip.devices.debug.{DMI, JTAG, CJTAG, APB}
+import freechips.rocketchip.devices.debug.{DebugModuleKey, DebugModuleParams}
 import freechips.rocketchip.diplomacy.AddressSet
+import freechips.rocketchip.tile.MaxHartIdBits
 import freechips.rocketchip.util.AsyncQueueParams
 import device.IMSICBusType
 
 case class YamlConfig(
+  Config: Option[String],
   PmemRanges: Option[List[MemoryRange]],
   PMAConfigs: Option[List[PMAConfigEntry]],
   EnableCHIAsyncBridge: Option[Boolean],
   L2CacheConfig: Option[L2CacheConfig],
   L3CacheConfig: Option[L3CacheConfig],
-  DebugModuleBaseAddr: Option[BigInt],
+  HartIDBits: Option[Int],
+  DebugAttachProtocals: Option[List[String]],
+  DebugModuleParams: Option[DebugModuleParams],
   WFIResume: Option[Boolean],
   SeperateDM: Option[Boolean],
   SeperateTLBus: Option[Boolean],
   SeperateTLBusRanges: Option[List[AddressSet]],
+  EnableSeperateTLBusAsyncBridge: Option[Boolean],
   IMSICBusType: Option[String],
   IMSICParams: Option[IMSICParams],
+  CHIIssue: Option[String],
+  WFIClockGate: Option[Boolean],
+  EnablePowerDown: Option[Boolean],
+  XSTopPrefix: Option[String],
+  EnableDFX: Option[Boolean],
+  EnableSramCtl: Option[Boolean],
+  EnableCHINS: Option[Boolean],
 )
 
 object YamlParser {
@@ -57,6 +71,9 @@ object YamlParser {
       case Right(value) => value
     }
     var newConfig = config
+    yamlConfig.Config.foreach { config =>
+      newConfig = ArgParser.getConfigByName(config)
+    }
     yamlConfig.PmemRanges.foreach { ranges =>
       newConfig = newConfig.alter((site, here, up) => {
         case SoCParamsKey => up(SoCParamsKey).copy(PmemRanges = ranges)
@@ -76,9 +93,24 @@ object YamlParser {
     }
     yamlConfig.L2CacheConfig.foreach(l2 => newConfig = newConfig.alter(l2))
     yamlConfig.L3CacheConfig.foreach(l3 => newConfig = newConfig.alter(l3))
-    yamlConfig.DebugModuleBaseAddr.foreach { addr =>
+    yamlConfig.DebugAttachProtocals.foreach { protocols =>
       newConfig = newConfig.alter((site, here, up) => {
-        case DebugModuleKey => up(DebugModuleKey).map(_.copy(baseAddress = addr))
+        case ExportDebug => DebugAttachParams(protocols = protocols.map {
+          case "DMI" => DMI
+          case "JTAG" => JTAG
+          case "CJTAG" => CJTAG
+          case "APB" => APB
+        }.toSet)
+      })
+    }
+    yamlConfig.HartIDBits.foreach { bits =>
+      newConfig = newConfig.alter((site, here, up) => {
+        case MaxHartIdBits => bits
+      })
+    }
+    yamlConfig.DebugModuleParams.foreach { params =>
+      newConfig = newConfig.alter((site, here, up) => {
+        case DebugModuleKey => Some(params)
       })
     }
     yamlConfig.WFIResume.foreach { enable =>
@@ -101,6 +133,13 @@ object YamlParser {
         case SoCParamsKey => up(SoCParamsKey).copy(SeperateTLBusRanges = ranges)
       })
     }
+    yamlConfig.EnableSeperateTLBusAsyncBridge.foreach { enable =>
+      newConfig = newConfig.alter((site, here, up) => {
+        case SoCParamsKey => up(SoCParamsKey).copy(
+          SeperateTLAsyncBridge = Option.when(enable)(AsyncQueueParams(depth = 1, sync = 3, safe = false))
+        )
+      })
+    }
     yamlConfig.IMSICBusType.foreach { busType =>
       newConfig = newConfig.alter((site, here, up) => {
         case SoCParamsKey => up(SoCParamsKey).copy(IMSICBusType = device.IMSICBusType.withName(busType))
@@ -109,6 +148,41 @@ object YamlParser {
     yamlConfig.IMSICParams.foreach { params =>
       newConfig = newConfig.alter((site, here, up) => {
         case SoCParamsKey => up(SoCParamsKey).copy(IMSICParams = params)
+      })
+    }
+    yamlConfig.CHIIssue.foreach { issue =>
+      newConfig = newConfig.alter((site, here, up) => {
+        case coupledL2.tl2chi.CHIIssue => issue
+      })
+    }
+    yamlConfig.WFIClockGate.foreach { enable =>
+      newConfig = newConfig.alter((site, here, up) => {
+        case SoCParamsKey => up(SoCParamsKey).copy(WFIClockGate = enable)
+      })
+    }
+    yamlConfig.EnablePowerDown.foreach { enable =>
+      newConfig = newConfig.alter((site, here, up) => {
+        case SoCParamsKey => up(SoCParamsKey).copy(EnablePowerDown = enable)
+      })
+    }
+    yamlConfig.XSTopPrefix.foreach { prefix =>
+      newConfig = newConfig.alter((site, here, up) => {
+        case SoCParamsKey => up(SoCParamsKey).copy(XSTopPrefix = Option.when(prefix.nonEmpty)(prefix))
+      })
+    }
+    yamlConfig.EnableDFX.foreach { enable =>
+      newConfig = newConfig.alter((site, here, up) => {
+        case XSTileKey => up(XSTileKey).map(_.copy(hasMbist = enable))
+      })
+    }
+    yamlConfig.EnableSramCtl.foreach { enable =>
+      newConfig = newConfig.alter((site, here, up) => {
+        case XSTileKey => up(XSTileKey).map(_.copy(hasSramCtl = enable))
+      })
+    }
+    yamlConfig.EnableCHINS.foreach { enable =>
+      newConfig = newConfig.alter((site, here, up) => {
+        case coupledL2.tl2chi.NonSecureKey => enable
       })
     }
     newConfig
