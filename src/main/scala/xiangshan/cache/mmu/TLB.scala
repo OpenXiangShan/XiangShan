0ca3be60970247caada08e6d323ce77703684fc2
Haoyuan Feng <fenghaoyuan19@mails.ucas.ac.cn>
2025-04-20 15:17:20 +0800

    fix(TLB): explicitly specify the signal width again when truncated (#4588)
    Similar to https://github.com/OpenXiangShan/XiangShan/pull/4471

--- src/main/scala/xiangshan/cache/mmu/TLB.scala
+++ src/main/scala/xiangshan/cache/mmu/TLB.scala
@@ -572,8 +572,10 @@ class TLB(Width: Int, nRespDups: Int = 1, Block: Seq[Boolean], q: TLBParameters)
       val s2xlate = io.ptw.resp.bits.s2xlate
       resp(idx).valid := true.B
       resp(idx).bits.miss := false.B
-      val s1_paddr = Cat(stage1.genPPN(get_pn(req_out(idx).vaddr)), get_off(req_out(idx).vaddr))
-      val s2_paddr = Cat(stage2.genPPNS2(get_pn(req_out(idx).vaddr)), get_off(req_out(idx).vaddr))
+      val s1_ppn = stage1.genPPN(get_pn(req_out(idx).vaddr))(ppnLen - 1, 0)
+      val s2_ppn = stage2.genPPNS2(get_pn(req_out(idx).vaddr))(ppnLen - 1, 0)
+      val s1_paddr = Cat(s1_ppn, get_off(req_out(idx).vaddr))
+      val s2_paddr = Cat(s2_ppn, get_off(req_out(idx).vaddr))
       for (d <- 0 until nRespDups) {
         resp(idx).bits.paddr(d) := Mux(s2xlate === onlyStage2 || s2xlate === allStage, s2_paddr, s1_paddr)
         resp(idx).bits.gpaddr(d) := s1_paddr
