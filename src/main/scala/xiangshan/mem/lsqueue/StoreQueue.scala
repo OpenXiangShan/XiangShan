5184d106c2b13f03f4b56d4079b11c5bf2399e42
Anzooooo <anzoso@outlook.com>
2025-05-07 17:15:59 +0800

    fix(StoreQueue): cbozero flag should not be set on exception

--- src/main/scala/xiangshan/mem/lsqueue/StoreQueue.scala
+++ src/main/scala/xiangshan/mem/lsqueue/StoreQueue.scala
@@ -939,7 +939,7 @@ class StoreQueue(implicit p: Parameters) extends XSModule
 
   // RegNext(io.sbuffer(i).fire) is used to alignment timing
   val isCboZeroToSbVec = (0 until EnsbufferWidth).map{ i =>
-    RegNext(io.sbuffer(i).fire) && uop(deqPtrExt(i).value).fuOpType === LSUOpType.cbo_zero && allocated(deqPtrExt(i).value)
+    RegNext(io.sbuffer(i).fire && io.sbuffer(i).bits.vecValid) && uop(deqPtrExt(i).value).fuOpType === LSUOpType.cbo_zero && allocated(deqPtrExt(i).value)
   }
   val cboZeroToSb        = isCboZeroToSbVec.reduce(_ || _)
   val cboZeroFlushSb     = GatedRegNext(cboZeroToSb)
