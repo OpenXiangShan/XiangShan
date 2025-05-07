a0d02cd00f32ef7cdec9703a8511e46e729f063f
chengguanghui <wissycgh@gmail.com>
2025-05-07 17:29:24 +0800

    fix(smstateen): add [m|h|s]stateen[1|2|3] CSRs
     * add [m|h|s]stateen[1|2|3] CSRs.
     * only reset mstateen[0|1|2|3] when init.
     * Bits in any stateen CSR that are defined to control state that a hart doesn’t implement are read-only zeros
    for that hart.

--- src/main/scala/xiangshan/backend/fu/NewCSR/MachineLevel.scala
+++ src/main/scala/xiangshan/backend/fu/NewCSR/MachineLevel.scala
@@ -381,7 +381,13 @@ trait MachineLevel { self: NewCSR =>
   }))
     .setAddr(CSRs.mconfigptr)
 
-  val mstateen0 = Module(new CSRModule("Mstateen", new MstateenBundle0)).setAddr(CSRs.mstateen0)
+  val mstateen0 = Module(new CSRModule("Mstateen0", new Mstateen0Bundle)).setAddr(CSRs.mstateen0)
+
+  val mstateen1 = Module(new CSRModule("Mstateen1", new MstateenNonZeroBundle)).setAddr(CSRs.mstateen1)
+
+  val mstateen2 = Module(new CSRModule("Mstateen2", new MstateenNonZeroBundle)).setAddr(CSRs.mstateen2)
+
+  val mstateen3 = Module(new CSRModule("Mstateen3", new MstateenNonZeroBundle)).setAddr(CSRs.mstateen3)
 
   // smrnmi extension
   val mnepc = Module(new CSRModule("Mnepc", new Epc) with TrapEntryMNEventSinkBundle {
@@ -432,6 +438,9 @@ trait MachineLevel { self: NewCSR =>
     mhartid,
     mconfigptr,
     mstateen0,
+    mstateen1,
+    mstateen2,
+    mstateen3,
     mnepc,
     mncause,
     mnstatus,
