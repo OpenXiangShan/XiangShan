4502dd2b44de1e3ba4a880b8721ff8dad0d19d2d
zhou tao <mayflyandtree@163.com>
2025-06-20 10:35:04 +0800

    style(IBuffer): modify the code formatting.

--- src/main/scala/xiangshan/frontend/IBuffer.scala
+++ src/main/scala/xiangshan/frontend/IBuffer.scala
@@ -99,14 +99,13 @@ class IBufEntry(implicit p: Parameters) extends XSBundle {
   val debug_seqNum     = InstSeqNum()
 
   def fromFetch(fetch: FetchToIBuffer, i: Int): IBufEntry = {
-    inst               := fetch.instrs(i)
-    pc                 := fetch.pc(i)
-    foldpc             := fetch.foldpc(i)
-    pd                 := fetch.pd(i)
-    pred_taken         := fetch.ftqPcOffset(i).valid
-    ftqPtr             := fetch.ftqPtr
-    ftqPcOffset.borrow := fetch.ftqPcOffset(i).bits.borrow
-    ftqPcOffset.offset := fetch.ftqPcOffset(i).bits.offset
+    inst        := fetch.instrs(i)
+    pc          := fetch.pc(i)
+    foldpc      := fetch.foldpc(i)
+    pd          := fetch.pd(i)
+    pred_taken  := fetch.ftqPcOffset(i).valid
+    ftqPtr      := fetch.ftqPtr
+    ftqPcOffset := fetch.ftqPcOffset(i).bits
     exceptionType := IBufferExceptionType.cvtFromFetchExcpAndCrossPageAndRVCII(
       fetch.exceptionType(i),
       fetch.crossPageIPFFix(i),
