58fd0c5f4df1108e8c8f41ee6fc2744befc5d328
ngc7331 <ngc7331@outlook.com>
2025-06-27 14:12:32 +0800

    fix(ubtb): check if t0 hits t1

--- src/main/scala/xiangshan/frontend/bpu/ubtb/Ubtb.scala
+++ src/main/scala/xiangshan/frontend/bpu/ubtb/Ubtb.scala
@@ -40,12 +40,19 @@ class Ubtb(implicit p: Parameters) extends BasePredictor with HasUbtbParameters 
   private val replacer = Module(new UbtbReplacer)
   replacer.io.usefulCnt := VecInit(entries.map(_.usefulCnt))
 
-  /* *** predict stage 0 *** */
+  /* *** predict stage 0 ***
+   * - io.startVAddr timing might be bad, simply cache it
+   */
   private val s0_fire = io.stageCtrl.s0_fire && io.enable
 
   private val s0_startVAddr = io.startVAddr
 
-  /* *** predict stage 1 *** */
+  /* *** predict stage 1 ***
+   * - read regfile
+   * - check if it's hit
+   * - generate prediction
+   * - update replacer
+   */
   private val s1_fire = io.stageCtrl.s1_fire && io.enable
 
   private val s1_startVAddr = RegEnable(s0_startVAddr, s0_fire)
@@ -76,7 +83,12 @@ class Ubtb(implicit p: Parameters) extends BasePredictor with HasUbtbParameters 
   replacer.io.predTouch.valid := s1_hit && s1_fire
   replacer.io.predTouch.bits  := s1_hitIdx
 
-  /* *** train stage 0 *** */
+  /* *** train stage 0 ***
+   * - read entries
+   * - check if hits entries
+   * - check if hits t1 stage
+   * - calculate hit flags
+   */
   private val t0_valid = io.train.valid
 
   private val t0_startVAddr  = io.train.bits.startVAddr
@@ -86,12 +98,34 @@ class Ubtb(implicit p: Parameters) extends BasePredictor with HasUbtbParameters 
   private val t0_target      = getEntryTarget(io.train.bits.target)
   private val t0_attribute   = io.train.bits.attribute
 
-  private val t0_hitOH  = VecInit(entries.map(e => e.valid && e.tag === t0_tag)).asUInt
-  private val t0_hit    = t0_hitOH.orR
-  private val t0_hitIdx = OHToUInt(t0_hitOH)
+  private val t0_hitOH = VecInit(entries.map(e => e.valid && e.tag === t0_tag)).asUInt
+  // t0 may hit t1, so we add a "real" prefix for entries hit
+  private val t0_realHit    = t0_hitOH.orR
+  private val t0_realHitIdx = OHToUInt(t0_hitOH)
 
-  // calculate hit states (flags), valid only when t0_hit
-  private val t0_hitEntry         = entries(t0_hitIdx)
+  // If there are two contiguous trains, the first one is too late to be written to the entries,
+  // the second train might be a false "not hit" and allocate a new entry, causing a multi-hit;
+  // or, the first may replace the entry, causing a false "hit" in the second train, causing wrong update.
+  // So, we define some of the t1 signals in advance, and use them to check if the contiguous trains are hit.
+  private val t1_valid        = Wire(Bool())
+  private val t1_hit          = Wire(Bool())
+  private val t1_tag          = Wire(UInt(TagWidth.W))
+  private val t1_updateIdx    = Wire(UInt(log2Up(NumEntries).W))
+  private val t1_hitEntry     = Wire(new UbtbEntry)
+  private val t1_updatedEntry = WireDefault(t1_hitEntry) // will be updated in t1, then write back to entries
+
+  // if t0_tag === t1_tag, t1 must be updating the entry, so we can see it as a hit, and use t1_updateIdx as hitIdx
+  private val t0_hitT1Update = t1_valid && t0_tag === t1_tag
+  // if t0 hits but t1 is replacing it, we should see it as not hit
+  private val t0_hitT1Victim = t1_valid && t0_realHitIdx === replacer.io.victim && t0_realHit && !t1_hit
+
+  // fix final hit
+  private val t0_hit = (t0_realHit || t0_hitT1Update) && !t0_hitT1Victim
+  // select hit entry: use t1_updatedEntry if t0_hitT1Update, otherwise use real hit entry
+  private val t0_hitIdx   = Mux(t0_hitT1Update, t1_updateIdx, t0_realHitIdx)
+  private val t0_hitEntry = Mux(t0_hitT1Update, t1_updatedEntry, entries(t0_realHitIdx))
+
+  // calculate hit flags, valid only when t0_hit
   private val t0_hitNotUseful     = t0_hitEntry.usefulCnt.isSaturateNegative
   private val t0_hitPositionLow   = t0_hitEntry.slot1.position > t0_position
   private val t0_hitPositionHigh  = t0_hitEntry.slot1.position < t0_position
@@ -100,16 +134,22 @@ class Ubtb(implicit p: Parameters) extends BasePredictor with HasUbtbParameters 
   private val t0_hitTargetSame    = t0_hitEntry.slot1.target === t0_target
   private val t0_hitTaken         = t0_hitEntry.slot1.takenCnt.isPositive
 
-  /* *** train stage 1 *** */
-  private val t1_valid       = RegNext(t0_valid, false.B)
-  private val t1_tag         = RegEnable(t0_tag, t0_valid)
+  /* *** train stage 1 ***
+   * - select victim
+   * - generate updated entry
+   * - update entries
+   * - update replacer
+   */
+  t1_valid := RegNext(t0_valid, false.B)
+  t1_tag   := RegEnable(t0_tag, t0_valid)
   private val t1_actualTaken = RegEnable(t0_actualTaken, t0_valid)
   private val t1_position    = RegEnable(t0_position, t0_valid)
   private val t1_target      = RegEnable(t0_target, t0_valid)
   private val t1_attribute   = RegEnable(t0_attribute, t0_valid)
 
-  private val t1_hit    = RegEnable(t0_hit, t0_valid)
+  t1_hit := RegEnable(t0_hit, t0_valid)
   private val t1_hitIdx = RegEnable(t0_hitIdx, t0_valid)
+  t1_hitEntry := RegEnable(t0_hitEntry, t0_valid)
 
   // hit states (flags), valid only when t1_hit
   private val t1_hitNotUseful     = RegEnable(t0_hitNotUseful, t0_valid)
@@ -120,26 +160,22 @@ class Ubtb(implicit p: Parameters) extends BasePredictor with HasUbtbParameters 
   private val t1_hitTargetSame    = RegEnable(t0_hitTargetSame, t0_valid)
   private val t1_hitTaken         = RegEnable(t0_hitTaken, t0_valid)
 
-  // select the entry: if hit, use the hit entry, otherwise use the victim from replacer (first not useful, or PLRU)
-  private val t1_updateIdx   = Mux(t1_hit, t1_hitIdx, replacer.io.victim)
-  private val t1_updateEntry = entries(t1_updateIdx)
-
   // init a new entry
   private def initEntryIfNotUseful(notUseful: Bool): Unit =
     when(notUseful) {
-      t1_updateEntry.valid := true.B
-      t1_updateEntry.tag   := t1_tag
-      t1_updateEntry.usefulCnt.resetPositive() // usefulCnt inits at strong positive, in/decrease by policy
+      t1_updatedEntry.valid := true.B
+      t1_updatedEntry.tag   := t1_tag
+      t1_updatedEntry.usefulCnt.resetPositive() // usefulCnt inits at strong positive, in/decrease by policy
       // slot1
-      t1_updateEntry.slot1.position  := t1_position
-      t1_updateEntry.slot1.attribute := t1_attribute
-      t1_updateEntry.slot1.target    := t1_target
-      t1_updateEntry.slot1.takenCnt.resetNeutral() // takenCnt inits at neutral (weak taken), in/decrease by policy
-      t1_updateEntry.slot1.isStaticTarget := true.B // inits at true, set to false when we see a different target
+      t1_updatedEntry.slot1.position  := t1_position
+      t1_updatedEntry.slot1.attribute := t1_attribute
+      t1_updatedEntry.slot1.target    := t1_target
+      t1_updatedEntry.slot1.takenCnt.resetNeutral() // takenCnt inits at neutral (weak taken), in/decrease by policy
+      t1_updatedEntry.slot1.isStaticTarget := true.B // inits at true, set to false when we see a different target
       // TODO: 2-taken train
-      t1_updateEntry.slot2.valid := false.B
+      t1_updatedEntry.slot2.valid := false.B
     }.otherwise {
-      t1_updateEntry.usefulCnt.decrease()
+      t1_updatedEntry.usefulCnt.value := t1_hitEntry.usefulCnt.getDecrease
     }
 
   when(t1_valid) {
@@ -158,7 +194,7 @@ class Ubtb(implicit p: Parameters) extends BasePredictor with HasUbtbParameters 
         t1_hitPositionSame && t1_actualTaken
       ) {
         // increase takenCnt
-        t1_updateEntry.slot1.takenCnt.increase()
+        t1_updatedEntry.slot1.takenCnt.value := t1_hitEntry.slot1.takenCnt.getIncrease
       }.elsewhen(
         // branch position match, and actual not taken
         t1_hitPositionSame && !t1_actualTaken ||
@@ -168,14 +204,14 @@ class Ubtb(implicit p: Parameters) extends BasePredictor with HasUbtbParameters 
           !t1_actualTaken && t1_hitTaken
       ) {
         // decrease takenCnt
-        t1_updateEntry.slot1.takenCnt.decrease()
+        t1_updatedEntry.slot1.takenCnt.value := t1_hitEntry.slot1.takenCnt.getDecrease
       }
       when(
         // branch position match, and both actual & predicted taken -> prediction correct, useful
         t1_hitPositionSame && t1_actualTaken && t1_hitTaken
       ) {
         // increase usefulCnt
-        t1_updateEntry.usefulCnt.increase()
+        t1_updatedEntry.usefulCnt.value := t1_hitEntry.usefulCnt.getIncrease
       }.elsewhen(
         // position mismatch -> not useful
         !t1_hitPositionSame ||
@@ -183,7 +219,7 @@ class Ubtb(implicit p: Parameters) extends BasePredictor with HasUbtbParameters 
           !t1_actualTaken
       ) {
         // decrease usefulCnt
-        t1_updateEntry.usefulCnt.decrease()
+        t1_updatedEntry.usefulCnt.value := t1_hitEntry.usefulCnt.getDecrease
       }
     }.otherwise {
       // attribute match, and is direct / indirect jump
@@ -194,14 +230,22 @@ class Ubtb(implicit p: Parameters) extends BasePredictor with HasUbtbParameters 
       }.elsewhen(t1_hitTargetSame) {
         // position match, and target match
         // increase usefulCnt
-        t1_updateEntry.usefulCnt.increase()
+        t1_updatedEntry.usefulCnt.value := t1_hitEntry.usefulCnt.getIncrease
       }.otherwise {
         // position match, but target mismatch (should not happen for direct jumps unless self-modifies)
-        // decrease usefulCnt and mark as not static target
-        t1_updateEntry.usefulCnt.decrease()
-        t1_updateEntry.slot1.isStaticTarget := false.B // target is not static anymore
+        // decrease usefulCnt
+        t1_updatedEntry.usefulCnt.value := t1_hitEntry.usefulCnt.getDecrease
+        // and, since we've seen a different target, target is not static anymore
+        t1_updatedEntry.slot1.isStaticTarget := false.B
       }
     }
+  }
+
+  // select the entry: if hit, use the hit entry, otherwise use the victim from replacer (first not useful, or Plru)
+  t1_updateIdx := Mux(t1_hit, t1_hitIdx, replacer.io.victim)
+  // and write back the updated entry
+  when(t1_valid) {
+    entries(t1_updateIdx) := t1_updatedEntry
   }
 
   // update replacer
@@ -209,6 +253,10 @@ class Ubtb(implicit p: Parameters) extends BasePredictor with HasUbtbParameters 
   replacer.io.trainTouch.bits  := t1_updateIdx
 
   /* *** perf *** */
+  XSPerfAccumulate("trainHitEntries", t0_valid && t0_realHit)
+  XSPerfAccumulate("trainHitT1Update", t0_valid && t0_hitT1Update)
+  XSPerfAccumulate("trainHitT1Victim", t0_valid && t0_hitT1Victim)
+
   XSPerfAccumulate("allocateNotUseful", t1_valid && !t1_hit && replacer.io.perf.replaceNotUseful)
   XSPerfAccumulate("allocatePlru", t1_valid && !t1_hit && !replacer.io.perf.replaceNotUseful)
   XSPerfAccumulate(
