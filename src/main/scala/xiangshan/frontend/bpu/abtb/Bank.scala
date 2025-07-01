4b5a39051ab6388334a94fa17ef1ff4354b0c971
YuanDL <2524247987@qq.com>
2025-07-01 16:59:23 +0800

    feat(bpu): add WriteBuffer & add counter update.

--- src/main/scala/xiangshan/frontend/bpu/abtb/Bank.scala
+++ src/main/scala/xiangshan/frontend/bpu/abtb/Bank.scala
@@ -20,6 +20,7 @@ import chisel3.util._
 import org.chipsalliance.cde.config.Parameters
 import utility.XSPerfAccumulate
 import utility.sram.SRAMTemplate
+import xiangshan.frontend.bpu.WriteBuffer
 
 /**
   * This module stores the ahead BTB entries.
@@ -51,18 +52,18 @@ class Bank(implicit p: Parameters) extends AheadBtbModule {
   // single port SRAM can not be written and read at the same time
   // read has higher priority than write
   // we use a write buffer to store the write requests when read and write are both valid
-  private val writeBuffer = Module(new Queue(new BankWriteReq, WriteBufferSize, pipe = true, flow = true))
+  private val writeBuffer = Module(new WriteBuffer(new BankWriteReq, WriteBufferSize, pipe = true))
 
   // writeReq is a ValidIO, it means that the new request will be dropped if the buffer is full
-  writeBuffer.io.enq.valid := io.writeReq.valid
-  writeBuffer.io.enq.bits  := io.writeReq.bits
+  writeBuffer.io.write.valid := io.writeReq.valid
+  writeBuffer.io.write.bits  := io.writeReq.bits
 
-  writeBuffer.io.deq.ready := sram.io.w.req.ready && !io.readReq.valid
+  writeBuffer.io.read.ready := sram.io.w.req.ready && !io.readReq.valid
 
-  private val writeValid   = writeBuffer.io.deq.valid && !io.readReq.valid
-  private val writeEntry   = writeBuffer.io.deq.bits.entry
-  private val writeSetIdx  = writeBuffer.io.deq.bits.setIdx
-  private val writeWayIdx  = writeBuffer.io.deq.bits.wayIdx
+  private val writeValid   = writeBuffer.io.read.valid && !io.readReq.valid
+  private val writeEntry   = writeBuffer.io.read.bits.entry
+  private val writeSetIdx  = writeBuffer.io.read.bits.setIdx
+  private val writeWayIdx  = writeBuffer.io.read.bits.wayIdx
   private val writeWayMask = UIntToOH(writeWayIdx)
 
   sram.io.w.apply(writeValid, writeEntry, writeSetIdx, writeWayMask)
@@ -71,6 +72,6 @@ class Bank(implicit p: Parameters) extends AheadBtbModule {
   io.writeResp.bits.setIdx := writeSetIdx
   io.writeResp.bits.wayIdx := writeWayIdx
 
-  XSPerfAccumulate("aBtb_bank_read_write_conflict", writeBuffer.io.deq.valid && io.readReq.valid)
-  XSPerfAccumulate("aBtb_bank_write_buffer_full", !writeBuffer.io.enq.ready)
+  XSPerfAccumulate("abtb_bank_read_write_conflict", writeBuffer.io.read.valid && io.readReq.valid)
+  XSPerfAccumulate("abtb_bank_write_buffer_full", !writeBuffer.io.write.ready)
 }
