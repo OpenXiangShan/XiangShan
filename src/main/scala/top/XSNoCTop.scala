a7d6794e83adc7f315fa99b76657c4dbfacd36c7
Zhu Yu <yulightenyu@gmail.com>
2025-07-03 12:27:18 +0800

    fix(XSNoCTop): fix exico cross clock domain

--- src/main/scala/top/XSNoCTop.scala
+++ src/main/scala/top/XSNoCTop.scala
@@ -117,8 +117,10 @@ trait HasCoreLowPowerImp[+L <: HasXSTile] { this: BaseXSSocImp with HasXSTileCHI
     val isWFI = withClockAndReset(clock, cpuReset_sync) {
       AsyncResetSynchronizerShiftReg(core.io.cpu_halt, 3, 0)
     }
+    val exitcoSyn = withClockAndReset(noc_clock, noc_reset_sync) {
+      AsyncResetSynchronizerShiftReg((!io_chi.syscoreq & !io_chi.syscoack),3, 0)}
     val exitco = withClockAndReset(clock, cpuReset_sync) {
-      AsyncResetSynchronizerShiftReg((!io_chi.syscoreq & !sync_chi_syscoack),3, 0)}
+      AsyncResetSynchronizerShiftReg((exitcoSyn),3, 0)}
     val QACTIVE = WireInit(false.B)
     val QACCEPTn = WireInit(false.B)
     cpu_no_op := lpState === sPOFFREQ
