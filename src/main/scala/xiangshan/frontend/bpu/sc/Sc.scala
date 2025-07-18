3a77374686efee8eb47521d45e09ff75aaabe39a
husipeng <husipeng24@qq.com>
2025-07-18 14:51:25 +0800

    feat(sc): add sc skeleton

---
+++ src/main/scala/xiangshan/frontend/bpu/sc/Sc.scala
@@ -0,0 +1,29 @@
+// Copyright (c) 2024-2025 Beijing Institute of Open Source Chip (BOSC)
+// Copyright (c) 2020-2025 Institute of Computing Technology, Chinese Academy of Sciences
+// Copyright (c) 2020-2021 Peng Cheng Laboratory
+//
+// XiangShan is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//          https://license.coscl.org.cn/MulanPSL2
+//
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+// EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+// MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+//
+// See the Mulan PSL v2 for more details.
+
+package xiangshan.frontend.bpu.sc
+
+import chisel3._
+import chisel3.util._
+import org.chipsalliance.cde.config.Parameters
+import utility.XSPerfAccumulate
+import xiangshan.frontend.bpu.BasePredictor
+
+/**
+ * This module is the implementation of the Statistical Corrector.
+ */
+class Sc(implicit p: Parameters) extends BasePredictor with HasScParameters with Helpers {
+  val io: ScIO = IO(new ScIO)
+}
