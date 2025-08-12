1c4117107dd55c2b96b05e84fb3388563c20820e
Easton Man <me@eastonman.com>
2025-08-12 16:58:00 +0800

    feat(bpu): add tage base table

--- src/main/scala/xiangshan/frontend/bpu/Bundles.scala
+++ src/main/scala/xiangshan/frontend/bpu/Bundles.scala
@@ -28,6 +28,7 @@ import xiangshan.frontend.bpu.mbtb.MainBtbMeta
 import xiangshan.frontend.bpu.phr.PhrPtr
 import xiangshan.frontend.bpu.ras.RasInternalMeta
 import xiangshan.frontend.bpu.ras.RasMeta
+import xiangshan.frontend.bpu.tage.TageMeta
 
 /* *** public const & type *** */
 class BranchAttribute extends Bundle {
@@ -219,6 +220,7 @@ class BpuMeta(implicit p: Parameters) extends BpuBundle {
   val mbtb: MainBtbMeta  = new MainBtbMeta
   val ras:  RasMeta      = new RasMeta
   val phr:  PhrPtr       = new PhrPtr
+  val tage: TageMeta     = new TageMeta
 }
 
 /* *** internal const & type *** */
--- src/main/scala/xiangshan/frontend/bpu/DummyBpu.scala
+++ src/main/scala/xiangshan/frontend/bpu/DummyBpu.scala
@@ -149,6 +149,8 @@ class DummyBpu(implicit p: Parameters) extends BpuModule with HalfAlignHelper {
   ras.io.commit.bits.meta        := commitUpdate.bits.meta.ras
   ras.io.commit.bits.cfiPosition := commitUpdate.bits.cfiPosition
 
+  tage.io.mbtbResult := mbtb.io.result
+
   private val s2_ftqPtr = RegEnable(io.fromFtq.enq_ptr, s1_fire)
   private val s3_ftqPtr = RegEnable(s2_ftqPtr, s2_fire)
 
@@ -222,8 +224,11 @@ class DummyBpu(implicit p: Parameters) extends BpuModule with HalfAlignHelper {
   private val s3_abtbMeta = RegEnable(s2_abtbMeta, s2_fire)
 
   // mbtb meta
-  val s3_mbtbMeta        = RegEnable(mbtb.io.meta, s2_fire)
-  private val s3_rasMeta = ras.io.specMeta
+  private val s3_mbtbMeta = RegEnable(mbtb.io.meta, s2_fire)
+  private val s3_rasMeta  = ras.io.specMeta
+
+  // tage meta
+  private val s3_tageMeta = tage.io.meta
 
   // phr meta
   val s2_phrMeta = RegEnable(phr.io.phrPtr, s1_fire)
@@ -239,6 +244,7 @@ class DummyBpu(implicit p: Parameters) extends BpuModule with HalfAlignHelper {
   s3_meta.mbtb := s3_mbtbMeta
   s3_meta.ras  := s3_rasMeta
   s3_meta.phr  := s3_phrMeta
+  s3_meta.tage := s3_tageMeta
   // TODO: other meta
 
   io.toFtq.meta.valid := s3_valid
