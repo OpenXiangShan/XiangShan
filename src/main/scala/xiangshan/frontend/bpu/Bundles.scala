0b4ccd3db841304e35f6bc1ae05e18e31a6b3929
xiaofeibao <1441675923@qq.com>
2025-08-20 16:13:49 +0800

    feat(backend): add bju reslove info to frontend

--- src/main/scala/xiangshan/frontend/bpu/Bundles.scala
+++ src/main/scala/xiangshan/frontend/bpu/Bundles.scala
@@ -153,7 +153,7 @@ class BranchInfo(implicit p: Parameters) extends BpuBundle {
 class BpuTrain(implicit p: Parameters) extends BpuBundle with HalfAlignHelper {
   val meta:       BpuMeta                = new BpuMeta
   val startVAddr: PrunedAddr             = PrunedAddr(VAddrBits)
-  val branches:   Vec[Valid[BranchInfo]] = Vec(3, Valid(new BranchInfo)) // FIXME: Should be BJU number
+  val branches:   Vec[Valid[BranchInfo]] = Vec(backendParams.BrhCnt, Valid(new BranchInfo))
 }
 
 // metadata for redirect (e.g. speculative state recovery) & training (e.g. rasPtr, phr)
