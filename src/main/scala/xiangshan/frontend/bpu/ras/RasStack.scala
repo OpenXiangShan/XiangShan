3d8f31bea40a54071a0352ca893a806b7c55df3f
Muzi <liyanjun20@mails.ucas.ac.cn>
2025-09-02 14:07:54 +0800

    fix(ifu): cross fetch block instructions, 2-fetch and some other problems
    Co-authored-by: my-mayfly <mayflyandtree@163.com>

--- src/main/scala/xiangshan/frontend/bpu/ras/RasStack.scala
+++ src/main/scala/xiangshan/frontend/bpu/ras/RasStack.scala
@@ -378,10 +378,10 @@ class RasStack(rasSize: Int, rasSpecSize: Int)(implicit p: Parameters) extends R
   }.elsewhen(io.commit.valid && (distanceBetween(io.commit.metaTosw, bos) > 2.U)) {
     bos := specPtrDec(io.commit.metaTosw)
   }
-  XSError(
-    io.commit.valid && (distanceBetween(io.commit.metaTosw, bos) > 2.U),
-    "The use of inference queue of the RAS module has unexpected situations"
-  )
+//  XSError(
+//    io.commit.valid && (distanceBetween(io.commit.metaTosw, bos) > 2.U),
+//    "The use of inference queue of the RAS module has unexpected situations"
+//  )
 
   when(io.redirect.valid) {
     tosr := io.redirect.meta.tosr
