81209cd45a540b7b2cc6154d5269b35932e9e27f
YuanDL <2524247987@qq.com>
2025-09-02 18:54:56 +0800

    feat(abtb): add diff ReplacementPolicy and PlruStateGen
    change styles of PlruStateGen

--- src/main/scala/xiangshan/frontend/bpu/abtb/AheadBtbReplacer.scala
+++ src/main/scala/xiangshan/frontend/bpu/abtb/AheadBtbReplacer.scala
@@ -19,6 +19,8 @@ import chisel3._
 import chisel3.util._
 import org.chipsalliance.cde.config.Parameters
 import utility.ReplacementPolicy
+import xiangshan.frontend.bpu.PlruStateGen
+import xiangshan.frontend.bpu.ReplacerState
 
 /**
   * This module implement the replacement policy for the ahead BTB.
@@ -26,19 +28,71 @@ import utility.ReplacementPolicy
 class AheadBtbReplacer(implicit p: Parameters) extends AheadBtbModule {
   val io: ReplacerIO = IO(new ReplacerIO)
 
-  private val replacer = ReplacementPolicy.fromString(Some("setplru"), NumWays, NumSets)
+  // use PlruStateGen caclulate next state and replace way
+  private val states           = Module(new ReplacerState(NumSets, NumWays, hasExtraReadPort = true))
+  private val predReplacerGen  = Module(new PlruStateGen(NumWays, accessSize = NumWays))
+  private val writeReplacerGen = Module(new PlruStateGen(NumWays))
+  private val writeTouch       = Wire(Valid(UInt(WayIdxWidth.W)))
+  private val touchWays        = Seq.fill(NumWays)(Wire(Valid(UInt(WayIdxWidth.W))))
 
-  when(io.writeValid) {
-    replacer.access(io.writeSetIdx, io.writeWayIdx)
-  }.elsewhen(io.readValid) {
-    val touchSets = Seq.fill(NumWays)(io.readSetIdx)
-    val touchWays = Seq.fill(NumWays)(Wire(Valid(UInt(WayIdxWidth.W))))
-    touchWays.zip(io.readWayMask).zipWithIndex.foreach { case ((t, r), i) =>
-      t.valid := r
-      t.bits  := i.U
+  writeTouch.valid              := io.writeValid
+  writeTouch.bits               := io.writeWayIdx
+  states.io.trainReadSetIdx     := io.writeSetIdx
+  states.io.trainWriteValid     := io.writeValid
+  states.io.trainWriteSetIdx    := io.writeSetIdx
+  states.io.trainWriteState     := writeReplacerGen.io.nextState
+  writeReplacerGen.io.stateIn   := states.io.trainReadState
+  writeReplacerGen.io.touchWays := Seq(writeTouch)
+
+  touchWays.zip(io.readWayMask).zipWithIndex.foreach { case ((t, r), i) =>
+    t.valid := r
+    t.bits  := i.U
+  }
+  states.io.predictReadSetIdx  := io.readSetIdx
+  states.io.predictWriteValid  := io.readValid && io.readWayMask.reduce(_ || _)
+  states.io.predictWriteSetIdx := io.readSetIdx
+  states.io.predictWriteState  := predReplacerGen.io.nextState
+  predReplacerGen.io.stateIn   := states.io.predictReadState
+  predReplacerGen.io.touchWays := touchWays
+
+  states.io.readSetIdx.foreach(_ := io.replaceSetIdx)
+  private val replacerState = states.io.readState.getOrElse(0.U)
+
+  private val genReplaceWay = writeReplacerGen.way(replacerState)
+
+  if (EnableCommitGHistDiff) {
+    // use ReplacementPolicy class caclulate next state replace way
+    val replacer = ReplacementPolicy.fromString(Some("setplru"), NumWays, NumSets)
+
+    val readWriteConflict = io.readValid && io.writeValid && (io.readSetIdx === io.writeSetIdx)
+    when(readWriteConflict) {
+      replacer.access(io.writeSetIdx, io.writeWayIdx)
+    }.otherwise {
+      when(io.writeValid) {
+        replacer.access(io.writeSetIdx, io.writeWayIdx)
+      }
+      when(io.readValid) {
+        val touchSets = Seq.fill(NumWays)(io.readSetIdx)
+        val touchWays = Seq.fill(NumWays)(Wire(Valid(UInt(WayIdxWidth.W))))
+        touchWays.zip(io.readWayMask).zipWithIndex.foreach { case ((t, r), i) =>
+          t.valid := r
+          t.bits  := i.U
+        }
+        replacer.access(touchSets, touchWays)
+      }
     }
-    replacer.access(touchSets, touchWays)
+    val replacerWay  = replacer.way(io.replaceSetIdx)
+    val replacerDiff = genReplaceWay === replacerWay
+    dontTouch(genReplaceWay)
+    dontTouch(replacerWay)
+    dontTouch(replacerDiff)
+    when(io.writeValid) {
+      assert(
+        replacerDiff,
+        "writeReplacerGen's replaceWay doesn't match replacer!"
+      )
+    }
   }
 
-  io.victimWayIdx := replacer.way(io.replaceSetIdx)
+  io.victimWayIdx := genReplaceWay
 }
