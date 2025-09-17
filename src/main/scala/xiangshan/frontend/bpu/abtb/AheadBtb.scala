de5fdd22b02b7b84dac47fc0c2b329da4538e30c
Yuandongliang <42736386+sleep-zzz@users.noreply.github.com>
2025-09-17 18:35:11 +0800

    fix(abtb): fix abtb meta signal X-Propagation (#5028)

--- src/main/scala/xiangshan/frontend/bpu/abtb/AheadBtb.scala
+++ src/main/scala/xiangshan/frontend/bpu/abtb/AheadBtb.scala
@@ -49,7 +49,13 @@ class AheadBtb(implicit p: Parameters) extends BasePredictor with Helpers {
   }
   io.resetDone := resetDone
 
-  private val takenCounter = Reg(Vec(NumBanks, Vec(NumSets, Vec(NumWays, new SaturateCounter(TakenCounterWidth)))))
+  private val takenCounter = RegInit(
+    VecInit.fill(NumBanks)(
+      VecInit.fill(NumSets)(
+        VecInit.fill(NumWays)(0.U.asTypeOf(new SaturateCounter(TakenCounterWidth)))
+      )
+    )
+  )
 
   // TODO: write ctr bypass to read
   // TODO: train after execution
@@ -163,7 +169,7 @@ class AheadBtb(implicit p: Parameters) extends BasePredictor with Helpers {
   // used for check abtb output
   io.debug_startVAddr := s2_startPc
 
-  private val meta = Wire(new AheadBtbMeta)
+  private val meta = WireInit(0.U.asTypeOf(new AheadBtbMeta))
   meta.valid           := s2_valid
   meta.hitMask         := s2_hitMask
   meta.taken           := s2_taken
