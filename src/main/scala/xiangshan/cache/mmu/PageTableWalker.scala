ef7ced4cb8f225ea7135fe985b226495e5a5e6af
Xu, Zefan <ceba_robot@outlook.com>
2025-09-22 20:35:52 +0800

    timing(LLPTW): refactor block_hptw_req to improve timing
    Commit 03938367 imports block_hptw_req to solve conflicts of syncing entries between sending mem req or sending hptw request. But block_hptw_req also made a bad timing path.
    
    This patch use a different way to solve that. As mem req has higher priority, we could put hptw req before mem req.

--- src/main/scala/xiangshan/cache/mmu/PageTableWalker.scala
+++ src/main/scala/xiangshan/cache/mmu/PageTableWalker.scala
@@ -742,15 +742,11 @@ class LLPTW(implicit p: Parameters) extends XSModule with HasPtwConst with HasPe
     mem_arb.io.in(i).valid := is_mems(i) && !io.mem.req_mask(i)
   }
 
-  // when an entry dup with another entry whose mem_arb.io.out.fire,
-  // should block hptw req and change state(i) to state_mem_waiting
-  val block_hptw_req = WireInit(VecInit(Seq.fill(l2tlbParams.llptwsize)(false.B)))
-
   // process hptw requests in serial
   val hyper_arb1 = Module(new RRArbiterInit(new LLPTWEntry(), l2tlbParams.llptwsize))
   for (i <- 0 until l2tlbParams.llptwsize) {
     hyper_arb1.io.in(i).bits := entries(i)
-    hyper_arb1.io.in(i).valid := is_hptw_req(i) && !(Cat(is_hptw_resp).orR) && !(Cat(is_last_hptw_resp).orR) && !block_hptw_req(i)
+    hyper_arb1.io.in(i).valid := is_hptw_req(i) && !(Cat(is_hptw_resp).orR) && !(Cat(is_last_hptw_resp).orR)
   }
   val hyper_arb2 = Module(new RRArbiterInit(new LLPTWEntry(), l2tlbParams.llptwsize))
   for(i <- 0 until l2tlbParams.llptwsize) {
@@ -832,7 +828,7 @@ class LLPTW(implicit p: Parameters) extends XSModule with HasPtwConst with HasPe
     to_hptw_req -> state_hptw_req
   ))
   val enq_state = Mux(from_pre(io.in.bits.req_info.source) && enq_state_normal =/= state_addr_check, state_idle, enq_state_normal)
-  when (io.in.fire  && (if (HasBitmapCheck) !io.in.bits.bitmapCheck.get.jmp_bitmap_check else true.B)) {
+  when (io.in.fire && (if (HasBitmapCheck) !io.in.bits.bitmapCheck.get.jmp_bitmap_check else true.B)) {
     // if prefetch req does not need mem access, just give it up.
     // so there will be at most 1 + FilterSize entries that needs re-access page cache
     // so 2 + FilterSize is enough to avoid dead-lock
@@ -890,6 +886,15 @@ class LLPTW(implicit p: Parameters) extends XSModule with HasPtwConst with HasPe
   val hpaddr = Cat(hptw_resp.genPPNS2(get_pn(gpaddr)), get_off(gpaddr))
   val addr = RegEnable(MakeAddr(io.in.bits.ppn(ppnLen - 1, 0), getVpnn(io.in.bits.req_info.vpn, 0)), io.in.fire)
 
+  when (hyper_arb1.io.out.fire) {
+    for (i <- state.indices) {
+      when (state(i) === state_hptw_req && entries(i).ppn === hyper_arb1.io.out.bits.ppn && entries(i).req_info.s2xlate === allStage && hyper_arb1.io.chosen === i.U) {
+        state(i) := state_hptw_resp
+        entries(i).wait_id := hyper_arb1.io.chosen
+      }
+    }
+  }
+
   io.pmp(0).req.valid := need_addr_check
   io.pmp(0).req.bits.addr := addr
   io.pmp(0).req.bits.cmd := TlbCmd.read
@@ -922,10 +927,10 @@ class LLPTW(implicit p: Parameters) extends XSModule with HasPtwConst with HasPe
         state(i) := state_mem_waiting
         entries(i).hptw_resp := entries(mem_arb.io.chosen).hptw_resp
         entries(i).wait_id := mem_arb.io.chosen
-        block_hptw_req(i) := true.B
       }
     }
   }
+
   when (io.mem.resp.fire) {
     state.indices.map{i =>
       when (state(i) === state_mem_waiting && io.mem.resp.bits.id === entries(i).wait_id) {
@@ -957,15 +962,6 @@ class LLPTW(implicit p: Parameters) extends XSModule with HasPtwConst with HasPe
     }
   }
 
-  when (hyper_arb1.io.out.fire) {
-    for (i <- state.indices) {
-      when (state(i) === state_hptw_req && entries(i).ppn === hyper_arb1.io.out.bits.ppn && entries(i).req_info.s2xlate === allStage && hyper_arb1.io.chosen === i.U) {
-        state(i) := state_hptw_resp
-        entries(i).wait_id := hyper_arb1.io.chosen
-      }
-    }
-  }
-
   when (hyper_arb2.io.out.fire) {
     for (i <- state.indices) {
       when (state(i) === state_last_hptw_req && entries(i).ppn === hyper_arb2.io.out.bits.ppn && entries(i).req_info.s2xlate === allStage && hyper_arb2.io.chosen === i.U) {
@@ -984,7 +980,9 @@ class LLPTW(implicit p: Parameters) extends XSModule with HasPtwConst with HasPe
         }
       }
     }
+  }
 
+  if (HasBitmapCheck) {
     when (io.bitmap.get.resp.fire) {
       for (i <- state.indices) {
         when (is_bitmap_resp(i) && io.bitmap.get.resp.bits.id === entries(i).wait_id) {
@@ -1023,6 +1021,7 @@ class LLPTW(implicit p: Parameters) extends XSModule with HasPtwConst with HasPe
       }
     }
   }
+
   when (io.out.fire) {
     assert(state(mem_ptr) === state_mem_out)
     state(mem_ptr) := state_idle
