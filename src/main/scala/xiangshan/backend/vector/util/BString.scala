83fc15cddbe515eef14e52922d044f2230f1d511
Xuan Hu <huxuan@bosc.ac.cn>
2025-10-14 17:30:29 +0800

    fix(BString): replace deprecated function
    * Use StringContext.standardInterpolator instead of the instance method

--- src/main/scala/xiangshan/backend/vector/util/BString.scala
+++ src/main/scala/xiangshan/backend/vector/util/BString.scala
@@ -5,7 +5,7 @@ import chisel3._
 object BString {
   implicit class BinaryStringHelper(private val sc: StringContext) extends AnyVal {
     def b(args: Any*): UInt = {
-      val str: String = sc.standardInterpolator(x => x, args)
+      val str: String = StringContext.standardInterpolator(x => x, args, sc.parts)
         .filterNot(x => x == ' ' || x == '_')
       require(str.forall(c => c == '0' || c == '1'))
       ("b" + str).U(str.length.W)
