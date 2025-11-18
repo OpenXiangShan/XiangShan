5038934cfb50d5b4ecffd6787045c4e8b05484af
good-circle <fenghaoyuan19@mails.ucas.ac.cn>
2025-11-18 18:57:57 +0800

    set DFT true & bitmap false as default

--- src/main/scala/xiangshan/Parameters.scala
+++ src/main/scala/xiangshan/Parameters.scala
@@ -60,7 +60,7 @@ case class XSCoreParameters
   VLEN: Int = 128,
   ELEN: Int = 64,
   HSXLEN: Int = 64,
-  HasBitmapCheck: Boolean = true,
+  HasBitmapCheck: Boolean = false,
   HasBitmapCheckDefault: Boolean = false,
   HasMExtension: Boolean = true,
   HasCExtension: Boolean = true,
