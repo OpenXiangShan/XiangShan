5850b6bf74accdd6ee22f123a24c9dd0ef4a3e07
Xu, Zefan <ceba_robot@outlook.com>
2025-11-21 10:57:38 +0800

    fix(SoC): fix typo in requirement

--- src/main/scala/system/SoC.scala
+++ src/main/scala/system/SoC.scala
@@ -189,8 +189,8 @@ trait HasSoCParameter {
 
   def HasMEMencryption = cvm.HasMEMencryption
   require((cvm.HasMEMencryption && (cvm.KeyIDBits > 0)) || (!cvm.HasMEMencryption),
-    "HasMEMencryption most set with KeyIDBits > 0")
-  require((cvm.KeyIDBits == 0) || tiles.head.HasBitmapCheck, "KeyIDBits > 0 most set with HasBitmapCheck")
+    "HasMEMencryption must set with KeyIDBits > 0")
+  require((cvm.KeyIDBits == 0) || tiles.head.HasBitmapCheck, "KeyIDBits > 0 must set with HasBitmapCheck")
 }
 
 trait HasPeripheralRanges {
