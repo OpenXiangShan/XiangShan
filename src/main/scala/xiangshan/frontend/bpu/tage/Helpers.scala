bb2642e3945059749aad116deced79e21abb627f
husipeng <husipeng24@qq.com>
2025-11-27 18:53:46 +0800

    test new idx

--- src/main/scala/xiangshan/frontend/bpu/tage/Helpers.scala
+++ src/main/scala/xiangshan/frontend/bpu/tage/Helpers.scala
@@ -45,14 +45,18 @@ trait Helpers extends HasTageParameters with HasXSParameter with HalfAlignHelper
 
   def getSetIndex(pc: PrunedAddr, hist: UInt, numSets: Int): UInt = {
     val setIdxWidth = log2Ceil(numSets / NumBanks)
-    pc(setIdxWidth - 1 + BankIdxWidth + FetchBlockSizeWidth, BankIdxWidth + FetchBlockSizeWidth) ^ hist
+    val offset      = BankIdxWidth + instOffsetBits
+    pc(setIdxWidth - 1 + offset, offset) ^ hist
   }
 
-  def getTag(pc: PrunedAddr, hist: UInt): UInt =
-    pc(TagWidth, 1) ^ hist
+  def getTag(pc: PrunedAddr, hist: UInt, numSets: Int): UInt = {
+    val setIdxWidth = log2Ceil(numSets / NumBanks)
+    val offset      = setIdxWidth + BankIdxWidth + instOffsetBits
+    pc(TagWidth - 1 + offset, offset) ^ hist
+  }
 
   def getBankIndex(pc: PrunedAddr): UInt =
-    pc(BankIdxWidth - 1 + FetchBlockSizeWidth, FetchBlockSizeWidth)
+    pc(BankIdxWidth - 1 + instOffsetBits, instOffsetBits)
 
   def getLongestHistTableOH(hitTableMask: Seq[Bool]): Seq[Bool] =
     PriorityEncoderOH(hitTableMask.reverse).reverse
