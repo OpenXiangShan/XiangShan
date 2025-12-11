0a64693fd4e1c2b7861fbd1462e7a69f4e88fe09
lihuijin <501296508@qq.com>
2025-12-11 15:29:22 +0800

    fix(StoreQueue): fix 'reqValid' logic bug, and use sqIdx to compare age

--- src/main/scala/xiangshan/mem/lsqueue/StoreQueue.scala
+++ src/main/scala/xiangshan/mem/lsqueue/StoreQueue.scala
@@ -116,8 +116,7 @@ class StoreExceptionBuffer(implicit p: Parameters) extends XSModule with HasCirc
         res(i).bits := bits(i)
       }
       val oldest = Mux(valid(0) && valid(1),
-        Mux(isAfter(bits(0).uop.robIdx, bits(1).uop.robIdx) ||
-          (bits(0).uop.robIdx === bits(1).uop.robIdx && bits(0).uop.uopIdx > bits(1).uop.uopIdx), res(1), res(0)),
+        Mux(isAfter(bits(0).uop.sqIdx, bits(1).uop.sqIdx), res(1), res(0)),
         Mux(valid(0) && !valid(1), res(0), res(1)))
       (Seq(oldest.valid), Seq(oldest.bits))
     } else {
@@ -127,7 +126,7 @@ class StoreExceptionBuffer(implicit p: Parameters) extends XSModule with HasCirc
     }
   }
 
-  val reqValid = req_valid && req.uop.robIdx.needFlush(io.redirect)
+  val reqValid = req_valid && !req.uop.robIdx.needFlush(io.redirect)
   val reqSel = selectOldest(s2_enqueue :+ reqValid, s2_req :+ req)
 
   req_valid := reqSel._1(0)
