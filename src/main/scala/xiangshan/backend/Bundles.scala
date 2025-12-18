635afa91f591960351488bdc028c1a9579634192
Xuan Hu <huxuan@bosc.ac.cn>
2025-12-18 13:53:10 +0800

    fix(Backend,RAT,Freelist,Rob): fix vl commit
    * Only the uop who writes vl register will alloc VTypeBuffer entry

--- src/main/scala/xiangshan/backend/Bundles.scala
+++ src/main/scala/xiangshan/backend/Bundles.scala
@@ -484,6 +484,7 @@ object Bundles {
     val srcLoadDependency  = Vec(numSrc, Vec(LoadPipelineWidth, UInt(LoadDependencyWidth.W)))
     val psrc            = Vec(numSrc, UInt(PhyRegIdxWidth.W))
     val pdest           = UInt(PhyRegIdxWidth.W)
+    val pdestVl         = UInt(VlPhyRegIdxWidth.W)
     val rasAction       = BranchAttribute.RasAction()
     // reg cache
     val useRegCache     = Vec(backendParams.numIntRegSrc, Bool())
@@ -543,7 +544,7 @@ object Bundles {
       })
     }
 
-    def needWriteRf: Bool = rfWen || fpWen || vecWen || v0Wen || vlWen
+    def needEnqRab: Bool = rfWen || fpWen || vecWen || v0Wen
 
     def connectRenameOutUop(source: RenameOutUop): Unit = {
       this := 0.U.asTypeOf(this)
