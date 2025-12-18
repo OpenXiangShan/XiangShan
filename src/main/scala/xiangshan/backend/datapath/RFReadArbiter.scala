ec6e13b32d06df4d42e7f3bba19bcadb8c948f23
xiaofeibao <1441675923@qq.com>
2025-12-18 11:01:45 +0800

    feat(RFReadArbiter): use OldestArbiter

--- src/main/scala/xiangshan/backend/datapath/RFReadArbiter.scala
+++ src/main/scala/xiangshan/backend/datapath/RFReadArbiter.scala
@@ -2,7 +2,7 @@ package xiangshan.backend.datapath
 
 import org.chipsalliance.cde.config.Parameters
 import chisel3._
-import chisel3.util.{Arbiter, DecoupledIO, RRArbiter, Valid, PopCount}
+import chisel3.util._
 import utils.SeqUtils.{MixedVec3, Seq3}
 import utils.{OptionWrapper, SeqUtils}
 import xiangshan.backend.BackendParams
@@ -10,11 +10,13 @@ import xiangshan.backend.datapath.DataConfig._
 import xiangshan.backend.datapath.RdConfig._
 import xiangshan.backend.regfile.PregParams
 import utility._
+import xiangshan.backend.rob.RobPtr
+import yunsuan.vector.LZD
 
 case class RFRdArbParams(
   inRdCfgs: Seq3[RdConfig],
   pregParams: PregParams,
-) {
+)(implicit p: Parameters) {
   require(inRdCfgs.nonEmpty)
 
   def genInputBundle: MixedVec3[DecoupledIO[RFArbiterBundle]] = {
@@ -25,13 +27,64 @@ case class RFRdArbParams(
   def portMax: Int = inRdCfgs.flatten.flatten.map(_.port).max
 }
 
-class RFArbiterBundle(var rdCfg: Option[RdConfig], pregWidth: Int) extends Bundle {
-  val addr = UInt(pregWidth.W)
+class RFArbiterBundle(var rdCfg: Option[RdConfig], pregWidth: Int)(implicit p: Parameters) extends Bundle {
+  val addr       = UInt(pregWidth.W)
+  val robIdx     = new RobPtr
+  val issueValid = Bool()
 
-  def this(rdCfg_ : RdConfig, pregWidth_ : Int) = this(Some(rdCfg_), pregWidth_)
+  def this(rdCfg_ : RdConfig, pregWidth_ : Int)(implicit p: Parameters) = this(Some(rdCfg_), pregWidth_)
 
-  def this(pregWidth_ :Int) = this(None, pregWidth_)
+  def this(pregWidth_ :Int)(implicit p: Parameters) = this(None, pregWidth_)
 }
+
+class OldestArbiterIO(pregWidth: Int, n: Int)(implicit p: Parameters) extends Bundle {
+  val in  = Flipped(Vec(n, Decoupled(new RFArbiterBundle(pregWidth))))
+  val out = Valid(new RFArbiterBundle(pregWidth))
+}
+// use robIdx to check age in T0, and adjustment priority in T2
+class OldestArbiter(pregWidth: Int, n: Int)(implicit p: Parameters) extends Module {
+  val io = IO(new OldestArbiterIO(pregWidth, n))
+  if (n == 1) {
+    io.out.valid := io.in.head.valid
+    io.out.bits := io.in.head.bits
+    io.in.head.ready := true.B
+  }
+  else {
+    val validVec = io.in.map(_.valid)
+    val validVecReg = RegNext(VecInit(io.in.map(x => x.valid)), VecInit.tabulate(n)(i => false.B))
+    val robIdxVecReg = RegNext(VecInit(io.in.map(x => x.bits.robIdx)))
+    val priorityVecReg = RegInit(VecInit((0 until n).map(i => (1 << i).U(n.W))))
+    val mergePriority = validVec.zip(priorityVecReg).map(x => Mux(x._1, x._2, 0.U)).reduce(_ | _)
+    val lzdMergePriority = LZD(Reverse(mergePriority))
+    val lzdHit = Wire(Vec(n, Bool()))
+    for (i <- 0 until n) {
+      // count valid and older than other num
+      val otherValidVec = validVecReg.zipWithIndex.filter(_._2 != i).map(_._1)
+      val otherRobIdxVec = robIdxVecReg.zipWithIndex.filter(_._2 != i).map(_._1)
+      val indexLowerInvalidVec = validVecReg.zipWithIndex.filter(_._2 < i).map(!_._1)
+      val olderThanOtherNum = PopCount(otherRobIdxVec.map(x => robIdxVecReg(i) < x).zip(otherValidVec).map(x => x._1 || !x._2))
+      val otherValidNum = PopCount(otherValidVec)
+      dontTouch(olderThanOtherNum)
+      dontTouch(otherValidNum)
+      val indexLowerInvalidNum = if (i == 0) 0.U else PopCount(indexLowerInvalidVec)
+      when(validVecReg(i)){
+        val minPriority = (1.U << (n-1))
+        priorityVecReg(i) := minPriority >> olderThanOtherNum
+      }.otherwise{
+        priorityVecReg(i) := 1.U << (otherValidNum + indexLowerInvalidNum)
+      }
+      assert(PopCount(priorityVecReg(i)) === 1.U, s"priorityRegVec_$i must be one hot")
+      assert(PopCount(priorityVecReg.map(x => x === priorityVecReg(i))) === 1.U, s"priorityVecReg must be different")
+
+      val lzdPriority = LZD(Reverse(priorityVecReg(i)))
+      lzdHit(i) := lzdPriority === lzdMergePriority
+      io.in(i).ready := !io.in(i).valid || lzdHit(i)
+    }
+    io.out.valid := validVec.reduce(_ | _)
+    io.out.bits := Mux1H(lzdHit, io.in.map(_.bits))
+  }
+}
+
 
 class RFReadArbiterIO(params: RFRdArbParams)(implicit p: Parameters) extends Bundle {
   private val pregWidth = params.pregParams.addrWidth
@@ -52,13 +105,9 @@ abstract class RFReadArbiterBase(val params: RFRdArbParams)(implicit p: Paramete
     .flatten.flatten
     .groupBy(_.bits.rdCfg.get.port)
     .map(x => (x._1, x._2.sortBy(_.bits.rdCfg.get.priority).toSeq))
-  protected val arbiters: Seq[Option[WBArbiter[RFArbiterBundle]]] = portRange.map { portIdx =>
-    Option.when(
-      inGroup.isDefinedAt(portIdx))(
-      Module(new WBArbiter(
-        new RFArbiterBundle(pregWidth),
-        inGroup(portIdx).size
-      ))
+  protected val arbiters: Seq[Option[OldestArbiter]] = portRange.map { portIdx =>
+    Option.when(inGroup.isDefinedAt(portIdx))(
+      Module(new OldestArbiter(pregWidth, inGroup(portIdx).size))
     )
   }
 
@@ -103,7 +152,6 @@ abstract class RFReadArbiterBase(val params: RFRdArbParams)(implicit p: Paramete
     val out = io.out(portIdx)
     if (arb.nonEmpty) {
       val arbOut = arb.get.io.out
-      arbOut.ready := true.B
       out.valid := arbOut.valid
       out.bits := arbOut.bits
     } else {
