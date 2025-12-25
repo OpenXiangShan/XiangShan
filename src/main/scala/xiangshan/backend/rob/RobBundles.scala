533b62f821371e4ce9a1dc77746f69a060747e41
linzhida <linzhida19@mails.ucas.ac.cn>
2025-12-25 19:08:42 +0800

    fix(RobPtr): rollback needFlush because isAfter is overrided for New RobPtr

--- src/main/scala/xiangshan/backend/rob/RobBundles.scala
+++ src/main/scala/xiangshan/backend/rob/RobBundles.scala
@@ -279,9 +279,8 @@ class RobPtr(entries: Int) extends CircularQueuePtr[RobPtr](
   def needFlush(redirect: Valid[Redirect]): Bool = {
     // TODO: is it only used for redirect? May be Fixed Later
     // TODO: Maybe We should reload some operation or func
-    val flushItself = this === redirect.bits.robIdx && this.isFormer === redirect.bits.robIdx.isFormer && redirect.bits.flushItself()
-    val flushHalf   = this === redirect.bits.robIdx && this.isFormer.asUInt < redirect.bits.robIdx.isFormer.asUInt
-      redirect.valid && (flushItself || flushHalf || isAfter(this, redirect.bits.robIdx))
+    val flushItself = redirect.bits.flushItself() && this === redirect.bits.robIdx
+    redirect.valid && (flushItself || isAfter(this, redirect.bits.robIdx))
   }
 
   def needFlush(redirect: Seq[Valid[Redirect]]): Bool = VecInit(redirect.map(needFlush)).asUInt.orR
