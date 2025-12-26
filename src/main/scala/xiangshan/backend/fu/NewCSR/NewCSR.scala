54f98b171ebd01d7754d3d649013d02804b6e3c9
sinceforYy <1017657683@qq.com>
2025-12-26 14:38:59 +0800

    chore(NewCSR): fix RegNext error usage

--- src/main/scala/xiangshan/backend/fu/NewCSR/NewCSR.scala
+++ src/main/scala/xiangshan/backend/fu/NewCSR/NewCSR.scala
@@ -1621,19 +1621,42 @@ class NewCSR(implicit val p: Parameters) extends Module
     diffHCSRState.vsatp       := vsatp.rdata.asUInt
     diffHCSRState.vsscratch   := vsscratch.rdata.asUInt
 
-    val platformIRPMeipChange = !platformIRP.MEIP &&  RegNext(platformIRP.MEIP) || platformIRP.MEIP && !RegNext(platformIRP.MEIP)
-    val platformIRPMtipChange = !platformIRP.MTIP &&  RegNext(platformIRP.MTIP) || platformIRP.MTIP && !RegNext(platformIRP.MTIP)
-    val platformIRPMsipChange = !platformIRP.MSIP &&  RegNext(platformIRP.MSIP) || platformIRP.MSIP && !RegNext(platformIRP.MSIP)
-    val platformIRPSeipChange = !platformIRP.SEIP &&  RegNext(platformIRP.SEIP) || platformIRP.SEIP && !RegNext(platformIRP.SEIP)
-    val platformIRPStipChange = !sstcIRGen.o.STIP &&  RegNext(sstcIRGen.o.STIP) || sstcIRGen.o.STIP && !RegNext(sstcIRGen.o.STIP)
-    val platformIRPVseipChange = !platformIRP.VSEIP &&  RegNext(platformIRP.VSEIP) ||
-                                  platformIRP.VSEIP && !RegNext(platformIRP.VSEIP) ||
-                                 !hgeip.rdata.asUInt(hstatus.regOut.VGEIN.asUInt) &&  RegNext(hgeip.rdata.asUInt(hstatus.regOut.VGEIN.asUInt)) ||
-                                  hgeip.rdata.asUInt(hstatus.regOut.VGEIN.asUInt) && !RegNext(hgeip.rdata.asUInt(hstatus.regOut.VGEIN.asUInt))
-    val platformIRPVstipChange = !sstcIRGen.o.VSTIP && RegNext(sstcIRGen.o.VSTIP) || sstcIRGen.o.VSTIP && !RegNext(sstcIRGen.o.VSTIP)
-    val fromAIAMeipChange = !fromAIA.meip && RegNext(fromAIA.meip) || fromAIA.meip && !RegNext(fromAIA.meip)
-    val fromAIASeipChange = !fromAIA.seip && RegNext(fromAIA.seip) || fromAIA.seip && !RegNext(fromAIA.seip)
-    val lcofiReqChange = !lcofiReq && RegNext(lcofiReq) || lcofiReq && !RegNext(lcofiReq)
+    val meipReg = RegInit(false.B)
+    val mtipReg = RegInit(false.B)
+    val msipReg = RegInit(false.B)
+    val seipReg = RegInit(false.B)
+    val stipReg = RegInit(false.B)
+    val vseipReg = RegInit(false.B)
+    val hgeipVgeinReg = RegInit(false.B)
+    val vstipReg = RegInit(false.B)
+    val aiaMeipReg = RegInit(false.B)
+    val aiaSeipReg = RegInit(false.B)
+    val lcofiReqReg = RegInit(false.B)
+    meipReg := platformIRP.MEIP
+    mtipReg := platformIRP.MTIP
+    msipReg := platformIRP.MSIP
+    seipReg := platformIRP.SEIP
+    stipReg := sstcIRGen.o.STIP
+    vseipReg := platformIRP.VSEIP
+    hgeipVgeinReg := hgeip.rdata.asUInt(hstatus.regOut.VGEIN.asUInt)
+    vstipReg := sstcIRGen.o.VSTIP
+    aiaMeipReg := fromAIA.meip
+    aiaSeipReg := fromAIA.seip
+    lcofiReqReg := lcofiReq
+
+    val platformIRPMeipChange = !platformIRP.MEIP &&  meipReg || platformIRP.MEIP && !meipReg
+    val platformIRPMtipChange = !platformIRP.MTIP &&  mtipReg || platformIRP.MTIP && !mtipReg
+    val platformIRPMsipChange = !platformIRP.MSIP &&  msipReg || platformIRP.MSIP && !msipReg
+    val platformIRPSeipChange = !platformIRP.SEIP &&  seipReg || platformIRP.SEIP && !seipReg
+    val platformIRPStipChange = !sstcIRGen.o.STIP &&  stipReg || sstcIRGen.o.STIP && !stipReg
+    val platformIRPVseipChange = !platformIRP.VSEIP &&  vseipReg ||
+                                  platformIRP.VSEIP && !vseipReg ||
+                                 !hgeip.rdata.asUInt(hstatus.regOut.VGEIN.asUInt) &&  hgeipVgeinReg ||
+                                  hgeip.rdata.asUInt(hstatus.regOut.VGEIN.asUInt) && !hgeipVgeinReg
+    val platformIRPVstipChange = !sstcIRGen.o.VSTIP && vstipReg || sstcIRGen.o.VSTIP && !vstipReg
+    val fromAIAMeipChange = !fromAIA.meip && aiaMeipReg || fromAIA.meip && !aiaMeipReg
+    val fromAIASeipChange = !fromAIA.seip && aiaSeipReg || fromAIA.seip && !aiaSeipReg
+    val lcofiReqChange = !lcofiReq && lcofiReqReg || lcofiReq && !lcofiReqReg
 
     val diffNonRegInterruptPendingEvent = DifftestModule(new DiffNonRegInterruptPendingEvent)
     diffNonRegInterruptPendingEvent.coreid           := hartId
@@ -1656,7 +1679,9 @@ class NewCSR(implicit val p: Parameters) extends Module
     val diffMhpmeventOverflowEvent = DifftestModule(new DiffMhpmeventOverflowEvent)
     diffMhpmeventOverflowEvent.coreid := hartId
     diffMhpmeventOverflowEvent.valid  := Cat(mhpmevents.zipWithIndex.map{ case (event, i) =>
-      !ofFromPerfCntVec(i) && RegNext(ofFromPerfCntVec(i)) || ofFromPerfCntVec(i) && !RegNext(ofFromPerfCntVec(i))
+      val ofFromPerfCntVecReg = RegInit(false.B)
+      ofFromPerfCntVecReg := ofFromPerfCntVec(i)
+      !ofFromPerfCntVec(i) && ofFromPerfCntVecReg || ofFromPerfCntVec(i) && !ofFromPerfCntVecReg
     }).orR
     diffMhpmeventOverflowEvent.mhpmeventOverflow := VecInit(mhpmevents.map(_.regOut.asInstanceOf[MhpmeventBundle].OF.asBool)).asUInt
 
