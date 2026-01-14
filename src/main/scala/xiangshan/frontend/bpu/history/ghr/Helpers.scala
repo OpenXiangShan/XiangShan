d9cf7a0810d0ef96bc4dd979eb26f3787ca26bf4
YuanDL <2524247987@qq.com>
2026-01-14 10:18:12 +0800

    fix(ghr): fix getNewGhr caclulate error

--- src/main/scala/xiangshan/frontend/bpu/history/ghr/Helpers.scala
+++ src/main/scala/xiangshan/frontend/bpu/history/ghr/Helpers.scala
@@ -21,7 +21,7 @@ import xiangshan.frontend.bpu.HalfAlignHelper
 
 trait Helpers extends HasGhrParameters with HalfAlignHelper {
   def getNewGhr(oldGhr: UInt, numLess: UInt, numHit: UInt, taken: Bool): UInt = {
-    val numShift = Mux(taken, numLess, numHit - 1.U)
+    val numShift = Mux(taken, numLess, Mux(numHit === 0.U, 0.U, numHit - 1.U))
     Cat(oldGhr << numShift, taken)(GhrHistoryLength - 1, 0)
   }
 }
