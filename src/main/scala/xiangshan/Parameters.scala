bb29693b9bea8999fc588b58bee249301b3f8c03
Frankslu <1653954675@qq.com>
2026-01-16 17:06:56 +0800

    submodule(CoupledL2): bump CoupledL2 (#5190)
    Co-authored-by: Yangyu Chen <cyy@cyyself.name>
    Co-authored-by: Yanqin Li <maxpicca@qq.com>
    Co-authored-by: Yan Yiming <yanyiming21a@mails.ucas.ac.cn>

--- src/main/scala/xiangshan/Parameters.scala
+++ src/main/scala/xiangshan/Parameters.scala
@@ -799,7 +799,7 @@ trait HasXSParameter {
   def LFSTEnable = true
 
   def PCntIncrStep: Int = 6
-  def numPCntHc: Int = 12
+  def numPCntHc: Int = 17
   def numPCntPtw: Int = 19
 
   def numCSRPCntFrontend = 8
