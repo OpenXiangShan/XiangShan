86e5bf061eb51b228251d983d47f0bd12b77931c
Frankslu <1653954675@qq.com>
2026-01-21 21:15:41 +0800

    fix(L2Top): fix AddressSet error

--- src/main/scala/xiangshan/L2Top.scala
+++ src/main/scala/xiangshan/L2Top.scala
@@ -161,7 +161,7 @@ class L2TopInlined()(implicit p: Parameters) extends LazyModule
   private def icacheCtrlAddressOpt: Option[AddressSet] = Option.when(icacheCtrlEnabled)(icacheCtrlAddress)
   private def dcacheCtrlAddressOpt: Option[AddressSet] = dcacheParameters.cacheCtrlAddressOpt
   private def cacheAddressSet: Seq[AddressSet] = (icacheCtrlAddressOpt ++ dcacheCtrlAddressOpt).toSeq
-  private def mmioFilters = if(SeperateBus != top.SeperatedBusType.NONE) (SeperateBusRanges ++ cacheAddressSet) else cacheAddressSet
+  private def mmioFilters = (if(SeperateBus != top.SeperatedBusType.NONE) (SeperateBusRanges ++ cacheAddressSet) else cacheAddressSet) :+ soc.BEURange
   mmio_port :=
     TLFilter(TLFilter.mSubtract(mmioFilters)) :=
     TLBuffer() :=
