977b7e217fc793a5ccb04c495bac8eef753949f6
Haoyuan Feng <fenghaoyuan19@mails.ucas.ac.cn>
2026-01-22 10:09:04 +0800

    fix(YamlParser): turn on safe to solve reset on pending tranaction (#5546)
    In commit
    https://github.com/OpenXiangShan/XiangShan/commit/eec2464832ac4b3aa79268d61bbf5d6b14c7ffb6,
    we only enabled the safe switch for CHIAsynBridge in SoC.scala. However,
    when generating configurations using YAML, the modification to the safe
    switch did not take effect. Therefore, this change also enables the safe
    switch for CHIAsynBridge in YamlParser.

--- src/main/scala/top/YamlParser.scala
+++ src/main/scala/top/YamlParser.scala
@@ -93,7 +93,7 @@ object YamlParser {
     yamlConfig.EnableCHIAsyncBridge.foreach { enable =>
       newConfig = newConfig.alter((site, here, up) => {
         case SoCParamsKey => up(SoCParamsKey).copy(
-          EnableCHIAsyncBridge = Option.when(enable)(AsyncQueueParams(depth = 16, sync = 3, safe = false))
+          EnableCHIAsyncBridge = Option.when(enable)(AsyncQueueParams(depth = 16, sync = 3, safe = true))
         )
       })
     }
