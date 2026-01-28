5e6f2c6c9fcf3b576f2885f563ddd8d9b3ba8917
xiaofeibao <1441675923@qq.com>
2026-01-28 09:33:54 +0800

    tmp: close regCache, remove aluSelectJump

--- scripts/generate_all.sh
+++ scripts/generate_all.sh
@@ -3,7 +3,7 @@ set -x
 
 cd $NOOP_HOME
 
-for module in Frontend Backend MemBlock L2Top XSTile XSTop
+for module in Region
 do
   python3 scripts/parser.py $module --config TLConfig --prefix bosc_ --sram-replace --xs-home $(pwd) > generate_$module.log
   mv generate_$module.log bosc_${module}-Release*
