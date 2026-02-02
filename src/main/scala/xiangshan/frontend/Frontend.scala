073ca6fb9d35a1224df2904ed757b550e9e7d609
Anzooooo <anzoso@outlook.com>
2026-02-02 19:22:29 +0800

    reformat

--- src/main/scala/xiangshan/frontend/Frontend.scala
+++ src/main/scala/xiangshan/frontend/Frontend.scala
@@ -168,7 +168,7 @@ class FrontendInlinedImp(outer: FrontendInlined) extends LazyModuleImp(outer)
   itlb.io.hartId := io.hartId
   itlb.io.base_connect(sfence, tlbCsr)
   itlb.io.flushPipe.foreach(_ := icache.io.itlbFlushPipe)
-  itlb.io.redirect := DontCare // itlb has flushpipe, don't need redirect signal
+  itlb.io.redirect      := DontCare // itlb has flushpipe, don't need redirect signal
   itlb.io.robPendingPtr := DontCare // only lsu
 
   val itlb_ptw = Wire(new VectorTlbPtwIO(coreParams.itlbPortNum))
