0247b14aabd4eda28037328197974ed9f56acf79
Anzooooo <anzoso@outlook.com>
2026-02-12 00:38:48 +0800

    feat(Vec): vec store supports partial replay

--- src/main/scala/xiangshan/mem/lsqueue/StoreMisalignBuffer.scala
+++ src/main/scala/xiangshan/mem/lsqueue/StoreMisalignBuffer.scala
@@ -636,6 +636,7 @@ class StoreMisalignBuffer(implicit p: Parameters) extends XSModule
       wb.bits.vstart            := req.uop.vpu.vstart
       wb.bits.vecTriggerMask    := 0.U
       wb.bits.nc                := globalNC
+      wb.bits.splitIndx         := req.splitIndx
     }
   }
 
