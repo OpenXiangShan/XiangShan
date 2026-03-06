6103aa1eb17cc49955633ee9ec5417db8b670fd4
edwu186 <wuchengkai1998@gmail.com>
2026-03-06 14:59:44 +0800

    feat(Turbo):  L1 to L2 Latency from 15 to 11

--- src/main/scala/xiangshan/cache/dcache/DCacheWrapper.scala
+++ src/main/scala/xiangshan/cache/dcache/DCacheWrapper.scala
@@ -997,7 +997,8 @@ class DCacheImp(outer: DCache) extends LazyModuleImp(outer) with HasDCacheParame
   missQueue.io.hartId := io.hartId
   missQueue.io.l2_pf_store_only := RegNext(io.l2_pf_store_only, false.B)
   missQueue.io.debugTopDown <> io.debugTopDown
-  missQueue.io.l2_hint <> RegNext(io.l2_hint)
+  //missQueue.io.l2_hint <> RegNext(io.l2_hint)
+  missQueue.io.l2_hint <> io.l2_hint
   missQueue.io.mainpipe_info := mainPipe.io.mainpipe_info
   missQueue.io.occupy_set.zip(ldu.map(_.io.occupy_set)).foreach { case (l, r) => l <> r }
   missQueue.io.occupy_fail.zip(ldu.map(_.io.occupy_fail)).foreach { case (l, r) => l <> r }
