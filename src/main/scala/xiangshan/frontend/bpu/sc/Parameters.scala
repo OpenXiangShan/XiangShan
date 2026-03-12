7267e290fb14b7cd180304e135d56a36fb9ce879
YuanDL <2524247987@qq.com>
2026-03-12 19:56:18 +0800

    feat(sc): change wb size to 8

--- src/main/scala/xiangshan/frontend/bpu/sc/Parameters.scala
+++ src/main/scala/xiangshan/frontend/bpu/sc/Parameters.scala
@@ -42,8 +42,8 @@ case class ScParameters(
     CtrWidth:            Int = 6,
     ThresholdWidth:      Int = 13,
     ThresholdInit:       Int = 1130, // magic number,greater than min and less than max
-    NumBanks:            Int = 4,
-    WriteBufferSize:     Int = 4,
+    NumBanks:            Int = 2,
+    WriteBufferSize:     Int = 8,
     EnableScTrace:       Boolean = true
 ) {}
 
