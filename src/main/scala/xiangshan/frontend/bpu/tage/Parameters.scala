a944da23b24496995c13e468d22281c9603bb2e7
Cao Jiaming <caojiaming@bosc.ac.cn>
2026-03-24 18:12:25 +0800

    bpu: fix tage folded history width and fix merge conflict

--- src/main/scala/xiangshan/frontend/bpu/tage/Parameters.scala
+++ src/main/scala/xiangshan/frontend/bpu/tage/Parameters.scala
@@ -28,9 +28,9 @@ case class TageParameters(
       new TageTableInfo(4096, 2, 9),
       new TageTableInfo(32768, 4, 17),
       new TageTableInfo(16384, 2, 29),
-      new TageTableInfo(16384, 2, 56),
-      new TageTableInfo(16384, 2, 109),
-      new TageTableInfo(16384, 2, 211),
+      new TageTableInfo(8192, 2, 56),
+      new TageTableInfo(4096, 2, 109),
+      new TageTableInfo(4096, 2, 211),
       new TageTableInfo(4096, 2, 397)
     ),
     NumBanks:              Int = 4, // to alleviate read-write conflicts in single-port SRAM
