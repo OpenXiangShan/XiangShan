e752c91242b731083d4541b42b70a542efdf9638
edwu186 <wuchengkai1998@gmail.com>
2026-03-30 11:31:48 +0800

    mem: L3 Cahce to 32MB

--- src/main/scala/top/Configs.scala
+++ src/main/scala/top/Configs.scala
@@ -531,7 +531,7 @@ class FuzzConfig(dummy: Int = 0) extends Config(
 ) with DeprecatedConfigWarning
 
 class DefaultConfig(n: Int = 1) extends Config(
-  OpenLLCConfig("16MB", ways = 16, banks = 4)
+  OpenLLCConfig("32MB", ways = 16, banks = 4)
     ++ L2CacheConfig("2MB", inclusive = true, banks = 4, tp = false)
     ++ WithNKBL1D(64, ways = 4)
     ++ new BaseConfig(n)
