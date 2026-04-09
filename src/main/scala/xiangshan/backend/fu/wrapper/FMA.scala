7d8d25758b71646e8a4e73404b82dc62d65175cc
zhaozukang <2391900735@qq.com>
2026-04-09 15:45:10 +0800

    chore(FMA):remove old packeage method

--- src/main/scala/xiangshan/backend/fu/wrapper/FMA.scala
+++ src/main/scala/xiangshan/backend/fu/wrapper/FMA.scala
@@ -8,11 +8,9 @@ import xiangshan.backend.fu.FuConfig
 import xiangshan.backend.fu.vector.Bundles.VSew
 import xiangshan.backend.fu.fpu.FpPipedFuncUnit
 import yunsuan.encoding.Opcode.Opcodes.FMacOpcode
-import yunsuan.VfpuType
 import yunsuan.fpu.FloatFMA
 
 class FMA(cfg: FuConfig)(implicit p: Parameters) extends FpPipedFuncUnit(cfg) {
-  XSError(io.in.valid && io.in.bits.ctrl.fuOpType === VfpuType.dummy, "fma OpType not supported")
 
   // io alias
   private val src0 = inData.src(0)
