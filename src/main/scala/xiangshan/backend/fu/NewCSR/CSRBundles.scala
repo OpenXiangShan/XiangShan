8f4eb32acd796be2e5d7b1bc414b48e850373983
sinceforYy <1017657683@qq.com>
2026-04-14 10:42:14 +0800

    fix(decode): merge fromCSR bundle
    [cherry-pick from 63482d078b]

--- src/main/scala/xiangshan/backend/fu/NewCSR/CSRBundles.scala
+++ src/main/scala/xiangshan/backend/fu/NewCSR/CSRBundles.scala
@@ -214,8 +214,5 @@ object CSRBundles {
     val hd_misalign_ld_enable = Output(Bool())
     val power_down_enable = Output(Bool())
     val flush_l2_enable = Output(Bool())
-    // Rename
-    val fusion_enable = Output(Bool())
-    val wfi_enable = Output(Bool())
   }
 }
