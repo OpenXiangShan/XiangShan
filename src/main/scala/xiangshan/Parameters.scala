3b2535f12c06a420104942d634877105d6be641e
Xuan Hu <huxuan@bosc.ac.cn>
2026-04-16 16:39:42 +0800

    refactor(VIAlu): upgrade code quality and timing
    * Remove VIPU temporarily.
    * Remove useless code.
    * Todo: support vector pma.

--- src/main/scala/xiangshan/Parameters.scala
+++ src/main/scala/xiangshan/Parameters.scala
@@ -489,7 +489,7 @@ case class XSCoreParameters
       IssueBlockParams(Seq(
         ExeUnitParams(
           "VFEX0",
-          Seq(VialuCfg, VfaluCfg, VfmaCfg, VimacCfg, VppuCfg, VipuCfg, VfcvtCfg, VmoveCfg),
+          Seq(VialuCfg, VfaluCfg, VfmaCfg, VimacCfg, VppuCfg, VfcvtCfg, VmoveCfg),
           Seq(VfWB(port = 3, 0), IntWB(port = 4, 1), FpWB(port = 6, 0)),
           Seq(Seq(VfRD(0, 0)), Seq(VfRD(1, 0)), Seq(VfRD(2, 0))),
           vlRD = VlRD(0, 0),
