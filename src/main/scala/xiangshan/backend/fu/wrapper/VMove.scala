89d69bc943da2d49b3a3a5fd3b547844b014f32e
Xuan Hu <huxuan@bosc.ac.cn>
2026-04-17 14:34:24 +0800

    feat(vector,fu): support multi stage output in new vector fu
    * Add LatDecoder to generate latency of uop.
    * Shrink latency of VMove to 0.
    * Only keep VialuCfg and VmoveCfg in Parameters.scala
      * Todo: support VfaluCfg, VfmaCfg, VimacCfg, VppuCfg, VfcvtCfg, VfdivCfg, VidivCfg
    
    Co-authored-by: wang sangping <wangsangping@node009.bosccluster.com>

--- src/main/scala/xiangshan/backend/fu/wrapper/VMove.scala
+++ src/main/scala/xiangshan/backend/fu/wrapper/VMove.scala
@@ -10,67 +10,37 @@ import xiangshan.backend.fu.vector.Utils.VecDataToMaskDataVec
 import xiangshan.backend.fu.vector.{Mgu, VecPipedFuncUnit}
 import yunsuan.encoding.Opcode.Opcodes.VMoveOpcode
 import yunsuan.vector.VectorMove.VectorMove
+import xiangshan.backend.vector.fu.util._
+import yunsuan.vector.Common._
 
-class VMove(cfg: FuConfig)(implicit p: Parameters) extends VecPipedFuncUnit(cfg) {
-  XSError(io.in.valid && !VMoveOpcode.isLegal(io.in.bits.ctrl.fuOpType), "VMove OpType not supported")
-
+class VMove(cfg: VecFuConfig)(implicit p: Parameters) extends VecFixLatFunc(cfg) {
+  // XSError(io.in.valid && !VMoveOpcode.isLegal(io.in.bits.ctrl.fuOpType), "VMove OpType not supported")
+ 
+  private implicit val opcode: UInt = fuOpType
   // param alias
   private val dataWidth = cfg.destDataBits
   private val vlenb = VLEN / 8
 
-  private val valid = io.in.valid
-
   private val vMove = Module(new VectorMove)
-  private val mgu = Module(new Mgu(dataWidth))
-
-  // mask
-  private val maskDataVec: Vec[UInt] = VecDataToMaskDataVec(srcMask, vsew)
-  private val maskVec = maskDataVec(vuopIdx)
 
   vMove match {
     case mod =>
-      mod.io.in.valid := valid
+      mod.io.in.valid := ex(0).valid
       mod.io.in.bits.opcode := fuOpType.take(VMoveOpcode.getWidth)
-      mod.io.in.bits.info.vm := vm
-      mod.io.in.bits.info.vsew := vsew
-      mod.io.in.bits.vs2 := vs2
-      mod.io.in.bits.vs1 := vs1
-      mod.io.in.bits.mask := maskVec
+      mod.io.in.bits.info.vm := ex0vm
+      mod.io.in.bits.info.vsew := VMoveOpcode.getElemWidth
+      mod.io.in.bits.vs2 := ex0vs2
+      mod.io.in.bits.vs1 := ex0vs1
+      mod.io.in.bits.mask := ex(0).data.v0.get // Todo: use 16b mask instead
   }
 
-  private val needNoMask = VMoveOpcode.needNoMask(fuOpType)
-  private val maskToMgu = Mux(needNoMask, allMaskTrue, srcMask)
-
-  private val vlIsOne = VMoveOpcode.vlIsOne(fuOpType)
-  private val vlIsZeroUpdate = VMoveOpcode.vlIsZeroUpdate(fuOpType)
-  private val isVmvnr = VMoveOpcode.isNR(fuOpType)
-  // when vstart >= vl, no need update vd, the old vd should be keep
-  // vmv<nr>r.v, evl = EMUL * VLEN / VSEW, when vstart >= evl, no need update vd
-  private val emul = Mux(isVmvnr, VLmul.m8, vlmul)
-  private val eleCnt = vlenb.U >> vsew
-  private val evl = Mux1H(Seq.tabulate(4)(i => (emul(1, 0) === i.U) -> (eleCnt << i.U)))
-  private val vlTmp = Mux(isVmvnr, evl.asUInt, vl)
-  private val newVl = Mux(vlIsOne, 1.U, vlTmp)
-  private val vstartGeVl = (vstart >= vlTmp || !vlTmp.orR) & !vlIsZeroUpdate
-
-  private val vd = vMove.io.out.bits.vd
-
-  mgu.io.in.vd := vd
-  mgu.io.in.oldVd := oldVd
-  mgu.io.in.mask := maskToMgu
-  mgu.io.in.info.ta := vta
-  mgu.io.in.info.ma := vma
-  mgu.io.in.info.vl := newVl
-  mgu.io.in.info.vlmul := emul
-  mgu.io.in.info.valid := valid
-  mgu.io.in.info.vstart := vstart
-  mgu.io.in.info.eew := vsew
-  mgu.io.in.info.vsew := vsew
-  mgu.io.in.info.vdIdx := vuopIdx
-  mgu.io.in.info.narrow := false.B
-  mgu.io.in.info.dstMask := false.B
-  mgu.io.in.isIndexedVls := false.B
-
-  io.out.bits.res.data := Mux(vstartGeVl, oldVd, mgu.io.out.vd)
+  out.ex(0).data.int.foreach(_ := vMove.io.out.bits.vd)
+  out.ex(0).data.fp.foreach(_ := vMove.io.out.bits.vd)
+  out.ex(0).data.vec.foreach(_.normal := vMove.io.out.bits.vd)
+  out.ex(0).data.vec.foreach(_.narrow := 0.U)
+  out.ex(0).data.vec.foreach(_.maskE8 := 0.U)
+  out.ex(0).data.vec.foreach(_.maskE16 := 0.U)
+  out.ex(0).data.vec.foreach(_.maskE32 := 0.U)
+  out.ex(0).data.vec.foreach(_.maskE64 := 0.U)
 }
 
