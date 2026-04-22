3cc0eb4e6abe67daa774045b44555e10a250786c
wangsangping <3361833374@qq.com>
2026-04-22 14:09:09 +0800

    feat(VecIssueQueue,VecRegion): add latency-aware fu wb busy table

--- src/main/scala/xiangshan/backend/vector/IssueParam.scala
+++ src/main/scala/xiangshan/backend/vector/IssueParam.scala
@@ -117,6 +117,20 @@ class IssueParam(
     exuParams.flatMap(_.getGpReadCfgs)
   }
 
+  private def collectWbPortIds(wbPorts: Seq[Option[Int]]): Seq[Int] = {
+    wbPorts.flatten.distinct.sorted
+  }
+
+  def intWbPortIds: Seq[Int] = collectWbPortIds(exuParams.map(_.getGpWriteCfg.map(_.port)))
+
+  def fpWbPortIds: Seq[Int] = collectWbPortIds(exuParams.map(_.getFpWriteCfg.map(_.port)))
+
+  def vpWbPortIds: Seq[Int] = collectWbPortIds(exuParams.map(_.getVpWriteCfg.map(_.port)))
+
+  def v0WbPortIds: Seq[Int] = collectWbPortIds(exuParams.map(_.getV0WriteCfg.map(_.port)))
+
+  def vlWbPortIds: Seq[Int] = collectWbPortIds(exuParams.map(_.getVlWriteCfg.map(_.port)))
+
   def getFpReadCfgs: Seq[RdConfig] = {
     exuParams.flatMap(_.getFpReadCfgs)
   }
