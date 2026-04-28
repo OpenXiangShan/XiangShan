6aa6dbaa4196e1adc2b0b39a4f8e77608a89084a
lixin <1037997956@qq.com>
2026-04-28 15:57:53 +0800

    mem: split dcache tag as 2 banks

--- src/main/scala/xiangshan/cache/dcache/meta/TagArray.scala
+++ src/main/scala/xiangshan/cache/dcache/meta/TagArray.scala
@@ -52,7 +52,7 @@ abstract class AbstractTagArray(implicit p: Parameters) extends DCacheModule {
   val TagEccParam = if(EnableTagEcc) Some(HasTagEccParam) else None
 }
 
-class TagSRAMBank(index: Int)(implicit p: Parameters) extends AbstractTagArray {
+class TagSRAMBank(index: Int, setBankIndex: Int)(implicit p: Parameters) extends AbstractTagArray {
   val io = IO(new Bundle() {
     val read = Flipped(DecoupledIO(new TagReadReq {
       override val way_en = UInt(DCacheWayDiv.W)
@@ -62,23 +62,27 @@ class TagSRAMBank(index: Int)(implicit p: Parameters) extends AbstractTagArray {
       override val way_en = UInt(DCacheWayDiv.W)
     }))
   })
+
+  private val setsPerBank = nSets / DCacheTagBanks
+  private val bankSelectedForRead = set_to_tag_bank(io.read.bits.idx) === setBankIndex.U
+  private val bankSelectedForWrite = set_to_tag_bank(io.write.bits.idx) === setBankIndex.U
+
   // TODO: reset is unnecessary?
-  val rst_cnt = RegInit(0.U(log2Up(nSets + 1).W))
-  val rst = rst_cnt < nSets.U
+  val rst_cnt = RegInit(0.U(log2Up(setsPerBank + 1).W))
+  val rst = rst_cnt < setsPerBank.U
   val rstVal = 0.U
-  val waddr = Mux(rst, rst_cnt, io.write.bits.idx)
+  val waddr = Mux(rst, rst_cnt, set_to_tag_bank_set(io.write.bits.idx))
   val wdata = Mux(rst, rstVal, io.write.bits.asECCTag())
   val wmask = Mux(rst || (DCacheWayDiv == 1).B, (-1).asSInt, io.write.bits.way_en.asSInt).asBools
-  val rmask = Mux(rst || (DCacheWayDiv == 1).B, (-1).asSInt, io.read.bits.way_en.asSInt).asBools
   when (rst) {
     rst_cnt := rst_cnt + 1.U
   }
 
-  val tag_array = Module(new SRAMTemplate(UInt(encTagBits.W), set = nSets, way = DCacheWayDiv,
+  val tag_array = Module(new SRAMTemplate(UInt(encTagBits.W), set = setsPerBank, way = DCacheWayDiv,
     shouldReset = false, holdRead = false, singlePort = true, withClockGate = true,
     hasMbist = hasMbist,  hasSramCtl = hasSramCtl, suffix = Some("dcsh_tag")))
 
-  val wen = rst || io.write.valid
+  val wen = rst || (io.write.valid && bankSelectedForWrite)
   io.write.ready := !rst
   tag_array.io.w.req.valid := wen
   tag_array.io.w.req.bits.apply(
@@ -88,10 +92,10 @@ class TagSRAMBank(index: Int)(implicit p: Parameters) extends AbstractTagArray {
   )
 
   // tag read
-  val ren = io.read.fire
-  io.read.ready := !wen
+  val ren = io.read.fire && bankSelectedForRead
+  io.read.ready := !wen || !bankSelectedForRead
   tag_array.io.r.req.valid := ren
-  tag_array.io.r.req.bits.apply(setIdx = io.read.bits.idx)
+  tag_array.io.r.req.bits.apply(setIdx = set_to_tag_bank_set(io.read.bits.idx))
   io.resp := tag_array.io.r.resp.data
 
   XSPerfAccumulate("part_tag_read_counter_" + index, tag_array.io.r.req.valid)
@@ -104,16 +108,43 @@ class TagArray(implicit p: Parameters) extends AbstractTagArray {
     val write = Flipped(DecoupledIO(new TagWriteReq))
   })
 
-  val tag_arrays = List.tabulate(nWays / DCacheWayDiv)(i => Module(new TagSRAMBank(i)))
-  tag_arrays.zipWithIndex.foreach { case (tag_array, i) =>
-    tag_array.io.read <> io.read
-    tag_array.io.read.bits.way_en := io.read.bits.way_en((i + 1) * DCacheWayDiv - 1, i * DCacheWayDiv)
-    tag_array.io.write <> io.write
-    tag_array.io.write.bits.way_en := io.write.bits.way_en((i + 1) * DCacheWayDiv - 1, i * DCacheWayDiv)
+  val wayDivs = nWays / DCacheWayDiv
+  val readBankSel = set_to_tag_bank(io.read.bits.idx)
+  val writeBankSel = set_to_tag_bank(io.write.bits.idx)
+  val readBankSelReg = RegInit(0.U((DCacheTagBankBits max 1).W))
+  when (io.read.fire) {
+    readBankSelReg := readBankSel
   }
-  io.resp.zip(tag_arrays.map(_.io.resp).flatten).foreach {
-    case (resp, bank_resp) =>
-      resp := bank_resp
+
+  val tag_arrays = Seq.tabulate(wayDivs, DCacheTagBanks) { (wayDivIdx, setBankIdx) =>
+    Module(new TagSRAMBank(wayDivIdx * DCacheTagBanks + setBankIdx, setBankIdx))
+  }
+
+  tag_arrays.zipWithIndex.foreach { case (setBankArrays, wayDivIdx) =>
+    setBankArrays.foreach { tag_array =>
+      tag_array.io.read.valid := io.read.valid
+      tag_array.io.read.bits.idx := io.read.bits.idx
+      tag_array.io.read.bits.way_en := io.read.bits.way_en((wayDivIdx + 1) * DCacheWayDiv - 1, wayDivIdx * DCacheWayDiv)
+
+      tag_array.io.write.valid := io.write.valid
+      tag_array.io.write.bits.idx := io.write.bits.idx
+      tag_array.io.write.bits.way_en := io.write.bits.way_en((wayDivIdx + 1) * DCacheWayDiv - 1, wayDivIdx * DCacheWayDiv)
+      tag_array.io.write.bits.vaddr := io.write.bits.vaddr
+      tag_array.io.write.bits.tag := io.write.bits.tag
+      tag_array.io.write.bits.ecc := io.write.bits.ecc
+    }
+  }
+
+  io.read.ready := VecInit(tag_arrays.head.map(_.io.read.ready))(readBankSel)
+  io.write.ready := VecInit(tag_arrays.head.map(_.io.write.ready))(writeBankSel)
+
+  val bankedResp = Wire(Vec(wayDivs, Vec(DCacheWayDiv, UInt(encTagBits.W))))
+  tag_arrays.zipWithIndex.foreach { case (setBankArrays, wayDivIdx) =>
+    bankedResp(wayDivIdx) := VecInit(setBankArrays.map(_.io.resp))(readBankSelReg)
+  }
+  io.resp.zip(bankedResp.flatten).foreach {
+    case (resp, bankResp) =>
+      resp := bankResp
   }
 }
 
@@ -145,13 +176,13 @@ class DuplicatedTagArray(readPorts: Int)(implicit p: Parameters) extends Abstrac
     array(i).io.write.bits.vaddr := io.write.bits.vaddr
     array(i).io.write.bits.tag := io.write.bits.tag
     array(i).io.write.bits.ecc := getECCFromEncTag(cacheParams.tagCode.encode(io.write.bits.tag))
-    io.write.ready := true.B
 
     array(i).io.read <> io.read(i)
     io.read(i).ready := array(i).io.read.ready
     io.resp(i) := array(i).io.resp
     tag_read_oh(i) := PopCount(array(i).io.read.fire)
   }
+  io.write.ready := array.head.io.write.ready
 
   XSPerfAccumulate("tag_read_counter", tag_read_oh.reduce(_ + _))
 }
