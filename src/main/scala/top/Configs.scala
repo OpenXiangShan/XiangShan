1cc3d196320725cbbd98e84e18a92fd882539511
lixin <1037997956@qq.com>
2026-05-14 15:01:45 +0800

    mem: set l2 as 128 mshr

--- src/main/scala/top/Configs.scala
+++ src/main/scala/top/Configs.scala
@@ -345,6 +345,7 @@ case class L2CacheConfig
         elaboratedTopDown = !site(DebugOptionsKey).FPGAPlatform,
         hasMbist = site(DFTOptionsKey).EnableMbist,
         hasSramCtl = site(DFTOptionsKey).EnableSramCtl,
+        mshrs = 32
       )),
       L2NBanks = banks
     ))
