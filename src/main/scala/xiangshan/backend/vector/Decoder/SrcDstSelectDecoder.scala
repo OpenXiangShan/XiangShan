78df0e8289417a30838032577700ca0135421a70
胡格致 <hugezhi23@mails.ucas.ac.cn>
2026-06-11 13:36:11 +0800

    refactor(VectorDecodeChannel): pass vstart into VectorDecodeChannel and use UopInfoRename instead of UopInfoRenameWithIllegal

--- src/main/scala/xiangshan/backend/vector/Decoder/SrcDstSelectDecoder.scala
+++ src/main/scala/xiangshan/backend/vector/Decoder/SrcDstSelectDecoder.scala
@@ -2,17 +2,20 @@ package xiangshan.backend.vector.Decoder
 
 import chisel3._
 import chisel3.experimental.hierarchy.{instantiable, public}
+import org.chipsalliance.cde.config.Parameters
 import xiangshan.backend.vector.Decoder.DecodeFields.VecDecodeChannel._
 import xiangshan.backend.vector.Decoder.InstPattern._
 import xiangshan.backend.vector.Decoder.RVVDecodeUtil._
 import xiangshan.backend.vector.Decoder.util.{DecodeBundle, DecodeTable}
 import xiangshan.backend.vector._
 import xiangshan.backend.vector.util.Verilog
+import xiangshan.{XSCoreParameters, XSCoreParamsKey}
+import top.ArgParser
 
 @instantiable
 class SrcDstSelectDecoder(
   instSeq: Seq[DecodePatternComb2[VecInstPattern, SewPattern]],
-) extends Module with HasVectorSettings {
+)(implicit p: Parameters) extends Module with HasVectorSettings {
   import SrcDstSelectDecoder._
 
   @public val in = IO(Input(new DecodeChannelInput))
@@ -38,9 +41,11 @@ class SrcDstSelectDecoder(
 
 object SrcDstSelectDecoder {
   def main(args: Array[String]): Unit = {
+    val (config, _, _) = ArgParser.parse(args)
+    val defaultConfig = config.alterPartial({ case XSCoreParamsKey => XSCoreParameters })
     val instPatterns = VecInstPattern.withSew(VecInstPattern.all)
     Verilog.emitVerilog(
-      new SrcDstSelectDecoder(instPatterns),
+      new SrcDstSelectDecoder(instPatterns)(defaultConfig),
       Array("--target-dir", "build/decoder")
     )
   }
