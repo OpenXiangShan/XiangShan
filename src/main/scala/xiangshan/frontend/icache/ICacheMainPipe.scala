0b38e02ab25fbb3d037826c7d79b7e8a049716fa
out-of-order55 <515897968@qq.com>
2026-06-23 17:13:41 +0800

    timing(icache,ifu): delay miss resp 1 cycle for better timing
    Co-authored-by: zhou tao <mayflyandtree@163.com>

--- src/main/scala/xiangshan/frontend/icache/ICacheMainPipe.scala
+++ src/main/scala/xiangshan/frontend/icache/ICacheMainPipe.scala
@@ -188,11 +188,16 @@ class ICacheMainPipe(implicit p: Parameters) extends ICacheModule
   private val s1_mshrDatas       = fromMiss.bits.data.asTypeOf(Vec(DataBanks, UInt(ICacheDataBits.W)))
   private val s1_mshrMaybeRvcMap = fromMiss.bits.maybeRvcMap.asTypeOf(Vec(DataBanks, UInt(MaxInstNumPerBank.W)))
 
+  private val s1_mshrValidReg       = RegNext(s1_mshrValid)
+  private val s1_bankMshrValidReg   = RegNext(s1_bankMshrValid)
+  private val s1_mshrDatasReg       = RegNext(s1_mshrDatas)
+  private val s1_mshrMaybeRvcMapReg = RegNext(s1_mshrMaybeRvcMap)
+
   private val s1_hits = VecInit((0 until MaxFetchReqNum).map { reqIdx =>
     VecInit((0 until PortNumber).map { portIdx =>
       DataHoldBypass(
-        s1_mshrValid(reqIdx)(portIdx) || s1_sramHits(reqIdx)(portIdx),
-        s1_mshrValid(reqIdx)(portIdx) || s1_sramValid(reqIdx)(portIdx)
+        s1_mshrValidReg(reqIdx)(portIdx) || s1_sramHits(reqIdx)(portIdx),
+        s1_mshrValidReg(reqIdx)(portIdx) || s1_sramValid(reqIdx)(portIdx)
       )
     })
   })
@@ -203,11 +208,11 @@ class ICacheMainPipe(implicit p: Parameters) extends ICacheModule
     VecInit((0 until DataBanks).map { bankIdx =>
       DataHoldBypass(
         Mux(
-          s1_bankMshrValid(reqIdx)(bankIdx),
-          s1_mshrDatas(bankIdx),
+          s1_bankMshrValidReg(reqIdx)(bankIdx),
+          s1_mshrDatasReg(bankIdx),
           s1_sramDatas(reqIdx)(bankIdx)
         ),
-        s1_bankMshrValid(reqIdx)(bankIdx) || s1_bankSramValid(reqIdx)(bankIdx)
+        s1_bankMshrValidReg(reqIdx)(bankIdx) || s1_bankSramValid(reqIdx)(bankIdx)
       )
     }).asUInt
   })
@@ -219,15 +224,15 @@ class ICacheMainPipe(implicit p: Parameters) extends ICacheModule
     VecInit((0 until DataBanks).map { bankIdx =>
       DataHoldBypass(
         Mux(
-          s1_bankMshrValid(reqIdx)(bankIdx),
-          s1_mshrMaybeRvcMap(bankIdx),
+          s1_bankMshrValidReg(reqIdx)(bankIdx),
+          s1_mshrMaybeRvcMapReg(bankIdx),
           Mux(
             s1_lineSel(reqIdx)(bankIdx),
             sramMaybeRvcMap(1)(bankIdx),
             sramMaybeRvcMap(0)(bankIdx)
           )
         ),
-        s1_bankMshrValid(reqIdx)(bankIdx) || s1_bankSramValid(reqIdx)(bankIdx)
+        s1_bankMshrValidReg(reqIdx)(bankIdx) || s1_bankSramValid(reqIdx)(bankIdx)
       )
     }).asUInt
   })
@@ -235,8 +240,8 @@ class ICacheMainPipe(implicit p: Parameters) extends ICacheModule
   private val s1_tlCorrupt = VecInit((0 until MaxFetchReqNum).map { reqIdx =>
     VecInit((0 until PortNumber).map { portIdx =>
       DataHoldBypass(
-        s1_mshrValid(reqIdx)(portIdx) && fromMiss.bits.corrupt,
-        s1_mshrValid(reqIdx)(portIdx) || s1_sramValid(reqIdx)(portIdx)
+        s1_mshrValidReg(reqIdx)(portIdx) && RegNext(fromMiss.bits.corrupt),
+        s1_mshrValidReg(reqIdx)(portIdx) || s1_sramValid(reqIdx)(portIdx)
       )
     })
   })
@@ -244,8 +249,8 @@ class ICacheMainPipe(implicit p: Parameters) extends ICacheModule
   private val s1_tlDenied = VecInit((0 until MaxFetchReqNum).map { reqIdx =>
     VecInit((0 until PortNumber).map { portIdx =>
       DataHoldBypass(
-        s1_mshrValid(reqIdx)(portIdx) && fromMiss.bits.denied,
-        s1_mshrValid(reqIdx)(portIdx) || s1_sramValid(reqIdx)(portIdx)
+        s1_mshrValidReg(reqIdx)(portIdx) && RegNext(fromMiss.bits.denied),
+        s1_mshrValidReg(reqIdx)(portIdx) || s1_sramValid(reqIdx)(portIdx)
       )
     })
   })
