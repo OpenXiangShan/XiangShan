9f1ef46343e73fc4f12e7b48675977d27905f38d
zhaozukang <2391900735@qq.com>
2026-06-29 17:05:14 +0800

    fix(jal,jalr):use first link uop to get right jumpType

--- src/main/scala/xiangshan/backend/vector/Decoder/Uop/ScalaUopTable.scala
+++ src/main/scala/xiangshan/backend/vector/Decoder/Uop/ScalaUopTable.scala
@@ -84,7 +84,7 @@ object ScalaUopTable {
 
     val tableJumpLink = JumpLinkType.mapUopcodes(
       _.JAL     -> Seq(link, j),
-      _.JALR    -> Seq(link, jr),
+      _.JALR    -> Seq(linkjr, jr),
     )
 
     tableI64Type ++ tableIType ++ tableJumpLink
