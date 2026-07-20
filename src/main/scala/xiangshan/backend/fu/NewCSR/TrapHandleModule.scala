2390bd2ae98e70be8989d2f334a76a05828f1208
sinceforYy <1017657683@qq.com>
2026-07-20 14:54:33 +0800

    fix(csr,dbltrp): fix s_EX_DT should be controlled by sstatus.SDT

--- src/main/scala/xiangshan/backend/fu/NewCSR/TrapHandleModule.scala
+++ src/main/scala/xiangshan/backend/fu/NewCSR/TrapHandleModule.scala
@@ -14,6 +14,7 @@ class TrapHandleModule extends Module {
   private val trapInfo = io.in.trapInfo
   private val privState = io.in.privState
   private val mstatus  = io.in.mstatus
+  private val sstatus  = io.in.sstatus
   private val vsstatus = io.in.vsstatus
   private val mnstatus = io.in.mnstatus
   private val mideleg = io.in.mideleg.asUInt
@@ -80,7 +81,7 @@ class TrapHandleModule extends Module {
 
   // sm/ssdbltrp
   private val m_EX_DT  = handleTrapUnderM  && mstatus.MDT.asBool  && hasTrap
-  private val s_EX_DT  = handleTrapUnderHS && mstatus.SDT.asBool  && hasTrap
+  private val s_EX_DT  = handleTrapUnderHS && sstatus.SDT.asBool  && hasTrap
   private val vs_EX_DT = handleTrapUnderVS && vsstatus.SDT.asBool && hasTrap
 
   private val dbltrpToMN = m_EX_DT && mnstatus.NMIE.asBool // NMI not allow double trap
@@ -126,6 +127,7 @@ class TrapHandleIO extends Bundle {
     })
     val privState = new PrivState
     val mstatus = new MstatusBundle
+    val sstatus = new SstatusBundle
     val vsstatus = new SstatusBundle
     val mnstatus = new MnstatusBundle
     val mideleg = new MidelegBundle
