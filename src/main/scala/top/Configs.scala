d149cf44698fdb79d1638e437cba6ad52a60a0dd
zhanglinjuan <zhanglinjuan16@mails.ucas.ac.cn>
2026-07-22 18:33:30 +0800

    fix(Configs): set EWA=0 and EndpointOrder for NC on ZhuJiang

--- src/main/scala/top/Configs.scala
+++ src/main/scala/top/Configs.scala
@@ -416,7 +416,9 @@ case class LLCConfig(llc: String) extends Config((site, here, up) => {
     up(XSTileKey).map { tile =>
       tile.copy(L2CacheParamsOpt = tile.L2CacheParamsOpt.map(_.copy(
         dataCheck = None,
-        enablePoison = false
+        enablePoison = false,
+        bufferableNC = false,
+        endpointOrderNC = true
       )))
     }
 })
