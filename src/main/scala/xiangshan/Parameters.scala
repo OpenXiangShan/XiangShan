22d355e38de0a719c6572570698708664574d14f
HeiHuDie <1042519051@qq.com>
2026-07-23 16:54:58 +0800

    peformance 192

--- src/main/scala/xiangshan/Parameters.scala
+++ src/main/scala/xiangshan/Parameters.scala
@@ -152,7 +152,7 @@ case class XSCoreParameters
   EnableBackendV2Config: Boolean = false,
   EnableDispatchIQBalanceOpt: Boolean = true,
   intPreg: PregParams = IntPregParams(
-    numEntries = 160,
+    numEntries = 192,
     numBank    = 4,
     numRead    = None,
     numWrite   = None,
