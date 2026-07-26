e20e77cbf11fc6bd9d2d504a2fb7d4ca1e67aca9
yangzechen <yangzechen21@mails.ucas.ac.cn>
2026-07-26 15:19:38 +0800

    perf: enable CDP and set reqFilterEntryNum to 32 for test

--- src/main/scala/top/Configs.scala
+++ src/main/scala/top/Configs.scala
@@ -302,7 +302,7 @@ case class L2CacheConfig
   banks: Int = 1,
   tp: Boolean = true,
   nl: Boolean = false,
-  cdp: Boolean = false,
+  cdp: Boolean = true,
   enablePC: Boolean = false, // Enable PC field for L1Param
   enableFlush: Boolean = false
 ) extends Config((site, here, up) => {
@@ -339,7 +339,7 @@ case class L2CacheConfig
         prefetch = Seq(BOPParameters()) ++
           (if (tp) Seq(TPParameters()) else Nil) ++
           (if (nl) Seq(NLParameters()) else Nil) ++
-          (if (cdp) Seq(CDPParameters()) else Nil) ++
+          (if (cdp) Seq(CDPParameters(reqFilterEntryNum=32)) else Nil) ++
           (if (p.prefetcher.nonEmpty) Seq(PrefetchReceiverParams()) else Nil),
         enableL2Flush = enableFlush,
         enablePerf = !site(DebugOptionsKey).FPGAPlatform && site(DebugOptionsKey).EnablePerfDebug,
