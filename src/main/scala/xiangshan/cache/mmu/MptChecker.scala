84f71054da7df6b8fe6bfa73dd751a5cb24febe8
HaoyuanLiuHZPH <2050576318@qq.com>
2026-07-30 18:01:44 +0800

    fix mpt walker bug

--- src/main/scala/xiangshan/cache/mmu/MptChecker.scala
+++ src/main/scala/xiangshan/cache/mmu/MptChecker.scala
@@ -1070,7 +1070,7 @@ class MptTableWalkerIO(implicit p: Parameters) extends MMUIOBaseBundle with MPTC
   val mem = new Bundle {
     val req  = DecoupledIO(new Bundle { val addr = UInt(PAddrBits.W) })
     val resp = Flipped(ValidIO(UInt(XLEN.W)))
-    // val mask = Input(Bool()) dont need？
+    val mask = Input(Bool())
   }
 
   val pmp = new Bundle {
@@ -1088,6 +1088,12 @@ class MPTTableWalker(implicit p: Parameters) extends XSModule with MPTCacheParam
   io.pmp.req.bits.cmd  := TlbCmd.read
 
   val flush = io.sfence.valid || io.csr.mmpt.changed
+
+  // The MPT walker shares one fixed memory source ID.  Keep the response
+  // qualified locally, while L2TLB's mask prevents that ID from being reused
+  // until a flushed request's late response has drained.
+  val waitingMemResp = RegInit(false.B)
+  val memRespAccepted = io.mem.resp.valid && waitingMemResp && !flush
 
   val pa = RegEnable(io.req.bits.reqPA, 0.U, io.req.fire)
   // Store the received request PA in a register, used to generate the PN1/2/3 offsets for synthesizing the table walk address.
@@ -1112,16 +1118,16 @@ class MPTTableWalker(implicit p: Parameters) extends XSModule with MPTCacheParam
   // Stores the returned permissions/lower-level address, or the incoming request address entry;
   // used to return permissions or synthesize the lower-level page table address with PA.
   io.refill.bits.refillData := mpteData // output alloc
-  val isLeafMpte = RegEnable(mpteResp.isLeaf, false.B, io.mem.resp.valid)
+  val isLeafMpte = RegEnable(mpteResp.isLeaf, false.B, memRespAccepted)
   io.refill.bits.isLeafMpte := isLeafMpte // tell cache if the current refill is leaf node
-  val mpteInvalid = RegEnable(!mpteResp.isValid, false.B, io.mem.resp.valid) // 1 level not on top of mem.resp
-  val rsvZeroError0 = RegEnable(mem.resp.bits(9, 2).orR, false.B, io.mem.resp.valid)
+  val mpteInvalid = RegEnable(!mpteResp.isValid, false.B, memRespAccepted) // 1 level not on top of mem.resp
+  val rsvZeroError0 = RegEnable(mem.resp.bits(9, 2).orR, false.B, memRespAccepted)
   // max 3 level or gate on top of mem.resp，NON ZERO error of mtpe
-  val rsvZeroError1 = RegEnable(mem.resp.bits(62, 58).orR, false.B, io.mem.resp.valid)
+  val rsvZeroError1 = RegEnable(mem.resp.bits(62, 58).orR, false.B, memRespAccepted)
   val rsvZeroError2 = false.B
   when(io.req.fire) {
     mpteData.apply(io.req.bits.hitAddr)
-  }.elsewhen(io.mem.resp.valid) {
+  }.elsewhen(memRespAccepted) {
     mpteData := mpteResp.data
   }
 
@@ -1137,7 +1143,7 @@ class MPTTableWalker(implicit p: Parameters) extends XSModule with MPTCacheParam
   val memAddr = mpteData.getAddr(pn) // gen addr 0 delay
   io.mem.req.bits.addr := memAddr
   io.pmp.req.valid := DontCare
-  io.pmp.req.bits.addr := Mux(io.mem.resp.valid, mpteResp.getAddr(pn), memAddr)
+  io.pmp.req.bits.addr := Mux(memRespAccepted, mpteResp.getAddr(pn), memAddr)
   // should be safer than just := memAddr
 
   // accessFault logic
@@ -1182,14 +1188,14 @@ class MPTTableWalker(implicit p: Parameters) extends XSModule with MPTCacheParam
       }
     }
     is(s_mem_req) {
-      mem.req.valid := true.B // req valid when not fire
+      mem.req.valid := !mem.mask // do not reuse the fixed source ID while an old response is outstanding
 
       when(io.mem.req.fire) { // just waiting, timing safe
         nextState := s_mem_resp // to wait resp
       }.otherwise {}
     }
     is(s_mem_resp) {             // unknown in delay,timing?
-      when(io.mem.resp.valid) { // do nothing,delay one cycle OPTPOINT*
+      when(memRespAccepted) { // do nothing,delay one cycle OPTPOINT*
         nextState        := s_addr_proc
         setPmpCheckLevel := true.B
       }
@@ -1208,6 +1214,14 @@ class MPTTableWalker(implicit p: Parameters) extends XSModule with MPTCacheParam
     }
 
   }
+  when(io.mem.req.fire) {
+    assert(!waitingMemResp, "MPT walker reused its memory source ID with a response outstanding")
+    waitingMemResp := true.B
+  }
+  when(memRespAccepted) {
+    assert(curState === s_mem_resp, "MPT walker accepted a memory response outside the wait state")
+    waitingMemResp := false.B
+  }
   when(flush){
     pa := 0.U
     level := "b1000".U(mptLevelLenOH.W)
@@ -1216,6 +1230,7 @@ class MPTTableWalker(implicit p: Parameters) extends XSModule with MPTCacheParam
     mpteInvalid := false.B
     rsvZeroError0 := false.B
     rsvZeroError1 := false.B
+    waitingMemResp := false.B
     curState := s_idle
   }
   // fsm end
@@ -1310,6 +1325,7 @@ class MptChecker(implicit p: Parameters) extends XSModule with HasPtwConst {
   io.mem.req.bits.addr    := mptTWInst.mem.req.bits.addr
   io.mem.req.valid        := mptTWInst.mem.req.valid
   mptTWInst.mem.req.ready := io.mem.req.ready
+  mptTWInst.mem.mask      := io.mem.mask
 
   mptTWInst.mem.resp.bits  := io.mem.resp.bits
   mptTWInst.mem.resp.valid := io.mem.resp.valid
