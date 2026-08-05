fcabcf0f0d8d7d4373f87ae0e45c78a8bdd8fa87
HeiHuDie <1042519051@qq.com>
2026-08-05 09:35:31 +0800

    performance reg 128 base

--- src/main/scala/top/Configs.scala
+++ src/main/scala/top/Configs.scala
@@ -531,8 +531,8 @@ class FuzzConfig(dummy: Int = 0) extends Config(
 ) with DeprecatedConfigWarning
 
 class DefaultConfig(n: Int = 1) extends Config(
-  new WithIntEarlyReleaseFunctional
-    ++ OpenLLCConfig("16MB", ways = 16, banks = 4)
+  //new WithIntEarlyReleaseFunctional
+  OpenLLCConfig("16MB", ways = 16, banks = 4)
     ++ L2CacheConfig("2MB", inclusive = true, banks = 4, tp = false)
     ++ WithNKBL1D(64, ways = 4)
     ++ new BaseConfig(n)
