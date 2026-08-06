081642a66022303594110f330dec3d7681c9b2da
Anzooooo <anzoso@outlook.com>
2026-08-06 18:52:21 +0800

    fix(SMB): fix stuck on misalign vector store

--- src/main/scala/xiangshan/mem/pipeline/StoreUnit.scala
+++ src/main/scala/xiangshan/mem/pipeline/StoreUnit.scala
@@ -416,6 +416,7 @@ class StoreUnit(implicit p: Parameters) extends XSModule
   io.lsq.bits.miss := s1_tlb_miss
   io.lsq.bits.isvec := s1_out.isvec || s1_frm_mab_vec
   io.lsq.bits.updateAddrValid := (!s1_in.isMisalign || s1_in.misalignWith16Byte) && (!s1_frm_mabuf || s1_in.isFinalSplit) || s1_exception
+  io.lsq.bits.isMisalign := s1_out.isMisalign && !s1_misalignNeedReplay
   // kill dcache write intent request when tlb miss or exception
   io.dcache.s1_kill  := (s1_tlb_miss || s1_exception || s1_out.mmio || s1_out.nc || s1_in.uop.robIdx.needFlush(io.redirect))
   io.dcache.s1_paddr := s1_paddr
