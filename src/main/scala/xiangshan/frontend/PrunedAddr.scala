f3b703656a4d4f95ef66630b8b16beddc28eb6f1
ngc7331 <ngc7331@outlook.com>
2026-08-17 16:46:40 +0800

    docs(Pc): add scaladoc
    Signed-off-by: ngc7331 <ngc7331@outlook.com>

--- src/main/scala/xiangshan/frontend/PrunedAddr.scala
+++ src/main/scala/xiangshan/frontend/PrunedAddr.scala
@@ -115,6 +115,43 @@ object PrunedAddrInit {
   }
 }
 
+/** Basically alias of PrunedAddr(VAddrBits)
+ *
+ * Note that Pc + UInt can be dangerous, it may overflow and violate Sv48x4.
+ * i.e. fallThroughPc = pc(0x3_ffff_ffff_fffe) + FetchBlockSize(0x20) =(overflow) 0x1e.
+ *
+ * In such case, TLB should raise a page fault at xepc=0x4_0000_0000_0000.
+ * But we cannot distinguish an overflow with only VAddrBits.
+ *
+ * Thus, Data path between this kind of producer(pc adder) and consumer(tlb and csr) should consider using GuardedPc().
+ * Example path:
+ *   - Bpu fallthrough -> Ftq -> ICache -> Itlb (used to do canonical check)
+ *   - ...                    -> Backend pcMem -> CSR (used to fill xepc/xtval)
+ *
+ * Other paths can still use Pc() to reduce area overhead, i.e. Bpu inner storage, backend targetMem.
+ *
+ * This also uses strictAssign mode to do width-checking at compile time, any assign like:
+ * {{{
+ *   val uint = UInt(XLEN.W)
+ *   val pc = Pc()
+ *   val guardedPc = GuardedPc()
+ *   pc := uint
+ *   pc := guardedPc
+ *   guardedPc := pc
+ * }}}
+ * will fail, should use:
+ * {{{
+ *   pc := uint(VAddrBits - 1, 0)
+ *   // or
+ *   pc := PrunedAddrInit(uint).truncate(VAddrBits)
+ *   pc := guardedPc.unGuard
+ *   val guardBit = Bool()
+ *   guardedPc := pc.guard(guardBit)
+ * }}}
+ *
+ * See also:
+ *   - https://github.com/OpenXiangShan/XiangShan/pull/6272
+ */
 class Pc(_l: Int)(implicit p: Parameters) extends PrunedAddr(_l, strictAssign = true) {
   require(_l == VAddrBits)
   // Hack: we cannot do this(VAddrBits):
@@ -125,9 +162,23 @@ class Pc(_l: Int)(implicit p: Parameters) extends PrunedAddr(_l, strictAssign = 
   override def +(offset: UInt):       Pc = PcInit(toUInt + offset)
   override def +(that:   PrunedAddr): Pc = PcInit(toUInt + that.toUInt)
 
+  /** Unsafe static .signGuard/.zeroGuard
+   * The actual guard bit should be determined by current translation mode:
+   *   - in Sv39/48, should be signExt-ed from bit[38/47]
+   *   - in bare/Sv39x4/48x4, should be zeroExt-ed
+   * these should be used with other assumption/protection, i.e.:
+   *   - Bpu reset_vector is a physical address, so it can be statically zeroExt-ed
+   *   - Ras return address can be checked by IFU pre-decoder / backend JumpUnit
+   */
   def signGuard: GuardedPc = this.signExt(GuardedVAddrBits).asTypeOf(new GuardedPc)
+
+  /** Unsafe: see comments for .signGuard */
   def zeroGuard: GuardedPc = this.zeroExt(GuardedVAddrBits).asTypeOf(new GuardedPc)
-  def guard(bit: Bool):      GuardedPc = Cat(bit, this.addr).asTypeOf(new GuardedPc)
+
+  /** apply a known guard bit to Pc */
+  def guard(bit: Bool): GuardedPc = Cat(bit, this.addr).asTypeOf(new GuardedPc)
+
+  /** inherit a guard bit from GuardedPc */
   def guard(ref: GuardedPc): GuardedPc = guard(ref.getGuard)
 }
 
@@ -143,6 +194,8 @@ object PcInit {
   }
 }
 
+/** Added 1 extra guard/overflow bit to class Pc() for canonical check, see notes for class Pc
+ */
 class GuardedPc(_l: Int)(implicit p: Parameters) extends PrunedAddr(_l, strictAssign = true) {
   require(_l == GuardedVAddrBits)
   // Hack: we cannot do this(VAddrBits):
@@ -153,8 +206,10 @@ class GuardedPc(_l: Int)(implicit p: Parameters) extends PrunedAddr(_l, strictAs
   override def +(offset: UInt):       GuardedPc = GuardedPcInit(toUInt + offset)
   override def +(that:   PrunedAddr): GuardedPc = GuardedPcInit(toUInt + that.toUInt)
 
+  /** get guard bit */
   def getGuard: Bool = addr(_l - instOffsetBits - 1) // highest bit
 
+  /** get Pc without guard bit, call this explicitly to do bit-width matching */
   def unGuard: Pc = this.truncate(VAddrBits).asTypeOf(new Pc)
 }
 
