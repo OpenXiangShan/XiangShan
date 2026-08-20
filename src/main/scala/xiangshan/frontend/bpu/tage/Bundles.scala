cb9ff1d8d4c6b624667a0fb6e95adbdbf5a9b3b7
HuSipeng <husipeng24@qq.com>
2026-08-20 17:24:02 +0800

    perf(Tage): optimize UseAltOnNa (#6328)
    Previously, `UseAltOnNa` recorded whether a given branch tended to
    prefer the alternate prediction. However, because there are too many
    branches, aliasing occurs easily, causing a single `UseAltOnNa` counter
    to map to multiple branches and interfere with one another. Now,
    `UseAltOnNa` reflects the state of each table instead—that is, whether a
    table has been sufficiently trained. If it has not been well trained,
    the predictor should choose the alternate prediction. The results show
    that this approach is more effective and also uses register resources
    more efficiently.

--- src/main/scala/xiangshan/frontend/bpu/tage/Bundles.scala
+++ src/main/scala/xiangshan/frontend/bpu/tage/Bundles.scala
@@ -69,6 +69,7 @@ class PhrToTageIO(implicit p: Parameters) extends TageBundle {
 class MainBtbToTageIO(implicit p: Parameters) extends TageBundle {
   val result:       Vec[Valid[Prediction]] = Input(Vec(NumBtbResultEntries, Valid(new Prediction)))
   val s1_positions: Vec[UInt]              = Input(Vec(NumBtbResultEntries, UInt(CfiPositionWidth.W)))
+  val baseConf:     Vec[Bool]              = Input(Vec(NumBtbResultEntries, Bool()))
 }
 
 class TageToScIO(implicit p: Parameters) extends TageBundle {
@@ -110,11 +111,13 @@ class TableWriteReq(implicit p: Parameters, info: TageTableInfo) extends TageBun
 
 class TageMetaEntry(implicit p: Parameters) extends TageBundle {
   val useProvider:       Bool            = Bool()
+  val hasAlt:            Bool            = Bool()
   val providerTableIdx:  UInt            = UInt(TableIdxWidth.W)
   val providerWayIdx:    UInt            = UInt(MaxNumWays.W)
   val providerTakenCtr:  SaturateCounter = TakenCounter()
   val providerUsefulCtr: SaturateCounter = UsefulCounter()
   val altOrBasePred:     Bool            = Bool()
+  val altConf:           Bool            = Bool()
 }
 
 class TageMeta(implicit p: Parameters) extends TageBundle {
@@ -168,6 +171,7 @@ class TrainInfo(implicit p: Parameters) extends TageBundle {
 
   val incUseAltOnNa: Bool = Bool()
   val decUseAltOnNa: Bool = Bool()
+  val useAltOnNaIdx: UInt = UInt(UseAltOnNaIdxWidth.W)
 
   val finalPred:   Bool = Bool()
   val actualTaken: Bool = Bool() // used for writeBuffer
