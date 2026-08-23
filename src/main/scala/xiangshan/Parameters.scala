a2371b292d72c8c986d055d2263009df3ff3db93
HeiHuDie <1042519051@qq.com>
2026-08-23 14:38:06 +0800

    peformance(rob): RobSize = 176

--- src/main/scala/xiangshan/Parameters.scala
+++ src/main/scala/xiangshan/Parameters.scala
@@ -114,7 +114,7 @@ case class XSCoreParameters
   StoreQueueSnapshotInterval: Int = 1, // must a power of 2
   SQUnalignQueueSize: Int = 2,
   VlsQueueSize: Int = 8,
-  RobSize: Int = 160,
+  RobSize: Int = 176,
   RabSize: Int = 352,
   VTypeBufferSize: Int = 64, // used to reorder vtype
   IssueQueueSize: Int = 20,
