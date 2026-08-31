b5ec9e2f89cf8b8b99ea757e9ddade407b2be3d1
Xuan Hu <huxuan@bosc.ac.cn>
2026-08-31 16:40:39 +0800

    feat(vector,vialu): support vector widen and narrow instructions

--- src/main/scala/xiangshan/backend/fu/wrapper/VIAluWrapper.scala
+++ src/main/scala/xiangshan/backend/fu/wrapper/VIAluWrapper.scala
@@ -9,7 +9,6 @@ import xiangshan.backend.vector.fu.Func._
 import xiangshan.backend.vector.fu.{VecFixLatFunc, VecFuConfig}
 import yunsuan.vector.Common.VSew
 import yunsuan.vector.VectorALU.VIAlu
-import yunsuan.vector.VectorALU.{Adder, Misc}
 
 // Todo: support double narrow
 class VIAluWrapper(cfg: VecFuConfig)(implicit p: Parameters) extends VecFixLatFunc(cfg) {
@@ -149,6 +148,8 @@ class VIAluWrapper(cfg: VecFuConfig)(implicit p: Parameters) extends VecFixLatFu
       vecData.maskE16 := Cat(vialus.map(_.out.ex0.mask.e16).reverse)
       vecData.maskE32 := Cat(vialus.map(_.out.ex0.mask.e32).reverse)
       vecData.maskE64 := Cat(vialus.map(_.out.ex0.mask.e64).reverse)
+      vecData.isWiden.get := isWiden.ex0
+      vecData.isNarrow.get := isNarrow.ex0
       vecData.vxsatE8.get := vialus.flatMap(_.out.ex0.vxsat.asBools)
       vecData.narrowVxsatE8.get := vialus.flatMap(_.out.ex0.narrowVxsat.asBools)
   }
@@ -161,6 +162,8 @@ class VIAluWrapper(cfg: VecFuConfig)(implicit p: Parameters) extends VecFixLatFu
       vecData.maskE16 := Cat(vialus.map(_.out.ex1.mask.e16).reverse)
       vecData.maskE32 := Cat(vialus.map(_.out.ex1.mask.e32).reverse)
       vecData.maskE64 := Cat(vialus.map(_.out.ex1.mask.e64).reverse)
+      vecData.isWiden.get := isWiden.ex1
+      vecData.isNarrow.get := isNarrow.ex1
       vecData.vxsatE8.get := vialus.flatMap(_.out.ex1.vxsat.asBools)
       vecData.narrowVxsatE8.get := vialus.flatMap(_.out.ex1.narrowVxsat.asBools)
   }
