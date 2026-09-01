3aedca94cd46b8d243e4c3d29f48e69a5daee93a
Yinan Xu <xuyinan@ict.ac.cn>
2026-09-01 10:12:40 +0800

    feat(script): add emulator option alias (#6448)
    This strictly aligns with kunminghu-v3.

--- scripts/xiangshan.py
+++ scripts/xiangshan.py
@@ -710,7 +710,7 @@ if __name__ == "__main__":
     parser.add_argument('--pgo-max-cycle', nargs='?', default=400000, type=int, help='maximun cycle to train pgo')
     parser.add_argument('--pgo-emu-args', nargs='?', default='--no-diff', type=str, help='emu arguments for pgo')
     parser.add_argument('--llvm-profdata', nargs='?', type=str, help='corresponding llvm-profdata command of clang to compile emu, do not set with GCC')
-    parser.add_argument('--rtl-simulator', choices=['verilator', 'gsim'], default='verilator', help='RTL simulator backend')
+    parser.add_argument('--rtl-simulator', '--emulator', dest='rtl_simulator', choices=['verilator', 'gsim'], default='verilator', help='RTL simulator backend')
 
     args = parser.parse_args()
 
