6e778770a8072cdc7667366e8097f7d4f487e953
Xuan Hu <huxuan@bosc.ac.cn>
2026-09-06 20:24:36 +0800

    refactor(IBuffer, PredInstAccept, PredUopNum): streamline code structure and improve readability

--- src/main/scala/xiangshan/frontend/ibuffer/PredUopNum.scala
+++ src/main/scala/xiangshan/frontend/ibuffer/PredUopNum.scala
@@ -18,44 +18,55 @@ package xiangshan.frontend.ibuffer
 import chisel3._
 import chisel3.util._
 import org.chipsalliance.cde.config.Parameters
-import xiangshan.backend.fu.vector.Bundles.{VType, Vstart}
-import xiangshan.backend.fu.wrapper.CSRToDecode
-import xiangshan.backend.vector.Decoder.{DecodeChannelsCore, NumUopOH}
-import xiangshan.backend.vector.HasVectorSettings
 import xiangshan._
+import xiangshan.backend.fu.vector.Bundles.Vstart
+import xiangshan.backend.fu.vector.Bundles.VType
+import xiangshan.backend.fu.wrapper.CSRToDecode
+import xiangshan.backend.vector.Decoder.DecodeChannelsCore
+import xiangshan.backend.vector.Decoder.NumUopOH
+import xiangshan.backend.vector.HasVectorSettings
 
-class PredUopNum(implicit p: Parameters) extends IBufferModule with HasVectorSettings {
-  class PredUopNumIO extends Bundle {
-    val valid    = Input(Vec(DecodeWidth, Bool()))
-    val inst     = Input(Vec(DecodeWidth, UInt(32.W)))
-    val vtype    = Input(Vec(DecodeWidth, VType()))
-    val fromCSR  = Input(new CSRToDecode)
-    val vstart   = Input(Vstart())
-
-    val uopNumOH = Output(Vec(DecodeWidth, NumUopOH()))
+object PredUopNum {
+  class In(implicit p: Parameters) extends IBufferBundle {
+    val valid   = Vec(DecodeWidth, Bool())
+    val inst    = Vec(DecodeWidth, UInt(32.W))
+    val vtype   = Vec(DecodeWidth, VType())
+    val fromCSR = new CSRToDecode
+    val vstart  = Vstart()
   }
 
-  val io = IO(new PredUopNumIO)
+  class Out(implicit p: Parameters) extends IBufferBundle {
+    val uopNumOH = Vec(DecodeWidth, NumUopOH())
+  }
+}
+
+class PredUopNum(implicit p: Parameters) extends IBufferModule with HasVectorSettings {
+
+  val in  = IO(Input(new PredUopNum.In))
+  val out = IO(Output(new PredUopNum.Out))
 
   val decodeCore = Module(new DecodeChannelsCore(
     mopWidth = DecodeWidth,
     uopWidth = RenameWidth,
-    numM2M4M8Channel = (DecodeWidth, DecodeWidth, DecodeWidth),
+    numM2M4M8Channel = (DecodeWidth, DecodeWidth, DecodeWidth)
   ))
 
   for (i <- 0 until DecodeWidth) {
-    decodeCore.in.mops(i).valid             := io.valid(i)
-    decodeCore.in.mops(i).bits.info.rawInst := io.inst(i)
-    decodeCore.in.mops(i).bits.info.vtype   := io.vtype(i)
-    decodeCore.in.mops(i).bits.info.fromCSR := io.fromCSR
-    decodeCore.in.mops(i).bits.info.vstart  := io.vstart
+    decodeCore.in.mops(i).valid             := in.valid(i)
+    decodeCore.in.mops(i).bits.info.rawInst := in.inst(i)
+    decodeCore.in.mops(i).bits.info.vtype   := in.vtype(i)
+    decodeCore.in.mops(i).bits.info.fromCSR := in.fromCSR
+    decodeCore.in.mops(i).bits.info.vstart  := in.vstart
   }
 
   for (i <- 0 until DecodeWidth) {
-    io.uopNumOH(i) := MuxCase(NumUopOH.N1, Seq(
-      !io.valid(i)                                                                   -> NumUopOH.N0,
-      (decodeCore.out.simChannel(i)(1).valid && !decodeCore.out.psdChannel(i).valid) -> NumUopOH.N2,
-      decodeCore.out.vecChannel(i)(0).valid                                          -> decodeCore.out.vecUopNumOH(i),
-    ))
+    out.uopNumOH(i) := MuxCase(
+      NumUopOH.N1,
+      Seq(
+        !in.valid(i)                                                                   -> NumUopOH.N0,
+        (decodeCore.out.simChannel(i)(1).valid && !decodeCore.out.psdChannel(i).valid) -> NumUopOH.N2,
+        decodeCore.out.vecChannel(i)(0).valid                                          -> decodeCore.out.vecUopNumOH(i)
+      )
+    )
   }
 }
