b82bc3059c2aa39c9c5d3667707572a5dca5f6c2
HeiHuDie <1042519051@qq.com>
2026-09-11 10:04:28 +0800

    fix(Bundles): fix checkformat

--- src/main/scala/xiangshan/frontend/ibuffer/Bundles.scala
+++ src/main/scala/xiangshan/frontend/ibuffer/Bundles.scala
@@ -48,15 +48,15 @@ class IBufBankPtr(implicit p: Parameters) extends CircularQueuePtr[IBufBankPtr](
     ) {}
 
 class IBufEntry(implicit p: Parameters) extends IBufferBundle {
-  val inst:           UInt       = UInt(32.W)
-  val pc:             Pc         = Pc()
-  val foldpc:         UInt       = UInt(MemPredPCWidth.W)
-  val isRvc:          Bool       = Bool()
-  val predTaken:      Bool       = Bool()
-  val fixedTaken:     Bool       = Bool()
-  val ftqPtr:         FtqPtr     = new FtqPtr
-  val instrEndOffset: UInt       = UInt(FetchBlockInstOffsetWidth.W)
-  val triggered:      UInt       = TriggerAction()
+  val inst:           UInt   = UInt(32.W)
+  val pc:             Pc     = Pc()
+  val foldpc:         UInt   = UInt(MemPredPCWidth.W)
+  val isRvc:          Bool   = Bool()
+  val predTaken:      Bool   = Bool()
+  val fixedTaken:     Bool   = Bool()
+  val ftqPtr:         FtqPtr = new FtqPtr
+  val instrEndOffset: UInt   = UInt(FetchBlockInstOffsetWidth.W)
+  val triggered:      UInt   = TriggerAction()
 
   val debug_seqNum: InstSeqNum = InstSeqNum()
 
