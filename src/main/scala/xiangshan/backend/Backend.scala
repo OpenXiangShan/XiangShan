9c891bd6cf17c131d9a4ba648898012fff927778
wangsangping <3361833374@qq.com>
2026-09-11 08:24:24 +0800

    fix(backend): guard optional vector diff and FPGA perf logic

--- src/main/scala/xiangshan/backend/Backend.scala
+++ src/main/scala/xiangshan/backend/Backend.scala
@@ -498,7 +498,9 @@ class BackendInlinedImp(override val wrapper: BackendInlined)(implicit p: Parame
   //Todo here need change design
   csrio.vpu.set_vtype.valid := commitVType.valid
   csrio.vpu.set_vtype.bits := ZeroExt(vtype, XLEN)
-  csrio.vpu.vl := vecRegion.out.diff.get.diffVl
+  vecRegion.out.diff.foreach { diff =>
+    csrio.vpu.vl := diff.diffVl
+  }
   csrio.vpu.dirty_vs := ctrlBlock.io.robio.csr.dirty_vs
   csrio.exception := ctrlBlock.io.robio.exception
   csrio.robDeqPtr := ctrlBlock.io.robio.robDeqPtr
