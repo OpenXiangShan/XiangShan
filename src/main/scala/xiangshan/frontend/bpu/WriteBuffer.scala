1d32f81beb43eee5c3753f965a832ed8cdc08423
YuanDL <2524247987@qq.com>
2026-09-21 18:45:05 +0800

    fix(WriteBuffer): change read idx to PriorityEncoder

--- src/main/scala/xiangshan/frontend/bpu/WriteBuffer.scala
+++ src/main/scala/xiangshan/frontend/bpu/WriteBuffer.scala
@@ -103,8 +103,6 @@ class WriteBuffer[T <: WriteReqBundle](
   private val readReadyVec = WireInit(VecInit(Seq.fill(numPorts)(false.B)))
   private val emptyVec     = WireInit(VecInit(Seq.fill(numPorts)(false.B)))
   private val fullVec      = WireInit(VecInit(Seq.fill(numPorts)(false.B)))
-  private val readArbiters =
-    Seq.fill(numPorts)(Module(new RRArbiter(UInt(log2Ceil(numEntries).W), numEntries)))
   // Replace is to prioritize replacing the entry of the same port as setIdx
   private val victimSameSetIdx = WireInit(VecInit.fill(numPorts)(0.U.asTypeOf(Valid(UInt(log2Ceil(numEntries).W)))))
 
@@ -117,13 +115,8 @@ class WriteBuffer[T <: WriteReqBundle](
   // Apply drain first. Write requests below override it when they update the same slot,
   // and flush overrides both at the end of the next-state calculation.
   for (rowIdx <- 0 until numPorts) {
-    readArbiters(rowIdx).io.in.zipWithIndex.foreach { case (in, entryIdx) =>
-      in.valid := dirty(rowIdx)(entryIdx)
-      in.bits  := entryIdx.U
-    }
-    readArbiters(rowIdx).io.out.ready := io.read(rowIdx).ready
-    val drainIdx = readArbiters(rowIdx).io.out.bits
-    when(readArbiters(rowIdx).io.out.fire) {
+    val drainIdx = PriorityEncoder(dirty(rowIdx))
+    when(io.read(rowIdx).fire) {
       nextDirty(rowIdx)(drainIdx) := false.B
     }
   }
@@ -266,9 +259,9 @@ class WriteBuffer[T <: WriteReqBundle](
     emptyVec(nRows)     := !readValidVec(nRows).reduce(_ || _)
     fullVec(nRows)      := readValidVec(nRows).reduce(_ && _)
     io.full(nRows)      := fullVec(nRows)
-    val readIdx = readArbiters(nRows).io.out.bits
+    val readIdx = PriorityEncoder(readValidVec(nRows))
 
-    io.read(nRows).valid := readArbiters(nRows).io.out.valid
+    io.read(nRows).valid := !emptyVec(nRows)
     io.read(nRows).bits  := DontCare
 
     when(readReadyVec(nRows) && !emptyVec(nRows)) {
