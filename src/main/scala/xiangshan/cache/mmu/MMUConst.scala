e22eac274c953c107c41bac2cdfc9f75d01a9070
Jiabin Wu <66947239+rezado@users.noreply.github.com>
2026-09-23 17:43:34 +0800

    perf(mmu): increase L2TLB superpage cache to 128 entries

--- src/main/scala/xiangshan/cache/mmu/MMUConst.scala
+++ src/main/scala/xiangshan/cache/mmu/MMUConst.scala
@@ -68,7 +68,7 @@ case class L2TLBParameters
   bcReservedBits: Int = 1,
   bcReplacer: Option[String] = Some("setplru"),
   // sp
-  spSize: Int = 16,
+  spSize: Int = 128,
   spReplacer: Option[String] = Some("plru"),
   // hash asid width
   hashAsidWidth: Int = 3,
